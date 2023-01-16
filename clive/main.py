621a14f8cc883d88c562210b695ee60775c326bc
Mateusz Żebrak <mzebrak@syncad.com>
2023-01-16 09:49:17 +0100

    Log in debug all defined settings during startup

--- clive/main.py
+++ clive/main.py
@@ -1,10 +1,12 @@
 from loguru import logger
 
+from clive.config import settings
 from clive.logger import configure_logger
 
 
 def main() -> None:
     configure_logger()
+    logger.debug(f"settings:\n{settings.as_dict()}")
     logger.info("clive.main.main() works!")
 
 
