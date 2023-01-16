1a549686d7058a438fc55f8254f7619ed4b894d0
Mateusz Żebrak <mzebrak@syncad.com>
2023-01-16 12:06:07 +0100

    Create Clive class holding prompt_toolkit Application

---
+++ clive/app.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Final
+
+from prompt_toolkit import Application
+from prompt_toolkit.key_binding import KeyBindings, KeyPressEvent
+from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.layout import Layout
+from prompt_toolkit.styles import Style
+
+from clive.config import settings
+from clive.ui.root_component import root_component
+
+if TYPE_CHECKING:
+    from prompt_toolkit.layout.layout import FocusableElement
+
+
+class Clive:
+    REFRESH_INTERVAL: Final[float] = 0.25
+
+    def __init__(self) -> None:
+        self.__app = self.__create_app()
+
+    def run(self) -> None:
+        from clive.ui.view_switcher import switch_view
+
+        switch_view("welcome")
+        self.__app.run()
+
+    def set_focus(self, container: FocusableElement) -> None:
+        self.__app.layout.focus(container)
+
+    def __create_app(self) -> Application[Any]:
+        return Application(
+            layout=Layout(root_component.container),
+            style=Style.from_dict(settings.style),
+            key_bindings=self.__get_key_bindings(),
+            full_screen=True,
+            mouse_support=True,
+            refresh_interval=self.REFRESH_INTERVAL,
+        )
+
+    @staticmethod
+    def __get_key_bindings() -> KeyBindings:
+        kb = KeyBindings()
+
+        kb.add(Keys.Tab)(focus_next)
+        kb.add(Keys.BackTab)(focus_previous)
+
+        @kb.add(Keys.ControlC)
+        def _(event: KeyPressEvent) -> None:
+            event.app.exit()
+
+        return kb
+
+
+clive = Clive()
--- clive/main.py
+++ clive/main.py
@@ -1,5 +1,6 @@
 from loguru import logger
 
+from clive.app import clive
 from clive.config import settings
 from clive.logger import configure_logger
 
@@ -8,6 +9,7 @@ def main() -> None:
     configure_logger()
     logger.debug(f"settings:\n{settings.as_dict()}")
     logger.info("clive.main.main() works!")
+    clive.run()
 
 
 if __name__ == "__main__":
