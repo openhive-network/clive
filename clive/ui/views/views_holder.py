b55b439a78680d73b2e454bbe5d8d9baf163144b
Mateusz Żebrak <mzebrak@syncad.com>
2023-01-18 11:07:16 +0100

    Move Welcome inside views package

--- clive/ui/views/views_holder.py
+++ clive/ui/views/views_holder.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from clive.ui.views.dashboard import Dashboard
-from clive.ui.welcome import Welcome
+from clive.ui.views.welcome import Welcome
 
 if TYPE_CHECKING:
     from clive.ui.view import View
