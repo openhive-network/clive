b8d9328a79ec6d1a0b81837d3a7ee8315cc25038
Mateusz Żebrak <mzebrak@syncad.com>
2023-01-18 11:04:36 +0100

    Annotate the views variable in views_holder

--- clive/ui/views/views_holder.py
+++ clive/ui/views/views_holder.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from clive.ui.views.dashboard import Dashboard
 from clive.ui.welcome_component import WelcomeComponent
 
-views = {
+if TYPE_CHECKING:
+    from clive.ui.view import View
+
+views: dict[str, View] = {
     "welcome": WelcomeComponent(),
     "dashboard": Dashboard(),
 }
