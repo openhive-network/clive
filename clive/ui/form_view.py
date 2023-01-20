bf7f6543cbbfc4ed13d3d91aa45f6b23b7aeb809
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-20 21:01:02 +0100

    Make FormView configurable - to be revisited

--- clive/ui/form_view.py
+++ clive/ui/form_view.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 from abc import ABC
 from typing import TYPE_CHECKING
 
-from clive.ui.component import Component
+from clive.ui.component import ConfigurableComponent
 
 if TYPE_CHECKING:
     from clive.ui.views.form import Form  # noqa: F401
 
 
-class FormView(Component["Form"], ABC):
+class FormView(ConfigurableComponent["Form"], ABC):
     """
     A view that is used to display in a multistep form.
     """
