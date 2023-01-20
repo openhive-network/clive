e606be0ef683d56cb6d2c80ed9d66fad033f81d2
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-20 20:45:03 +0100

    Add set of clases for future implementation

---
+++ clive/ui/views/help_view.py
@@ -0,0 +1,9 @@
+from prompt_toolkit.layout import AnyContainer
+from prompt_toolkit.widgets import Label
+
+from clive.ui.view import View
+
+
+class HelpView(View):
+    def _create_container(self) -> AnyContainer:
+        return Label(text=self.__class__.__name__)
