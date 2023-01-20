489b276b9c6458f12dec90c49b42568e6de40af7
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-20 20:46:44 +0100

    Implement ConfigurableComponent that allows to delay initialization

--- clive/ui/component.py
+++ clive/ui/component.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import TypeVar
+from functools import update_wrapper
+from typing import Any, Callable, Literal, TypeVar
 
 from prompt_toolkit.layout import AnyContainer
 
@@ -16,13 +17,16 @@ class Component(Parented[T], Rebuildable, ABC):
 
     def __init__(self, parent: T) -> None:
         super().__init__(parent)
-        self._container = self._create_container()
+        self._on_init()
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}()"
 
     def __repr__(self) -> str:
         return str(self)
+
+    def _on_init(self) -> None:
+        self._container = self._create_container()
 
     @property
     def container(self) -> AnyContainer:
@@ -32,8 +36,32 @@ class Component(Parented[T], Rebuildable, ABC):
     def _create_container(self) -> AnyContainer:
         """Create a container containing all the elements that define the layout."""
 
-    def _rebuild(self, self_only: bool = False) -> None:
+    def _rebuild(self) -> None:
         """Rebuilds the current component and then calls the _rebuild method of its parent to propagate the change."""
         self._container = self._create_container()
-        if not self_only:
+        if isinstance(self._parent, Rebuildable):
             self._parent._rebuild()
+
+
+class ConfigurableComponent(Component[T]):
+    def _on_init(self) -> None:
+        """
+        This method is called in constructor, but this class
+        has delayed initialization (to __exit__) so there is
+        nothing to do in constructor
+        """
+        self.__is_editable: bool = False
+        self.__is_configured: bool = False
+
+    def __enter__(self):
+        self.__is_editable = True
+        return self
+
+    def __exit__(self, exception_type: type, exception_value: Any, exception_traceback: Any) -> Literal[False]:
+        if exception_value is None:
+            self._container = self._create_container()
+            self.__is_configured = True
+
+        self.__is_editable = False
+        return False
+
