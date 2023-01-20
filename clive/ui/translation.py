46e4cea4049b030dc696178a2b0fdc52c5c368ee
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-20 20:52:52 +0100

    Add translation TODO

---
+++ clive/ui/translation.py
@@ -0,0 +1,12 @@
+def t(txt: str) -> str:
+    """This function is interface for translation module
+
+    TODO: use ``gettext`` library for translation
+
+    Args:
+        txt (str): Text to translate
+
+    Returns:
+        str: Translated text in proper locale
+    """
+    return txt
