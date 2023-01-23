623e160c36ceaaf95cee70deff29b60ba6c0f33a
Mateusz Żebrak <mzebrak@syncad.com>
2023-01-23 10:08:28 +0100

    Include form and mock in views holder

--- clive/ui/views/views_holder.py
+++ clive/ui/views/views_holder.py
@@ -4,7 +4,9 @@ from typing import TYPE_CHECKING
 
 from clive.ui.view_manager import view_manager
 from clive.ui.views.dashboard import Dashboard
+from clive.ui.views.form import Form
 from clive.ui.views.welcome import Welcome
+from tests.unit.test_side_panel_based_typing import MockSidePanelBasedConcrete
 
 if TYPE_CHECKING:
     from clive.ui.view import View
@@ -12,4 +14,6 @@ if TYPE_CHECKING:
 views: dict[str, View] = {
     "welcome": Welcome(view_manager),
     "dashboard": Dashboard(),
+    "form": Form(),
+    "mock": MockSidePanelBasedConcrete(),
 }
