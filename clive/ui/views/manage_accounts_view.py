296189b360a16b1a67461fa55eb254bfc00e6fd7
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-25 15:03:06 +0100

    Add View that manages active accounts

--- clive/ui/views/manage_accounts_view.py
+++ clive/ui/views/manage_accounts_view.py
@@ -1,9 +1,46 @@
-from prompt_toolkit.layout import AnyContainer
-from prompt_toolkit.widgets import Label
+from __future__ import annotations
 
-from clive.ui.view import View
+from typing import Sequence
 
+from prompt_toolkit.widgets import Button
+from prompt_toolkit.key_binding import KeyBindings
 
-class ManageAccountsView(View):
-    def _create_container(self) -> AnyContainer:
-        return Label(text=self.__class__.__name__)
+from clive.ui.components.buttons_menu import ButtonsMenu
+from clive.ui.components.radiolist_with_model import RadioListWithModel
+from clive.ui.floats.create_account_float import CreateAccountFloat
+from clive.ui.form_view import FormView
+from clive.ui.form_view_convertible import FormViewConvertible
+from clive.ui.quick_form_view import QuickFormView
+from clive.ui.views.button_based import ButtonsBased
+from clive.ui.views.form import Form
+from clive.storage.mock_database import Account, MockDB
+
+class ManageAccountsButtons(ButtonsMenu["ManageAccountsView"]):
+    def __init__(self, parent: ManageAccountsView) -> None:
+        super().__init__(parent)
+
+    def _create_buttons(self) -> Sequence[Button]:
+        return [
+            Button("Create account", handler=self.__create_account),
+            Button("Delete account", handler=self.__delete_account)
+        ]
+
+    def _get_key_bindings(self) -> KeyBindings:
+        return KeyBindings()
+
+    def __delete_account(self) -> None:
+        MockDB.ACCOUNTS.remove(self._parent.main_panel.current_item[0])
+
+    def __create_account(self) -> None:
+        def on_account_object_created(account: Account):
+            MockDB.ACCOUNTS.append(account)
+            self._parent.main_panel._rebuild()
+        CreateAccountFloat(on_account_object_created)
+
+class ManageAccountsView(FormViewConvertible, ButtonsBased[RadioListWithModel["ManageAccountsView"], ManageAccountsButtons]):
+    def __init__(self) -> None:
+        super().__init__(RadioListWithModel(self, lambda: [(acc, acc.name) for acc in MockDB.ACCOUNTS]), ManageAccountsButtons(self))
+
+    @staticmethod
+    def convert_to_form_view(parent: Form) -> FormView:
+        return QuickFormView(parent=parent, view=ManageAccountsView(), request_buttons={})
