a0cc1f9530a7411a17def42721e1c577e669e1e6
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-25 15:01:39 +0100

    Add base class for floats

---
+++ clive/ui/floats/base_float.py
@@ -0,0 +1,19 @@
+from abc import ABC
+
+from clive.ui.containerable import Containerable
+from clive.ui.get_view_manager import get_view_manager
+from clive.ui.focus import set_focus
+
+class BaseFloat(Containerable, ABC):
+    """
+    By deriving from this class you can create float which
+    will be automatically visible after creation
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        get_view_manager().menu.float = self
+        set_focus(self.container)
+
+    def _close(self) -> None:
+        get_view_manager().menu.float = None
