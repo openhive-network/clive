42b99e1765ec4fbdfbf6d928197ebad5872972f1
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-26 17:48:03 +0100

    Update mock db schema

--- clive/storage/mock_database.py
+++ clive/storage/mock_database.py
@@ -1,23 +1,31 @@
-
 from dataclasses import dataclass
 from enum import IntEnum
 from typing import List
+
 
 class AccountType(IntEnum):
     ACTIVE = 0
     WATCHED = 1
 
+
 @dataclass
 class Account:
     name: str
-    account_type: AccountType
-    key_names: Optional[List[str]]
+
+
+@dataclass
+class PrivateKey:
+    key_name: str
+    key: str
+
+
+@dataclass
+class ActiveAccount(Account):
+    keys: List[PrivateKey]
 
 
 class MockDB:
-    ACCOUNTS: List[Account] = [
-        Account("A"*4, AccountType.ACTIVE, ["0"*14, "1"*14]),
-        Account("B"*4, AccountType.WATCHED, None),
-        Account("C"*4, AccountType.ACTIVE, ["0"*14]),
-        Account("D"*4, AccountType.WATCHED, None)
-    ]
+    MAIN_ACTIVE_ACCOUNT: ActiveAccount = ActiveAccount(
+        "MAIN_ACCOUNT" * 4, [PrivateKey("default", "X" * 14), PrivateKey("memo", "Y" * 14)]
+    )
+    ACCOUNTS: List[Account] = [Account(f"WATCHED_ACCOUNT_{i}") for i in range(10)]
