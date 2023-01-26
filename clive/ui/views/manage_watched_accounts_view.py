f57683704bb3872f6df609adeb57dfe333b7cc79
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-26 17:55:19 +0100

    Move place of ManageWatchedAccounts, ManagePrivateKeys and removed ManageAccounts

--- clive/ui/views/manage_watched_accounts_view.py
+++
@@ -1,9 +0,0 @@
-from prompt_toolkit.layout import AnyContainer
-from prompt_toolkit.widgets import Label
-
-from clive.ui.view import View
-
-
-class ManageWatchedAccountsView(View):
-    def _create_container(self) -> AnyContainer:
-        return Label(text=self.__class__.__name__)
