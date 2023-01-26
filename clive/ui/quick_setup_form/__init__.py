82a3da4a9b4378bcc29d9b3171157302a9780b73
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-26 21:43:34 +0100

    Add implementation of abstract methods to QuickSetup

--- clive/ui/quick_setup_form/__init__.py
+++ clive/ui/quick_setup_form/__init__.py
@@ -1,1 +1,3 @@
 from clive.ui.quick_setup_form.quick_setup import QuickSetup
+
+__all__ = ["QuickSetup"]
--- clive/ui/quick_setup_form/quick_setup.py
+++ clive/ui/quick_setup_form/quick_setup.py
@@ -1,10 +1,34 @@
-from clive.ui.manage_keys.manage_keys_view import ManageKeysView
-from clive.ui.manage_watched_accounts.manage_watched_accounts_view import ManageWatchedAccountView
+from clive.exceptions import FormNotFinishedException
+from clive.storage.mock_database import MockDB
+from clive.ui.manage_keys import ManageKeysView
+from clive.ui.manage_watched_accounts import ManageWatchedAccountView
+from clive.ui.set_node_address import SetNodeAddressView
+from clive.ui.view_switcher import switch_view
 from clive.ui.views.form import Form
 
 
 class QuickSetup(Form):
     def __init__(self) -> None:
         super().__init__(
-            [ManageKeysView.convert_to_form_view(self), ManageWatchedAccountView.convert_to_form_view(self)]
+            [
+                ManageKeysView.convert_to_form_view(self),
+                ManageWatchedAccountView.convert_to_form_view(self),
+                SetNodeAddressView.convert_to_form_view(self),
+            ]
         )
+
+    def cancel(self) -> None:
+        switch_view("dashboard")
+
+    def finish(self) -> None:
+        checks = [
+            MockDB.MAIN_ACTIVE_ACCOUNT is not None,
+            len(MockDB.MAIN_ACTIVE_ACCOUNT.keys) > 0,
+            MockDB.NODE_ADDRESS is not None,
+        ]
+        if not all(checks):
+            raise FormNotFinishedException(
+                **dict(zip(["is main account set", "is any private key set", "is any node address set"], checks))
+            )
+        else:
+            switch_view("dashboard")
