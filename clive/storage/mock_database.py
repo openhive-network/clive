28849c6c636f044a0a97fa3cebdd2056814c6296
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-26 21:40:28 +0100

    Add NodeAddress to MockDB

--- clive/storage/mock_database.py
+++ clive/storage/mock_database.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import IntEnum
-from typing import List
+from typing import List, Optional
 
 
 class AccountType(IntEnum):
@@ -24,8 +24,26 @@ class ActiveAccount(Account):
     keys: List[PrivateKey]
 
 
+@dataclass
+class NodeAddress:
+    proto: str
+    host: str
+    port: Optional[int] = None
+
+    def __str__(self) -> str:
+        return f"{self.proto}://{self.host}" + ("" if self.port is None else f":{self.port}")
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
 class MockDB:
     MAIN_ACTIVE_ACCOUNT: ActiveAccount = ActiveAccount(
         "MAIN_ACCOUNT" * 4, [PrivateKey("default", "X" * 14), PrivateKey("memo", "Y" * 14)]
     )
     ACCOUNTS: List[Account] = [Account(f"WATCHED_ACCOUNT_{i}") for i in range(10)]
+    NODE_ADDRESS: Optional[NodeAddress] = NodeAddress("https", "api.hive.blog")
+    BACKUP_NODE_ADDRESSES: List[NodeAddress] = [
+        NodeAddress("http", "localhost", 8090),
+        NodeAddress("http", "hive-6.pl.syncad.com", 18090),
+    ]
