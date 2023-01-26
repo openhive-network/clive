b10fc440e9f08e7b1920fdd6aa1b54b908456716
Mateusz Żebrak <mzebrak@syncad.com>
2023-01-26 15:14:34 +0100

    Create preliminary version of MenuRestricted

---
+++ clive/ui/menus/restricted/menu_restricted.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from prompt_toolkit.layout import AnyContainer
+from prompt_toolkit.widgets import MenuItem
+
+from clive.ui.menus.menu import Menu
+from clive.ui.menus.restricted.menu_restricted_handlers import MenuRestrictedHandlers
+
+
+class MenuRestricted(Menu[MenuRestrictedHandlers]):
+    def __init__(self, body: AnyContainer) -> None:
+        super().__init__(body, MenuRestrictedHandlers(self))
+
+    def _create_menu(self) -> list[MenuItem]:
+        return [
+            MenuItem(
+                "Wallet",
+                children=[
+                    MenuItem("Dashboard", handler=self._handlers.dashboard),
+                    MenuItem("Exit", handler=self._handlers.exit),
+                ],
+            ),
+            MenuItem(
+                "Options",
+                children=[
+                    MenuItem("Set Node Address", handler=self._handlers.options_set_node_address),
+                    MenuItem("Set Theme", handler=self._handlers.options_set_theme),
+                ],
+            ),
+            MenuItem(
+                "Help",
+                children=[
+                    MenuItem("Help", handler=self._handlers.help),
+                    MenuItem("About", handler=self._handlers.about),
+                    MenuItem("Contribute", handler=self._handlers.contribute),
+                ],
+            ),
+        ]
