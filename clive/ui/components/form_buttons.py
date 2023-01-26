60828d46cce08c2ebac1ab138dfbe8dcb33f18ba
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-26 17:52:51 +0100

    Add method that merges Form buttons and current form view buttons

--- clive/ui/components/form_buttons.py
+++ clive/ui/components/form_buttons.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import re
+from typing import TYPE_CHECKING, Callable, List
 
 from prompt_toolkit.key_binding import KeyBindings, KeyPressEvent
 from prompt_toolkit.keys import Keys
@@ -9,11 +10,14 @@ from prompt_toolkit.widgets import Button
 from clive.ui.components.buttons_menu import ButtonsMenu
 
 if TYPE_CHECKING:
+    from clive.ui.form_view import FormView
     from clive.ui.views.form import Form  # noqa: F401
+
+STANDARD_FUNCTIONAL_KEYS: List[Keys] = [getattr(Keys, f"F{i}") for i in range(1, 13)]
 
 
 class FormButtons(ButtonsMenu["Form"]):
-    def _create_buttons(self) -> list[Button]:
+    def _create_buttons(self) -> List[Button]:
         return [
             Button("F1 Previous", handler=self.__f1_action),
             Button("F2 Next", handler=self.__f2_action),
@@ -36,3 +40,46 @@ class FormButtons(ButtonsMenu["Form"]):
 
     def __f3_action(self, event: KeyPressEvent | None = None) -> None:
         self._parent.cancel()
+
+    @staticmethod
+    def merge_buttons_and_actions(parent: Form, other: FormView) -> FormButtons:
+        class FormButtonsMerged(FormButtons):
+            def __init__(self, parent: Form) -> None:
+                self.__additional_buttons = other.requested_buttons()
+                self.__allowed_keys = STANDARD_FUNCTIONAL_KEYS.copy()
+
+                for kb in super()._get_key_bindings().bindings:
+                    for key in kb.keys:
+                        assert isinstance(key, Keys)
+                        self.__allowed_keys.remove(key)
+
+                super().__init__(parent)
+
+            def _create_buttons(self) -> List[Button]:
+                buttons = super()._create_buttons()
+                button_caption_regex = re.compile(r"^(F\d\d? )?(.*)")
+                for i, kv in enumerate(self.__additional_buttons.items()):
+                    button_caption = kv[0]
+                    if (button_caption_match := button_caption_regex.match(button_caption)) is not None:
+                        button_caption = button_caption_match.group(2)
+                    buttons.append(
+                        Button(
+                            text=f"{self.__allowed_keys[i].capitalize()} {button_caption}",
+                            handler=self.__create_button_handler(kv[1]),
+                        )
+                    )
+                return buttons
+
+            def _get_key_bindings(self) -> KeyBindings:
+                bindings = super()._get_key_bindings()
+                for i, kv in enumerate(other.requested_buttons().items()):
+                    bindings.add(self.__allowed_keys[i])(self.__create_button_handler(kv[1]))  # type: ignore
+                return bindings
+
+            def __create_button_handler(self, handler: Callable[[], None]) -> Callable[[], None]:
+                def handler_impl(_: KeyPressEvent | None = None) -> None:
+                    handler()
+
+                return handler_impl
+
+        return FormButtonsMerged(parent)
