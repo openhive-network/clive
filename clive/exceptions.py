f87c00a12f937da947ba8b6397eb0153c1cb321e
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-26 21:40:12 +0100

    Add exception for uncompleted Forms

--- clive/exceptions.py
+++ clive/exceptions.py
@@ -1,3 +1,6 @@
+from typing import Dict
+
+
 class CliveException(Exception):
     """Base class for all clive exceptions."""
 
@@ -8,3 +11,16 @@ class ViewException(CliveException):
 
 class ViewDoesNotExist(ViewException):
     """Raised when a view does not exist."""
+
+
+class FormNotFinishedException(ViewException):
+    """Raised when user choose to finish, but form is not finished"""
+
+    def __init__(self, **kwargs: bool) -> None:
+        self.exception_message = self.__format_exception(kwargs)
+        super().__init__(self.exception_message)
+
+    def __format_exception(self, report: Dict[str, bool]) -> str:
+        return "Form completion report\n" + "\n".join(
+            [f'{key} = {("OK" if value else "FAIL")}' for key, value in report.items()]
+        )
