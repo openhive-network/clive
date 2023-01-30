22cc293f22bfce3bc365702cad0f710065b43682
Mateusz Żebrak <mzebrak@syncad.com>
2023-01-30 11:37:07 +0100

    Add CLI command style update

--- clive/cli/style.py
+++ clive/cli/style.py
@@ -1,6 +1,8 @@
 import rich
 import typer
+from prompt_toolkit.styles import parse_color
 
+from clive import config
 from clive.config import settings
 
 app = typer.Typer()
@@ -16,3 +18,18 @@ def list_(name: str = typer.Argument(None, help="Show only the style with the sp
         rich.print(settings.as_dict()["STYLE"][name])
     except KeyError:
         typer.secho(f'Style with name "{name}" was not found.', fg=typer.colors.RED)
+
+
+@app.command()
+def update(
+    name: str = typer.Argument(..., help="The name of the style to set."),
+    value: str = typer.Argument(..., help="The value of the style to set."),
+) -> None:
+    """Set a style value."""
+    try:
+        parse_color(value)
+    except ValueError:
+        typer.secho(f'"{value}" is not a valid color.', fg=typer.colors.RED)
+        return
+
+    config.update("style.toml", {"default": {"style": {name: value}}})
