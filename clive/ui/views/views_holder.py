34697cc0d1018ed3b8d603f1f1159e117d1b8882
Mateusz Żebrak <mzebrak@syncad.com>
2023-01-30 09:18:40 +0100

    Remove mock test view from views_holder

--- clive/ui/views/views_holder.py
+++ clive/ui/views/views_holder.py
@@ -6,7 +6,6 @@ from clive.ui.dashboard.dashboard import Dashboard
 from clive.ui.views.login import Login
 from clive.ui.views.registration import Registration
 from clive.ui.views.welcome import Welcome
-from tests.unit.test_side_panel_based_typing import MockSidePanelBasedConcrete
 
 if TYPE_CHECKING:
     from clive.ui.view import View
@@ -15,6 +14,5 @@ views: dict[str, Callable[[], View]] = {
     "welcome": lambda: Welcome(),
     "login": lambda: Login(),
     "dashboard": lambda: Dashboard(),
-    "mock": lambda: MockSidePanelBasedConcrete(),
     "registration": lambda: Registration(),
 }
