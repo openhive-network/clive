fa8823ad7276d21697d488221574f504280f82cc
Krzysztof Mochocki <kmochocki@syncad.com>
2023-01-31 17:54:51 +0100

    Add new type of exception to be thrown in UI

--- clive/exceptions.py
+++ clive/exceptions.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import Dict
 
 
@@ -13,14 +14,26 @@ class ViewDoesNotExist(ViewException):
     """Raised when a view does not exist."""
 
 
-class FormNotFinishedException(ViewException):
+class FloatException(CliveException, ABC):
+    def info(self) -> str:
+        return self._get_info()
+
+    @abstractmethod
+    def _get_info(self) -> str:
+        """Returns formated informations about exception"""
+
+
+class FormNotFinishedException(FloatException):
     """Raised when user choose to finish, but form is not finished"""
 
     def __init__(self, **kwargs: bool) -> None:
         self.exception_message = self.__format_exception(kwargs)
         super().__init__(self.exception_message)
 
+    def _get_info(self) -> str:
+        return self.exception_message
+
     def __format_exception(self, report: Dict[str, bool]) -> str:
-        return "Form completion report\n" + "\n".join(
-            [f'{key} = {("OK" if value else "FAIL")}' for key, value in report.items()]
+        return "Form completion report\n\n" + "\n".join(
+            [f'    - {key} = {("OK" if value else "FAIL")}' for key, value in report.items()]
         )
