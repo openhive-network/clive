4384fe345a7a7f78b6fcfbedb1a06a35be1cec3b
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-03 13:27:57 +0100

    Update mock recalc

--- clive/storage/mock_database.py
+++ clive/storage/mock_database.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
+import random
 from dataclasses import dataclass
 from datetime import datetime
 from enum import IntEnum
-from random import random
 from typing import List, Optional
 
 from clive.get_clive import get_clive
@@ -43,27 +43,20 @@ class NodeAddress:
         return str(self)
 
 
-def rand(value: float, x_min: float = 10, x_max: float = 10_000) -> float:
-    x = value * (1.0 + (0.5 - random()))
-    if x >= x_min and x <= x_max:
-        return x
-    return value
-
-
 @dataclass
 class DataFromNodeT:
-    reputation: float = rand(100)
-    hive_balance: float = rand(100.0)
-    hive_power_balance: float = rand(100.0)
-    hive_dollars: float = rand(100.0)
-    hive_savings: float = rand(100.0)
-    hbd_savings: float = rand(100.0)
-    hbd_unclaimed: float = rand(100.0)
-    hp_unclaimed: float = rand(100.0)
-    hive_unclaimed: float = rand(100.0)
-    rc: int = int(rand(10, 10, 90))
-    voting_power: float = rand(100.0)
-    down_vote_power: float = rand(100.0)
+    reputation: float = random.uniform(0, 100)
+    hive_balance: float = random.uniform(0, 100)
+    hive_power_balance: float = random.uniform(0, 100)
+    hive_dollars: float = random.uniform(0, 100)
+    hive_savings: float = random.uniform(0, 100)
+    hbd_savings: float = random.uniform(0, 100)
+    hbd_unclaimed: float = random.uniform(0, 100)
+    hp_unclaimed: float = random.uniform(0, 100)
+    hive_unclaimed: float = random.uniform(0, 100)
+    rc: int = random.randint(0, 100)
+    voting_power: int = random.randint(0, 100)
+    down_vote_power: int = random.randint(0, 100)
 
     last_refresh: datetime = datetime.now()
 
@@ -74,11 +67,10 @@ class DataFromNodeT:
         self.last_refresh: datetime = datetime.now()
 
         for key, value in self.__dict__.items():
-            if isinstance(value, (int, float)):
-                if key != "rc":
-                    setattr(self, key, rand(value))
-                else:
-                    setattr(self, key, rand(value, 10, 90))
+            if isinstance(value, float):
+                setattr(self, key, random.uniform(0, 100))
+            elif isinstance(value, int):
+                setattr(self, key, random.randint(0, 100))
 
 
 class MockDB:
