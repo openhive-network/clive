c533c6050b826235b45a4872564bf89be2960419
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-06 08:08:16 +0100

    Make Parented MRO friendly

--- clive/ui/parented.py
+++ clive/ui/parented.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC
-from typing import Generic, TypeVar
+from typing import Any, Generic, TypeVar
 
 T = TypeVar("T")
 
@@ -9,5 +9,8 @@ T = TypeVar("T")
 class Parented(Generic[T], ABC):
     """An abstract class for objects that have a parent."""
 
-    def __init__(self, parent: T) -> None:
+    def __init__(self, parent: T, *args: Any, **kwargs: Any) -> None:
         self._parent = parent
+
+        # Multiple inheritance friendly, passes arguments to next object in MRO.
+        super().__init__(*args, **kwargs)
