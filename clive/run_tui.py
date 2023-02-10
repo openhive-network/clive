3291c63ba8b75998306950ad261238f8750ad367
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-10 11:36:18 +0100

    Redefine ctrl+c binding so it exits with explicitly set code 0

--- clive/run_tui.py
+++ clive/run_tui.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import sys
+
 
 def run_tui() -> None:
     from clive.ui.get_clive import get_clive
     from clive.util import prepare_before_launch
 
     prepare_before_launch()
-    get_clive().run()
+    reply = get_clive().run()
+    sys.exit(reply)
--- clive/ui/clive.py
+++ clive/ui/clive.py
@@ -1,15 +1,24 @@
 from __future__ import annotations
 
 from textual.app import App, ComposeResult
+from textual.binding import Binding
 from textual.widgets import Footer, Header
 
 
-class Clive(App[str]):
+class Clive(App[int]):
     """A singleton instance of the Clive app."""
+
+    BINDINGS = [
+        Binding("ctrl+c", "quit", "Quit"),
+    ]
 
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
         yield Footer()
+
+    async def action_quit(self) -> None:
+        self.log.debug("ctrl+c pressed, quitting...")
+        self.exit(0)
 
 
 clive = Clive()
