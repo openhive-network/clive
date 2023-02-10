be7dbb0e1cbda17530fd72c9b6262674e4c0ebe4
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-10 16:20:15 +0100

    Add test for dev entry point

--- tests/functional/test_startup.py
+++ tests/functional/test_startup.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import subprocess
+import time
 from typing import Final
 
 
@@ -14,3 +15,20 @@ def test_if_entry_point_works() -> None:
 
     # ASSERT
     assert status == exit_code_successful, f"`{entry_point}` command failed because of: `{result}`"
+
+
+def test_if_dev_entry_point_works() -> None:
+    # ARRANGE
+    entry_point: Final[str] = "clive-dev"
+
+    # ACT
+    process = subprocess.Popen([entry_point], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Give the process some time to start
+    time.sleep(1)
+
+    # ASSERT
+    running = not process.poll()
+    assert running, f"App failed to start.  Stderr: {process.stderr.read().decode() if process.stderr else 'N/A'}"
+
+    process.terminate()  # If it is running, terminate it - that means app was launched successfully
