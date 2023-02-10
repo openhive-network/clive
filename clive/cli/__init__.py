d0c442a91527e51fb3708bc2f2a29cbe5ec055bd
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-10 10:23:48 +0100

    Add --version and -V switches

--- clive/cli/__init__.py
+++ clive/cli/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Final
+from typing import Final, Optional
 
 import typer
 
@@ -16,6 +16,17 @@ cli = typer.Typer(help=HELP, rich_markup_mode="rich", context_settings={"help_op
 mock = typer.Typer()
 
 cli.add_typer(mock, name="mock", help="Just a mock subcommand to enable typer subcommands.")
+
+
+@cli.callback(invoke_without_command=True)
+def _main(
+    version: Optional[bool] = typer.Option(
+        None, "--version", "-V", help="Show the current version and exit.", is_eager=True
+    ),
+) -> None:
+    if version:
+        typer.echo(f"CLIVE Version: {VERSION.base} ({VERSION.commit}) {'dirty!' if VERSION.dirty else ''}")
+        raise typer.Exit()
 
 
 @cli.command()
