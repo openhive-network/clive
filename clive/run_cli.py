c755413fe41ea2111c77c1cec002fcbb7aa0d0aa
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-10 10:41:56 +0100

    Configure logger and log current settings before launching tui or cli

--- clive/run_cli.py
+++ clive/run_cli.py
@@ -3,5 +3,7 @@ from __future__ import annotations
 
 def run_cli() -> None:
     from clive.cli import cli
+    from clive.util import prepare_before_launch
 
+    prepare_before_launch()
     cli()
--- clive/run_tui.py
+++ clive/run_tui.py
@@ -3,5 +3,7 @@ from __future__ import annotations
 
 def run_tui() -> None:
     from clive.ui.get_clive import get_clive
+    from clive.util import prepare_before_launch
 
+    prepare_before_launch()
     get_clive().run()
