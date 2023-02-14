19826d257922cf0be217d98fcd7e2fb02a27be25
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-14 15:08:49 +0100

    Update node address in background task

--- clive/ui/app.py
+++ clive/ui/app.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+import asyncio
+import uuid
 from pathlib import Path
 
 from textual.app import App
 from textual.binding import Binding
 from textual.reactive import var
 
+from clive.storage.mock_database import NodeAddress, mock_db
 from clive.ui.dashboard.dashboard import Dashboard
 from clive.ui.quit.quit import Quit
 from clive.version import VERSION_INFO
@@ -19,7 +22,6 @@ class Clive(App[int]):
     CSS_PATH = [
         Path(__file__).parent / "quit/quit.scss",
         Path(__file__).parent / "widgets/header.scss",
-        Path(__file__).parent / "widgets/titled.scss",
     ]
 
     BINDINGS = [
@@ -35,7 +37,14 @@ class Clive(App[int]):
     """Synchronize the expanded header state in all created header objects."""
 
     def on_mount(self) -> None:
+        asyncio.create_task(self.background_task())
         self.push_screen("dashboard")
+
+    async def background_task(self) -> None:
+        while True:
+            await asyncio.sleep(3)
+            self.log("Updating mock node_address...")
+            mock_db.node_address = NodeAddress("https", str(uuid.uuid4()))
 
 
 clive_app = Clive()
