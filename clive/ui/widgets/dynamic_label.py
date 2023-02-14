0f084fdb61c6c4d81e2ccd66fb0fe12f8bb20ea6
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-14 15:25:23 +0100

    Make DynamicLabel accept prefix

--- clive/ui/widgets/dynamic_label.py
+++ clive/ui/widgets/dynamic_label.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from textual.reactive import watch
 from textual.widgets import Label
@@ -12,10 +12,14 @@ if TYPE_CHECKING:
 class DynamicLabel(Label):
     """A label that can be updated dynamically when a reactive variable changes."""
 
-    def __init__(self, obj_to_watch: Reactable, attribute_name: str) -> None:
+    def __init__(self, obj_to_watch: Reactable, attribute_name: str, *, prefix: str = "") -> None:
         super().__init__("DynamicLabel was not updated!")
         self.__obj_to_watch = obj_to_watch
         self.__attribute_name = attribute_name
+        self.__prefix = prefix
 
     def on_mount(self) -> None:
-        watch(self.__obj_to_watch, self.__attribute_name, lambda value: self.update(str(value)))
+        watch(self.__obj_to_watch, self.__attribute_name, self.on_attribute_changed)
+
+    def on_attribute_changed(self, value: Any) -> None:
+        self.update(f"{self.__prefix}{value}")
