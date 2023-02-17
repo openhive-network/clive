dae6f5ca3d61f980069a8ad1b903825763a2ab1a
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-17 11:33:10 +0100

    Create and use update_reactive method

--- clive/ui/app.py
+++ clive/ui/app.py
@@ -2,12 +2,12 @@ from __future__ import annotations
 
 import asyncio
 import uuid
-from copy import deepcopy
 from pathlib import Path
+from typing import Any, Callable
 
 from textual.app import App
 from textual.binding import Binding
-from textual.reactive import reactive, var
+from textual.reactive import var
 
 from clive.enums import AppMode
 from clive.storage.mock_database import NodeAddress, NodeData, ProfileData
@@ -48,7 +48,7 @@ class Clive(App[int]):
 
     profile_data = var(ProfileData())
 
-    app_state = reactive(AppState(), repaint=False, always_update=True)
+    app_state = var(AppState())
 
     def on_mount(self) -> None:
         asyncio.create_task(self.background_task())
@@ -56,12 +56,13 @@ class Clive(App[int]):
         self.push_screen("dashboard_inactive")
 
     async def background_task(self) -> None:
+        def __update_function(profile_data: ProfileData) -> None:
+            profile_data.node_address = NodeAddress("https", str(uuid.uuid4()))
+
         while True:
             await asyncio.sleep(3)
             self.log("Updating mock node_address...")
-            new_data = deepcopy(self.profile_data)
-            new_data.node_address = NodeAddress("https", str(uuid.uuid4()))
-            self.profile_data = new_data
+            self.update_reactive("profile_data", __update_function)
 
     async def debug_task(self) -> None:
         while True:
@@ -71,12 +72,43 @@ class Clive(App[int]):
             self.log("=================================================")
 
     def activate(self) -> None:
-        self.app_state.mode = AppMode.ACTIVE
-        self.app_state = self.app_state  # we need to trigger descriptor __set__ method
+        def __update_function(app_state: AppState) -> None:
+            app_state.mode = AppMode.ACTIVE
+
+        self.update_reactive("app_state", __update_function)
 
     def deactivate(self) -> None:
-        self.app_state.mode = AppMode.INACTIVE
-        self.app_state = self.app_state  # we need to trigger descriptor __set__ method
+        def __update_function(app_state: AppState) -> None:
+            app_state.mode = AppMode.INACTIVE
+
+        self.update_reactive("app_state", __update_function)
+
+    def update_reactive(self, attribute_name: str, update_function: Callable[[Any], None]) -> None:
+        """
+        Reactive attributes of Textual are unable to detect changes to their own attributes
+        (if we are dealing with a non-primitive type like a custom class).
+        In order to notify watchers of a reactive attribute, it would have to be re-instantiated with the modified
+        attributes. (See https://github.com/Textualize/textual/discussions/1099#discussioncomment-4047932)
+        This is where this method comes in handy.
+        """
+        try:
+            attribute = getattr(self, attribute_name)
+        except AttributeError as error:
+            raise AttributeError(f"{error}. Available ones are: {list(self._reactives)}") from error
+
+        descriptor = self.__class__.__dict__[attribute_name]
+
+        update_function(attribute)  # modify attributes of the reactive attribute
+
+        # now we trigger the descriptor.__set__ method like the `self.attribute_name = value` would do
+        if not descriptor._always_update:
+            # that means, watchers won't be notified unless __ne__ returns False, we could bypass with `always_update`
+            descriptor._always_update = True
+            setattr(self, attribute_name, attribute)
+            descriptor._always_update = False
+        else:
+            # we just need to trigger descriptor.__set__
+            setattr(self, attribute_name, attribute)
 
 
 clive_app = Clive()
