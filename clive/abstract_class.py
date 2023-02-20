9a42806b8d8b3736f2943a21cfdc0880103579af
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-20 10:01:49 +0100

    Create AbstractClassMessagePump

--- clive/abstract_class.py
+++ clive/abstract_class.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
-from abc import ABC
+from abc import ABC, ABCMeta
 from typing import Any
+
+from textual.message_pump import MessagePumpMeta
 
 
 class AbstractClass(ABC):
@@ -10,10 +12,31 @@ class AbstractClass(ABC):
     Even when no abstract methods were defined, this class cannot be instantiated. (default ABC allows for that)
     """
 
-    def __new__(cls, *args, **kwargs) -> Any:  # type: ignore
-        if AbstractClass in cls.__bases__:
+    def __new__(cls, abstract_type: type[AbstractClass] | None = None, *args, **kwargs) -> Any:  # type: ignore
+        abstract_type = AbstractClass if abstract_type is None else abstract_type
+        if abstract_type in cls.__bases__:
             raise TypeError(f"Abstract class `{cls.__name__}` cannot be instantiated.")
 
         if super().__new__ is object.__new__ and cls.__init__ is not object.__init__:
             return super().__new__(cls)
         return super().__new__(cls, *args, **kwargs)
+
+
+class MessagePumpABCMeta(MessagePumpMeta, ABCMeta):
+    """
+    Combine MessagePumpMeta and ABCMeta into a single metaclass.
+    Resolves the issue with:
+        TypeError: metaclass conflict: the metaclass of a derived class must be a (non-strict) subclass of the
+        metaclasses of all its bases
+    """
+
+
+class AbstractClassMessagePump(AbstractClass, metaclass=MessagePumpABCMeta):
+    """
+    Class used to mark all MessagePump derivatives (widget, screen, etc.) as Abstract.
+
+    See the :class:`AbstractClass` class for more details.
+    """
+
+    def __new__(cls, *args: Any, **kwargs: Any) -> Any:
+        return super().__new__(cls, AbstractClassMessagePump, *args, **kwargs)
--- clive/ui/shared/base_screen.py
+++ clive/ui/shared/base_screen.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod
 from typing import TYPE_CHECKING
 
-from textual.message_pump import MessagePumpMeta
 from textual.widgets import Footer
 
+from clive.abstract_class import AbstractClassMessagePump
 from clive.ui.widgets.clive_screen import CliveScreen
 from clive.ui.widgets.header import Header
 
@@ -13,16 +13,7 @@ if TYPE_CHECKING:
     from textual.app import ComposeResult
 
 
-class MessagePumpABCMeta(MessagePumpMeta, ABCMeta):
-    """
-    Combine MessagePumpMeta and ABCMeta into a single metaclass.
-    Resolves the issue with:
-        TypeError: metaclass conflict: the metaclass of a derived class must be a (non-strict) subclass of the
-        metaclasses of all its bases
-    """
-
-
-class BaseScreen(CliveScreen, metaclass=MessagePumpABCMeta):
+class BaseScreen(CliveScreen, AbstractClassMessagePump):
     def compose(self) -> ComposeResult:
         yield Header()
         yield from self.create_main_panel()
