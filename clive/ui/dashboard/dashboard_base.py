8bf230c16e0f002430bacdfd7dab9b99227cecf7
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-27 11:51:20 +0100

    Redesign dashboard

--- clive/ui/dashboard/dashboard_base.py
+++ clive/ui/dashboard/dashboard_base.py
@@ -2,169 +2,78 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from textual.containers import Container
-from textual.widgets import Static
+from textual.containers import Container, Horizontal
+from textual.widget import Widget
+from textual.widgets import Label, Static
 
+from clive.storage.mock_database import AccountType
 from clive.ui.shared.base_screen import BaseScreen
-from clive.ui.widgets.titled_label import TitledLabel
+from clive.ui.widgets.ellipsed_static import EllipsedStatic
 
 if TYPE_CHECKING:
     from textual.app import ComposeResult
 
-    from clive.storage.mock_database import NodeData, ProfileData
 
-
-class Body(Container, can_focus=True):
-    pass
-
-
-class Section(Container):
-    pass
-
-
-class SectionTitle(Static):
-    pass
-
-
-class General(Section):
+class BalanceStats(Widget):
     def compose(self) -> ComposeResult:
-        yield SectionTitle(self.__class__.__name__)
-        yield TitledLabel(
-            "My account",
-            obj_to_watch=self.app,
-            attribute_name="profile_data",
-            callback=self.__get_account_name,
-        )
-        yield TitledLabel(
-            "Reputation",
-            obj_to_watch=self.app,
-            attribute_name="node_data",
-            callback=self.__get_reputation,
-        )
-        yield TitledLabel("Estimated account value", "10.24 USD")
-
-    @staticmethod
-    def __get_account_name(profile_data: ProfileData) -> str:
-        return profile_data.active_account.name
-
-    @staticmethod
-    def __get_reputation(node_data: NodeData) -> str:
-        return f"{node_data.reputation:.2f}"
+        yield Static("RC", classes="title")
+        yield EllipsedStatic("VOTING", classes="title title-variant")
+        yield EllipsedStatic("DOWNVOTING", classes="title")
+        yield EllipsedStatic("HIVEPOWER", classes="title title-variant")
+        yield Static("75%", classes="percentage")
+        yield Static("5d", classes="time")
+        yield Static("65%", classes="percentage")
+        yield Static("4d", classes="time")
+        yield Static("55%", classes="percentage")
+        yield Static("3d", classes="time")
+        yield Static("170M", id="hivepower-value")
 
 
-class Accounts(Section):
+class ActivityStats(Widget):
     def compose(self) -> ComposeResult:
-        yield SectionTitle(self.__class__.__name__)
-        yield TitledLabel("Last owner update", "2021-01-01 00:00:00")
-        yield TitledLabel("Last account update", "2021-01-01 00:00:00")
-        yield TitledLabel("Recovery account", "hive.recovery")
+        yield Static("", classes="empty")
+        yield EllipsedStatic("LIQUID", classes="title")
+        yield EllipsedStatic("SAVINGS", classes="title title-variant")
+        yield Static("HIVE", classes="token")
+        yield Static("100.000", classes="amount")
+        yield Static("100.000", classes="amount amount-variant")
+        yield EllipsedStatic("HBD", classes="token token-variant")
+        yield Static("100.000", classes="amount amount-variant")
+        yield Static("100.000", classes="amount")
 
 
-class Money(Section):
+class AccountInfo(Container):
+    def __init__(self, account_name: str, *, account_type: AccountType):
+        super().__init__()
+        self.__account_name = account_name
+        self.__account_type = account_type
+
     def compose(self) -> ComposeResult:
-        yield SectionTitle(self.__class__.__name__)
-        yield TitledLabel(
-            "HIVE balance",
-            obj_to_watch=self.app,
-            attribute_name="node_data",
-            callback=self.__get_hive_balance,
-        )
-        yield TitledLabel(
-            "HP balance",
-            obj_to_watch=self.app,
-            attribute_name="node_data",
-            callback=self.__get_hp_balance,
-        )
-        yield TitledLabel(
-            "HBD balance",
-            obj_to_watch=self.app,
-            attribute_name="node_data",
-            callback=self.__get_hbd_balance,
-        )
-        yield TitledLabel(
-            "HIVE savings balance",
-            obj_to_watch=self.app,
-            attribute_name="node_data",
-            callback=self.__get_hive_savings_balance,
-        )
-        yield TitledLabel(
-            "HBD savings balance",
-            obj_to_watch=self.app,
-            attribute_name="node_data",
-            callback=self.__get_hbd_savings_balance,
-        )
-
-    @staticmethod
-    def __get_hive_balance(node_data: NodeData) -> str:
-        return f"{node_data.hive_balance:.2f} HIVE"
-
-    @staticmethod
-    def __get_hp_balance(node_data: NodeData) -> str:
-        return f"{node_data.hive_power_balance:.2f} HP"
-
-    @staticmethod
-    def __get_hbd_balance(node_data: NodeData) -> str:
-        return f"{node_data.hive_dollars:.2f} HBD"
-
-    @staticmethod
-    def __get_hive_savings_balance(node_data: NodeData) -> str:
-        return f"{node_data.hive_savings:.2f} HIVE"
-
-    @staticmethod
-    def __get_hbd_savings_balance(node_data: NodeData) -> str:
-        return f"{node_data.hbd_savings:.2f} HBD"
+        yield EllipsedStatic(f"{self.__account_type.capitalize()}: {self.__account_name}")
+        yield Label("2x ALARMS!", id="account-alarms")
+        yield Static()
+        yield Label("LAST:")
+        yield Label("Transaction: 10.01.23")
+        yield Label("Update: 10.01.23")
 
 
-class Powers(Section):
+class AccountRow(Container):
+    def __init__(self, account_name: str, *, account_type: AccountType = AccountType.ACTIVE) -> None:
+        super().__init__(classes=account_type)
+        self.__account_name = account_name
+        self.__account_type = account_type
+
     def compose(self) -> ComposeResult:
-        yield SectionTitle(self.__class__.__name__)
-        yield TitledLabel(
-            "Resource credits (RC)",
-            obj_to_watch=self.app,
-            attribute_name="node_data",
-            callback=self.__get_rc,
-        )
-        yield TitledLabel(
-            "Voting power",
-            obj_to_watch=self.app,
-            attribute_name="node_data",
-            callback=self.__get_voting_power,
-        )
-        yield TitledLabel(
-            "Down voting power",
-            obj_to_watch=self.app,
-            attribute_name="node_data",
-            callback=self.__get_down_voting_power,
-        )
-
-    @staticmethod
-    def __get_rc(node_data: NodeData) -> str:
-        return f"{node_data.rc:.2f} %"
-
-    @staticmethod
-    def __get_voting_power(node_data: NodeData) -> str:
-        return f"{node_data.voting_power:.2f} %"
-
-    @staticmethod
-    def __get_down_voting_power(node_data: NodeData) -> str:
-        return f"{node_data.down_vote_power:.2f} %"
-
-
-class History(Section):
-    def compose(self) -> ComposeResult:
-        yield SectionTitle(self.__class__.__name__)
-        yield Static("No history")
+        with Horizontal():
+            yield AccountInfo(self.__account_name, account_type=self.__account_type)
+            with Container(id="tables"):
+                yield BalanceStats()
+                yield Static()
+                yield ActivityStats()
 
 
 class DashboardBase(BaseScreen):
     def create_main_panel(self) -> ComposeResult:
-        yield Body(
-            General(),
-            Accounts(),
-            Money(),
-            Powers(),
-            History(),
-        )
-
-    def on_mount(self) -> None:
-        self.query_one(Body).focus()
+        yield AccountRow("vogel")
+        yield AccountRow("gtg", account_type=AccountType.WATCHED)
+        yield AccountRow("veryverylonglongname", account_type=AccountType.WATCHED)
