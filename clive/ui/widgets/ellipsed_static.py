ff64ebeb2e6f3c22f66ddb7910be8321d222c72c
Mateusz Żebrak <mzebrak@syncad.com>
2023-02-27 10:16:05 +0100

    Add EllipsedStatic widget

---
+++ clive/ui/widgets/ellipsed_static.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from rich.text import Text
+from textual.widgets import Static
+
+
+class EllipsedStatic(Static):
+    """A static widget that renders a string with ellipsis if it overflows."""
+
+    def __init__(self, renderable: str = "", *, id_: str | None = None, classes: str | None = None):
+        super().__init__(id=id_, classes=classes)
+        self.__renderable = renderable
+
+    def render(self) -> Text:
+        return Text(self.__renderable, no_wrap=True, overflow="ellipsis")
