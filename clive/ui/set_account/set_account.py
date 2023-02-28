6c82f9c74107138ca9c0c03ab91d6b28d622ec25
Krzysztof Mochocki <kmochocki@syncad.com>
2023-02-28 20:40:49 +0100

    Add very simple screen to set account name

---
+++ clive/ui/set_account/set_account.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from textual.binding import Binding
+from textual.containers import Horizontal
+from textual.widgets import Input, Label
+
+from clive.ui.shared.form_screen import FormScreen
+
+if TYPE_CHECKING:
+    from textual.app import ComposeResult
+
+
+class SetAccount(FormScreen):
+    BINDINGS = [Binding("f10", "save_account_name", "Save")]
+
+    def create_main_panel(self) -> ComposeResult:
+        yield Label("Account name:\t @")
+        yield Input(
+            str(self.app.profile_data.active_account.name), placeholder="e.x.: cookingwithkasia", id="set_account_name"
+        )
+
+    def action_save_account_name(self) -> None:
+        self.app.profile_data.active_account.name = self.get_widget_by_id("set_account_name", expect_type=Input).value
