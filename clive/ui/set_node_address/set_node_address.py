3f786ea13eb5a2108fea9bfc117e33f2430084ac
Krzysztof Mochocki <kmochocki@syncad.com>
2023-02-28 20:40:29 +0100

    Add very simple screen to set node address

---
+++ clive/ui/set_node_address/set_node_address.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from urllib.parse import urlparse
+
+from textual.binding import Binding
+from textual.containers import Horizontal
+from textual.widgets import Input, Label
+
+from clive.storage.mock_database import NodeAddress
+from clive.ui.shared.form_screen import FormScreen
+
+if TYPE_CHECKING:
+    from textual.app import ComposeResult
+
+
+class SetNodeAddress(FormScreen):
+    BINDINGS = [Binding("f10", "save_node_address", "Save")]
+
+    def create_main_panel(self) -> ComposeResult:
+        with Horizontal():
+            yield Label("Set node address:\t")
+            yield Input(str(self.app.profile_data.node_address), placeholder="e.x.: https://api.hive.blog", id="set_node_address")
+
+    def action_save_node_address(self) -> None:
+        try:
+            url = urlparse(self.get_widget_by_id("set_node_address", expect_type=Input).value)
+            self.app.profile_data.node_address = NodeAddress(url.scheme, url.netloc, url.port)
+        except ValueError:
+            pass
