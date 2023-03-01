03311413555b25485f5e8a888925f7731ca01c9b
Krzysztof Mochocki <kmochocki@syncad.com>
2023-03-01 19:12:40 +0100

    Improve switching mechanism in authority inputs

--- clive/ui/manage_authorities/widgets/authority_inputs.py
+++ clive/ui/manage_authorities/widgets/authority_inputs.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Final, cast
 
 from textual.containers import Horizontal
 from textual.widgets import Input, Static, Switch
@@ -47,8 +47,10 @@ class AuthorityInputSwitch(Horizontal):
         id: str | None = None,
         classes: str | None = None,
     ) -> None:
+        self.__hidden_class: Final[str] = "hidden"
         self.__right = right
         self.__left = left
+        self.__left.add_class(self.__hidden_class)
         super().__init__(id=id, classes=classes)
 
     def compose(self) -> ComposeResult:
@@ -56,13 +58,14 @@ class AuthorityInputSwitch(Horizontal):
         yield Switch(False, id="input_type")
         yield Static("Input path to private key")
 
-    def on_mount(self) -> None:
-        def set_hidden(obj: AuthorityInput, value: bool) -> None:
-            if value:
-                obj.add_class("hidden")
+    def __swap_visibility(self) -> None:
+        for auth_in in [self.__right, self.__left]:
+            if auth_in.has_class(self.__hidden_class):
+                auth_in.remove_class(self.__hidden_class)
             else:
-                obj.remove_class("hidden")
+                auth_in.add_class(self.__hidden_class)
 
-        s = self.query_one(Switch)
-        self.__right.watch(s, "value", lambda: set_hidden(self.__right, s.value), init=True)
-        self.__left.watch(s, "value", lambda: set_hidden(self.__left, not s.value), init=True)
+    def on_switch_changed(self, event: Switch.Changed) -> None:
+        sender = cast(Switch, event.sender)
+        if sender.id == "input_type":
+            self.__swap_visibility()
