98c3c2e13830ff8b508a3c6ce81de8078bd98b34
Krzysztof Mochocki <kmochocki@syncad.com>
2023-03-01 22:56:07 +0100

    Reenable SetNodeAddressForm and small style fixes

--- clive/ui/onboarding/onboarding.py
+++ clive/ui/onboarding/onboarding.py
@@ -4,12 +4,13 @@ from typing import Iterator
 
 from clive.ui.manage_authorities import NewAuthorityForm
 from clive.ui.registration.registration import RegistrationForm
+from clive.ui.set_node_address.set_node_address import SetNodeAddressForm
 from clive.ui.shared.form import Form, ScreenBuilder
 
 
 class Onboarding(Form):
     def register_screen_builders(self) -> Iterator[ScreenBuilder]:
         yield RegistrationForm
-        # yield SetNodeAddress
+        yield SetNodeAddressForm
         # yield SetAccount
         yield NewAuthorityForm
