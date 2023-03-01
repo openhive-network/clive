14728cbed2c83d2ef6247f01239015cf44bc7609
Krzysztof Mochocki <kmochocki@syncad.com>
2023-03-01 22:36:54 +0100

    Fix registration screen

--- clive/ui/registration/registration.py
+++ clive/ui/registration/registration.py
@@ -18,7 +18,7 @@ class RegistrationCommon(BaseScreen):
     def create_main_panel(self) -> ComposeResult:
         with DialogContainer():
             yield Static("Profile name", classes="label")
-            yield Input(placeholder="e.x.: Master", id="profile_name_input")
+            yield Input(self.app.profile_data.name, placeholder="e.x.: Master", id="profile_name_input")
             yield Static("Password", classes="label")
             yield Input(placeholder="Password", password=True, id="password_input")
             yield Static("Repeat password", classes="label")
