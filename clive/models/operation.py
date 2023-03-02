0417f62c937cf5dae03e534724428ef264df9a3b
Krzysztof Mochocki <kmochocki@syncad.com>
2023-03-02 03:33:49 +0100

    Add id field that grant uniqueness of operations in cart

--- clive/models/operation.py
+++ clive/models/operation.py
@@ -2,13 +2,26 @@ from __future__ import annotations
 
 import dataclasses
 import json
+from itertools import count
+from typing import ClassVar
 
 from clive.abstract_class import AbstractClass
 
 
 @dataclasses.dataclass
 class Operation(AbstractClass):
+    __instance_count: ClassVar[count[int]] = count(0)
+
     type_: str
+
+    # this field is just to grant uniqueness of each operation
+    _id: int = dataclasses.field(
+        default_factory=lambda: next(Operation.__instance_count),
+        init=False,
+        hash=True,
+        repr=False,
+        compare=True,
+    )
 
     def as_json(self) -> str:
         return json.dumps(dataclasses.asdict(self), indent=4)
