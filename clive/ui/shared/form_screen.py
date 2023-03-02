107783c89aab495d0f46df4a3fdb2e23d96a38ce
Krzysztof Mochocki <kmochocki@syncad.com>
2023-03-02 01:43:16 +0100

    Separate FormScreen to atomic classes

--- clive/ui/shared/form_screen.py
+++ clive/ui/shared/form_screen.py
@@ -9,23 +9,34 @@ if TYPE_CHECKING:
     from clive.ui.shared.form import Form
 
 
-class FormScreen(DOMNode):
-    BINDINGS = [
-        Binding("ctrl+p", "previous_screen", "Previous screen"),
-        Binding("ctrl+n", "next_screen", "Next screen"),
-    ]
-
-    def set_form_owner(self, owner: "Form") -> FormScreen:
+class FormScreenBase(DOMNode):
+    def set_form_owner(self, owner: "Form") -> FormScreenBase:
         self._owner = owner
         return self
 
-    def __is_owner_set(self) -> bool:
+    def _is_owner_set(self) -> bool:
         return hasattr(self, "_owner") and self._owner is not None
 
+
+class FirstFormScreen(FormScreenBase):
+    BINDINGS = [
+        Binding("ctrl+n", "next_screen", "Next screen"),
+    ]
+
     def action_next_screen(self) -> None:
-        assert self.__is_owner_set()
+        assert self._is_owner_set()
         self._owner.action_next_screen()
 
+
+class LastFormScreen(FormScreenBase):
+    BINDINGS = [
+        Binding("ctrl+p", "previous_screen", "Previous screen"),
+    ]
+
     def action_previous_screen(self) -> None:
-        assert self.__is_owner_set()
+        assert self._is_owner_set()
         self._owner.action_previous_screen()
+
+
+class FormScreen(FirstFormScreen, LastFormScreen):
+    pass
