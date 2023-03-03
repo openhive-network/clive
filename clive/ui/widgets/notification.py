88cd652a626dfa026e0edc426ee773f71e8c8b09
Mateusz Żebrak <mzebrak@syncad.com>
2023-03-03 14:12:54 +0100

    Introduce predefined formats with categories of Notification

--- clive/ui/widgets/notification.py
+++ clive/ui/widgets/notification.py
@@ -1,9 +1,22 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, Literal
+
 from textual.widgets import Static
+
+if TYPE_CHECKING:
+    from rich.console import RenderableType
 
 
 class Notification(Static):
+    def __init__(
+        self, renderable: RenderableType, *, category: Literal["success", "info", "warning", "error"] | None = None
+    ) -> None:
+        super().__init__(self.__format_renderable(renderable, category))
+
+        if category:
+            self.add_class(category)
+
     def on_mount(self) -> None:
         self.set_timer(3, self.remove)
 
@@ -12,3 +25,18 @@ class Notification(Static):
 
     def send(self) -> None:
         self.app.mount(self)
+
+    @staticmethod
+    def __format_renderable(
+        renderable: RenderableType, category: Literal["success", "info", "warning", "error"] | None = None
+    ) -> RenderableType:
+        if category == "success":
+            return f"✅ {renderable}"
+        elif category == "info":
+            return f"ℹ️  {renderable}"  # noqa: RUF001
+        elif category == "warning":
+            return f"📢 {renderable}"
+        elif category == "error":
+            return f"⚠️  {renderable}"
+        else:
+            return renderable
