df0e294c6e6f1eda174a7fc7abcc76a328e8289a
Mateusz Żebrak <mzebrak@syncad.com>
2023-03-06 12:37:31 +0100

    Bind escape on NewAuthority screen

--- clive/ui/manage_authorities/new_authority.py
+++ clive/ui/manage_authorities/new_authority.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from textual.binding import Binding
 from textual.widgets import Static
 
 from clive.ui.manage_authorities.widgets.authority_form import AuthorityForm
@@ -11,7 +12,7 @@ if TYPE_CHECKING:
     from textual.app import ComposeResult
 
 
-class NewAuthority(AuthorityForm):
+class NewAuthorityBase(AuthorityForm):
     def on_authority_form_saved(self, event: AuthorityForm.Saved) -> None:
         self.app.profile_data.active_account.keys.append(event.authority)
         self.app.update_reactive("profile_data")
@@ -20,6 +21,12 @@ class NewAuthority(AuthorityForm):
         return "create authority"
 
 
-class NewAuthorityForm(NewAuthority, FormScreen):
+class NewAuthority(NewAuthorityBase):
+    BINDINGS = [
+        Binding("escape", "pop_screen", "Authorities"),
+    ]
+
+
+class NewAuthorityForm(NewAuthorityBase, FormScreen):
     def _submit_buttons(self) -> ComposeResult:
         yield Static()
