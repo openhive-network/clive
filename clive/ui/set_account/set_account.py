922e58812db792ae1af4afbc90d8fc907636034e
Mateusz Żebrak <mzebrak@syncad.com>
2023-03-07 14:10:50 +0100

    Switch to context managers in SetAccount

--- clive/ui/set_account/set_account.py
+++ clive/ui/set_account/set_account.py
@@ -39,16 +39,15 @@ class SetAccount(BaseScreen, FormScreen):
     BINDINGS = [Binding("f10", "save_account_name", "Save")]
 
     def create_main_panel(self) -> ComposeResult:
-        yield ViewBag(
-            BigTitle("set account name"),
-            Label("Account name:\t @"),
-            Input(
+        with ViewBag():
+            yield BigTitle("set account name")
+            yield Label("Account name:\t @")
+            yield Input(
                 str(self.app.profile_data.active_account.name),
                 placeholder="e.x.: cookingwithkasia",
                 id="set_account_name",
                 highlighter=AccountNameHighlighter(),
-            ),
-        )
+            )
 
     def action_save_account_name(self) -> None:
         account_name = self.get_widget_by_id("set_account_name", expect_type=Input).value
