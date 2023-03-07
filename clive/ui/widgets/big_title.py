ac70df09e2833317bd3f861b366bdbbf5ea471c5
Mateusz Żebrak <mzebrak@syncad.com>
2023-03-07 13:09:29 +0100

    Refactor BigTitle
    - micro optimalization of lower() call
    - removed unneded and unused init params
      which could lead to undefined behaviour
      (expand, shrink)

--- clive/ui/widgets/big_title.py
+++ clive/ui/widgets/big_title.py
@@ -17,31 +17,15 @@ class BigTitle(Static):
     }
     """
 
-    def __init__(
-        self,
-        title: str = "",
-        *,
-        expand: bool = False,
-        shrink: bool = False,
-        markup: bool = True,
-        name: str | None = None,
-        id: str | None = None,
-        classes: str | None = None,
-        disabled: bool = False,
-    ) -> None:
+    def __init__(self, title: str = "", *, id: str | None = None, classes: str | None = None) -> None:
         self.__title = title
         self.__translated = self.__translate(title)
         self.__translated_width = self.__translated.index("\n")
 
         super().__init__(
             self.__translated,
-            expand=expand,
-            shrink=shrink,
-            markup=markup,
-            name=name,
             id=id,
             classes=classes,
-            disabled=disabled,
         )
 
     def on_resize(self) -> None:
@@ -58,10 +42,10 @@ class BigTitle(Static):
         self.update(self.__translated)
         self.remove_class("-compact")
 
-    def __translate(self, text: str) -> str:
+    @staticmethod
+    def __translate(text: str) -> str:
         result = ["", "", ""]
-        for char in text:
-            char = char.lower()
+        for char in text.lower():
             if char in FONT:
                 for i, line in enumerate(result):
                     result[i] = line + " " + FONT[char][i]
