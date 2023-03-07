4cad9563dd5f2c281c30b86aee232d83b2aa0cb5
Mateusz Żebrak <mzebrak@syncad.com>
2023-03-07 09:39:03 +0100

    Include ManageAuthorities in screen for selecting config options

--- clive/ui/config/config_active.py
+++ clive/ui/config/config_active.py
@@ -6,19 +6,23 @@ from textual.binding import Binding
 from textual.widgets import Button
 
 from clive.ui.config.config_base import ConfigBase
+from clive.ui.manage_authorities import ManageAuthorities
 
 
 class ConfigActive(ConfigBase):
     BINDINGS = [
-        Binding("f2", "mock", "Mock"),
+        Binding("f2", "manage_authorities", "Manage authorities"),
     ]
 
     def additional_buttons(self) -> Iterable[Button]:
-        yield Button("Mock", id="mock")
+        yield Button("Manage authorities", id="manage-authorities")
 
     def action_mock(self) -> None:
         self.app.deactivate()
 
+    def action_manage_authorities(self) -> None:
+        self.app.push_screen(ManageAuthorities())
+
     def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "mock":
-            self.action_mock()
+        if event.button.id == "manage-authorities":
+            self.action_manage_authorities()
