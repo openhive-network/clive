9ab804f9aa9d24f9c6cc12d5adbfd76ee1f3895a
Mateusz Żebrak <mzebrak@syncad.com>
2023-03-13 11:35:30 +0100

    Make CliveScreen inherit from CliveWidget like textual Screen does

--- clive/ui/widgets/clive_screen.py
+++ clive/ui/widgets/clive_screen.py
@@ -1,22 +1,13 @@
 from __future__ import annotations
 
-import typing
-
 from textual.screen import Screen
 
-if typing.TYPE_CHECKING:
-    from clive.ui.app import Clive
+from clive.ui.widgets.clive_widget import CliveWidget
 
 
-class CliveScreen(Screen):
+class CliveScreen(Screen, CliveWidget):
     """
     An ordinary textual screen that also knows what type of application it belongs to.
 
     Inspired by: https://github.com/Textualize/textual/discussions/1099#discussioncomment-4049612
     """
-
-    @property
-    def app(self) -> Clive:
-        from clive.ui.app import Clive  # noqa: TCH001  # To avoid circular imports
-
-        return typing.cast(Clive, super().app)
