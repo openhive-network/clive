8bc196d84b023a4a51970661544b54fa83bf79e6
Mateusz Żebrak <mzebrak@syncad.com>
2023-03-14 13:18:59 +0100

    Store tasks in a single dict

--- clive/ui/background_tasks.py
+++ clive/ui/background_tasks.py
@@ -18,7 +18,6 @@ class BackgroundTasks:
     def __init__(self, app: Clive) -> None:
         self.__app = app
         self.__tasks: TasksDict = {}
-        self.__delayed_tasks: TasksDict = {}
         self.__long_running_tasks = [
             asyncio.create_task(self.__update_data_from_node()),
             *[asyncio.create_task(self.__debug_log()) if settings.LOG_DEBUG_LOOP else []],
@@ -28,26 +27,19 @@ class BackgroundTasks:
     def tasks(self) -> TasksDict:
         return self.__tasks.copy()
 
-    @property
-    def delayed_tasks(self) -> TasksDict:
-        return self.__delayed_tasks.copy()
-
     def run(self, name: str, function: Callable[[], Any]) -> None:
         """Create a new background task."""
         self.__tasks[name] = asyncio.create_task(function())
 
     def run_after(self, time: timedelta, function: Callable[[], Any], name: str | None = None) -> None:
         """Run a function after a given time."""
-        name = name or f"run_after_{function.__name__}"
-
-        self.__delayed_tasks[name] = asyncio.create_task(self.__wait_before_call(time, function))
+        name = name or f"delayed-{function.__name__}"
+        self.__tasks[name] = asyncio.create_task(self.__wait_before_call(time, function))
 
     def cancel(self, name: str, *, remove: bool = True) -> None:
         """Cancel a background task."""
         if name in self.__tasks:
             self.__tasks[name].cancel()
-        elif name in self.__delayed_tasks:
-            self.__delayed_tasks[name].cancel()
 
         if remove:
             self.__tasks.pop(name, None)
