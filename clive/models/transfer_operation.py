e7c93c88229891e57343afa235f3ced3ec61f472
Mateusz Żebrak <mzebrak@syncad.com>
2023-03-23 12:46:59 +0100

    Allow for empty memo in transfer operation

--- clive/models/transfer_operation.py
+++ clive/models/transfer_operation.py
@@ -17,7 +17,6 @@ class TransferOperation(Operation):
     def is_valid(self) -> bool:
         return (
             self.asset in ["HBD", "TBD", "HIVE", "TESTS"]
-            and len(self.memo) > 0
             and len(self.amount) > 0
             and len(self.to) > 0
             and len(self.from_) > 0
