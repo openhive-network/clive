5daff2c29e6885fc0790a44522e2a34ef1ff899d
Mateusz Żebrak <mzebrak@syncad.com>
2023-03-27 15:10:59 +0200

    Use context manager in Quit

--- clive/ui/quit/quit.py
+++ clive/ui/quit/quit.py
@@ -23,15 +23,12 @@ class Quit(BaseScreen):
         self.query(Button).first().focus()
 
     def create_main_panel(self) -> ComposeResult:
-        yield DialogContainer(
-            Static("Are you sure you want to quit?", id="question"),
-            Static("(You can also confirm by pressing Ctrl+C again)", id="hint"),
-            Horizontal(
-                Button("Quit", variant="error", id="quit"),
-                Button("Cancel", variant="primary", id="cancel"),
-                id="buttons",
-            ),
-        )
+        with DialogContainer():
+            yield Static("Are you sure you want to quit?", id="question")
+            yield Static("(You can also confirm by pressing Ctrl+C again)", id="hint")
+            with Horizontal(id="buttons"):
+                yield Button("Quit", variant="error", id="quit")
+                yield Button("Cancel", variant="primary", id="cancel")
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == "quit":
