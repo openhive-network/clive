37939b5488b21b74278443722a149ca1ed39f4af
Mateusz Żebrak <mzebrak@syncad.com>
2023-04-07 08:56:19 +0200

    Share Typer common options by common_options decorator (merge_args)

--- clive/__private/cli/common.py
+++ clive/__private/cli/common.py
@@ -1,9 +1,37 @@
-from __future__ import annotations
+from collections.abc import Callable
+from functools import wraps
+from typing import Any, Optional
 
 import typer
+from merge_args import merge_args  # type: ignore
+from pydantic import BaseModel
 
-Broadcast = typer.Option(..., help="Broadcast the transaction.", show_default=False)
-Sign = typer.Option(..., help="Key alias to sign the transaction with.")
-Profile = typer.Option(..., help="The profile to use.")
-Password = typer.Option(..., help="The password to use.")
-SaveFile = typer.Option(None, help="The file to save the transaction to.")
+
+class Common(BaseModel):
+    """
+    Common options for some commands.
+    Inspired by https://github.com/tiangolo/typer/issues/296#issuecomment-1381269597
+    """
+
+    broadcast: bool = typer.Option(..., help="Broadcast the transaction.", show_default=False)
+    sign: Optional[str] = typer.Option(..., help="Key alias to sign the transaction with.")
+    profile: Optional[str] = typer.Option(..., help="The profile to use.")
+    password: Optional[str] = typer.Option(..., help="The password to use.")
+    save_file: Optional[str] = typer.Option(None, help="The file to save the transaction to.")
+
+
+def common_options(func: Callable[..., None]) -> Any:
+    @merge_args(func)
+    @wraps(func, assigned=["__module__", "__name__", "__doc__", "__anotations__"])
+    def wrapper(
+        ctx: typer.Context,
+        broadcast: bool = Common().broadcast,  # noqa: ARG001
+        sign: Optional[str] = Common().sign,  # noqa: ARG001
+        profile: Optional[str] = Common().profile,  # noqa: ARG001
+        password: Optional[str] = Common().password,  # noqa: ARG001
+        save_file: Optional[str] = Common().save_file,  # noqa: ARG001
+        **kwargs: Any,
+    ) -> None:
+        return func(ctx=ctx, **kwargs)
+
+    return wrapper
--- clive/__private/cli/transfer.py
+++ clive/__private/cli/transfer.py
@@ -1,11 +1,9 @@
-from __future__ import annotations
-
 from pathlib import Path
 from typing import Final, Optional
 
 import typer
 
-from clive.__private.cli import common
+from clive.__private.cli.common import Common, common_options
 from clive.__private.core.perform_actions_on_transaction import perform_actions_on_transaction
 from clive.__private.storage.mock_database import PrivateKey
 from clive.models.transfer_operation import TransferOperation
@@ -21,25 +19,21 @@ transfer = typer.Typer(
 
 
 @transfer.callback(invoke_without_command=True)
+@common_options
 def _main(
+    ctx: typer.Context,
     from_: str = typer.Option(..., "--from", help="The account to transfer from."),
     to: str = typer.Option(..., help="The account to transfer to."),
     amount: str = typer.Option(..., help="The amount to transfer. (e.g. 2.500 HIVE)"),
     memo: Optional[str] = typer.Option(None, help="The memo to attach to the transfer."),
-    broadcast: bool = common.Broadcast,
-    sign: Optional[str] = common.Sign,
-    profile: Optional[str] = common.Profile,
-    password: Optional[str] = common.Password,
-    save_file: Optional[str] = common.SaveFile,
 ) -> None:
-    saved_args = locals()
-    typer.echo(f"Transfer command invoked with params: {saved_args}")
+    common = Common(**ctx.params)
 
     value, asset = amount.split(" ")
 
     perform_actions_on_transaction(
         TransferOperation(from_=from_, to=to, amount=value, asset=asset.upper(), memo=memo),
-        sign_key=PrivateKey("temporary", sign) if sign else None,
-        save_file_path=Path(save_file) if save_file else None,
-        broadcast=broadcast,
+        sign_key=PrivateKey("temporary", common.sign) if common.sign else None,
+        save_file_path=Path(common.save_file) if common.save_file else None,
+        broadcast=common.broadcast,
     )
