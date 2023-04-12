8547eb3c5ffcfd5a9bd888ae28dcf1f96054569b
kmochocki <kmochocki@syncad.com>
2023-04-12 10:37:03 +0000

    Refactor create profile so it uses exceptions

--- clive/__private/ui/create_profile/create_profile.py
+++ clive/__private/ui/create_profile/create_profile.py
@@ -15,9 +15,22 @@ from clive.__private.ui.shared.form_screen import FormScreen
 from clive.__private.ui.widgets.clive_button import CliveButton
 from clive.__private.ui.widgets.dialog_container import DialogContainer
 from clive.__private.ui.widgets.notification import Notification
+from clive.exceptions import FormValidationError
 
 if TYPE_CHECKING:
     from textual.app import ComposeResult
+
+
+class InputTooShortError(FormValidationError):
+    def __init__(self, *, expected_length: int, given_value: str) -> None:
+        super().__init__(
+            f"Expected length of {expected_length}, but string of {len(given_value)} given", given_value=given_value
+        )
+
+
+class RepeatedPasswordIsDifferentError(FormValidationError):
+    def __init__(self) -> None:
+        super().__init__("Repeated password is different than original one")
 
 
 class ButtonsContainer(Horizontal):
@@ -42,29 +55,42 @@ class CreateProfileCommon(BaseScreen, Contextual[ProfileData], ABC):
         """Additional content to be added to the form."""
         return []
 
-    def _create_profile(self) -> bool:
-        """
-        Collects the data from the form and creates a profile.
-        :return: True if the profile was created successfully, False otherwise.
-        """
+    def _get_valid_args(self) -> tuple[str, str]:
+        """Selects all input fields and validates them, if something is invalid throws an exception"""
         minimum_input_length: Final[int] = 3
 
         profile_name = self.get_widget_by_id("profile_name_input", expect_type=Input).value
         password = self.get_widget_by_id("password_input", expect_type=Input).value
         repeated_password = self.get_widget_by_id("repeat_password_input", expect_type=Input).value
 
-        if not all(
-            [
-                len(profile_name) >= minimum_input_length,
-                len(password) >= minimum_input_length,
-                (password == repeated_password),
-            ]
-        ):
-            Notification("Failed the validation process! Could not continue", category="error").show()
-            return False
+        if len(profile_name) < minimum_input_length:
+            raise InputTooShortError(expected_length=minimum_input_length, given_value=profile_name)
+
+        if len(password) < minimum_input_length:
+            raise InputTooShortError(expected_length=minimum_input_length, given_value=profile_name)
+
+        if password != repeated_password:
+            raise RepeatedPasswordIsDifferentError()
+
+        return profile_name, password
+
+    def _create_profile(self) -> None:
+        """
+        Collects the data from the form and creates a profile.
+        :return: True if the profile was created successfully, False otherwise.
+        """
+
+        profile_name, password = self._get_valid_args()
         self.context.name = profile_name
         self.context.password = password
         self.post_message(ProfileDataUpdated())
+
+    def create_profile_with_gui_support(self) -> bool:
+        try:
+            self._create_profile()
+        except FormValidationError:
+            Notification("Failed the validation process! Could not continue", category="error").show()
+            return False
         self._show_notification_on_profile_created()
         return True
 
@@ -93,7 +119,7 @@ class CreateProfile(CreateProfileCommon):
         self.app.pop_screen()
 
     def action_create_profile(self) -> None:
-        if self._create_profile():
+        if self.create_profile_with_gui_support():
             self.app.pop_screen()
             self._show_notification_on_profile_created()  # show it on the new screen, or it won't be visible
 
@@ -105,7 +131,5 @@ class CreateProfile(CreateProfileCommon):
 
 
 class CreateProfileForm(CreateProfileCommon, FormScreen[ProfileData]):
-    def action_next_screen(self) -> None:
-        if self._create_profile():
-            super().action_next_screen()
-            self._show_notification_on_profile_created()  # show it on the new screen, or it won't be visible
+    def apply_and_validate(self) -> None:
+        self._create_profile()
