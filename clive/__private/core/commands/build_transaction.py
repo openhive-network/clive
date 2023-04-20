bd791024da16a19fd21d6026312a608ec6eb3a3b
kmochocki <kmochocki@syncad.com>
2023-04-20 09:33:32 +0000

    Add command block to build transaction

---
+++ clive/__private/core/commands/build_transaction.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from clive.__private.core.commands.command import Command
+from clive.__private.core.transaction import Transaction
+
+if TYPE_CHECKING:
+    from clive.models.operation import Operation
+
+
+class BuildTransaction(Command[Transaction]):
+    def __init__(self, operations: list[Operation]) -> None:
+        super().__init__(result_default=None)
+        self.__operations = operations
+
+    def execute(self) -> None:
+        self._result = Transaction(operations=self.__operations)
--- clive/__private/core/commands/commands.py
+++ clive/__private/core/commands/commands.py
@@ -3,20 +3,31 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from clive.__private.core.commands.activate import Activate
+from clive.__private.core.commands.build_transaction import BuildTransaction
 from clive.__private.core.commands.deactivate import Deactivate
-from clive.__private.core.commands.transfer import Transfer
 
 if TYPE_CHECKING:
     from datetime import timedelta
 
     from clive import World
+    from clive.__private.core.transaction import Transaction
+    from clive.models.operation import Operation
 
 
 class Commands:
     def __init__(self, world: World) -> None:
         self.__world = world
 
-        self.transfer = Transfer
+    def activate(self, *, password: str) -> None:
+        Activate(self.__world.beekeeper, wallet=self.__world.profile_data.name, password=password).execute()
+
+    def deactivate(self) -> None:
+        Deactivate(self.__world.beekeeper, wallet=self.__world.profile_data.name).execute()
+
+    def build_transaction(self, *, operations: list[Operation]) -> Transaction:
+        bt = BuildTransaction(operations=operations)
+        bt.execute()
+        return bt.result
 
     def activate(self, *, active_mode_time: timedelta | None = None) -> None:
         Activate(self.__world.app_state, self.__world.background_tasks, active_mode_time=active_mode_time).execute()
