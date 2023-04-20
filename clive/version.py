d9176a9322059680dc3970a3610a925d1c80502a
Mateusz Żebrak <mzebrak@syncad.com>
2023-04-20 09:22:41 +0200

    Fix issue with using clive without git
    fixes error:
    - RuntimeError: This does not appear to be a Git project

--- clive/version.py
+++ clive/version.py
@@ -4,6 +4,25 @@ from typing import Final
 
 from dunamai import Version
 
-VERSION: Final[Version] = Version.from_git()
 
-VERSION_INFO: Final[str] = f"{VERSION.base} ({VERSION.commit}) {'dirty!' if VERSION.dirty else ''}"
+def __get_version() -> Version:
+    try:
+        return Version.from_git()
+    except RuntimeError:
+        from importlib.metadata import version
+
+        return Version.parse(version("clive"))
+
+
+VERSION: Final[Version] = __get_version()
+
+
+def __get_commit_part() -> str:
+    return f" ({VERSION.commit})" if VERSION.commit else ""
+
+
+def __get_dirty_part() -> str:
+    return " dirty!" if VERSION.dirty else ""
+
+
+VERSION_INFO: Final[str] = f"{VERSION.base}{__get_commit_part()}{__get_dirty_part()}"
