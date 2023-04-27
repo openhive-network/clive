cfafb6ecec4ee9f9ee6753996e549bcdd42f145d
kmochocki <kmochocki@syncad.com>
2023-04-27 12:09:38 +0000

    Remove cache to prevent potential bugs

--- clive/__private/core/app_state.py
+++ clive/__private/core/app_state.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
-import cachetools
-
 if TYPE_CHECKING:
     from clive.__private.core.world import World
 
@@ -15,7 +13,6 @@ class AppState:
 
     world: World
 
-    @cachetools.cached(cache=cachetools.TTLCache(maxsize=1, ttl=1.0))
     def is_active(self) -> bool:
         wallets = self.world.beekeeper.api.list_wallets().wallets
         for wallet in wallets:
