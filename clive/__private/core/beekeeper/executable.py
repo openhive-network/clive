1bd2cb0506edbbcde06befc1890adb7093e159df
Mateusz Żebrak <mzebrak@syncad.com>
2023-05-08 17:48:18 +0200

    Rework and simplify close method of beekeeper executable

--- clive/__private/core/beekeeper/executable.py
+++ clive/__private/core/beekeeper/executable.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+import signal
+import subprocess
+import warnings
 from pathlib import Path
-from signal import SIGINT
-from subprocess import Popen, TimeoutExpired
+from subprocess import Popen
 from typing import TYPE_CHECKING, TextIO
 
 from clive.__private.config import settings
+from clive.__private.logger import logger
 from clive.exceptions import CliveError
 
 if TYPE_CHECKING:
@@ -39,8 +42,10 @@ class BeekeeperExecutable:
             raise BeekeeperNotConfiguredError()
 
         self.__process: Popen[bytes] | None = None
-        self.__stderr: TextIO | None = None
-        self.__stdout: TextIO | None = None
+        self.__files: dict[str, TextIO | None] = {
+            "stdout": None,
+            "stderr": None,
+        }
         self.__pid_file: Path | None = None
 
     def run(self, config: BeekeeperConfig) -> None:
@@ -59,58 +64,51 @@ class BeekeeperExecutable:
         config_filename = config.wallet_dir / "config.ini"
         config.save(config_filename)
 
-        # prepare output
-        self.__stdout = (config.wallet_dir / "stdout.log").open("wt", encoding="utf-8")
-        self.__stderr = (config.wallet_dir / "stderr.log").open("wt", encoding="utf-8")
+        self.__prepare_files_for_streams(config.wallet_dir)
 
         self.__pid_file.touch(exist_ok=False)
-        self.__process = Popen[bytes](
+        self.__process = Popen(
             [self.__executable, "--data-dir", config.wallet_dir.as_posix()],
-            stdout=self.__stdout,
-            stderr=self.__stderr,
+            stdout=self.__files["stdout"],
+            stderr=self.__files["stderr"],
         )
         with self.__pid_file.open(mode="w") as file:
             file.write(f"{self.__process.pid}")
-
-    def __wait_for_kill(self) -> None:
-        if self.__process is not None and self.__process.wait(5.0) != 0:
-            if self.__process.stderr is not None:
-                self.__process.stderr.flush()
-
-            if self.__process.stdout is not None:
-                self.__process.stdout.flush()
-
-            raise BeekeeperNonZeroExitCodeError()
 
     def close(self) -> None:
         if self.__process is None:
             assert self.__pid_file is None
             return
-        try:
-            self.__process.send_signal(SIGINT)
-            self.__wait_for_kill()
-        except TimeoutExpired:
-            try:
-                self.__process.kill()
-                self.__wait_for_kill()
-            except TimeoutExpired:
-                try:
-                    self.__process.terminate()
-                    self.__wait_for_kill()
-                except TimeoutError as e:
-                    raise BeekeeperDidNotClosedError() from e
-        finally:
-            if self.__stderr is not None:
-                self.__stderr.close()
-            if self.__stdout is not None:
-                self.__stdout.close()
-            self.__process = None
 
-            assert self.__pid_file is not None and self.__pid_file.exists()
-            self.__pid_file.unlink()
-            self.__pid_file = None
+        self.__process.send_signal(signal.SIGINT)
+        try:
+            return_code = self.__process.wait(timeout=10)
+            logger.debug(f"Beekeeper closed with return code of `{return_code}`.")
+        except subprocess.TimeoutExpired:
+            self.__process.kill()
+            self.__process.wait()
+            warnings.warn(
+                "Beekeeper process was force-closed with SIGKILL, because didn't close before timeout", stacklevel=2
+            )
+        self.__close_files_for_streams()
+
+        assert self.__pid_file is not None and self.__pid_file.exists()
+        self.__pid_file.unlink()
+        self.__pid_file = None
+        self.__process = None
 
     @classmethod
     def get_path_from_settings(cls) -> Path | None:
         path_raw = settings.get("beekeeper.path")
         return Path(path_raw) if path_raw is not None else None
+
+    def __prepare_files_for_streams(self, directory: Path) -> None:
+        for name in self.__files:
+            path = directory / f"{name}.log"
+            self.__files[name] = path.open("w", encoding="utf-8")
+
+    def __close_files_for_streams(self) -> None:
+        for name, file in self.__files.items():
+            if file is not None:
+                file.close()
+                self.__files[name] = None
