438559ca9d8e98fe364dae14c9a44fb1a0ab6376
kmochocki <kmochocki@syncad.com>
2023-05-29 12:30:29 +0000

    Add database_api integration

--- clive/__private/core/node/api/apis.py
+++ clive/__private/core/node/api/apis.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from clive.__private.core.node.api.database_api import DatabaseApi
 from clive.__private.core.node.api.network_broadcast_api import NetworkBroadcastApi
 
 if TYPE_CHECKING:
@@ -11,3 +12,4 @@ if TYPE_CHECKING:
 class Apis:
     def __init__(self, node: Node) -> None:
         self.network_broadcast = NetworkBroadcastApi(node)
+        self.database_api = DatabaseApi(node)
