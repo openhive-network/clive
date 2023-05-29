77702eab59846647052cd5a37f4bdfb6991a0a44
kmochocki <kmochocki@syncad.com>
2023-05-29 12:28:50 +0000

    Add missing type definitions

--- clive/models.py
+++ clive/models.py
@@ -14,7 +14,14 @@ from schemas.__private.hive_fields_basic_schemas import (
     HiveInt,
 )
 from schemas.__private.hive_fields_custom_schemas import Signature as SchemasSignature
-from schemas.__private.operations import Hf26OperationType, HF26OperationTypes, Hf26VirtualOperationType
+from schemas.__private.operation_objects import Hf26ApiOperationObject, Hf26ApiVirtualOperationObject
+from schemas.__private.operations import (
+    Hf26OperationRepresentationType,
+    Hf26OperationType,
+    HF26OperationTypes,
+    Hf26VirtualOperationRepresentationType,
+    Hf26VirtualOperationType,
+)
 from schemas.__private.preconfigured_base_model import Operation as SchemasBaseOperationType
 from schemas.__private.preconfigured_base_model import VirtualOperation as SchemasBaseVirtualOperationType
 from schemas.transaction_model.transaction import Hf26Transaction
@@ -23,6 +30,11 @@ Operation = Hf26OperationType
 VirtualOperation = Hf26VirtualOperationType
 OperationBaseClass = SchemasBaseOperationType
 VirtualOperationBaseClass = SchemasBaseVirtualOperationType
+OperationRepresentationType = Hf26OperationRepresentationType
+VirtualOperationRepresentationType = Hf26VirtualOperationRepresentationType
+ApiOperationObject = Hf26ApiOperationObject
+ApiVirtualOperationObject = Hf26ApiVirtualOperationObject
+
 
 Signature = SchemasSignature
 
