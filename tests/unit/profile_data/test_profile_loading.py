7678d14dab20c9e5366773a1fbb3d18781036b9e
Mateusz Żebrak <mzebrak@syncad.com>
2023-05-30 09:24:43 +0200

    Reenable skipped tests

--- tests/unit/profile_data/test_profile_loading.py
+++ tests/unit/profile_data/test_profile_loading.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import pytest
-
 import clive
 
 if TYPE_CHECKING:
@@ -15,12 +13,12 @@ def test_if_profile_is_loaded(world: World, wallet_name: str) -> None:
     assert world.profile_data.name == wallet_name
 
 
-@pytest.mark.skip(reason="no idea how to recreate")
 def test_if_lastly_used_profile_is_loaded() -> None:
     # ARRANGE
     expected_profile_name = "first"
     world = clive.World(expected_profile_name)
     world.profile_data.save()  # this one should be lastly used
+    world.close()  # close world so beekeeper can be closed
 
     # ACT
     world = clive.World()
@@ -29,13 +27,13 @@ def test_if_lastly_used_profile_is_loaded() -> None:
     assert world.profile_data.list_profiles() == [expected_profile_name]
 
 
-@pytest.mark.skip(reason="no idea how to recreate")
 def test_if_correct_profile_is_loaded_when_something_is_stored() -> None:
     # ARRANGE
     expected_profile_name = "first"
     additional_profile_name = "second"
     world = clive.World(additional_profile_name)
     world.profile_data.save()  # lastly used is other than we're loading
+    world.close()  # close world so beekeeper can be closed
 
     # ACT
     world = clive.World(expected_profile_name)
@@ -44,7 +42,6 @@ def test_if_correct_profile_is_loaded_when_something_is_stored() -> None:
     assert world.profile_data.name == expected_profile_name
 
 
-@pytest.mark.skip(reason="no idea how to recreate")
 def test_loading_profile_without_given_name_when_no_lastly_used() -> None:
     # ACT
     world = clive.World()
