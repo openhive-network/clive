1bae97ce68655fb5842d20eeae2857b5e3e81551
Mateusz Żebrak <mzebrak@syncad.com>
2023-05-30 09:11:47 +0200

    Fix issue with too dir names generated by tests

--- tests/unit/wax/test_serialize_transaction.py
+++ tests/unit/wax/test_serialize_transaction.py
@@ -7,7 +7,7 @@ import wax
 from .consts import ENCODING, VALID_TRXS
 
 
-@pytest.mark.parametrize("key", list(VALID_TRXS.keys()))
+@pytest.mark.parametrize("key", list(VALID_TRXS.keys()), ids=range(len(VALID_TRXS)))
 def test_serialize_transaction(key: str) -> None:
     result = wax.serialize_transaction(VALID_TRXS[key].encode(ENCODING))
     assert result.status == wax.python_error_code.ok
--- tests/unit/wax/test_validate_transaction.py
+++ tests/unit/wax/test_validate_transaction.py
@@ -7,7 +7,9 @@ import wax
 from .consts import ENCODING, VALID_IDS_WITH_TRANSACTIONS
 
 
-@pytest.mark.parametrize("trx_id", list(VALID_IDS_WITH_TRANSACTIONS.keys()))
+@pytest.mark.parametrize(
+    "trx_id", list(VALID_IDS_WITH_TRANSACTIONS.keys()), ids=range(len(VALID_IDS_WITH_TRANSACTIONS))
+)
 def test_valid_transaction(trx_id: str) -> None:
     result = wax.validate_transaction(VALID_IDS_WITH_TRANSACTIONS[trx_id].encode(ENCODING))
     assert result.status == wax.python_error_code.ok
