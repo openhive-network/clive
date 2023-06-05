1c37922908cb1bfffbd6b775092d7ad1020fd6d5
Mateusz Żebrak <mzebrak@syncad.com>
2023-06-05 13:22:25 +0200

    Remove Dockerfile.CI and it's scripts

--- scripts/build-ci-base-image.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-REGISTRY="registry.gitlab.syncad.com/hive/clive"
-BASE_IMAGE="ubuntu22.04"
-VERSION="3"
-
-URL="${REGISTRY}/ci-base-image:${BASE_IMAGE}-${VERSION}"
-
-docker build -t "${URL}" -f Dockerfile.CI .
