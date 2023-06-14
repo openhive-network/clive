46e099145639fd9d29fd7e1d3311b5ba1ca7b66a
Mateusz Żebrak <mzebrak@syncad.com>
2023-06-14 15:20:18 +0200

    Raise exception instead of returning bool in CommandSafe.execute_safely

--- clive/__private/core/commands/command_safe.py
+++ clive/__private/core/commands/command_safe.py
@@ -3,16 +3,21 @@ from __future__ import annotations
 from abc import ABC
 from collections.abc import Callable
 from dataclasses import dataclass
-from typing import TypeVar
+from typing import TypeVar, ClassVar
 
 from clive.__private.core.clive_import import get_clive
 from clive.__private.core.commands.command import Command
 from clive.__private.ui.widgets.notification import Notification
+from clive.exceptions import CliveError
 
 CommandT = TypeVar("CommandT")
 
 ExecutionNotPossibleCallbackT = Callable[[], None]
 ExecutionNotPossibleCallbackOptionalT = ExecutionNotPossibleCallbackT | None
+
+
+class CommandSafeExecutionError(CliveError):
+    """Raised when (CommandSafe) safe command execution fails due to some reason."""
 
 
 @dataclass(kw_only=True)
@@ -22,16 +27,29 @@ class CommandSafe(Command[CommandT], ABC):
     """
 
     execution_not_possible_callback: ExecutionNotPossibleCallbackOptionalT = None
+    _exception: ClassVar[CliveError] = CommandSafeExecutionError()
 
-    def execute_safely(self) -> bool:
+    def execute_safely(self) -> None:
+        """
+        Raises:
+            CommandExecutionFailed: if the execution of the command has failed when executed via `execute_safely`.
+
+        Example usage:
+
+        try:
+            some_command.execute_safely()
+        except CommandSafeExecutionError:
+            # handle situation when command execution has failed
+        else:
+            # handle situation when command execution succeeded
+        """
         if not self._is_execution_possible():
             if callback := self.execution_not_possible_callback:
                 callback()
             self._on_execution_failed()
-            return False
+            raise self._exception
 
         self.execute()
-        return True
 
     def _is_execution_possible(self) -> bool:
         return True
