d16647290d86db198d83dfe93332547826341014
Mateusz Żebrak <mzebrak@syncad.com>
2023-06-15 08:23:46 +0200

    Make SyncDataWithBeekeeper command remove hanging keys

--- clive/__private/core/commands/sync_data_with_beekeeper.py
+++ clive/__private/core/commands/sync_data_with_beekeeper.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING
 
 from clive.__private.core.commands.command_in_active import CommandInActive
 from clive.__private.core.commands.import_key import ImportKey
+from clive.__private.core.commands.remove_key import RemoveKey
+from clive.__private.storage.mock_database import PublicKey
 
 if TYPE_CHECKING:
     from clive.__private.core.beekeeper import Beekeeper
@@ -17,6 +19,21 @@ class SyncDataWithBeekeeper(CommandInActive[None]):
     beekeeper: Beekeeper
 
     def _execute(self) -> None:
+        self.__remove_hanging_keys()
+        self.__import_pending_keys()
+
+    def __remove_hanging_keys(self) -> None:
+        wallet_name = self.profile_data.name
+        keys_to_keep = self.profile_data.working_account.keys
+        keys_in_beekeeper = self.beekeeper.api.get_public_keys().keys
+
+        for key in keys_in_beekeeper:
+            if key not in keys_to_keep:
+                RemoveKey(
+                    app_state=self.app_state, beekeeper=self.beekeeper, wallet=wallet_name, key_to_remove=PublicKey(key)
+                ).execute()
+
+    def __import_pending_keys(self) -> None:
         for alias, key in self.profile_data.working_account.keys_to_import.items():
             imported = ImportKey(
                 app_state=self.app_state,
