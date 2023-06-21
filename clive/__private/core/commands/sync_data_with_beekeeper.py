9df0d973c48b8ca306af6293160017a21156a21c
Mateusz Żebrak <mzebrak@syncad.com>
2023-06-21 12:33:30 +0200

    Make SyncDataWithBeekeeper handle unknown keys that appeared in th beekeeper

--- clive/__private/core/commands/sync_data_with_beekeeper.py
+++ clive/__private/core/commands/sync_data_with_beekeeper.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 from clive.__private.core.commands.abc.command import Command
 from clive.__private.core.commands.abc.command_in_active import CommandInActive
 from clive.__private.core.commands.import_key import ImportKey
+from clive.__private.storage.mock_database import PublicKeyAliased
 
 if TYPE_CHECKING:
     from clive.__private.core.beekeeper.handle import Beekeeper
@@ -18,6 +19,10 @@ class SyncDataWithBeekeeper(CommandInActive, Command):
     beekeeper: Beekeeper
 
     def _execute(self) -> None:
+        self.__import_pending_keys()
+        self.__sync_missing_keys()
+
+    def __import_pending_keys(self) -> None:
         for alias, key in self.profile_data.working_account.keys_to_import.items():
             imported = ImportKey(
                 app_state=self.app_state,
@@ -28,3 +33,13 @@ class SyncDataWithBeekeeper(CommandInActive, Command):
             ).execute_with_result()
             self.profile_data.working_account.keys.append(imported)
         self.profile_data.working_account.keys_to_import.clear()
+
+    def __sync_missing_keys(self) -> None:
+        keys_in_clive = self.profile_data.working_account.keys
+        keys_in_beekeeper = self.beekeeper.api.get_public_keys().keys
+
+        keys_missing_in_clive = [key for key in keys_in_beekeeper if key not in keys_in_clive]
+
+        self.profile_data.working_account.keys.extend(
+            PublicKeyAliased(value=key, alias=key) for key in keys_missing_in_clive
+        )
