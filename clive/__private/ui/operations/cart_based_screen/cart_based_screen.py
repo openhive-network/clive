bab8c7ef3a7a10aeb056e590354df4fdc1767049
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-06-21 12:12:45 +0200

    Split CartBasedScreen into two classes

--- clive/__private/ui/operations/cart_based_screen/cart_based_screen.py
+++ clive/__private/ui/operations/cart_based_screen/cart_based_screen.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from abc import abstractmethod
 from typing import TYPE_CHECKING
 
+from textual.binding import Binding
 from textual.containers import Container, Horizontal
 
 from clive.__private.abstract_class import AbstractClassMessagePump
@@ -21,7 +22,31 @@ class RightContainer(Container):
     """A container that holds the right side of the screen."""
 
 
-class CartBasedScreen(BaseScreen, AbstractClassMessagePump):
+class CartBasedScreenOperation(BaseScreen, AbstractClassMessagePump):
+    """Base class for Operations screen, which shows list of available operations"""
+
+    def create_main_panel(self) -> ComposeResult:
+        with Horizontal():
+            with LeftContainer():
+                yield from self.create_left_panel()
+            with RightContainer():
+                yield CartOverview()
+
+    @abstractmethod
+    def create_left_panel(self) -> ComposeResult:
+        """Should yield the left panel widgets."""
+
+
+class CartBasedScreenOperations(BaseScreen, AbstractClassMessagePump):
+    """Base class for all screens which all Operations screen"""
+
+    BINDINGS = [
+        Binding("escape", "pop_screen", "Cancel"),
+        Binding("f2", "add_to_cart", "Add to cart"),
+        Binding("f5", "fast_broadcast", "Fast broadcast"),
+        Binding("f10", "finalize", "Finalize transaction"),
+    ]
+
     def create_main_panel(self) -> ComposeResult:
         with Horizontal():
             with LeftContainer():
