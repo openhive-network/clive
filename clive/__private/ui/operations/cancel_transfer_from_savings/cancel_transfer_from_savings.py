1f58650f736907104a31f2361fcccaed0f202161
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-06-22 13:32:37 +0200

    Make from field auto-fill

--- clive/__private/ui/operations/cancel_transfer_from_savings/cancel_transfer_from_savings.py
+++ clive/__private/ui/operations/cancel_transfer_from_savings/cancel_transfer_from_savings.py
@@ -9,6 +9,7 @@ from textual.widgets import Input, Static
 
 from clive.__private.ui.operations.cart_based_screen.cart_based_screen import CartBasedScreen
 from clive.__private.ui.widgets.big_title import BigTitle
+from clive.__private.ui.widgets.ellipsed_static import EllipsedStatic
 from clive.__private.ui.widgets.notification import Notification
 from clive.__private.ui.widgets.view_bag import ViewBag
 from schemas.operations import CancelTransferFromSavingsOperation
@@ -23,6 +24,10 @@ class Body(Grid):
     """All the content of the screen, excluding the title"""
 
 
+class PlaceTaker(Static):
+    """Container used for making correct layout of a grid."""
+
+
 class CancelTransferFromSavings(CartBasedScreen):
     BINDINGS = [
         Binding("escape", "pop_screen", "Cancel"),
@@ -34,7 +39,6 @@ class CancelTransferFromSavings(CartBasedScreen):
     def __init__(self) -> None:
         super().__init__()
 
-        self.__from_input = Input(placeholder="e.g.: alice")
         self.__request_id_input = Input(placeholder="e.g.: 1000. Notice: default value is 0")
 
     def create_left_panel(self) -> ComposeResult:
@@ -42,7 +46,8 @@ class CancelTransferFromSavings(CartBasedScreen):
             yield BigTitle("Cancel transfer from savings")
             with Body():
                 yield Static("from", classes="label")
-                yield self.__from_input
+                yield EllipsedStatic(str(self.app.world.profile_data.working_account.name), id_="from-label")
+                yield PlaceTaker()
                 yield Static("request_id", classes="label")
                 yield self.__request_id_input
 
@@ -50,9 +55,12 @@ class CancelTransferFromSavings(CartBasedScreen):
         try:
             if self.__request_id_input.value:
                 return CancelTransferFromSavingsOperation(
-                    From=str(self.__from_input.value), request_id=self.__request_id_input.value
+                    From=str(self.app.world.profile_data.working_account.name),
+                    request_id=int(self.__request_id_input.value),
                 )
-            return CancelTransferFromSavingsOperation(From=str(self.__from_input.value))  # noqa: TRY300
+            return CancelTransferFromSavingsOperation(
+                From=str(self.app.world.profile_data.working_account.name)
+            )  # noqa: TRY300
 
         except ValidationError as error:
             Notification(f"Operation failed the validation process.\n{error}", category="error").show()
