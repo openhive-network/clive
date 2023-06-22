7d42c1af5f5b9a4537bf14b203a9a2cb071930ac
kmochocki <kmochocki@syncad.com>
2023-06-22 09:27:50 +0000

    Add missing tests for wax

---
+++ tests/unit/wax/test_calculate_manabar_full_regeneration_time.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+import pytest
+
+import wax
+
+
+def date(*, day: int, hour: int = 0, minute: int = 0, second: int = 0) -> int:
+    return int(
+        datetime.utcnow()
+        .replace(year=1970, month=1, day=1 + day, hour=hour, minute=minute, second=second, microsecond=0)
+        .timestamp()
+    )
+
+
+@pytest.mark.parametrize(
+    "now, max_mana, current_mana, last_update_time, expected",
+    (
+        (date(day=5), 100, 0, date(day=0), date(day=5)),
+        (date(day=5), 100, 50, date(day=2, hour=12), date(day=5)),
+        (date(day=5), 100, 80, date(day=4), date(day=5)),
+        (date(day=4), 100, 80, date(day=4), date(day=5)),
+        (date(day=6), 100, 80, date(day=4), date(day=6)),
+        (date(day=3), 100, 20, date(day=1), date(day=5)),
+    ),
+)
+def test_proper_calculate_manabar_full_regeneration_time(
+    now: int, max_mana: int, current_mana: int, last_update_time: int, expected: int
+) -> None:
+    response = wax.calculate_manabar_full_regeneration_time(now, max_mana, current_mana, last_update_time)
+    assert not response.exception_message
+    assert response.result
+    assert int(response.result.decode()) == expected
