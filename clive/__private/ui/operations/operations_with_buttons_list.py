6c64ef0467dd95a13d1da9b88811d808982eb995
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-06-26 13:02:53 +0200

    Move operations imports into __init__.py

--- clive/__private/ui/operations/operations_with_buttons_list.py
+++ clive/__private/ui/operations/operations_with_buttons_list.py
@@ -3,35 +3,35 @@ from __future__ import annotations
 
 from typing import Any, Final
 
-from clive.__private.ui.operations.account_witness_proxy.account_witness_proxy import AccountWitnessProxy
-from clive.__private.ui.operations.account_witness_vote.account_witnes_vote import AccountWitnessVote
-from clive.__private.ui.operations.cancel_transfer_from_savings.cancel_transfer_from_savings import (
+from clive.__private.ui.operations import (
+    AccountWitnessProxy,
+    AccountWitnessVote,
     CancelTransferFromSavings,
+    ChangeRecoveryAccount,
+    ClaimAccount,
+    Convert,
+    DelegateVestingShares,
+    DeleteComment,
+    EscrowApprove,
+    EscrowDispute,
+    EscrowRelease,
+    EscrowTransfer,
+    LimitOrderCancel,
+    LimitOrderCreate,
+    RecurrentTransfer,
+    RemoveProposal,
+    SetResetAccount,
+    SetWithdrawVestingRoute,
+    TransferFromSavings,
+    TransferToAccount,
+    TransferToSavings,
+    TransferToVesting,
+    UpdateProposal,
+    UpdateProposalVotes,
+    Vote,
+    WithdrawVesting,
+    WitnessBlockApprove,
 )
-from clive.__private.ui.operations.change_recovery_account.change_recovery_account import ChangeRecoveryAccount
-from clive.__private.ui.operations.claim_account.claim_account import ClaimAccount
-from clive.__private.ui.operations.covnert.covnert import Convert
-from clive.__private.ui.operations.delegate_vesting_shares.delegate_vesting_shares import DelegateVestingShares
-from clive.__private.ui.operations.delete_comment.delete_comment import DeleteComment
-from clive.__private.ui.operations.escrow_approve.escrow_approve import EscrowApprove
-from clive.__private.ui.operations.escrow_dispute.escrow_dispute import EscrowDispute
-from clive.__private.ui.operations.escrow_release.escrow_release import EscrowRelease
-from clive.__private.ui.operations.escrow_transfer.escrow_transfer import EscrowTransfer
-from clive.__private.ui.operations.limit_order_cancel.limit_order_cancel import LimitOrderCancel
-from clive.__private.ui.operations.limit_order_create.limit_order_create import LimitOrderCreate
-from clive.__private.ui.operations.recurrent_transfer.recurrent_transfer import RecurrentTransfer
-from clive.__private.ui.operations.remove_proposal.remove_proposal import RemoveProposal
-from clive.__private.ui.operations.set_reset_account.set_reset_account import SetResetAccount
-from clive.__private.ui.operations.set_withdraw_vesting_route.set_withdraw_vesting_route import SetWithdrawVestingRoute
-from clive.__private.ui.operations.transfer_from_savings.transfer_from_savings import TransferFromSavings
-from clive.__private.ui.operations.transfer_to_account.transfer_to_account import TransferToAccount
-from clive.__private.ui.operations.transfer_to_savings.transfer_to_savings import TransferToSavings
-from clive.__private.ui.operations.transfer_to_vesting.transfer_to_vesting import TransferToVesting
-from clive.__private.ui.operations.update_proposal.update_proposal import UpdateProposal
-from clive.__private.ui.operations.update_proposal_votes.update_proposal_votes import UpdateProposalVotes
-from clive.__private.ui.operations.vote.vote import Vote
-from clive.__private.ui.operations.withdraw_vesting.withdraw_vesting import WithdrawVesting
-from clive.__private.ui.operations.witness_block_approve.witness_block_approve import WitnessBlockApprove
 
 OPERATIONS_AND_BUTTONS: Final[dict[str, Any]] = {
     "account-transfer-button": TransferToAccount,
