94e7e1b3bd93132e7259df8d71cf149cadf620d9
Mateusz Żebrak <mzebrak@syncad.com>
2023-06-27 11:52:46 +0200

    Raise InvalidPasswordError when CommandPasswordSecured fails for this reason

--- clive/__private/core/commands/abc/command_secured.py
+++ clive/__private/core/commands/abc/command_secured.py
@@ -2,8 +2,14 @@ from __future__ import annotations
 
 from abc import ABC
 from dataclasses import dataclass
+from typing import Final
 
 from clive.__private.core.commands.abc.command import Command
+from clive.exceptions import CliveError, CommunicationError
+
+
+class InvalidPasswordError(CliveError):
+    ERROR_MESSAGE: Final[str] = "Invalid password for wallet: "
 
 
 @dataclass(kw_only=True)
@@ -13,3 +19,11 @@ class CommandPasswordSecured(Command, ABC):
     """
 
     password: str
+
+    def execute(self) -> None:
+        try:
+            super().execute()
+        except CommunicationError as error:
+            if InvalidPasswordError.ERROR_MESSAGE in str(error):
+                raise InvalidPasswordError(f"Command {self.__class__.__name__} received wrong password.") from error
+            raise
