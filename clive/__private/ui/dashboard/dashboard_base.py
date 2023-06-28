a3859ec58e4ceab15d01073e2fc16e5ddc54f45b
kmochocki <kmochocki@syncad.com>
2023-06-28 11:07:25 +0000

    Make ManabarRepresentation child of AccountReferencingWidget and refactor given lambda to create_dynamic_label

--- clive/__private/ui/dashboard/dashboard_base.py
+++ clive/__private/ui/dashboard/dashboard_base.py
@@ -23,8 +23,6 @@ if TYPE_CHECKING:
 
     from textual.app import ComposeResult
 
-    from clive.__private.core.profile_data import ProfileData
-
 
 class ContainerTitle(Static):
     """A title for working/watched accounts container"""
@@ -44,44 +42,36 @@ class AccountReferencingWidget(CliveWidget):
         self._account = account
         super().__init__(name=name, classes=classes)
 
-    def create_dynamic_label(
-        self, foo: Callable[[ProfileData, Account], str], classes: str | None = None
-    ) -> DynamicLabel:
+    def create_dynamic_label(self, foo: Callable[[], str], classes: str | None = None) -> DynamicLabel:
         return DynamicLabel(
             self.app.world,
             "profile_data",
-            lambda pd: foo(pd, self._account) if self._account.name else "NULL",
+            lambda _: foo() if self._account.name else "NULL",
             classes=classes,
         )
 
 
-class ManabarRepresentation(Container, CliveWidget):
-    def __init__(self, manabar: Manabar, name: str, id_: str | None = None, classes: str | None = None) -> None:
+class ManabarRepresentation(AccountReferencingWidget, CliveWidget):
+    def __init__(self, account: Account, manabar: Manabar, name: str, classes: str | None = None) -> None:
         self.__manabar = manabar
         self.__name = name
-        super().__init__(id=id_, classes=classes)
+        super().__init__(account=account, classes=classes)
 
     def compose(self) -> ComposeResult:
         suppressed_units: Final[list[str]] = ["minutes", "seconds", "microseconds"]
-        yield DynamicLabel(
-            self.app.world,
-            "profile_data",
-            lambda _: f"{self.__manabar.percentage :.2f}% {self.__name}",
+        yield self.create_dynamic_label(
+            lambda: f"{self.__manabar.percentage :.2f}% {self.__name}",
             classes="percentage",
         )
-        yield DynamicLabel(
-            self.app.world,
-            "profile_data",
-            lambda _: f"{humanize.naturalsize(self.__manabar.value, binary=False)}".replace(" Bytes", "")
+        yield self.create_dynamic_label(
+            lambda: f"{humanize.naturalsize(self.__manabar.value, binary=False)}".replace(" Bytes", "")
             .upper()
             .replace(" ", "")
             + " HP",
             classes="hivepower-value",
         )
-        yield DynamicLabel(
-            self.app.world,
-            "profile_data",
-            lambda _: f"{humanize.precisedelta(self.__manabar.full_regeneration, suppress=suppressed_units)}",
+        yield self.create_dynamic_label(
+            lambda: f"{humanize.precisedelta(self.__manabar.full_regeneration, suppress=suppressed_units)}",
             classes="time",
         )
 
@@ -90,9 +80,11 @@ class BalanceStats(AccountReferencingWidget):
     full_caption: Final[str] = "full!"
 
     def compose(self) -> ComposeResult:
-        yield ManabarRepresentation(self._account.data.rc_manabar, "RC", classes="even-manabar")
-        yield ManabarRepresentation(self._account.data.vote_manabar, "VOTING", classes="odd-manabar")
-        yield ManabarRepresentation(self._account.data.downvote_manabar, "DOWNVOTING", classes="even-manabar")
+        yield ManabarRepresentation(self._account, self._account.data.rc_manabar, "RC", classes="even-manabar")
+        yield ManabarRepresentation(self._account, self._account.data.vote_manabar, "VOTING", classes="odd-manabar")
+        yield ManabarRepresentation(
+            self._account, self._account.data.downvote_manabar, "DOWNVOTING", classes="even-manabar"
+        )
 
 
 class ActivityStats(AccountReferencingWidget):
@@ -101,18 +93,18 @@ class ActivityStats(AccountReferencingWidget):
         yield EllipsedStatic("LIQUID", classes="title")
         yield EllipsedStatic("SAVINGS", classes="title title-variant")
         yield Static("HIVE", classes="token")
-        yield self.create_dynamic_label(lambda _, acc: Asset.pretty_amount(acc.data.hive_balance), "amount")
+        yield self.create_dynamic_label(lambda: Asset.pretty_amount(self._account.data.hive_balance), "amount")
         yield self.create_dynamic_label(
-            lambda _, acc: Asset.pretty_amount(acc.data.hive_savings),
+            lambda: Asset.pretty_amount(self._account.data.hive_savings),
             "amount amount-variant",
         )
         yield Static("HBD", classes="token token-variant")
         yield self.create_dynamic_label(
-            lambda _, acc: Asset.pretty_amount(acc.data.hive_dollars),
+            lambda: Asset.pretty_amount(self._account.data.hive_dollars),
             "amount amount-variant",
         )
         yield self.create_dynamic_label(
-            lambda _, acc: Asset.pretty_amount(acc.data.hbd_savings),
+            lambda: Asset.pretty_amount(self._account.data.hbd_savings),
             "amount",
         )
 
