b49ef2b23e5dfc595f17e23747788981d730684d
Mateusz Żebrak <mzebrak@syncad.com>
2023-07-05 12:21:52 +0200

    Add `clive list node` command

--- clive/__private/cli/list.py
+++ clive/__private/cli/list.py
@@ -22,3 +22,13 @@ def keys(
     public_keys = common.world.profile_data.working_account.keys
 
     typer.echo(f"{common.profile}, your keys are:\n{list(public_keys)}")
+
+
+@list_.command()
+@WithWorld.decorator(use_beekeeper=False)
+def node(
+    ctx: typer.Context,
+) -> None:
+    """List address of the currently selected node."""
+    common = WithWorld(**ctx.params)
+    typer.echo(common.world.node.address)
