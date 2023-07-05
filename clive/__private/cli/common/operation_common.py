c6cfba99f1575517e11765d0fb2fa08fad1ab097
Mateusz Żebrak <mzebrak@syncad.com>
2023-07-05 10:54:25 +0200

    Include beekeeper_remote parameter in the transfer command

--- clive/__private/cli/common/operation_common.py
+++ clive/__private/cli/common/operation_common.py
@@ -7,8 +7,9 @@ from merge_args import merge_args  # type: ignore[import]
 
 from clive import World
 from clive.__private.cli.common.base import PreconfiguredBaseModel
-from clive.__private.cli.common.options import profile_option
+from clive.__private.cli.common.options import beekeeper_remote_option, profile_option
 from clive.__private.util import ExitCallHandler
+from clive.core.url import Url
 
 P = ParamSpec("P")
 
@@ -24,6 +25,7 @@ class OperationCommon(PreconfiguredBaseModel):
 
     profile: Optional[str] = profile_option
     sign: str = typer.Option(..., help="Key alias to sign the transaction with.", show_default=False)
+    beekeeper_remote: Optional[str] = beekeeper_remote_option
     broadcast: bool = typer.Option(..., help="Broadcast the transaction.", show_default=False)
     save_file: Optional[str] = typer.Option(None, help="The file to save the transaction to.", show_default=False)
     world: World
@@ -38,12 +40,19 @@ class OperationCommon(PreconfiguredBaseModel):
             ctx: typer.Context,
             profile: Optional[str] = common.profile,
             sign: str = common.sign,  # noqa: ARG001
+            beekeeper_remote: Optional[str] = common.beekeeper_remote,
             broadcast: bool = common.broadcast,  # noqa: ARG001
             save_file: Optional[str] = common.save_file,  # noqa: ARG001
             *args: P.args,
             **kwargs: Any,
         ) -> None:
-            with ExitCallHandler(World(profile_name=profile), finally_callback=lambda w: w.close()) as world:
+            with ExitCallHandler(
+                World(
+                    profile_name=profile,
+                    beekeeper_remote_endpoint=Url.parse(beekeeper_remote) if beekeeper_remote else None,
+                ),
+                finally_callback=lambda w: w.close(),
+            ) as world:
                 ctx.params.update(world=world)
                 return func(ctx, *args, **kwargs)
 
