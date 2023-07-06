1eb8daffd4774a68f82bb6f649ea98b471b39ae3
Mateusz Żebrak <mzebrak@syncad.com>
2023-07-06 10:31:54 +0200

    Clean up in test_profile_loading

--- tests/unit/profile_data/test_profile_loading.py
+++ tests/unit/profile_data/test_profile_loading.py
@@ -1,10 +1,23 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import pytest
 import typer
 
 from clive.__private.core.profile_data import ProfileCouldNotBeLoadedError
 from clive.__private.core.world import TyperWorld, World
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+
+@pytest.fixture
+def world_cleanup() -> Iterator[list[World]]:
+    worlds_to_close: list[World] = []
+    yield worlds_to_close
+    for world in worlds_to_close:
+        world.close()
 
 
 def test_if_profile_is_loaded(world: World, wallet_name: str) -> None:
@@ -12,7 +25,7 @@ def test_if_profile_is_loaded(world: World, wallet_name: str) -> None:
     assert world.profile_data.name == wallet_name
 
 
-def test_if_lastly_used_profile_is_loaded() -> None:
+def test_if_lastly_used_profile_is_loaded(world_cleanup: list[World]) -> None:
     # ARRANGE
     expected_profile_name = "first"
     world = World(expected_profile_name)
@@ -21,12 +34,13 @@ def test_if_lastly_used_profile_is_loaded() -> None:
 
     # ACT
     world = World()
+    world_cleanup.append(world)
 
     # ASSERT
     assert world.profile_data.list_profiles() == [expected_profile_name]
 
 
-def test_if_correct_profile_is_loaded_when_something_is_stored() -> None:
+def test_if_correct_profile_is_loaded_when_something_is_stored(world_cleanup: list[World]) -> None:
     # ARRANGE
     expected_profile_name = "first"
     additional_profile_name = "second"
@@ -36,6 +50,7 @@ def test_if_correct_profile_is_loaded_when_something_is_stored() -> None:
 
     # ACT
     world = World(expected_profile_name)
+    world_cleanup.append(world)
 
     # ASSERT
     assert world.profile_data.name == expected_profile_name
@@ -60,13 +75,16 @@ def test_loading_non_existing_profile_with_auto_create_disabled() -> None:
     assert exception_message in str(error.value)
 
 
-def test_loading_existing_profile_with_auto_create_disabled() -> None:
+def test_loading_existing_profile_with_auto_create_disabled(world_cleanup: list[World]) -> None:
     # ARRANGE
     profile_name = "existing_profile"
     world = World(profile_name)
     world.profile_data.save()
     world.close()
 
-    # ACT & ASSERT
+    # ACT
     world = TyperWorld(profile_name)
+    world_cleanup.append(world)
+
+    # ASSERT
     assert world.profile_data.name == profile_name
