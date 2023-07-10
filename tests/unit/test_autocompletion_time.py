c9e3db628dc269ea0c12b8fa4d2b2480109d10e8
Mateusz Żebrak <mzebrak@syncad.com>
2023-07-10 11:59:37 +0200

    Add test for autocompletion time

---
+++ tests/unit/test_autocompletion_time.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import subprocess
+
+from clive.__private.logger import logger
+
+
+def test_autocompletion_time() -> None:
+    # ARRANGE
+    seconds_threshold = 0.5
+    command = "export _CLIVE_COMPLETE=1 && time python3 -X importtime clive/main.py --help"
+
+    # ACT
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+    # ASSERT
+    output = result.stderr.strip()
+
+    import_time = float(output.split("\n")[-2].split("user")[0])
+
+    logger.info(f"Cumulative import time: {import_time:.2f}s")
+    logger.info(output)
+
+    message = (
+        f"Autocompletion time `{import_time:.2f}s` exceeds `{seconds_threshold}s`\n"
+        f"Please check for any unnecessary imports in autocompletion mode."
+    )
+    assert import_time < seconds_threshold, message
