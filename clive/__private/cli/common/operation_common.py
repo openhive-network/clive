8f55e735c83fc3dd4ebac9d0d85e1644e27bf37f
Mateusz Żebrak <mzebrak@syncad.com>
2023-07-12 14:32:09 +0200

    Broadcast operation-related cli commands by default

--- clive/__private/cli/common/operation_common.py
+++ clive/__private/cli/common/operation_common.py
@@ -29,7 +29,7 @@ class OperationCommon(PreconfiguredBaseModel):
     password: str = typer.Option(..., help="Password to unlock the wallet.", show_default=False)
     sign: str = typer.Option(..., help="Key alias to sign the transaction with.", show_default=False)
     beekeeper_remote: Optional[str] = options.beekeeper_remote_option
-    broadcast: bool = typer.Option(..., help="Whether broadcast the transaction. (i.e. dry-run)", show_default=False)
+    broadcast: bool = typer.Option(True, help="Whether broadcast the transaction. (i.e. dry-run)")
     save_file: Optional[str] = typer.Option(None, help="The file to save the transaction to.", show_default=False)
     world: "World"
 
