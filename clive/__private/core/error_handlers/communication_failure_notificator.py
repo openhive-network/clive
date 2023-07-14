422ad0d3776b836b942bb6c356c5879c310a2d91
Mateusz Żebrak <mzebrak@syncad.com>
2023-07-14 12:30:19 +0200

    Create ErrorHandlerContextManager abstraction

--- clive/__private/core/error_handlers/communication_failure_notificator.py
+++ clive/__private/core/error_handlers/communication_failure_notificator.py
@@ -1,51 +1,29 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Final
+from typing import Final
 
 from clive.__private.core.clive_import import get_clive
+from clive.__private.core.error_handlers.error_handler_context_manager import (
+    ErrorHandlerContextManager,
+    ResultNotAvailable,
+)
 from clive.__private.logger import logger
 from clive.__private.ui.widgets.notification import Notification
 from clive.exceptions import CommunicationError
 
-if TYPE_CHECKING:
-    from collections.abc import Callable
-    from types import TracebackType
 
-    from typing_extensions import Self
-
-
-class CommunicationFailureNotificator:
+class CommunicationFailureNotificator(ErrorHandlerContextManager):
     """A context manager that notifies about errors of `CommunicatorError` type."""
 
     SEARCHED_AND_PRINTED_MESSAGES: Final[dict[str, str]] = {
         "does not have sufficient funds": "You don't have enough funds to perform this operation.",
     }
 
-    def __init__(self) -> None:
-        self.error_occurred = False
-
-    def __enter__(self) -> Self:
-        return self
-
-    def __exit__(
-        self, exc_type: type[BaseException] | None, exc_val: BaseException | None, exc_tb: TracebackType | None
-    ) -> bool:
-        self.error_occurred = exc_type is not None
-
-        if self.error_occurred and isinstance(exc_val, CommunicationError):
-            self.__notify(exc_val)
-            return True
-        return False
-
-    def execute(self, func: Callable[..., None]) -> None:
-        try:
-            func()
-        except CommunicationError as error:
-            self.error_occurred = True
+    def _handle_error(self, error: BaseException) -> ResultNotAvailable:
+        if isinstance(error, CommunicationError):
             self.__notify(error)
-        except Exception:
-            self.error_occurred = True
-            raise
+            return ResultNotAvailable(error)
+        raise error
 
     def __notify(self, exception: CommunicationError) -> None:
         message = self.__determine_message(exception)
---
+++ clive/__private/core/error_handlers/error_handler_context_manager.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, TypeVar
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+    from types import TracebackType
+
+    from typing_extensions import Self
+
+ExecuteResultT = TypeVar("ExecuteResultT")
+
+
+@dataclass
+class ResultNotAvailable:
+    exception: BaseException
+
+
+class ErrorHandlerContextManager(ABC):
+    def __init__(self) -> None:
+        self._error: BaseException | None = None
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> bool:
+        """Return false if exception should be re-raised."""
+        if exc_val is not None:
+            self._error = exc_val
+            try:
+                self._handle_error(exc_val)
+            except BaseException:  # noqa: BLE001
+                return False
+            else:
+                return True
+        return False
+
+    @abstractmethod
+    def _handle_error(self, error: BaseException) -> ResultNotAvailable:
+        """Handle all the errors. Reraise if error should not be handled."""
+
+    @property
+    def error(self) -> BaseException | None:
+        return self._error
+
+    @property
+    def error_occurred(self) -> bool:
+        return self.error is not None
+
+    def execute(self, func: Callable[[], ExecuteResultT]) -> ExecuteResultT | ResultNotAvailable:
+        try:
+            return func()
+        except Exception as error:  # noqa: BLE001
+            self._error = error
+            return self._handle_error(error)
