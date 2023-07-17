141f01a2fcc2e448bd5a7008c73b2221f4e4e16e
Mateusz Żebrak <mzebrak@syncad.com>
2023-07-17 10:29:35 +0000

    Rename trx_id to sig_digest

--- tests/unit/wax/test_calculate_sig_digest.py
+++ tests/unit/wax/test_calculate_sig_digest.py
@@ -7,9 +7,9 @@ import wax
 from .consts import ENCODING, MAINNET_CHAIN_ID, VALID_SIG_DIGEST_WITH_TRANSACTIONS
 
 
-@pytest.mark.parametrize("trx_id", list(VALID_SIG_DIGEST_WITH_TRANSACTIONS.keys()))
-def test_proper_sig_digest(trx_id: str) -> None:
-    result = wax.calculate_sig_digest(VALID_SIG_DIGEST_WITH_TRANSACTIONS[trx_id].encode(ENCODING), MAINNET_CHAIN_ID)
+@pytest.mark.parametrize("sig_digest", list(VALID_SIG_DIGEST_WITH_TRANSACTIONS))
+def test_proper_sig_digest(sig_digest: str) -> None:
+    result = wax.calculate_sig_digest(VALID_SIG_DIGEST_WITH_TRANSACTIONS[sig_digest].encode(ENCODING), MAINNET_CHAIN_ID)
     assert result.status == wax.python_error_code.ok
     assert not result.exception_message
-    assert result.result.decode(ENCODING) == trx_id
+    assert result.result.decode(ENCODING) == sig_digest
