2ad24dfdb95716ba2ab579db0f6071dde614a8c1
Mateusz Żebrak <mzebrak@syncad.com>
2023-07-18 15:33:34 +0200

    Specify datetime json encoder for CliveBaseModel
    This issue comes from pydantic ignoring json
    encoders in nested structures (does not apply them
    recursively):
    https://github.com/pydantic/pydantic/issues/2277
    
    Fix for this will be available in pydantic v2.
    
    At first I assumed schemas are serializing datetime incorrectly,
    but it turns out that Clive is wrapping models from schemas inside
    JSONRPCProtocol model, which inherits from CliveBaseModel.
    That's why we need a json encoder defined once again in Clive.

--- clive/models/base.py
+++ clive/models/base.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
+from datetime import datetime
+
 from pydantic import BaseModel
+
+from clive.__private.core.communication import CustomJSONEncoder
 
 
 class CliveBaseModel(BaseModel):
     class Config:
         allow_population_by_field_name = True
+        json_encoders = {datetime: lambda d: d.strftime(CustomJSONEncoder.TIME_FORMAT_WITH_MILLIS)}
