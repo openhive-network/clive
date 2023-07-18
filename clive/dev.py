ad9114429ce750ae0f2420046d9259b58fdb7015
Mateusz Żebrak <mzebrak@syncad.com>
2023-07-18 16:41:05 +0200

    Fix dev run

--- clive/dev.py
+++ clive/dev.py
@@ -1,13 +1,24 @@
 from __future__ import annotations
 
-from textual.cli.cli import run_app
+import os
+
+from textual.cli._run import run_app
+from textual.features import parse_features
 
 
 def main() -> None:
     from clive.__private.before_launch import prepare_before_launch
 
+    environment = dict(os.environ)
+
+    features = set(parse_features(environment.get("TEXTUAL", "")))
+    features.add("debug")
+    features.add("devtools")
+
+    environment["TEXTUAL"] = ",".join(sorted(features))
+
     prepare_before_launch()
-    run_app(["--dev", "clive.dev_import:clive_app"])
+    run_app("clive/main.py", [], environment)
 
 
 if __name__ == "__main__":
