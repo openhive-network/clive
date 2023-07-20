945b2fa24f2706c6f307db6d7105df49aba2434c
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-07-20 13:37:40 +0200

    Create screens for witness-operations

---
+++ clive/__private/ui/operations/raw/witness_set_properties/witness_set_properties.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from textual.containers import Grid
+from textual.widgets import Input, Static
+
+from clive.__private.ui.operations.raw_operation_base_screen import RawOperationBaseScreen
+from clive.__private.ui.widgets.big_title import BigTitle
+from clive.__private.ui.widgets.placeholders_constants import ACCOUNT_NAME_PLACEHOLDER, ASSET_AMOUNT_PLACEHOLDER
+from clive.__private.ui.widgets.view_bag import ViewBag
+from clive.models import Asset
+from schemas.operations import WitnessSetPropertiesOperation
+
+if TYPE_CHECKING:
+    from textual.app import ComposeResult
+
+
+class Body(Grid):
+    """All the content of the screen, excluding the title."""
+
+
+class PlaceTaker(Static):
+    """Container used for making correct layout of a grid."""
+
+
+class WitnessSetProperties(RawOperationBaseScreen):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.__witness_input = Input(placeholder=ACCOUNT_NAME_PLACEHOLDER)
+        self.__account_creation_fee_input = Input(placeholder=ASSET_AMOUNT_PLACEHOLDER)
+        self.__maximum_block_size_input = Input(value="131072", placeholder="maximum block size")
+        self.__hbd_interest_rate_input = Input(value="1000", placeholder="hbd interest rate")
+
+    def create_left_panel(self) -> ComposeResult:
+        with ViewBag():
+            yield BigTitle("Witness set properties")
+            with Body():
+                yield Static("witness", classes="label")
+                yield self.__witness_input
+                yield BigTitle("Props")
+                yield PlaceTaker()
+                yield Static("account creation fee", classes="label")
+                yield self.__account_creation_fee_input
+                yield Static("maximum block size", classes="label")
+                yield self.__maximum_block_size_input
+                yield Static("hbd interest rate", classes="label")
+                yield self.__hbd_interest_rate_input
+
+    def _create_operation(self) -> WitnessSetPropertiesOperation:
+        props_field = {
+            "account_creation_fee": Asset.hive(self.__account_creation_fee_input.value),
+            "maximum_block_size": int(self.__maximum_block_size_input.value),
+            "hbd_interest_rate": int(self.__hbd_interest_rate_input.value),
+        }
+
+        return WitnessSetPropertiesOperation(witness=self.__witness_input.value, props=props_field)
---
+++ clive/__private/ui/operations/raw/witness_update/witness_update.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from textual.containers import Grid
+from textual.widgets import Input, Static
+
+from clive.__private.ui.operations.raw_operation_base_screen import RawOperationBaseScreen
+from clive.__private.ui.widgets.big_title import BigTitle
+from clive.__private.ui.widgets.ellipsed_static import EllipsedStatic
+from clive.__private.ui.widgets.placeholders_constants import ASSET_AMOUNT_PLACEHOLDER, KEY_PLACEHOLDER
+from clive.__private.ui.widgets.view_bag import ViewBag
+from clive.models import Asset
+from schemas.operations import WitnessUpdateOperation
+
+if TYPE_CHECKING:
+    from textual.app import ComposeResult
+
+
+class Body(Grid):
+    """All the content of the screen, excluding the title."""
+
+
+class PlaceTaker(Static):
+    """Container used for making correct layout of a grid."""
+
+
+class WitnessUpdate(RawOperationBaseScreen):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.__url_input = Input(placeholder="e.g: witness-category/my-witness")
+        self.__block_signing_key_input = Input(placeholder=KEY_PLACEHOLDER)
+        self.__account_creation_fee_input = Input(placeholder=ASSET_AMOUNT_PLACEHOLDER)
+        self.__maximum_block_size_input = Input(value="131072", placeholder="maximum block size")
+        self.__hbd_interest_rate_input = Input(value="1000", placeholder="hbd interest rate")
+        self.__fee_input = Input(placeholder=ASSET_AMOUNT_PLACEHOLDER)
+
+    def create_left_panel(self) -> ComposeResult:
+        with ViewBag():
+            yield BigTitle("Witness update")
+            with Body():
+                yield Static("owner", classes="label")
+                yield EllipsedStatic(self.app.world.profile_data.working_account.name, id_="owner-label")
+                yield Static("url", classes="label")
+                yield self.__url_input
+                yield Static("block signing key", classes="label")
+                yield self.__block_signing_key_input
+                yield Static("fee", classes="label")
+                yield self.__fee_input
+                yield BigTitle("Props")
+                yield PlaceTaker()
+                yield Static("account creation fee", classes="label")
+                yield self.__account_creation_fee_input
+                yield Static("maximum block size", classes="label")
+                yield self.__maximum_block_size_input
+                yield Static("hbd interest rate", classes="label")
+                yield self.__hbd_interest_rate_input
+
+    def _create_operation(self) -> WitnessUpdateOperation[Asset.Hive]:
+        props_field = {
+            "account_creation_fee": Asset.hive(self.__account_creation_fee_input.value),
+            "maximum_block_size": int(self.__maximum_block_size_input.value),
+            "hbd_interest_rate": int(self.__hbd_interest_rate_input.value),
+        }
+
+        return WitnessUpdateOperation(
+            owner=self.app.world.profile_data.name,
+            url=self.__url_input.value,
+            block_signing_key=self.__block_signing_key_input.value,
+            props=props_field,
+            fee=Asset.hive(self.__fee_input.value),
+        )
