275173c5d0520b52701576ab176eb0d2e3edca78
kmochocki <kmochocki@syncad.com>
2023-07-25 13:00:37 +0000

    Disable schema checks to enable use of clive on mainnet

--- clive/main.py
+++ clive/main.py
@@ -2,11 +2,18 @@ from __future__ import annotations
 
 import sys
 
+from pydantic import Extra
+
 from clive.__private.cli import cli
 from clive.__private.cli.completion import is_tab_completion_active
 from clive.__private.core._thread import thread_pool
 from clive.__private.run_cli import run_cli
 from clive.__private.run_tui import run_tui
+from schemas.policies import ExtraFields, set_policies
+
+
+def __disable_schemas_extra_fields_check() -> None:
+    set_policies(ExtraFields(policy=Extra.allow))
 
 
 def __any_arguments_given() -> bool:
@@ -14,9 +21,11 @@ def __any_arguments_given() -> bool:
 
 
 def main() -> None:
-    if is_tab_completion_active():
-        cli()
-        return
+    __disable_schemas_extra_fields_check()
+    with thread_pool:
+        if is_tab_completion_active():
+            cli()
+            return
 
     if not __any_arguments_given():
         run_tui()
@@ -26,5 +35,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    with thread_pool:
-        main()
+    main()
