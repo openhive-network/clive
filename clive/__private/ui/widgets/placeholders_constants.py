1675eabe3456978810d1e76b3d945e86e24d6503
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-07-25 12:45:47 +0200

    Create file with common used placeholders

---
+++ clive/__private/ui/widgets/placeholders_constants.py
@@ -0,0 +1,21 @@
+"""File with placeholders that are used very often."""
+from __future__ import annotations
+
+from typing import Final
+
+ASSET_AMOUNT_PLACEHOLDER: Final[str] = "e.g: 1.000"
+ACCOUNT_NAME_PLACEHOLDER: Final[str] = "e.g: alice"
+ACCOUNT_NAME2_PLACEHOLDER: Final[str] = "e.g: bob"
+WITNESS_PLACEHOLDER: Final[str] = "e.g: hiveio"
+ACCOUNT_AUTHS_PLACEHOLDER: Final[str] = "e.g: alice, 1; bob, 1. Notice: split pair of values by ;"
+KEY_AUTHS_PLACEHOLDER: Final[str] = (
+    "e.g: STM6vJmrwaX5TjgTS9dPH8KsArso5m91fVodJvv91j7G765wqcNM9, 1. Notice: split pair of values by ;"
+)
+JSON_DATA_PLACEHOLDER: Final[str] = "e.g: {}"
+WEIGHT_THRESHOLD_PLACEHOLDER: Final[str] = "e.g: 1"
+ID_PLACEHOLDER: Final[str] = "e.g: 1000"
+PERMLINK_PLACEHOLDER: Final[str] = "e.g: a-post-by-alice"
+DATE_PLACEHOLDER: Final[str] = "e.g: e.g: 2019-08-27T11:22:39"
+KEY_PLACEHOLDER: Final[str] = "e.g: STM8ZSyzjPm48GmUuMSRufkVYkwYbZzbxeMysAVp7KFQwbTf98TcG"
+MEMO_PLACEHOLDER: Final[str] = "e.g.: For the coffee!"
+PERCENT_PLACEHOLDER: Final[str] = "e.g: 10"
