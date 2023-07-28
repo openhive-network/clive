850408ccb081f943f5e6544a298e17d00399e7f0
kmochocki <kmochocki@syncad.com>
2023-07-28 13:05:48 +0000

    Fix problem with operation validation

--- clive/models/aliased.py
+++ clive/models/aliased.py
@@ -8,6 +8,7 @@ from schemas.__private.operations import (
     Hf26OperationType,
     Hf26VirtualOperationRepresentationType,
     Hf26VirtualOperationType,
+    get_hf26_representation,
 )
 from schemas.__private.preconfigured_base_model import (
     Operation as SchemasBaseOperationType,
@@ -26,3 +27,9 @@ VirtualOperationRepresentationType = Hf26VirtualOperationRepresentationType
 ApiOperationObject = Hf26ApiOperationObject
 ApiVirtualOperationObject = Hf26ApiVirtualOperationObject
 Signature = SchemasSignature
+
+
+def convert_to_representation(
+    op: Operation | VirtualOperation,
+) -> OperationRepresentationType | VirtualOperationRepresentationType:
+    return get_hf26_representation(op.get_name())(type=op.get_name(), value=op)
