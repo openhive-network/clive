e0239e3abe23f67424925a3fa11f0623e0b270d2
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-08-01 14:50:03 +0200

    Move AccountNameHiglighter to acccount_name_input

--- clive/__private/ui/widgets/inputs/account_name_input.py
+++ clive/__private/ui/widgets/inputs/account_name_input.py
@@ -1,8 +1,32 @@
 from __future__ import annotations
 
-from clive.__private.ui.set_account.set_account import AccountNameHighlighter
+from re import Pattern, compile
+from typing import TYPE_CHECKING, Final
+
+from rich.highlighter import Highlighter
+
 from clive.__private.ui.widgets.inputs.base_input import BaseInput
 from clive.__private.ui.widgets.placeholders_constants import ACCOUNT_NAME_PLACEHOLDER
+
+if TYPE_CHECKING:
+    from rich.text import Text
+
+
+class AccountNameHighlighter(Highlighter):
+    MAX_ACCOUNT_NAME_LENGTH: Final[int] = 14
+    HIVE_ACCOUNT_NAME_REGEX: Final[Pattern[str]] = compile(
+        r"^((([a-z0-9])[a-z0-9\-]{1,14}([a-z0-9]))(\.([a-z0-9])[a-z0-9\-]{1,14}([a-z0-9]))*)$"
+    )
+
+    @classmethod
+    def is_valid_account_name(cls, name: str) -> bool:
+        return len(name) <= cls.MAX_ACCOUNT_NAME_LENGTH and (cls.HIVE_ACCOUNT_NAME_REGEX.match(name) is not None)
+
+    def highlight(self, text: Text) -> None:
+        if self.is_valid_account_name(str(text)):
+            text.stylize("green")
+        else:
+            text.stylize("red")
 
 
 class AccountNameInput(BaseInput):
