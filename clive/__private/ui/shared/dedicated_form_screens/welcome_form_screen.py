193ba800424fca35d40fe4a8d9471cf1b712e31c
Mateusz Żebrak <mzebrak@syncad.com>
2023-08-03 09:24:47 +0200

    Align title to the center on WelcomeFormScreen

--- clive/__private/ui/shared/dedicated_form_screens/welcome_form_screen.py
+++ clive/__private/ui/shared/dedicated_form_screens/welcome_form_screen.py
@@ -27,7 +27,7 @@ class WelcomeFormScreen(BaseScreen, FirstFormScreen[ContextT]):
 
     def create_main_panel(self) -> ComposeResult:
         with DialogContainer("welcome"):
-            yield Static(self.__title)
+            yield Static(self.__title, id="title")
             yield CliveButton("Start!", id_="welcome_button_start")
 
     @on(CliveButton.Pressed, "#welcome_button_start")
