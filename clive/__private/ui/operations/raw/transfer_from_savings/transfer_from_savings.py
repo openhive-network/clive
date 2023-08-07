ce727654c2609aafaa95f91c57ddda9c2ce471d7
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-08-07 09:31:17 +0200

    Change implementation of transfer-from-savings according to the new yield from style

--- clive/__private/ui/operations/raw/transfer_from_savings/transfer_from_savings.py
+++ clive/__private/ui/operations/raw/transfer_from_savings/transfer_from_savings.py
@@ -3,16 +3,17 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from textual.containers import Grid
-from textual.widgets import Input, Static
 
 from clive.__private.core.get_default_from_model import get_default_from_model
 from clive.__private.ui.operations.raw_operation_base_screen import RawOperationBaseScreen
 from clive.__private.ui.widgets.big_title import BigTitle
 from clive.__private.ui.widgets.ellipsed_static import EllipsedStatic
+from clive.__private.ui.widgets.inputs.account_name_input import AccountNameInput
 from clive.__private.ui.widgets.inputs.amount_input import AmountInput
 from clive.__private.ui.widgets.inputs.custom_input import CustomInput
+from clive.__private.ui.widgets.inputs.input_label import InputLabel
 from clive.__private.ui.widgets.inputs.memo_input import MemoInput
-from clive.__private.ui.widgets.placeholders_constants import ACCOUNT_NAME_PLACEHOLDER, ID_PLACEHOLDER
+from clive.__private.ui.widgets.placeholders_constants import ID_PLACEHOLDER
 from clive.__private.ui.widgets.view_bag import ViewBag
 from schemas.operations import TransferFromSavingsOperation
 
@@ -32,27 +33,24 @@ class TransferFromSavings(RawOperationBaseScreen):
 
         default_request_id = str(get_default_from_model(TransferFromSavingsOperation, "request_id", int))
 
-        self.__to_input = Input(placeholder=ACCOUNT_NAME_PLACEHOLDER)
+        self.__to_input = AccountNameInput(label="to")
         self.__request_id_input = CustomInput(
             label="request id", default_value=default_request_id, placeholder=ID_PLACEHOLDER
         )
         self.__amount_input = AmountInput()
-        self.__memo_input = MemoInput()
+        self.__memo_input = MemoInput(label="memo")
 
     def create_left_panel(self) -> ComposeResult:
         with ViewBag():
             yield BigTitle("Transfer from savings")
             with Body():
-                yield Static("from", classes="label")
+                yield InputLabel("from")
                 yield EllipsedStatic(self.app.world.profile_data.working_account.name, id_="from-label")
-                yield Static("to", classes="label")
-                yield self.__to_input
-                yield Static("request id", classes="label")
-                yield self.__request_id_input
-                yield Static("amount", classes="label")
+                yield from self.__to_input.compose()
+                yield from self.__request_id_input.compose()
+                yield InputLabel("amount")
                 yield self.__amount_input
-                yield Static("memo", classes="label")
-                yield self.__memo_input
+                yield from self.__memo_input.compose()
 
     def _create_operation(self) -> TransferFromSavingsOperation[Asset.Hive, Asset.Hbd] | None:
         amount = self.__amount_input.amount
