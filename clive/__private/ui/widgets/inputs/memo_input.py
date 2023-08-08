47c7ac89097b0e9b7bb07bff70006cbb366720ca
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-08-08 13:16:09 +0200

    Create custom input type for string inputs - TextInput

--- clive/__private/ui/widgets/inputs/memo_input.py
+++ clive/__private/ui/widgets/inputs/memo_input.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from clive.__private.ui.widgets.inputs.custom_input import CustomInput
+from clive.__private.ui.widgets.inputs.text_input import TextInput
 from clive.__private.ui.widgets.placeholders_constants import MEMO_PLACEHOLDER
 
 
-class MemoInput(CustomInput):
+class MemoInput(TextInput):
     def __init__(self, label: str = "memo", placeholder: str = MEMO_PLACEHOLDER, value: str | None = None):
         super().__init__(label=label, placeholder=placeholder, value=value)
