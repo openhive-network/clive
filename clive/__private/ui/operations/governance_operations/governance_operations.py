deb37dffcdbc48cd6f67944e2aa0579ed78c2b77
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-08-08 16:20:13 +0200

    Implement WitnessTab

--- clive/__private/ui/operations/governance_operations/governance_operations.py
+++ clive/__private/ui/operations/governance_operations/governance_operations.py
@@ -4,8 +4,8 @@ from typing import TYPE_CHECKING
 
 from textual import on
 from textual.binding import Binding
-from textual.containers import Container, Horizontal
-from textual.widgets import Static, TabbedContent
+from textual.containers import Container, Horizontal, Vertical
+from textual.widgets import DataTable, Input, SelectionList, Static, TabbedContent
 
 from clive.__private.ui.operations.cart_based_screen.cart_based_screen import CartBasedScreen
 from clive.__private.ui.operations.raw.account_witness_proxy.account_witness_proxy import AccountWitnessProxy
@@ -69,8 +69,87 @@ class ProxyTab(ScrollableTabPane, CliveWidget):
             yield ProxyAbsent()
 
 
-class WitnessesTab(ScrollableTabPane):
+class WitnessList(SelectionList[str]):
+    pass
+
+
+ROWS = [("Rank", "Witness", "Votes received", "Price feed", "")]
+
+
+class VoteForWitnessTable(DataTable[str]):
+    pass
+
+
+class DeleteVoteForWitnessTable(DataTable[str]):
+    pass
+
+
+class WitnessesTab(ScrollableTabPane, CliveWidget):
     """Tab with all activities related with witnesses."""
+
+    def compose(self) -> ComposeResult:
+        witnesses = [
+            "1 blocktrades",
+            "2 stoodkev",
+            "3 gtg",
+            "4 steempeak",
+            "5 arcange",
+            "6 ausbitbank",
+            "7 roelandp",
+            "8 gtg",
+            "9 vote",
+            "10 witness",
+            "11 witness2",
+            "12 hive",
+            "13 alice",
+            "14 bob",
+            "15 charlie",
+            "16 hiveio",
+            "17 bob2",
+            "18 alice2",
+            "19 alice3",
+            "20 alice5",
+            "21 bob5",
+            "22bob7",
+            "23 bob8",
+            "24 alice7",
+            "25 charlie2",
+            "26 charlie3",
+            "27 charlie5",
+            "28 charlie8",
+            "29 charlie10",
+            "30 charlie11",
+        ]
+        witnesses_list = [(witness, witness) for witness in witnesses]
+
+        witnesses_selection_list: WitnessList = WitnessList(*witnesses_list)
+
+        with Horizontal(id="test"):
+            yield witnesses_selection_list
+            with Vertical(id="witness-information-tables"):
+                yield Static("You will vote for the witnesses:", classes="vote-witness-information")
+                yield VoteForWitnessTable()
+                yield Static("You will cancel votes for the witnesses:", classes="vote-witness-information")
+                yield DeleteVoteForWitnessTable()
+        yield Input("Place for filters", id="filters")
+
+    def on_mount(self) -> None:
+        table_vote = self.query_one(VoteForWitnessTable)
+        table_vote.add_columns(*ROWS[0])
+
+        table_delete_vote = self.query_one(DeleteVoteForWitnessTable)
+        table_delete_vote.add_columns(*ROWS[0])
+
+    @on(WitnessList.SelectedChanged)
+    def add_or_delete_vote(self) -> None:
+        id_ = 1
+        votes = self.query_one(VoteForWitnessTable)
+        witnesses = self.query_one(WitnessList).selected
+
+        for witness in witnesses:
+            if not votes.rows.get(witness):
+                votes.add_row(str(id_), "blocktrades", "122", "0.355", "delete")
+                id_ += 1
 
 
 class ProposalsTab(ScrollableTabPane):
