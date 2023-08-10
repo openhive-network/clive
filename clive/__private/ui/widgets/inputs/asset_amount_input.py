a01d5dd47bfff7f25ea7096c26f4714b2ca4971d
Mateusz Żebrak <mzebrak@syncad.com>
2023-08-10 15:44:09 +0200

    Move wrapper class out of AssetAmountInput.compose

--- clive/__private/ui/widgets/inputs/asset_amount_input.py
+++ clive/__private/ui/widgets/inputs/asset_amount_input.py
@@ -14,32 +14,37 @@ if TYPE_CHECKING:
 
 
 class AssetAmountInput(Horizontal):
+    class Wrapper(Horizontal):
+        DEFAULT_CSS = """
+        Wrapper Input {
+            width: 60%;
+        }
+        """
+
+        def __init__(self, input_: AmountInput, currency_selector: CurrencySelectorLiquid) -> None:
+            self.__input = input_
+            self.__currency_selector = currency_selector
+            super().__init__()
+
+        def compose(self) -> ComposeResult:
+            yield self.__input
+            yield self.__currency_selector
+
     def __init__(self, label: str = "amount") -> None:
-        self.__input_value = AmountInput(label=label)
+        self.__input = AmountInput(label=label)
         self.__currency_selector = CurrencySelectorLiquid()
 
         super().__init__()
 
     def compose(self) -> ComposeResult:
-        label, input_ = self.__input_value.compose()
-        currency_selector = self.__currency_selector
-
-        class AssetInput(Horizontal):
-            DEFAULT_CSS = """
-                AssetInput Input {
-                    width: 60%;
-            }
-            """
-
-            def compose(self) -> ComposeResult:
-                yield input_
-                yield currency_selector
+        input_: AmountInput
+        label, input_ = self.__input.compose()  # type: ignore[assignment]
 
         yield label
-        yield AssetInput()
+        yield self.Wrapper(input_, self.__currency_selector)
 
     @property
     def amount(self) -> Asset.Hive | Asset.Hbd | None:
-        if self.__input_value.value:
-            return self.__currency_selector.create_asset(self.__input_value.value)
+        if self.__input.value:
+            return self.__currency_selector.create_asset(self.__input.value)
         return None
