97455769ce41d2412931a2aa8279d66491723b53
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-08-16 15:37:42 +0200

    Add highlighter for memo_key_input

--- clive/__private/ui/widgets/inputs/memo_key_input.py
+++ clive/__private/ui/widgets/inputs/memo_key_input.py
@@ -1,7 +1,31 @@
 from __future__ import annotations
 
+from re import Pattern, compile
+from typing import TYPE_CHECKING, Final
+
+from rich.highlighter import Highlighter
+
 from clive.__private.ui.widgets.inputs.text_input import TextInput
 from clive.__private.ui.widgets.placeholders_constants import KEY_PLACEHOLDER
+
+if TYPE_CHECKING:
+    from rich.text import Text
+
+
+class MemoKeyHighlighter(Highlighter):
+    HIVE_MEMO_KEY_REGEX: Final[Pattern[str]] = compile(
+        r"^(?:STM|TST)[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{7,51}$"
+    )
+
+    @classmethod
+    def is_valid_memo_keys(cls, memo_key: str) -> bool:
+        return cls.HIVE_MEMO_KEY_REGEX.match(memo_key) is not None
+
+    def highlight(self, text: Text) -> None:
+        if self.is_valid_memo_keys(str(text)):
+            text.stylize("green")
+        else:
+            text.stylize("red")
 
 
 class MemoKeyInput(TextInput):
