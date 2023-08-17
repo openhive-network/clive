78db7e3555896b1e7e8e176036cf1af91cac1579
kmochocki <kmochocki@syncad.com>
2023-08-17 17:28:50 +0000

    Remove command `clive run`

--- clive/__private/cli/__init__.py
+++ clive/__private/cli/__init__.py
@@ -7,7 +7,6 @@ import typer
 from clive.__private.cli.beekeeper import beekeeper
 from clive.__private.cli.list import list_
 from clive.__private.cli.transfer import transfer
-from clive.__private.run_tui import run_tui
 
 HELP: Final[str] = """
 CLI tool for the Clive TUI application to interact with the [bold red]Hive[/bold red] blockchain :bee: \n
@@ -22,7 +21,7 @@ cli.add_typer(beekeeper, name="beekeeper")
 
 
 @cli.callback(invoke_without_command=True)
-def _main(
+def main(
     version: Optional[bool] = typer.Option(
         None, "--version", "-V", help="Show the current version and exit.", is_eager=True
     ),
@@ -32,9 +31,3 @@ def _main(
 
         typer.echo(f"CLIVE Version: {VERSION_INFO}")
         raise typer.Exit
-
-
-@cli.command()
-def run() -> None:
-    """Launch the TUI application."""
-    run_tui()
