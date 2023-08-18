b0c8cebc5a869979fa7e62a5c6c3e04d3b043214
Mateusz Żebrak <mzebrak@syncad.com>
2023-08-18 10:06:02 +0200

    Remove workaround for ScrollableTabPane
    Explaination:
    Previously it was observable that on the
    Operations tab panes, there was a problem
    with showing the last element while scrollbar
    is activated. It was partly visible or not visible
    at all.
    
    Now textual has fixed that in 0.33.0

--- clive/__private/ui/widgets/scrollable_tab_pane.py
+++ clive/__private/ui/widgets/scrollable_tab_pane.py
@@ -7,9 +7,7 @@ class ScrollableTabPane(TabPane):
     """Tab with auto overflow-y to provide auto-scroll."""
 
     DEFAULT_CSS = """
-    /* padding is added, because textual cut off last element of TabPane*/
     ScrollableTabPane {
         overflow-y: auto;
-        padding-bottom: 4;
     }
     """
