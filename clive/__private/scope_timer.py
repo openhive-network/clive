979f19133a8792214b24b363419d492579b3fe02
kmochocki <kmochocki@syncad.com>
2023-08-23 09:13:40 +0000

    Add scoped timer

---
+++ clive/__private/scope_timer.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from time import perf_counter
+from typing import TYPE_CHECKING
+
+from clive.__private.logger import logger
+
+if TYPE_CHECKING:
+    from types import TracebackType
+
+    from typing_extensions import Self
+
+
+class ScopeStopper:
+    def __init__(self, name: str) -> None:
+        self.__start = self.__now()
+        self.__name = name
+
+    def __now(self) -> float:
+        return perf_counter()
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, _: type[Exception] | None, ex: Exception | None, ___: TracebackType | None) -> None:
+        logger.debug(f"{self.__name} took: {self.__now() - self.__start :.6f}s")
