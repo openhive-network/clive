dc7cd409ada1f9705db475613fdbe71822e44921
kmochocki <kmochocki@syncad.com>
2023-08-23 09:16:57 +0000

    Make update node data using batch node

--- clive/__private/core/commands/update_node_data.py
+++ clive/__private/core/commands/update_node_data.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from asyncio import gather
+from collections import defaultdict
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta, timezone
 from math import ceil
@@ -8,20 +8,29 @@ from typing import TYPE_CHECKING, Final, cast
 
 from clive.__private.core.commands.abc.command_with_result import CommandWithResult
 from clive.__private.core.iwax import calculate_current_manabar_value, calculate_manabar_full_regeneration_time
+from clive.__private.scope_timer import ScopeStopper
 from clive.exceptions import CommunicationError
 from clive.models import Asset
-from schemas.database_api.response_schemas import GetDynamicGlobalProperties
+from schemas.database_api.response_schemas import (
+    FindAccounts,
+    GetDynamicGlobalProperties,
+    ListChangeRecoveryAccountRequests,
+    ListDeclineVotingRightsRequests,
+    ListOwnerHistories,
+)
 
 if TYPE_CHECKING:
-    from collections.abc import Awaitable
     from types import TracebackType
 
     from clive.__private.core.node.node import Node
     from clive.__private.storage import mock_database
     from clive.__private.storage.accounts import Account
     from schemas.__private.hive_fields_custom_schemas import Manabar
+    from schemas.account_history_api.response_schemas import GetAccountHistory
     from schemas.database_api.fundaments_of_reponses import AccountItemFundament
     from schemas.rc_api.fundaments_of_responses import RcAccount
+    from schemas.rc_api.response_schemas import FindRcAccounts
+    from schemas.reputation_api.response_schemas import GetAccountReputations
 
     AccountFundamentT = AccountItemFundament[Asset.Hive, Asset.Hbd, Asset.Vests]
     CoreAccountsInfoT = dict[str, AccountFundamentT]
@@ -51,22 +60,39 @@ class UpdateNodeData(CommandWithResult[DynamicGlobalPropertiesT]):
     accounts: list[Account] = field(default_factory=list)
 
     @dataclass
-    class AccountApiInfo:
+    class _AccountApiInfo:
         core: AccountFundamentT
         last_history_entry: datetime = field(default_factory=lambda: datetime.fromtimestamp(0))
         rc: RcAccount[Asset.Vests] | None = None
         warnings: int = 0
         reputation: int = 0
 
-    async def _execute(self) -> None:
-        downvote_vote_ratio: Final[int] = 4
+    @dataclass
+    class _HarvestedData:
+        core_raw: AccountFundamentT | None = None
+        rc_raw: RcAccount[Asset.Vests] | None = None
+        reputation_raw: GetAccountReputations | None = None
+        last_interaction_info_raw: GetAccountHistory | None = None
+        decline_voting_rights_raw: ListDeclineVotingRightsRequests | None = None
+        recovery_account_in_progress: ListChangeRecoveryAccountRequests | None = None
+        owner_key_change_in_progress: ListOwnerHistories | None = None
 
+    async def _execute(self) -> None:
         self._result = await self.node.api.database_api.get_dynamic_global_properties()
         if not self.accounts:
             return
 
-        api_accounts = await self.__harvest_data_from_api()
+        with ScopeStopper("harvesting"):
+            api_accounts = await self.__harvest_data_from_api()
+
+        with ScopeStopper("processing"):
+            self.__process_data(api_accounts)
+
+    def __process_data(self, api_accounts: dict[Account, _AccountApiInfo]) -> None:
+        downvote_vote_ratio: Final[int] = 4
+
         gdpo = self._result
+        assert gdpo is not None  # mypy check
 
         for account, info in api_accounts.items():
             account.data.reputation = info.reputation
@@ -101,143 +127,154 @@ class UpdateNodeData(CommandWithResult[DynamicGlobalPropertiesT]):
 
             account.data.last_refresh = self.__normalize_datetime(datetime.utcnow())
 
-    async def __harvest_data_from_api(self) -> dict[Account, AccountApiInfo]:
+    async def __harvest_data_from_api(self) -> dict[Account, _AccountApiInfo]:
+        non_virtual_operations_filter: Final[int] = 0x3FFFFFFFFFFFF
+
         account_names = [acc.name for acc in self.accounts if acc.name]
         if not account_names:
             return {}
 
-        self.__core_accounts_info: CoreAccountsInfoT = {}
+        core_accounts_raw: FindAccounts | None = None
+        rc_accounts_raw: FindRcAccounts[Asset.Vests] | None = None
+        harvested_data: defaultdict[str, UpdateNodeData._HarvestedData] = defaultdict(
+            lambda: UpdateNodeData._HarvestedData()
+        )
 
-        core_accounts_info_future = self.__submit_gathering_base_information(account_names)
-        rc_accounts_info_future = self.__submit_gathering_rc_information(account_names)
+        async with self.node.batch() as node:
+            core_accounts_raw = await node.api.database_api.find_accounts(accounts=account_names)
+            rc_accounts_raw = await node.api.rc_api.find_rc_accounts(accounts=account_names)
 
-        harvested_data: dict[str, Awaitable[tuple[int, datetime]]] = {}
-        for account in self.accounts:
-            harvested_data[account.name] = gather(
-                self.__get_account_reputation(account.name),
-                self.__get_account_last_history_entry(account.name),
-            )
+            for account in self.accounts:
+                harvested_data[account.name].reputation_raw = await node.api.reputation_api.get_account_reputations(
+                    account_lower_bound=account.name, limit=1
+                )
 
-        self.__core_accounts_info = await core_accounts_info_future
-        assert len(self.__core_accounts_info) == len(self.accounts), "invalid amount of accounts after rc api call"
+                harvested_data[account.name].last_interaction_info_raw = (
+                    await node.api.account_history_api.get_account_history(
+                        account=account.name,
+                        limit=1,
+                        operation_filter_low=non_virtual_operations_filter,
+                        include_reversible=True,
+                    )
+                )
 
-        warnings = {account.name: self.__submit_gathering_account_warnings(account) for account in self.accounts}
+                harvested_data[account.name].decline_voting_rights_raw = (
+                    await node.api.database_api.list_decline_voting_rights_requests(
+                        start=account.name, limit=1, order="by_account"
+                    )
+                )
+                harvested_data[account.name].recovery_account_in_progress = (
+                    await node.api.database_api.list_change_recovery_account_requests(
+                        start=account.name, limit=1, order="by_account"
+                    )
+                )
+                harvested_data[account.name].owner_key_change_in_progress = (
+                    await node.api.database_api.list_owner_histories(
+                        start=(account.name, datetime.fromtimestamp(0)), limit=1
+                    )
+                )
 
-        rc_accounts_info = await rc_accounts_info_future
-        assert rc_accounts_info == {} or len(rc_accounts_info) == len(
+        assert core_accounts_raw is not None, "core accounts cannot be not"
+        assert len(core_accounts_raw.accounts) == len(
             self.accounts
         ), "invalid amount of accounts after database api call"
+        assert len(harvested_data) == len(self.accounts), "not all accounts has been processed"
 
-        result: dict[Account, UpdateNodeData.AccountApiInfo] = {}
+        with SuppressNotExistingApi("rc_api"):
+            assert len(rc_accounts_raw.rc_accounts) == len(
+                self.accounts
+            ), "invalid amount of accounts after rc api call"
+            for rc_account in rc_accounts_raw.rc_accounts:
+                harvested_data[rc_account.account].rc_raw = rc_account
+
+        for core_account in core_accounts_raw.accounts:
+            harvested_data[core_account.name].core_raw = core_account
+
+        result: dict[Account, UpdateNodeData._AccountApiInfo] = {}
         for account in self.accounts:
-            reputation, last_history_entry = await harvested_data[account.name]
-            account_warning_count = await warnings[account.name]
-            result[account] = self.AccountApiInfo(
-                core=self.__core_accounts_info[account.name],
-                rc=rc_accounts_info.get(account.name),
-                last_history_entry=last_history_entry,
-                warnings=account_warning_count,
-                reputation=reputation,
+            account_data_raw = harvested_data[account.name]
+            assert account_data_raw.core_raw is not None  # mypy check
+            result[account] = self._AccountApiInfo(
+                core=account_data_raw.core_raw,
+                rc=account_data_raw.rc_raw,
+                last_history_entry=self.__get_account_last_history_entry(account_data_raw),
+                warnings=self.__calculate_warnings(account_data_raw),
+                reputation=self.__get_account_reputation(account_data_raw),
             )
         return result
 
-    async def __submit_gathering_base_information(self, accounts: list[str]) -> CoreAccountsInfoT:
-        return {
-            str(acc.name): acc for acc in (await self.node.api.database_api.find_accounts(accounts=accounts)).accounts
-        }
+    def __get_account_last_history_entry(self, raw: UpdateNodeData._HarvestedData) -> datetime:
+        with SuppressNotExistingApi("account_history_api"):
+            assert raw.last_interaction_info_raw is not None
+            return self.__normalize_datetime(raw.last_interaction_info_raw.history[0][1].timestamp)
+        return datetime.fromtimestamp(0, timezone.utc)
 
-    async def __submit_gathering_rc_information(self, accounts: list[str]) -> RcAccountsInfoT:
-        with SuppressNotExistingApi("rc_api"):
-            return {
-                acc.account: acc for acc in (await self.node.api.rc_api.find_rc_accounts(accounts=accounts)).rc_accounts
-            }
-        return {}
-
-    async def __submit_gathering_account_warnings(self, account: Account) -> int:
-        async def __wait_for_core_info_and_evaluate_warnings() -> int:
-            return sum(
-                [
-                    await self.__check_for_recurrent_transfers(self.__core_accounts_info[account.name]),
-                    await self.__check_is_governance_is_expiring(self.__core_accounts_info[account.name]),
-                ]
-            )
-
-        part_1_of_sum = __wait_for_core_info_and_evaluate_warnings()
-        part_2_of_sum = sum(
+    def __calculate_warnings(self, raw: UpdateNodeData._HarvestedData) -> int:
+        return sum(
             [
-                await foo
-                for foo in [
-                    self.__check_is_recovery_account_not_warning_listed(account),
-                    self.__check_is_declining_voting_rights_in_progress(account),
-                    self.__check_is_changing_recovery_account_is_in_progress(account),
-                    self.__check_is_owner_key_change_is_in_progress(account),
+                check(raw)
+                for check in [
+                    self.__check_for_recurrent_transfers,
+                    self.__check_is_governance_is_expiring,
+                    self.__check_is_recovery_account_not_warning_listed,
+                    self.__check_is_declining_voting_rights_in_progress,
+                    self.__check_is_changing_recovery_account_is_in_progress,
+                    self.__check_is_owner_key_change_is_in_progress,
                 ]
             ]
         )
-        return (await part_1_of_sum) + part_2_of_sum
 
-    async def __check_is_recovery_account_not_warning_listed(self, account: Account) -> int:
+    def __check_is_recovery_account_not_warning_listed(self, raw: UpdateNodeData._HarvestedData) -> int:
         warning_recovery_accounts: Final[set[str]] = {"steem"}
-        return int(account.data.recovery_account in warning_recovery_accounts)
 
-    async def __check_is_declining_voting_rights_in_progress(self, account: Account) -> int:
-        requests = (
-            await self.node.api.database_api.list_decline_voting_rights_requests(
-                start=account.name, limit=1, order="by_account"
-            )
-        ).requests
-        return int(bool(requests) and requests[0].account == account.name)
+        assert raw.core_raw is not None  # mypy check
 
-    async def __check_is_changing_recovery_account_is_in_progress(self, account: Account) -> int:
-        requests = (
-            await self.node.api.database_api.list_change_recovery_account_requests(
-                start=account.name, limit=1, order="by_account"
-            )
-        ).requests
-        return int(bool(requests) and requests[0].account_to_recover == account.name)
+        return int(raw.core_raw.recovery_account in warning_recovery_accounts)
 
-    async def __check_is_owner_key_change_is_in_progress(self, account: Account) -> int:
-        owner_auths = (
-            await self.node.api.database_api.list_owner_histories(
-                start=(account.name, datetime.fromtimestamp(0)), limit=1
-            )
-        ).owner_auths
-        return int(bool(owner_auths) and owner_auths[0].account == account.name)
+    def __check_is_declining_voting_rights_in_progress(self, raw: UpdateNodeData._HarvestedData) -> int:
+        assert raw.decline_voting_rights_raw is not None  # mypy check
+        assert raw.core_raw is not None  # mypy check
 
-    async def __check_for_recurrent_transfers(self, account_info: AccountFundamentT) -> int:
-        assert account_info is not None, "account_info.core cannot be None"
-        return int(account_info.open_recurrent_transfers > 0)
+        requests = raw.decline_voting_rights_raw.requests
+        return int(bool(requests) and requests[0].account == raw.core_raw.name)
 
-    async def __check_is_governance_is_expiring(self, account_info: AccountFundamentT) -> int:
-        assert account_info is not None, "account_info.core cannot be None"
+    def __check_is_changing_recovery_account_is_in_progress(self, raw: UpdateNodeData._HarvestedData) -> int:
+        assert raw.recovery_account_in_progress is not None  # mypy check
+        assert raw.core_raw is not None  # mypy check
+
+        requests = raw.recovery_account_in_progress.requests
+        return int(bool(requests) and requests[0].account_to_recover == raw.core_raw.name)
+
+    def __check_is_owner_key_change_is_in_progress(self, raw: UpdateNodeData._HarvestedData) -> int:
+        assert raw.owner_key_change_in_progress is not None  # mypy check
+        assert raw.core_raw is not None  # mypy check
+
+        owner_auths = raw.owner_key_change_in_progress.owner_auths
+        return int(bool(owner_auths) and owner_auths[0].account == raw.core_raw.name)
+
+    def __check_for_recurrent_transfers(self, raw: UpdateNodeData._HarvestedData) -> int:
+        assert raw.core_raw is not None  # mypy check
+
+        return int(raw.core_raw.open_recurrent_transfers > 0)
+
+    def __check_is_governance_is_expiring(self, raw: UpdateNodeData._HarvestedData) -> int:
         warning_period_in_days: Final[int] = 31
+
+        assert raw.core_raw is not None  # mypy check
+
         return int(
-            account_info.governance_vote_expiration_ts - timedelta(days=warning_period_in_days)
+            raw.core_raw.governance_vote_expiration_ts - timedelta(days=warning_period_in_days)
             > self.__normalize_datetime(datetime.utcnow())
         )
 
-    async def __get_account_last_history_entry(self, account_name: str) -> datetime:
-        non_virtual_operations_filter: Final[int] = 0x3FFFFFFFFFFFF
-        return self.__normalize_datetime(
-            (
-                await self.node.api.account_history_api.get_account_history(
-                    account=account_name,
-                    limit=1,
-                    operation_filter_low=non_virtual_operations_filter,
-                    include_reversible=True,
-                )
-            )
-            .history[0][1]
-            .timestamp
-        )
+    def __get_account_reputation(self, raw: UpdateNodeData._HarvestedData) -> int:
+        assert raw.core_raw is not None  # mypy check
+        assert raw.reputation_raw is not None  # mypy check
 
-    async def __get_account_reputation(self, account_name: str) -> int:
         with SuppressNotExistingApi("reputation_api"):
-            reputation = (
-                await self.node.api.reputation_api.get_account_reputations(account_lower_bound=account_name, limit=1)
-            ).reputations
+            reputation = raw.reputation_raw.reputations
             assert len(reputation) == 1
-            assert reputation[0].account == account_name, "reputation data malformed"
+            assert reputation[0].account == raw.core_raw.name, "reputation data malformed"
             return int(reputation[0].reputation)
         return 0
 
