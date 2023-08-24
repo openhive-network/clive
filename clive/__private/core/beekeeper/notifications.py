1050ff0f5e045aaf4533f79e582d927345c1f39a
Mateusz Żebrak <mzebrak@syncad.com>
2023-08-24 13:18:06 +0200

    Refactor BeekeeperNotificationsServer so notification decorator can be used

--- clive/__private/core/beekeeper/notifications.py
+++ clive/__private/core/beekeeper/notifications.py
@@ -1,14 +1,65 @@
 from __future__ import annotations
 
 from asyncio import Event
-from typing import TYPE_CHECKING, Any, Protocol
+from collections.abc import Callable
+from functools import wraps
+from typing import Any, ParamSpec, Protocol, TypeVar
 
 from clive.__private.core.beekeeper.notification_http_server import AsyncHttpServer, JsonT
 from clive.__private.logger import logger
 from clive.core.url import Url
 
-if TYPE_CHECKING:
-    from collections.abc import Callable
+NotificationNameT = str
+NotificationMessageT = dict[str, Any]
+NotificationDetailsT = dict[str, Any]
+NotificationHandlerT = Callable[[Any, NotificationMessageT], Any]
+NotificationHandlerConditionT = Callable[[NotificationDetailsT], bool]
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+PreWrapFuncT = Callable[[Any, NotificationDetailsT], R]
+PostWrapFuncT = Callable[[Any, NotificationMessageT], R | None]
+
+
+class NotificationDecorator(Protocol):
+    def __call__(
+        self, name_filter: NotificationNameT | None = None, *, condition: NotificationHandlerConditionT | None = None
+    ) -> Callable[[PreWrapFuncT[R]], PostWrapFuncT[R]]:
+        ...
+
+
+def make_notification_decorator(
+    handlers: set[NotificationHandlerT],
+) -> NotificationDecorator:
+    def notification(
+        name_filter: NotificationNameT | None = None, *, condition: NotificationHandlerConditionT | None = None
+    ) -> Callable[[PreWrapFuncT[R]], PostWrapFuncT[R]]:
+        def decorator(
+            func: PreWrapFuncT[R],
+        ) -> PostWrapFuncT[R]:
+            @wraps(func)
+            def wrapper(this: BeekeeperNotificationsServer, message: NotificationMessageT) -> R | None:
+                self = this
+                assert isinstance(self, BeekeeperNotificationsServer)
+                assert isinstance(message, dict)
+
+                notification_name = message["name"]
+                notification_details = message["value"]
+                if name_filter is not None and name_filter != notification_name:
+                    return None
+
+                if condition is not None and not condition(notification_details):
+                    return None
+
+                return func(self, notification_details)
+
+            handlers.add(wrapper)
+            return wrapper
+
+        return decorator
+
+    return notification
 
 
 class WalletClosingListener(Protocol):
@@ -17,6 +68,9 @@ class WalletClosingListener(Protocol):
 
 
 class BeekeeperNotificationsServer:
+    _CLS_HANDLERS: set[NotificationHandlerT] = set()
+    notification = make_notification_decorator(_CLS_HANDLERS)
+
     def __init__(
         self, token_cb: Callable[[], str], notify_closing_wallet_name_cb: Callable[[], str] | None = None
     ) -> None:
@@ -38,29 +92,36 @@ class BeekeeperNotificationsServer:
 
     def notify(self, message: JsonT) -> None:
         logger.info(f"Got notification: {message}")
-        name = message["name"]
-        details = message["value"]
 
-        if name == "webserver listening":
-            if details["type"] == "HTTP":
-                self.http_endpoint = self.__parse_endpoint_notification(details)
-                logger.debug(f"Got notification with http address on: {self.http_endpoint}")
-                self.http_listening_event.set()
-        elif name == "hived_status" and details["current_status"] == "signals attached":
-            logger.debug("Beekeeper reports to be ready")
-            self.ready.set()
-        elif name == "opening_beekeeper_failed":
-            details = details["connection"]
-            assert details["type"] == "HTTP"
-            self.http_endpoint = self.__parse_endpoint_notification(details)
-            logger.debug(f"Got notification with http address, but beekeeper failed when opening: {self.http_endpoint}")
-            self.opening_beekeeper_failed.set()
-        elif name == "Attempt of closing all wallets":
-            logger.debug("Got notification about closing all wallets")
-            if self.__is_tracked_wallet_closing(details):
-                self.__notify_listeners_about_wallets_closing()
-            else:
-                logger.debug("The tracked wallet is not closing, ignoring notification")
+        for handler in self._CLS_HANDLERS:
+            handler(self, message)
+
+    @notification("webserver_listening", condition=lambda details: details["type"] == "HTTP")
+    def handle_http_webserver_listening(self, _: NotificationDetailsT) -> None:
+        logger.debug(f"Got notification with http address on: {self.http_endpoint}")
+        self.http_listening_event.set()
+
+    @notification("hived_status", condition=lambda details: details["current_status"] == "signals attached")
+    def handle_signals_attached(self, _: NotificationDetailsT) -> None:
+        logger.debug("Beekeeper reports to be ready")
+        self.ready.set()
+
+    @notification("opening_beekeeper_failed")
+    def handle_opening_beekeeper_failed(self, details: NotificationDetailsT) -> None:
+        details = details["connection"]
+        assert details["type"] == "HTTP"
+        self.http_endpoint = self.__parse_endpoint_notification(details)
+        logger.debug(f"Got notification with http address, but beekeeper failed when opening: {self.http_endpoint}")
+        self.opening_beekeeper_failed.set()
+
+    @notification("Attempt of closing all wallets")
+    def handle_attempt_closing_all_wallets_received(self, details: NotificationDetailsT) -> None:
+        logger.debug("Got notification about closing all wallets")
+        if not self.__is_tracked_wallet_closing(details):
+            logger.debug("The tracked wallet is not closing, ignoring notification")
+            return
+
+        self.__notify_listeners_about_wallets_closing()
 
     @classmethod
     def __parse_endpoint_notification(cls, details: dict[str, str]) -> Url:
