b25c2f54fdb933da2abaa0780b4549217a304926
Mateusz Żebrak <mzebrak@syncad.com>
2023-08-29 07:41:03 +0200

    Remove the Create profile button from dashboard

--- clive/__private/ui/dashboard/dashboard_inactive.py
+++ clive/__private/ui/dashboard/dashboard_inactive.py
@@ -5,14 +5,12 @@ from textual.binding import Binding
 
 from clive.__private.ui.activate.activate import Activate
 from clive.__private.ui.config.config_inactive import ConfigInactive
-from clive.__private.ui.create_profile.create_profile import CreateProfile
 from clive.__private.ui.dashboard.dashboard_base import DashboardBase
 
 
 class DashboardInactive(DashboardBase):
     BINDINGS = [
         Binding("f4", "activate", "Activate"),
-        Binding("f7", "create_profile", "Create profile"),
         Binding("f9", "config", "Config"),
     ]
 
@@ -21,9 +19,6 @@ class DashboardInactive(DashboardBase):
 
     def action_config(self) -> None:
         self.app.push_screen(ConfigInactive())
-
-    def action_create_profile(self) -> None:
-        self.app.push_screen(CreateProfile())
 
     @on(Activate.Succeeded)
     def activate_succeeded(self) -> None:
