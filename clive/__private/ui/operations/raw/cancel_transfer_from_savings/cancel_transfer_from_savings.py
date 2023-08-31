27612c17d7d168a69cd795b5ec0bcb5786349b21
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-08-31 13:19:19 +0200

    Remove raw-part of cancel_transfer_from_savings -> unused now

--- clive/__private/ui/operations/raw/cancel_transfer_from_savings/cancel_transfer_from_savings.py
+++ clive/__private/ui/operations/raw/cancel_transfer_from_savings/cancel_transfer_from_savings.py
@@ -3,17 +3,13 @@ from __future__ import annotations
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from textual import on
 from textual.containers import Grid, ScrollableContainer
-from textual.widgets import Input, Pretty, Static
+from textual.widgets import Static
 
-from clive.__private.core.get_default_from_model import get_default_from_model
 from clive.__private.ui.operations.raw_operation_base_screen import RawOperationBaseScreen
 from clive.__private.ui.widgets.big_title import BigTitle
 from clive.__private.ui.widgets.ellipsed_static import EllipsedStatic
-from clive.__private.ui.widgets.inputs.id_input import IdInput
 from clive.__private.ui.widgets.inputs.input_label import InputLabel
-from clive.__private.ui.widgets.view_bag import ViewBag
 from clive.models import Asset
 from schemas.operations import CancelTransferFromSavingsOperation
 
@@ -23,83 +19,45 @@ if TYPE_CHECKING:
     from schemas.database_api.fundaments_of_reponses import SavingsWithdrawalsFundament
 
 
-class Body(Grid):
-    """Content that includes from and request_id parameter. In savings tab also parameters of transfer."""
+class CancelTransferParameters(Grid):
+    """Content that includes from and request_id parameter - cancel transfer from savings parameters."""
 
 
 class FromSavingsTransferParameters(Grid):
     """Content that includes data about transfer from savings when cancelling it by button."""
 
 
-class PlaceTaker(Static):
-    """Use to ensure proper display of the grid."""
-
-
 class CancelTransferFromSavings(RawOperationBaseScreen):
-    def __init__(self, cancelling_transfer: SavingsWithdrawalsFundament[Asset.Hive, Asset.Hbd] | None = None) -> None:
-        if cancelling_transfer:
-            self.__request_id = cancelling_transfer.request_id
-            self.__to_account = cancelling_transfer.to
-            self.__amount = cancelling_transfer.amount
-            self.__memo = cancelling_transfer.memo
-            self.__realized_on = datetime.strftime(cancelling_transfer.complete, "%Y-%m-%dT%H:%M:%S")
-        self.__cancelling_transfer = cancelling_transfer
+    def __init__(self, transfer: SavingsWithdrawalsFundament[Asset.Hive, Asset.Hbd]) -> None:
         super().__init__()
-
-        if self.__cancelling_transfer is None:
-            default_request_id = get_default_from_model(CancelTransferFromSavingsOperation, "request_id", int)
-            self.__request_id_input = IdInput(label="request id", value=default_request_id)
+        self.__transfer = transfer
 
     def create_left_panel(self) -> ComposeResult:
-        with ViewBag():
-            yield BigTitle("Cancel transfer from savings")
-            with ScrollableContainer(), Body():
+        yield BigTitle("Cancel transfer")
+        with ScrollableContainer():
+            with CancelTransferParameters():
                 yield InputLabel("from")
                 yield EllipsedStatic(self.app.world.profile_data.working_account.name, classes="parameters-label")
-                if self.__cancelling_transfer is None:
-                    yield from self.__request_id_input.compose()
-                    yield PlaceTaker()
-                    yield Static("Data of the searched transfer:", id="transfer-searching-label")
-                    yield PlaceTaker()
-                    yield Pretty("No transfer with the specified request_id")
-                else:
-                    yield InputLabel("request id")
-                    yield EllipsedStatic(str(self.__request_id), classes="parameters-label")
-            if self.__cancelling_transfer is not None:
-                with FromSavingsTransferParameters():
-                    yield Static("to-account", id="to-column")
-                    yield Static("realized-on (UTC)", id="realized-column")
-                    yield Static("amount", id="amount-column")
-                    yield Static("memo", id="memo-column")
-                    yield Static(self.__to_account, classes="transfer-parameters")
-                    yield Static(self.__realized_on, classes="transfer-parameters")
-                    yield Static(Asset.to_legacy(self.__amount), classes="transfer-parameters")
-                    yield Static(self.__memo, classes="transfer-parameters")
-
-    @on(Input.Changed)
-    def find_typed_id(self, event: Input.Changed) -> None:
-        transfer: SavingsWithdrawalsFundament[Asset.Hive, Asset.Hbd] = SavingsWithdrawalsFundament[
-            Asset.Hive, Asset.Hbd
-        ](id_=1, to="bob", from_="alice", memo="", request_id=0, amount=Asset.hbd(1), complete="1970-01-01T00:00:00")
-        # TODO: transfer will be searched in database api, .find_savings_withdrawals
-
-        transfer_pretty: Pretty = self.query_one(Pretty)
-        if str(transfer.request_id) == event.value:
-            transfer_pretty.update(
-                {
-                    "to": transfer.to,
-                    "amount": Asset.to_legacy(transfer.amount),
-                    "realized_on": datetime.strftime(transfer.complete, "%Y-%m-%dT%H:%M:%S"),
-                }
-            )
-            transfer_pretty.update("No transfer with the specified request_id")
+                yield InputLabel("request id")
+                yield EllipsedStatic(str(self.__transfer.request_id), classes="parameters-label")
+            with FromSavingsTransferParameters():
+                yield Static("to-account", id="to-column")
+                yield Static("realized-on (UTC)", id="realized-column")
+                yield Static("amount", id="amount-column")
+                yield Static("memo", id="memo-column")
+                yield Static(self.__transfer.to, classes="transfer-parameters")
+                yield Static(self.__realized_on, classes="transfer-parameters")
+                yield Static(Asset.to_legacy(self.__transfer.amount), classes="transfer-parameters")
+                yield Static(self.__transfer.memo, classes="transfer-parameters")
+            yield Static()
 
     def _create_operation(self) -> CancelTransferFromSavingsOperation | None:
-        request_id = self.__request_id_input.value
-        if not request_id:
-            return None
-
+        request_id = self.__transfer.request_id
         return CancelTransferFromSavingsOperation(
             from_=self.app.world.profile_data.working_account.name,
             request_id=request_id,
         )
+
+    @property
+    def __realized_on(self) -> str:
+        return datetime.strftime(self.__transfer.complete, "%Y-%m-%dT%H:%M:%S")
