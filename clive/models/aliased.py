b8f6879933531183e5fcff53ffe8c07fef5a5ed4
Mateusz Żebrak <mzebrak@syncad.com>
2023-09-18 09:03:40 +0200

    Adjust imports from schemas

--- clive/models/aliased.py
+++ clive/models/aliased.py
@@ -3,19 +3,21 @@ from __future__ import annotations
 from schemas.__private.hive_fields_basic_schemas import AccountName as SchemasAccountName
 from schemas.__private.hive_fields_basic_schemas import AssetBase as SchemasAssetBase
 from schemas.__private.hive_fields_custom_schemas import Signature as SchemasSignature
+from schemas.__private.operation import Operation as SchemasBaseOperationType
 from schemas.__private.operation_objects import Hf26ApiOperationObject, Hf26ApiVirtualOperationObject
-from schemas.__private.operations import (
+from schemas.__private.virtual_operation import VirtualOperation as SchemasBaseVirtualOperationType
+from schemas.operations import (
+    AnyOperation,
     Hf26OperationRepresentationType,
-    Hf26OperationType,
-    Hf26VirtualOperationRepresentationType,
-    Hf26VirtualOperationType,
 )
-from schemas.__private.preconfigured_base_model import Operation as SchemasBaseOperationType
-from schemas.__private.preconfigured_base_model import VirtualOperation as SchemasBaseVirtualOperationType
+from schemas.virtual_operations import (
+    AnyVirtualOperation,
+    Hf26VirtualOperationRepresentationType,
+)
 
 AssetBase = SchemasAssetBase
-Operation = Hf26OperationType
-VirtualOperation = Hf26VirtualOperationType
+Operation = AnyOperation
+VirtualOperation = AnyVirtualOperation
 OperationBaseClass = SchemasBaseOperationType
 VirtualOperationBaseClass = SchemasBaseVirtualOperationType
 OperationRepresentationType = Hf26OperationRepresentationType
