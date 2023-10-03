ea1ce0093653d23f0bb6d62d2ffe26560a7f5d60
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-10-03 14:50:10 +0200

    Implement the Witness container - row of the witnesses table

--- clive/__private/ui/operations/governance_operations/governance_operations.py
+++ clive/__private/ui/operations/governance_operations/governance_operations.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from textual.widgets import TabbedContent
+from textual.containers import Grid, ScrollableContainer
+from textual.widgets import Label, Static, TabbedContent
 
 from clive.__private.ui.operations.operation_base_screen import OperationBaseScreen
 from clive.__private.ui.widgets.scrollable_tab_pane import ScrollableTabPane
@@ -10,6 +11,21 @@ from clive.__private.ui.widgets.witness_checkbox import WitnessCheckbox
 
 if TYPE_CHECKING:
     from textual.app import ComposeResult
+
+
+class Witness(Grid):
+    def __init__(self, rank: int, name: str, votes: str) -> None:
+        super().__init__()
+        self.__rank = rank
+        self.__name = name
+        self.__votes = votes
+
+    def compose(self) -> ComposeResult:
+        yield WitnessCheckbox()
+        yield Label(str(self.__rank), classes="witness-rank")
+        yield Label(self.__name, classes="witness-name")
+        yield Label(self.__votes, classes="witness-votes")
+        yield Label("details", classes="witness-details")
 
 
 class Proxy(ScrollableTabPane):
@@ -21,10 +37,12 @@ class Proposals(ScrollableTabPane):
 
 
 class Witnesses(ScrollableTabPane):
-    """TabPane with all content about proposals."""
+    """TabPane with all content about witnesses."""
 
     def compose(self) -> ComposeResult:
-        yield WitnessCheckbox()
+        with ScrollableContainer(id="witnesses"):
+            yield Witness(1, "blockades", "78M HP")
+            yield Static()
 
 
 class Governance(OperationBaseScreen):
