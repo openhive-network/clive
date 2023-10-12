337b64aad0aa29833bb74efd6b92ebef54a4af60
Mateusz Żebrak <mzebrak@syncad.com>
2023-10-12 09:20:53 +0200

    Implement loading transaction from file

--- clive/__private/core/commands/commands.py
+++ clive/__private/core/commands/commands.py
@@ -13,6 +13,7 @@ from clive.__private.core.commands.data_retrieval.update_node_data import Update
 from clive.__private.core.commands.deactivate import Deactivate
 from clive.__private.core.commands.fast_broadcast import FastBroadcast
 from clive.__private.core.commands.import_key import ImportKey
+from clive.__private.core.commands.load_transaction import LoadTransaction
 from clive.__private.core.commands.remove_key import RemoveKey
 from clive.__private.core.commands.save_binary import SaveToFileAsBinary
 from clive.__private.core.commands.save_json import SaveToFileAsJson
@@ -110,6 +111,9 @@ class Commands(Generic[WorldT]):
             if binary
             else SaveToFileAsJson(transaction=transaction, file_path=path)
         )
+
+    async def load_transaction_from_file(self, *, path: Path) -> CommandWithResultWrapper[Transaction]:
+        return await self.__surround_with_exception_handlers(LoadTransaction(file_path=path))
 
     async def broadcast(self, *, transaction: Transaction) -> CommandWrapper:
         return await self.__surround_with_exception_handlers(Broadcast(node=self._world.node, transaction=transaction))
