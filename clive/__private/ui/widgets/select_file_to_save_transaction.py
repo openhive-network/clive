b2c430281541774a94ff43be9d3288d024d01f00
Mateusz Żebrak <mzebrak@syncad.com>
2023-10-12 08:40:31 +0200

    Make it possible to save transaction optionally signed

--- clive/__private/ui/widgets/select_file_to_save_transaction.py
+++ clive/__private/ui/widgets/select_file_to_save_transaction.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from textual.containers import Horizontal
 from textual.widgets import Checkbox
 
 from clive.__private.ui.widgets.select_file import SelectFile
@@ -11,19 +12,42 @@ if TYPE_CHECKING:
     from textual.app import ComposeResult
 
 
+class Switches(Horizontal):
+    """Container for the switches."""
+
+    DEFAULT_CSS = """
+    Switches {
+        margin: 1 0;
+        height: auto;
+    }
+    """
+
+
 class SelectFileToSaveTransaction(SelectFile):
     @dataclass
     class Saved(SelectFile.Saved):
         """Emitted when user saves the form."""
 
         binary: bool = False
+        signed: bool = False
 
     def __init__(self) -> None:
         super().__init__(file_must_exist=False)
-        self.__checkbox = Checkbox("Binary?")
+        self.__binary_checkbox = Checkbox("Binary?")
+        self.__signed_checkbox = Checkbox("Signed?")
+
+    @property
+    def is_binary(self) -> bool:
+        return self.__binary_checkbox.value
+
+    @property
+    def is_signed(self) -> bool:
+        return self.__signed_checkbox.value
 
     def additional_content_after_input(self) -> ComposeResult:
-        yield self.__checkbox
+        with Switches():
+            yield self.__binary_checkbox
+            yield self.__signed_checkbox
 
     def _create_saved_message(self) -> Saved:
-        return self.Saved(file_path=self.file_path, binary=self.__checkbox.value)
+        return self.Saved(file_path=self.file_path, binary=self.is_binary, signed=self.is_signed)
