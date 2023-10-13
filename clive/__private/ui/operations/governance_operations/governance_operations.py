beb0c86bcee459d51dad6765e6bfec36add6a0fc
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-10-13 08:51:11 +0200

    Start using CliveTabbedContent instead of the usual TabbedContent in governance

--- clive/__private/ui/operations/governance_operations/governance_operations.py
+++ clive/__private/ui/operations/governance_operations/governance_operations.py
@@ -6,11 +6,12 @@ from textual import on
 from textual.binding import Binding
 from textual.containers import Container, Grid, Horizontal, VerticalScroll
 from textual.css.query import NoMatches
-from textual.widgets import Label, Static, TabbedContent
+from textual.widgets import Label, Static
 
 from clive.__private.ui.get_css import get_relative_css_path
 from clive.__private.ui.operations.governance_operations.governance_data import GovernanceData, GovernanceDataProvider
 from clive.__private.ui.operations.operation_base_screen import OperationBaseScreen
+from clive.__private.ui.widgets.clive_tabbed_content import CliveTabbedContent
 from clive.__private.ui.widgets.clive_widget import CliveWidget
 from clive.__private.ui.widgets.scrollable_tab_pane import ScrollableTabPane
 from clive.__private.ui.widgets.witness_checkbox import WitnessCheckbox, WitnessCheckBoxChanged
@@ -191,7 +192,7 @@ class Governance(OperationBaseScreen):
     ]
 
     def create_left_panel(self) -> ComposeResult:
-        with GovernanceDataProvider() as provider, TabbedContent():
+        with GovernanceDataProvider() as provider, CliveTabbedContent():
             yield Proxy("Proxy")
             yield Witnesses(provider, "Witnesses")
             yield Proposals("Proposals")
