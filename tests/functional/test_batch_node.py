6d9744e03f1c320f3da9219333aae204b1c28fda
Mateusz Żebrak <mzebrak@syncad.com>
2023-10-16 09:13:07 +0200

    Rename tests in test_batch_more to be more clear

--- tests/functional/test_batch_node.py
+++ tests/functional/test_batch_node.py
@@ -22,7 +22,7 @@ async def test_batch_node(init_node: tt.InitNode, world: World) -> None:  # noqa
     assert len(config.dict(by_alias=True)) != 0
 
 
-async def test_false_batch_node(init_node: tt.InitNode, world: World) -> None:  # noqa: ARG001
+async def test_batch_node_response_not_ready(init_node: tt.InitNode, world: World) -> None:  # noqa: ARG001
     async with world.node.batch() as node:
         dynamic_properties = await node.api.database_api.get_dynamic_global_properties()
 
@@ -30,7 +30,7 @@ async def test_false_batch_node(init_node: tt.InitNode, world: World) -> None:  
             _ = dynamic_properties.head_block_id
 
 
-async def test_false_batch_node_error_response(init_node: tt.InitNode, world: World) -> None:  # noqa: ARG001
+async def test_batch_node_error_response(init_node: tt.InitNode, world: World) -> None:  # noqa: ARG001
     with pytest.raises(CommunicationError, match="Invalid cast"):  # noqa: PT012
         async with world.node.batch() as node:
             await node.api.database_api.find_accounts(accounts=123)  # type: ignore
