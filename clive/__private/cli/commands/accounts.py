d7ea516c38296d37a812251c7ec3d43a5c95dab4
Mateusz Żebrak <mzebrak@syncad.com>
2023-10-17 15:51:31 +0200

    Make clive profile show command show more info (node addresses)

--- clive/__private/cli/commands/accounts.py
+++ clive/__private/cli/commands/accounts.py
@@ -8,6 +8,9 @@ from clive.__private.cli.commands.abc.profile_based_command import ProfileBasedC
 @dataclass(kw_only=True)
 class AccountsList(ProfileBasedCommand):
     async def run(self) -> None:
+        self._show_accounts_info()
+
+    def _show_accounts_info(self) -> None:
         profile = self.profile_data
         if profile.is_working_account_set():
             typer.echo(f"Working account: {profile.working_account.name}")
--- clive/__private/cli/commands/profile.py
+++ clive/__private/cli/commands/profile.py
@@ -10,7 +10,14 @@ from clive.__private.core.profile_data import ProfileAlreadyExistsError, Profile
 
 @dataclass(kw_only=True)
 class ProfileShow(AccountsList):
-    pass
+    async def run(self) -> None:
+        self._show_profile_info()
+        self._show_accounts_info()
+
+    def _show_profile_info(self) -> None:
+        profile = self.profile_data
+        typer.echo(f"Node address: {profile._node_address}")
+        typer.echo(f"Backup node addresses: {[str(url) for url in profile.backup_node_addresses]}")
 
 
 @dataclass(kw_only=True)
