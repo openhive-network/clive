a4941046585625c2d73df398286962ce32ac5e27
Mateusz Żebrak <mzebrak@syncad.com>
2023-10-17 09:59:36 +0200

    Raise TransactionAlreadySignedError from the Sign command

--- clive/__private/core/commands/sign.py
+++ clive/__private/core/commands/sign.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from clive.__private.core.commands.abc.command import CommandError
 from clive.__private.core.commands.abc.command_in_active import CommandInActive
 from clive.__private.core.commands.abc.command_with_result import CommandWithResult
 from clive.__private.core.iwax import calculate_sig_digest
@@ -13,6 +14,14 @@ if TYPE_CHECKING:
     from clive.__private.core.keys import PublicKey
 
 
+class SignCommandError(CommandError):
+    pass
+
+
+class TransactionAlreadySignedError(SignCommandError):
+    pass
+
+
 @dataclass(kw_only=True)
 class Sign(CommandInActive, CommandWithResult[Transaction]):
     beekeeper: Beekeeper
@@ -21,6 +30,9 @@ class Sign(CommandInActive, CommandWithResult[Transaction]):
     chain_id: str
 
     async def _execute(self) -> None:
+        if self.transaction.is_signed():
+            raise TransactionAlreadySignedError(self, "Transaction is already signed!")
+
         sig_digest = calculate_sig_digest(self.transaction, self.chain_id)
         result = await self.beekeeper.api.sign_digest(sig_digest=sig_digest, public_key=self.key.value)
         self.transaction.signatures = [Signature(result.signature)]
