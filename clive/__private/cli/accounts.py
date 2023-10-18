03e3f5fc9d05b97101772fadf8bbbf50c5fea669
Mateusz Żebrak <mzebrak@syncad.com>
2023-10-18 10:59:17 +0200

    Add `clive accounts working set` command

--- clive/__private/cli/accounts.py
+++ clive/__private/cli/accounts.py
@@ -4,6 +4,10 @@ from clive.__private.cli.common.with_profile import WithProfile
 
 accounts = typer.Typer(name="accounts", help="Manage your working/watched account(s).")
 
+working = typer.Typer(name="working", help="Manage your working account.")
+
+accounts.add_typer(working)
+
 
 @accounts.command(name="list")
 @WithProfile.decorator
@@ -13,3 +17,16 @@ async def list_(ctx: typer.Context) -> None:
 
     common = WithProfile(**ctx.params)
     await AccountsList(profile_data=common.profile_data).run()
+
+
+@working.command(name="set")
+@WithProfile.decorator
+async def set_(
+    ctx: typer.Context,
+    account_name: str = typer.Option(..., help="The name of the account to set.", show_default=False),
+) -> None:
+    """Set the working account."""
+    from clive.__private.cli.commands.accounts import AccountsWorkingSet
+
+    common = WithProfile(**ctx.params)
+    await AccountsWorkingSet(profile_data=common.profile_data, account_name=account_name).run()
--- clive/__private/cli/commands/accounts.py
+++ clive/__private/cli/commands/accounts.py
@@ -1,8 +1,11 @@
+import errno
 from dataclasses import dataclass
 
 import typer
 
 from clive.__private.cli.commands.abc.profile_based_command import ProfileBasedCommand
+from clive.__private.cli_error import CLIError
+from clive.__private.storage.accounts import WorkingAccount
 
 
 @dataclass(kw_only=True)
@@ -18,3 +21,14 @@ class AccountsList(ProfileBasedCommand):
             typer.echo("Working account is not set.")
         typer.echo(f"Watched accounts: {[account.name for account in profile.watched_accounts]}")
         typer.echo(f"Known accounts: {[account.name for account in profile.known_accounts]}")
+
+
+@dataclass(kw_only=True)
+class AccountsWorkingSet(ProfileBasedCommand):
+    account_name: str
+
+    async def run(self) -> None:
+        if self.profile_data.is_working_account_set():
+            raise CLIError("Working account is already set.", errno.EEXIST)
+
+        self.profile_data.working_account = WorkingAccount(name=self.account_name)
