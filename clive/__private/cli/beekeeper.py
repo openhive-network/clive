81f195a91f7b9b2208150ebc861adde7b6917be7
Mateusz Żebrak <mzebrak@syncad.com>
2023-10-18 13:31:00 +0200

    Add `clive beekeeper import-key` command

--- clive/__private/cli/beekeeper.py
+++ clive/__private/cli/beekeeper.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
 import typer
 
+from clive.__private.cli.common import WithWorld, options
 from clive.__private.cli.common.with_beekeeper import WithBeekeeper
 from clive.__private.core._async import asyncio_run
 
@@ -32,3 +35,25 @@ def close() -> None:
     from clive.__private.cli.commands.beekeeper import BeekeeperClose
 
     asyncio_run(BeekeeperClose().run())
+
+
+@beekeeper.command()
+@WithWorld.decorator()
+async def import_key(
+    ctx: typer.Context,
+    key: str = typer.Option(
+        ..., help="The key to import. This can be a path to a file or a key itself.", show_default=False
+    ),
+    alias: Optional[str] = typer.Option(
+        None,
+        help="The alias to use for the key. (If not given, calculated public key will be used)",
+        show_default=False,
+    ),
+    password: str = options.password_option,
+    beekeeper_remote: Optional[str] = options.beekeeper_remote_option,  # noqa: ARG001 # used in WithWorld
+) -> None:
+    """Import a key into the Beekeeper, and make it ready to use for Clive."""
+    from clive.__private.cli.commands.beekeeper import BeekeeperImportKey
+
+    common = WithWorld(**ctx.params)
+    await BeekeeperImportKey(world=common.world, password=password, key_or_path=key, alias=alias).run()
--- clive/__private/cli/commands/beekeeper.py
+++ clive/__private/cli/commands/beekeeper.py
@@ -4,12 +4,21 @@ import os
 import signal
 import time
 from dataclasses import dataclass
+from pathlib import Path
 
 import typer
 
 from clive.__private.cli.commands.abc.external_cli_command import ExternalCLICommand
+from clive.__private.cli.commands.abc.world_based_command import WorldBasedCommand
 from clive.__private.cli_error import CLIError
 from clive.__private.core.beekeeper import Beekeeper
+from clive.__private.core.commands.activate import ActivateInvalidPasswordError, WalletDoesNotExistsError
+from clive.__private.core.keys import (
+    KeyAliasAlreadyInUseError,
+    PrivateKey,
+    PrivateKeyAliased,
+    PrivateKeyInvalidFormatError,
+)
 
 
 @dataclass(kw_only=True)
@@ -99,3 +108,51 @@ class BeekeeperClose(ExternalCLICommand):
             if err.errno == errno.ESRCH:
                 return False
         return True
+
+
+@dataclass(kw_only=True)
+class BeekeeperImportKey(WorldBasedCommand):
+    password: str
+    key_or_path: str | Path
+    """str might be a path to a file or a private key value."""
+
+    alias: str | None = None
+
+    @property
+    def private_key_aliased(self) -> PrivateKeyAliased:
+        key_or_path = Path(self.key_or_path)
+
+        if key_or_path.is_file():
+            private_key = PrivateKey.from_file(key_or_path)
+        else:
+            try:
+                private_key = PrivateKey(value=str(self.key_or_path))
+            except PrivateKeyInvalidFormatError as error:
+                raise CLIError(str(error), errno.EINVAL) from None
+
+        alias = self.alias if self.alias else private_key.calculate_public_key().value
+
+        return private_key.with_alias(alias)
+
+    async def run(self) -> None:
+        profile_data = self.world.profile_data
+        if not profile_data.is_working_account_set():
+            raise CLIError("Working account is not set", errno.ENOENT)
+
+        typer.echo("Importing key...")
+
+        try:
+            profile_data.working_account.keys.add_to_import(self.private_key_aliased)
+        except KeyAliasAlreadyInUseError as error:
+            raise CLIError(str(error), errno.EEXIST) from None
+
+        try:
+            await self.world.commands.activate(password=self.password)
+        except ActivateInvalidPasswordError:
+            raise CLIError("Invalid password.") from None
+        except WalletDoesNotExistsError:
+            raise CLIError("Wallet does not exists.") from None
+
+        await self.world.commands.sync_data_with_beekeeper()
+
+        typer.echo("Key imported.")
