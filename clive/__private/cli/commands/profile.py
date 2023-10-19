0b052a036d3861152c6c3869d18e42b006f35187
Mateusz Żebrak <mzebrak@syncad.com>
2023-10-19 08:44:16 +0200

    Show profile name in clive profile show command

--- clive/__private/cli/commands/profile.py
+++ clive/__private/cli/commands/profile.py
@@ -18,6 +18,7 @@ class ProfileShow(AccountsList):
 
     def _show_profile_info(self) -> None:
         profile = self.profile_data
+        typer.echo(f"Profile name: {profile.name}")
         typer.echo(f"Node address: {profile.node_address}")
         typer.echo(f"Backup node addresses: {[str(url) for url in profile.backup_node_addresses]}")
 
