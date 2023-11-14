c269c64f939d151e0d1e626ba7e648447aeb8855
Mateusz Żebrak <mzebrak@syncad.com>
2023-11-14 15:42:18 +0100

    Make worldbasedcommand inherit from beekeeperbasedcommand

--- clive/__private/cli/commands/abc/world_based_command.py
+++ clive/__private/cli/commands/abc/world_based_command.py
@@ -1,15 +1,14 @@
 from abc import ABC
 from dataclasses import dataclass
 
-import typer
-
-from clive.__private.cli.commands.abc.external_cli_command import ExternalCLICommand
+from clive.__private.cli.commands.abc.beekeeper_based_command import BeekeeperBasedCommand
+from clive.__private.core.beekeeper import Beekeeper
 from clive.__private.core.world import TyperWorld, World
 from clive.core.url import Url
 
 
 @dataclass(kw_only=True)
-class WorldBasedCommand(ExternalCLICommand, ABC):
+class WorldBasedCommand(BeekeeperBasedCommand, ABC):
     """A command that requires a world."""
 
     profile_name: str
@@ -25,15 +24,8 @@ class WorldBasedCommand(ExternalCLICommand, ABC):
         return self._world
 
     @property
-    def beekeeper_remote_url(self) -> Url | None:
-        if self.beekeeper_remote is None:
-            return None
-        if isinstance(self.beekeeper_remote, Url):
-            return self.beekeeper_remote
-        return Url.parse(self.beekeeper_remote)
-
-    async def _run(self) -> None:
-        """The actual implementation of the command."""
+    def beekeeper(self) -> Beekeeper:
+        return self.world.beekeeper
 
     async def run(self) -> None:
         if self.use_beekeeper:
@@ -46,12 +38,3 @@ class WorldBasedCommand(ExternalCLICommand, ABC):
         ) as world:
             self._world = world
             await self._run()
-
-    def _print_launching_beekeeper(self) -> None:
-        message = (
-            "Launching beekeeper..."
-            if not self.beekeeper_remote_url
-            else f"Using beekeeper at {self.beekeeper_remote_url}"
-        )
-
-        typer.echo(message)
