913285b8803e9a2d0ed7a4416cd60845fcfe9759
Mateusz Żebrak <mzebrak@syncad.com>
2023-12-01 12:19:23 +0100

    Refactor test_transaction_status testsn

--- tests/functional/commands/test_transaction_status.py
+++ tests/functional/commands/test_transaction_status.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from clive.__private.core.keys.keys import PrivateKeyAliased
+from clive.__private.core.keys import PrivateKeyAliased
 from clive.__private.logger import logger
 from clive.exceptions import CommunicationError
 from clive.models import Asset
@@ -26,35 +26,26 @@ async def test_transaction_status_in_blockchain(
             key_to_import=PrivateKeyAliased(value=str(init_node_extra_apis.config.private_key[0]), alias="some-alias")
         )
     ).result_or_raise
-
     operation = TransferOperation(from_="initminer", to="null", amount=Asset.hive(2), memo="for testing")
-    transaction_without_signature = (
-        await world.commands.build_transaction(content=operation, update_metadata=True)
-    ).result_or_raise
-    transaction = (
-        await world.commands.sign(
-            transaction=transaction_without_signature,
-            sign_with=pubkey,
-        )
-    ).result_or_raise
-
-    await world.commands.broadcast(transaction=transaction)
+    transaction = (await world.commands.fast_broadcast(content=operation, sign_with=pubkey)).result_or_raise
     init_node_extra_apis.wait_number_of_blocks(1)
 
     transaction_id = transaction.with_hash().transaction_id
 
-    # ACT & ASSERT
-    status = await world.commands.find_transaction(transaction_id=transaction_id)
-    result = status.result_or_raise
+    # ACT
+    result = (await world.commands.find_transaction(transaction_id=transaction_id)).result_or_raise
     logger.info(f"{result}")
+
+    # ASSERT
     assert "reversible" in result.status
 
 
 async def test_transaction_status_unknown(world: World, init_node_extra_apis: tt.InitNode) -> None:  # noqa: ARG001
-    # ACT & ASSERT
-    status = await world.commands.find_transaction(transaction_id="deadbeef")
-    result = status.result_or_raise
+    # ACT
+    result = (await world.commands.find_transaction(transaction_id="deadbeef")).result_or_raise
     logger.info(f"{result}")
+
+    # ASSERT
     assert "unknown" in result.status
 
 
