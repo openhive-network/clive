1eb80cee5f70352a03b39ccf6e39800b5a524914
Mateusz Żebrak <mzebrak@syncad.com>
2023-12-04 12:34:34 +0100

    Update transaction from file metadata if not set so the updated values would be already displayed on TransactionSummary

--- clive/__private/ui/transaction_summary/from_file.py
+++ clive/__private/ui/transaction_summary/from_file.py
@@ -33,8 +33,16 @@ class AlreadySignedHint(Label):
 
 class TransactionSummaryFromFile(TransactionSummaryCommon):
     def __init__(self, transaction: Transaction, file_path: Path) -> None:
-        super().__init__(transaction)
+        super().__init__()
+        self.__transaction = transaction
         self.__file_path = file_path
+
+    async def _initialize_transaction(self) -> Transaction:
+        transaction = self.__transaction
+        if not transaction.is_tapos_set():
+            self.notify("TaPoS metadata was not set, updating automatically...")
+            await self.app.world.commands.update_transaction_metadata(transaction=transaction)
+        return transaction
 
     def action_save(self) -> None:
         self.app.push_screen(SelectFileToSaveTransaction())
@@ -43,8 +51,8 @@ class TransactionSummaryFromFile(TransactionSummaryCommon):
         return f"(Loaded from [blue]{self.__file_path}[/])"
 
     def _actions_container_content(self) -> ComposeResult:
-        if self.transaction.is_signed():
-            yield AlreadySignedHint(self.transaction)
+        if self.__transaction.is_signed():
+            yield AlreadySignedHint(self.__transaction)
         else:
             yield KeyHint("Sign with key:")
             yield self._select_key
