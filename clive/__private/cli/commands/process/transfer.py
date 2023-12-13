dfeaf603d0ddaa90d61f2915a427a51a77711bef
Marcin Sobczyk <msobczyk@syncad.com>
2023-12-13 18:04:40 +0100

    Add optional --from option in cli process transfer

--- clive/__private/cli/commands/process/transfer.py
+++ clive/__private/cli/commands/process/transfer.py
@@ -7,13 +7,14 @@ from schemas.operations import TransferOperation
 
 @dataclass(kw_only=True)
 class Transfer(OperationCommand):
+    from_account: str
     to: str
     amount: str
     memo: str
 
     async def _create_operation(self) -> TransferOperation:
         return TransferOperation(
-            from_=self.world.profile_data.working_account.name,
+            from_=self.from_account,
             to=self.to,
             amount=Asset.from_legacy(self.amount.upper()),
             memo=self.memo,
