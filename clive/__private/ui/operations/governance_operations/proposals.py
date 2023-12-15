ff1e962366d13937bd77cc350a3b8774d551c8bd
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-12-15 15:42:16 +0100

    Creation of a mechanism for split proposals

--- clive/__private/ui/operations/governance_operations/proposals.py
+++ clive/__private/ui/operations/governance_operations/proposals.py
@@ -481,45 +481,41 @@ class Proposals(TabPane, OperationActionBindings):
         )
 
     def _create_operations(self) -> list[Operation] | None:
-        operations_to_perform = self.app.query_one(ProposalsActions).actions_to_perform
-        proposals_to_vote = [proposal_id for proposal_id, approve in operations_to_perform.items() if approve]
-        proposals_to_unvote = [proposal_id for proposal_id, approve in operations_to_perform.items() if not approve]
-
-        if max(len(proposals_to_vote), len(proposals_to_unvote)) > MAX_NUMBER_OF_PROPOSAL_IDS_IN_SINGLE_OPERATION:
-            self.notify(
-                (
-                    "The number of proposals to (un)vote for in single operation is restricted to"
-                    f" {MAX_NUMBER_OF_PROPOSAL_IDS_IN_SINGLE_OPERATION}"
-                ),
-                severity="error",
-            )
-            return None
-
         working_account_name = self.app.world.profile_data.working_account.name
 
-        operations_to_return: list[Operation] = []
+        batched_proposals_ids_to_unvote = self.__split_proposals(approve=False)
+        batched_proposals_ids_to_vote = self.__split_proposals(approve=True)
 
-        if not proposals_to_vote and not proposals_to_unvote:
+        if not batched_proposals_ids_to_unvote and not batched_proposals_ids_to_vote:
             return None
 
-        if proposals_to_vote:
-            proposals_to_vote.sort()
-            vote_operation = UpdateProposalVotesOperation(
+        return self.__create_vote_operations(
+            batched_proposals_ids_to_vote, working_account_name, approve=True
+        ) + self.__create_vote_operations(
+            batched_proposals_ids_to_unvote, working_account_name, approve=False
+        )  # type: ignore[return-value]
+
+    def __create_vote_operations(
+        self, batched_proposal_ids: list[list[int]], working_account_name: str, approve: bool
+    ) -> list[UpdateProposalVotesOperation]:
+        return [
+            UpdateProposalVotesOperation(
                 voter=working_account_name,
-                proposal_ids=proposals_to_vote,
-                approve=True,
+                proposal_ids=proposal_ids,
+                approve=approve,
                 extensions=[],
             )
-            operations_to_return.append(vote_operation)
+            for proposal_ids in batched_proposal_ids
+        ]
 
-        if proposals_to_unvote:
-            proposals_to_unvote.sort()
-            unvote_operation = UpdateProposalVotesOperation(
-                voter=working_account_name,
-                proposal_ids=proposals_to_unvote,
-                approve=False,
-                extensions=[],
-            )
-            operations_to_return.append(unvote_operation)
+    def __split_proposals(self, approve: bool = True) -> list[list[int]]:
+        operations_to_perform = self.app.query_one(ProposalsActions).actions_to_perform
+        proposals_ids_to_return = [
+            proposal_id for proposal_id, action_approve in operations_to_perform.items() if action_approve == approve
+        ]
+        proposals_ids_to_return.sort()
 
-        return operations_to_return
+        return [
+            proposals_ids_to_return[i : i + MAX_NUMBER_OF_PROPOSAL_IDS_IN_SINGLE_OPERATION]
+            for i in range(0, len(proposals_ids_to_return), MAX_NUMBER_OF_PROPOSAL_IDS_IN_SINGLE_OPERATION)
+        ]
