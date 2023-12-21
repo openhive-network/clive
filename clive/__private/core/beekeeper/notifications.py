967f572eab64be9f35ee47e17c4d21463f21fe89
Wiesław Kędzierski <wkedzierski@syncad.com>
2023-12-21 14:47:18 +0100

    Add BeekeeperNotificationHandler for incomming notifications.

--- clive/__private/core/beekeeper/notifications.py
+++ clive/__private/core/beekeeper/notifications.py
@@ -20,6 +20,80 @@ class WalletClosingListener(Protocol):
 
 
 class BeekeeperNotificationsServer:
+    class BeekeeperNotificationsHandler:
+        def __init__(self, beekeeper_ofitication_erver: BeekeeperNotificationsServer):
+            self.beekeeperNotificationServer = beekeeper_ofitication_erver
+            self.__notifications = {
+                "webserver listening": self.__webserver_listening_notification,
+                "hived_status": self.__hived_status_notification,
+                "Opening beekeeper failed": self.__opening_beekeeper_failed_notification,
+                "Attempt of closing all wallets": self.__attempt_of_closing_all_wallets_notification,
+            }
+
+        def notify(self, message: JsonT) -> None:
+            name = message["name"]
+            self.notification = self.__notifications.get(name, self.__unknown_notification)
+            self.notification(message=message)
+
+        def __webserver_listening_notification(self, message: JsonT) -> None:
+            details = message["value"]
+            if details["type"] == "HTTP":
+                self.beekeeperNotificationServer.beekeeper_webserver_http_endpoint = self.__parse_endpoint_notification(
+                    details
+                )
+                logger.debug(
+                    "Got notification with webserver http address on:"
+                    f" {self.beekeeperNotificationServer.beekeeper_webserver_http_endpoint}"
+                )
+                self.beekeeperNotificationServer.http_listening_event.set()
+            else:
+                logger.error(f'Received unsupported type : {details["type"]}')
+
+        def __hived_status_notification(self, message: JsonT) -> None:
+            details = message["value"]
+            if details["current_status"] == "signals attached":
+                logger.debug("Beekeeper reports to be ready")
+                self.beekeeperNotificationServer.ready.set()
+            elif details["current_status"] == "interrupted":
+                logger.debug("Beekeeper has terminated.")
+                self.beekeeperNotificationServer.terminated.set()
+            elif details["current_status"] == "beekeeper is starting":
+                logger.debug("Beekeeper is starting.")
+            else:
+                logger.error(f'Received unsupported hived_status : {details["current_status"]}')
+
+        def __opening_beekeeper_failed_notification(self, message: JsonT) -> None:
+            details = message["value"]
+            details = details["connection"]
+            assert details["type"] == "HTTP"
+            self.beekeeperNotificationServer.beekeeper_webserver_http_endpoint = self.__parse_endpoint_notification(
+                details
+            )
+            logger.debug(
+                "Got notification with webserver http address, but beekeeper failed when opening:"
+                f" {self.beekeeperNotificationServer.beekeeper_webserver_http_endpoint}"
+            )
+            self.beekeeperNotificationServer.opening_beekeeper_failed.set()
+
+        def __attempt_of_closing_all_wallets_notification(self, message: JsonT) -> None:
+            details = message["value"]
+            logger.debug("Got notification about closing all wallets")
+            if self.beekeeperNotificationServer.__is_tracked_wallet_closing(details):
+                self.beekeeperNotificationServer.__notify_listeners_about_wallets_closing()
+            else:
+                logger.debug("The tracked wallet is not closing, ignoring notification")
+
+        def __unknown_notification(self, message: JsonT) -> None:
+            name = message["name"]
+            logger.error(f"Received unsupported notification `{name}`.")
+
+        @classmethod
+        def __parse_endpoint_notification(cls, details: dict[str, str]) -> Url:
+            return Url.parse(
+                f"{details['address'].replace('0.0.0.0', '127.0.0.1')}:{details['port']}",
+                protocol=details["type"].lower(),
+            )
+
     def __init__(
         self, token_cb: Callable[[], str], notify_closing_wallet_name_cb: Callable[[], str] | None = None
     ) -> None:
@@ -31,15 +105,20 @@ class BeekeeperNotificationsServer:
         self.opening_beekeeper_failed = Event()
         self.http_listening_event = Event()
         self.ready = Event()
+        self.terminated = Event()
         self.__beekeeper_webserver_http_endpoint_from_notification: Url | None = None
         self.__wallet_closing_listeners: set[WalletClosingListener] = set()
-        self.__http_endpoint: Url | None = None
+        self.__beekeeper_notification_handler = BeekeeperNotificationsServer.BeekeeperNotificationsHandler(self)
 
     @property
     def beekeeper_webserver_http_endpoint(self) -> Url:
         message = "Beekeeper webserver HTTP endpoint is not known yet"
         assert self.__beekeeper_webserver_http_endpoint_from_notification is not None, message
         return self.__beekeeper_webserver_http_endpoint_from_notification
+
+    @beekeeper_webserver_http_endpoint.setter
+    def beekeeper_webserver_http_endpoint(self, value: Url) -> None:
+        self.__beekeeper_webserver_http_endpoint_from_notification = value
 
     @property
     def http_endpoint(self) -> Url:
@@ -52,40 +131,7 @@ class BeekeeperNotificationsServer:
 
     def notify(self, message: JsonT) -> None:
         logger.info(f"Got notification: {message}")
-        name = message["name"]
-        details = message["value"]
-
-        if name == "webserver listening":
-            if details["type"] == "HTTP":
-                self.__beekeeper_webserver_http_endpoint_from_notification = self.__parse_endpoint_notification(details)
-                logger.debug(
-                    f"Got notification with webserver http address on: {self.beekeeper_webserver_http_endpoint}"
-                )
-                self.http_listening_event.set()
-        elif name == "hived_status" and details["current_status"] == "signals attached":
-            logger.debug("Beekeeper reports to be ready")
-            self.ready.set()
-        elif name == "Opening beekeeper failed":
-            details = details["connection"]
-            assert details["type"] == "HTTP"
-            self.__beekeeper_webserver_http_endpoint_from_notification = self.__parse_endpoint_notification(details)
-            logger.debug(
-                "Got notification with webserver http address, but beekeeper failed when opening:"
-                f" {self.beekeeper_webserver_http_endpoint}"
-            )
-            self.opening_beekeeper_failed.set()
-        elif name == "Attempt of closing all wallets":
-            logger.debug("Got notification about closing all wallets")
-            if self.__is_tracked_wallet_closing(details):
-                self.__notify_listeners_about_wallets_closing()
-            else:
-                logger.debug("The tracked wallet is not closing, ignoring notification")
-
-    @classmethod
-    def __parse_endpoint_notification(cls, details: dict[str, str]) -> Url:
-        return Url.parse(
-            f"{details['address'].replace('0.0.0.0', '127.0.0.1')}:{details['port']}", protocol=details["type"].lower()
-        )
+        self.__beekeeper_notification_handler.notify(message=message)
 
     async def close(self) -> None:
         await self.server.close()
