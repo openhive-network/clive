ae5b829c457a48b56889d08a871b97a19cecf171
Marek <marek@syncad.com>
2024-01-16 16:36:39 +0000

    Move 'assert_operations_placed_in_blockchain' to clive_local_tools/tui/utils.py

--- tests/clive-local-tools/clive_local_tools/tui/utils.py
+++ tests/clive-local-tools/clive_local_tools/tui/utils.py
@@ -10,6 +10,9 @@ from clive.__private.ui.widgets.titled_label import TitledLabel
 if TYPE_CHECKING:
     from clive_local_tools.tui.types import CliveApp
 
+    from schemas.operations import AnyOperation
+    from schemas.operations.representations import HF26Representation
+
 
 def get_mode(app: CliveApp) -> str:
     """Do not call while onboarding process."""
@@ -25,3 +28,25 @@ def log_current_view(app: CliveApp, *, nodes: bool = False) -> None:
     tt.logger.debug(f"screen: {app.screen}, focused: {app.focused}")
     if nodes:
         tt.logger.debug(f'nodes: {app.query("*").nodes}')
+
+
+def assert_operations_placed_in_blockchain(
+    node: tt.InitNode, transaction_id: str, *expected_operations: AnyOperation
+) -> None:
+    transaction = node.api.account_history.get_transaction(
+        id=transaction_id, include_reversible=True  # type: ignore[call-arg] # TODO: id -> id_ after helpy bug fixed
+    )
+    operations_to_check = list(expected_operations)
+    for operation_representation in transaction.operations:
+        _operation_representation: HF26Representation[AnyOperation] = operation_representation
+        operation = _operation_representation.value
+        if operation in operations_to_check:
+            operations_to_check.remove(operation)
+
+    message = (
+        "Operations missing in blockchain.\n"
+        f"Operations: {operations_to_check}\n"
+        "were not found in the transaction:\n"
+        f"{transaction}."
+    )
+    assert not operations_to_check, message
