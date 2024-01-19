42688f218528a4d14f01484aeb5f98707dcc900b
Jakub Ziębiński <ziebinskijakub@gmail.com>
2024-01-19 09:31:09 +0100

    Rebuild the proxy tab when the proxy has been changed

--- clive/__private/ui/operations/governance_operations/proxy/proxy.py
+++ clive/__private/ui/operations/governance_operations/proxy/proxy.py
@@ -21,28 +21,18 @@ class ScrollablePart(ScrollableContainer, can_focus=False):
     pass
 
 
-class Proxy(TabPane, CliveWidget):
-    """TabPane with all content about proxy."""
+class ProxyBaseContainer(Container):
+    """Base widget for the ProxyNotSet and ProxySet container to properly catch both by the query_one."""
 
-    DEFAULT_CSS = get_css_from_relative_path(__file__)
 
-    def __init__(self, title: TextType):
-        super().__init__(title=title)
-        self.__proxy_input = AccountNameInput()
-        self.__current_proxy = self.app.world.profile_data.working_account.data.proxy
+class ProxyInput(AccountNameInput):
+    pass
 
-    @property
-    def new_proxy(self) -> str | None:
-        return self.__proxy_input.value
 
+class ProxyNotSet(ProxyBaseContainer):
     def compose(self) -> ComposeResult:
-        content = self.__compose_proxy_set if self.__current_proxy else self.__compose_proxy_not_set
-        with ScrollablePart():
-            yield from content()
-
-    def __compose_proxy_not_set(self) -> ComposeResult:
         yield AccountNameInput(label="current proxy", value="Not set", disabled=True)
-        yield self.__proxy_input
+        yield ProxyInput()
         with Container(id="set-button-container"):
             yield CliveButton("Set proxy", id_="set-proxy-button")
         yield Static(
@@ -50,12 +40,47 @@ class Proxy(TabPane, CliveWidget):
             id="proxy-set-information",
         )
 
-    def __compose_proxy_set(self) -> ComposeResult:
-        yield AccountNameInput(label="current proxy", value=self.__current_proxy, disabled=True)
-        yield self.__proxy_input
+
+class ProxySet(ProxyBaseContainer):
+    def __init__(self, current_proxy: str) -> None:
+        """
+        Initialize the ProxySet Container.
+
+        Args:
+        ----
+        current_proxy: Proxy that is currently assigned to the account.
+        """
+        super().__init__()
+        self._current_proxy = current_proxy
+
+    def compose(self) -> ComposeResult:
+        yield AccountNameInput(label="current proxy", value=self._current_proxy, disabled=True)
+        yield ProxyInput()
         with Horizontal(id="modify-proxy-buttons"):
             yield CliveButton("Change proxy", id_="set-proxy-button")
             yield CliveButton("Remove proxy", id_="remove-proxy-button")
+
+
+class Proxy(TabPane, CliveWidget):
+    """TabPane with all content about proxy."""
+
+    DEFAULT_CSS = get_css_from_relative_path(__file__)
+
+    def __init__(self, title: TextType):
+        super().__init__(title=title)
+        self._current_proxy = self.app.world.profile_data.working_account.data.proxy
+
+    def on_mount(self) -> None:
+        self.watch(self.app.world, "profile_data", self.sync_when_proxy_changed)
+
+    @property
+    def new_proxy(self) -> str | None:
+        return self.query_one(ProxyInput).value
+
+    def compose(self) -> ComposeResult:
+        content = ProxySet(self._current_proxy) if self._current_proxy else ProxyNotSet()
+        with ScrollablePart():
+            yield content
 
     @on(Button.Pressed, "#set-proxy-button")
     def set_new_proxy(self) -> None:
@@ -67,3 +92,14 @@ class Proxy(TabPane, CliveWidget):
     @on(Button.Pressed, "#remove-proxy-button")
     def remove_proxy(self) -> None:
         self.app.push_screen(AccountWitnessProxy(is_raw=False))
+
+    def sync_when_proxy_changed(self) -> None:
+        proxy_profile_data = self.app.world.profile_data.working_account.data.proxy
+        if self._current_proxy != proxy_profile_data:
+            self._current_proxy = proxy_profile_data
+
+            content = ProxySet(self._current_proxy) if self._current_proxy else ProxyNotSet()
+
+            with self.app.batch_update():
+                self.query_one(ProxyBaseContainer).remove()
+                self.query_one(ScrollablePart).mount(content)
