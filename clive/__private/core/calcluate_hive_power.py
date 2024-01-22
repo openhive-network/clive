e7a3acb1f92a2f76547ab63a1b00b7290a82fcbf
Jakub Ziębiński <ziebinskijakub@gmail.com>
2024-01-22 12:20:41 +0100

    Move calculate_hive_power and vests_to_hive methods methods to separate files

---
+++ clive/__private/core/calcluate_hive_power.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from math import ceil
+from typing import TYPE_CHECKING
+
+from clive.__private.core.vests_to_hive import vests_to_hive
+
+if TYPE_CHECKING:
+    from clive.models import Asset
+    from clive.models.aliased import DynamicGlobalProperties
+
+
+def calculate_hive_power(gdpo: DynamicGlobalProperties, vests_value: Asset.Vests | int) -> int:
+    vests_to_hive_amount = int(vests_to_hive(vests_value, gdpo).amount)
+    precision = gdpo.total_reward_fund_hive.get_asset_information().precision
+    return int(ceil(vests_to_hive_amount / (10**precision)))
