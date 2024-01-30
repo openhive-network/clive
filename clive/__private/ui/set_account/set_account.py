d38e24761f6611045c835028e8739ee602311475
Mateusz Żebrak <mzebrak@syncad.com>
2024-01-30 11:08:46 +0100

    Use new inputs on SetAccount screen

--- clive/__private/ui/set_account/set_account.py
+++ clive/__private/ui/set_account/set_account.py
@@ -11,7 +11,8 @@ from clive.__private.ui.get_css import get_relative_css_path
 from clive.__private.ui.shared.base_screen import BaseScreen
 from clive.__private.ui.shared.form_screen import FormScreen
 from clive.__private.ui.widgets.big_title import BigTitle
-from clive.__private.ui.widgets.inputs.account_name_input import AccountNameInput
+from clive.__private.ui.widgets.inputs_new.account_name_input import AccountNameInput
+from clive.__private.ui.widgets.inputs_new.clive_validated_input import CliveValidatedInputError
 from clive.exceptions import FormValidationError
 
 if TYPE_CHECKING:
@@ -24,26 +25,33 @@ class ScrollablePart(ScrollableContainer, can_focus=False):
     """All the content of the screen, excluding the title."""
 
 
+class WorkingAccountCheckbox(Checkbox):
+    def __init__(self) -> None:
+        super().__init__("Working account?", value=True)
+
+
 class SetAccount(BaseScreen, FormScreen[ProfileData]):
     CSS_PATH = [get_relative_css_path(__file__)]
 
     def __init__(self, owner: Form[ProfileData]) -> None:
         super().__init__(owner)
 
-        self.__account_name_input = AccountNameInput(placeholder="Please enter hive account name, without @")
+        self._account_name_input = AccountNameInput(
+            placeholder="Please enter hive account name, without @", include_title_in_placeholder_when_blurred=False
+        )
+        self._working_account_checkbox = WorkingAccountCheckbox()
 
     def create_main_panel(self) -> ComposeResult:
         yield BigTitle("set account name")
         with ScrollablePart():
-            yield self.__account_name_input
-            yield Checkbox("Working account?", value=True)
+            yield self._account_name_input
+            yield self._working_account_checkbox
 
     async def apply_and_validate(self) -> None:
-        with self.app.suppressed_notifications():
-            account_name = self.__account_name_input.value
-
-        if not account_name:
-            raise FormValidationError("Invalid account name")
+        try:
+            account_name = self._account_name_input.value_or_error
+        except CliveValidatedInputError as error:
+            raise FormValidationError(str(error)) from error
 
         if self.__is_working_account():
             self.context.set_working_account(account_name)
@@ -53,4 +61,4 @@ class SetAccount(BaseScreen, FormScreen[ProfileData]):
             self.context.watched_accounts.add(Account(name=account_name))
 
     def __is_working_account(self) -> bool:
-        return self.query_one(Checkbox).value
+        return self.query_one(WorkingAccountCheckbox).value
