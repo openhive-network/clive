e10d41d00d490fc2874f5b0bf82da3eb11e74786
Mateusz Żebrak <mzebrak@syncad.com>
2024-01-31 10:48:56 +0100

    Use new inputs on ConfirmWithPassowrd
    Known issue:
    - seems like ConfirmWithPassword is not working as expected
      when wrong password was given

--- clive/__private/ui/confirm_with_password/confirm_with_password.py
+++ clive/__private/ui/confirm_with_password/confirm_with_password.py
@@ -4,12 +4,13 @@ from collections.abc import Awaitable, Callable
 from typing import TYPE_CHECKING
 
 from textual.binding import Binding
-from textual.widgets import Input, Static
+from textual.widgets import Static
 
 from clive.__private.core.commands.abc.command_secured import InvalidPasswordError
 from clive.__private.ui.get_css import get_relative_css_path
 from clive.__private.ui.shared.base_screen import BaseScreen
 from clive.__private.ui.widgets.dialog_container import DialogContainer
+from clive.__private.ui.widgets.inputs.text_input import TextInput
 
 if TYPE_CHECKING:
     from textual.app import ComposeResult
@@ -33,7 +34,7 @@ class ConfirmWithPassword(BaseScreen):
     ) -> None:
         self.__result_callback = result_callback
         self.__action_name = action_name
-        self.__password_input = Input(placeholder="Password", password=True, id="password_input")
+        self._password_input = TextInput("Password", password=True, id="password_input")
         super().__init__()
 
     def create_main_panel(self) -> ComposeResult:
@@ -41,21 +42,22 @@ class ConfirmWithPassword(BaseScreen):
             yield Static("Please enter your password to continue.", id="hint")
             if self.__action_name:
                 yield Static(f"Action: {self.__action_name}", id="action")
-            yield self.__password_input
+            yield self._password_input
 
     async def action_cancel(self) -> None:
         self.app.pop_screen()
         await self.__result_callback("")
 
     async def action_confirm(self) -> None:
+        password = self._password_input.value_or_none()
+        if password is None:
+            return
+
         with self.app.batch_update():
             self.app.pop_screen()
 
             try:
-                await self.__result_callback(self.__get_password_input())
+                await self.__result_callback(password)
             except InvalidPasswordError:
                 self.app.push_screen(ConfirmWithPassword(self.__result_callback, self.__action_name))
                 self.notify("Invalid password", severity="error")
-
-    def __get_password_input(self) -> str:
-        return self.__password_input.value
