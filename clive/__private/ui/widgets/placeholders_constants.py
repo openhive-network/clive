02d2e11cb6b46f208c535858c808cfc8cd563f95
Mateusz Żebrak <mzebrak@syncad.com>
2024-01-31 15:40:03 +0100

    Remove unused placeholder constants

--- clive/__private/ui/widgets/placeholders_constants.py
+++ clive/__private/ui/widgets/placeholders_constants.py
@@ -4,21 +4,14 @@ from __future__ import annotations
 from typing import Final
 
 NUMERIC_PLACEHOLDER: Final[str] = "e.g: 1.000"
+INTEGER_PLACEHOLDER: Final[str] = "e.g: 1"
+
 ACCOUNT_NAME_PLACEHOLDER: Final[str] = "e.g: alice"
 ACCOUNT_NAME2_PLACEHOLDER: Final[str] = "e.g: bob"
 ACCOUNT_NAME_PATTERN_PLACEHOLDER: Final[str] = "part of the name e.g: ali"
+
 PROFILE_NAME_PLACEHOLDER: Final[str] = "e.g: Master"
-WITNESS_PLACEHOLDER: Final[str] = "e.g: hiveio"
-ACCOUNT_AUTHS_PLACEHOLDER: Final[str] = "e.g: alice, 1; bob, 1. Notice: split pair of values by ;"
-KEY_AUTHS_PLACEHOLDER: Final[str] = (
-    "e.g: STM6vJmrwaX5TjgTS9dPH8KsArso5m91fVodJvv91j7G765wqcNM9, 1. Notice: split pair of values by ;"
-)
-JSON_DATA_PLACEHOLDER: Final[str] = "e.g: {}"
-INTEGER_PLACEHOLDER: Final[str] = "e.g: 1"
-ID_PLACEHOLDER: Final[str] = "e.g: 1000"
-PERMLINK_PLACEHOLDER: Final[str] = "e.g: a-post-by-alice"
-DATE_PLACEHOLDER: Final[str] = "e.g: e.g: 2019-08-27T11:22:39"
-KEY_PLACEHOLDER: Final[str] = "e.g: STM8ZSyzjPm48GmUuMSRufkVYkwYbZzbxeMysAVp7KFQwbTf98TcG"
+
 MEMO_PLACEHOLDER: Final[str] = "e.g.: For the coffee!"
-PERCENT_PLACEHOLDER: Final[str] = "e.g: 10"
+
 KEY_ALIAS_PLACEHOLDER: Final[str] = "e.g: my-active-key"
