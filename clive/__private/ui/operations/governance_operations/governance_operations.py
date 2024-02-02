a54cc21610c2c7d8aa863f28132cb4719fe077af
Mateusz Żebrak <mzebrak@syncad.com>
2024-02-02 14:56:46 +0100

    Make use of new TabbedContent.active_pane property

--- clive/__private/ui/operations/governance_operations/governance_operations.py
+++ clive/__private/ui/operations/governance_operations/governance_operations.py
@@ -14,12 +14,7 @@ from clive.__private.ui.operations.operation_base_screen import OperationBaseScr
 from clive.__private.ui.widgets.clive_tabbed_content import CliveTabbedContent
 
 if TYPE_CHECKING:
-    from typing import Final
-
     from textual.app import ComposeResult
-
-WITNESSES_TAB_LABEL: Final[str] = "Witnesses"
-PROPOSALS_TAB_NAME: Final[str] = "Proposals"
 
 
 class Governance(OperationBaseScreen):
@@ -31,18 +26,19 @@ class Governance(OperationBaseScreen):
     def create_left_panel(self) -> ComposeResult:
         with WitnessesDataProvider(paused=True), ProposalsDataProvider(paused=True), CliveTabbedContent():
             yield Proxy("Proxy")
-            yield Witnesses(WITNESSES_TAB_LABEL)
-            yield Proposals(PROPOSALS_TAB_NAME)
+            yield Witnesses("Witnesses")
+            yield Proposals("Proposals")
 
     @on(CliveTabbedContent.TabActivated)
     def change_provider_status(self, event: CliveTabbedContent.TabActivated) -> None:
         witnesses_provider = self.query_one(WitnessesDataProvider)
         proposals_provider = self.query_one(ProposalsDataProvider)
 
-        if str(event.tab.label) == PROPOSALS_TAB_NAME:
+        active_pane = event.tabbed_content.active_pane
+        if isinstance(active_pane, Proposals):
             witnesses_provider.pause()
             proposals_provider.restart()
-        elif str(event.tab.label) == WITNESSES_TAB_LABEL:
+        elif isinstance(active_pane, Witnesses):
             proposals_provider.pause()
             witnesses_provider.restart()
         else:
