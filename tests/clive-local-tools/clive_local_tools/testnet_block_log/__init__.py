9e3e7f1215166adbc1bbca091a056b128e6b4102
Marcin Sobczyk <msobczyk@syncad.com>
2024-02-19 10:06:42 +0100

    add testnet_block_log constants to module __all__

--- tests/clive-local-tools/clive_local_tools/testnet_block_log/__init__.py
+++ tests/clive-local-tools/clive_local_tools/testnet_block_log/__init__.py
@@ -1,5 +1,16 @@
 from __future__ import annotations
 
+from .constants import CREATOR_ACCOUNT, PROPOSALS, WATCHED_ACCOUNTS, WITNESSES, WORKING_ACCOUNT
 from .prepared_data import get_alternate_chain_spec_path, get_block_log, get_config, get_time_offset
 
-__all__ = ["get_alternate_chain_spec_path", "get_block_log", "get_config", "get_time_offset"]
+__all__ = [
+    "CREATOR_ACCOUNT",
+    "PROPOSALS",
+    "WATCHED_ACCOUNTS",
+    "WITNESSES",
+    "WORKING_ACCOUNT",
+    "get_alternate_chain_spec_path",
+    "get_block_log",
+    "get_config",
+    "get_time_offset",
+]
