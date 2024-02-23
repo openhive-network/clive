91414c8e721a41264e51c95e7c8a9a421260ae75
Konrad Botor <kbotor@syncad.com>
2024-02-23 12:24:48 +0100

    Tag Docker image with Git tag if possible

--- scripts/build_local_instance.sh
+++ scripts/build_local_instance.sh
@@ -12,7 +12,7 @@ REGISTRY_URL="registry.gitlab.syncad.com/hive/clive"
 
 # EDIT THESE VARIABLES
 #    IMAGE could be get from prepare_hived_image job like: https://gitlab.syncad.com/hive/clive/-/jobs/787089#L342
-IMAGE="registry.gitlab.syncad.com/hive/hive/testnet-base_instance:testnet-base_instance-8233eeb01b15b2ea557e7a6d5c81f9e81b996b75"
+IMAGE="registry.gitlab.syncad.com/hive/hive/testnet-base_instance:fe0084e"
 CLIVE_VERSION="1.27.5.2"
 BUILD_TESTNET=1
 
