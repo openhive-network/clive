9c35714cd51b37884343bc0a6c07b4a9d56de6d3
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-03-22 13:45:32 +0100

    Add account list management screen

---
+++ clive/__private/ui/account_list_management/known_accounts.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from textual.widgets import TabPane
+
+
+class KnownAccounts(TabPane):
+    """TabPane used to add and delete known accounts."""
---
+++ clive/__private/ui/account_list_management/watched_accounts.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from textual.widgets import TabPane
+
+
+class WatchedAccounts(TabPane):
+    """TabPane used to add and delete watched accounts."""
