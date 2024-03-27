9e371722ebd85bcc85079384543075f6db6b0fd6
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-03-27 09:37:34 +0100

    Implementation of the watched accounts TabPane

--- clive/__private/ui/account_list_management/watched_accounts.py
+++ clive/__private/ui/account_list_management/watched_accounts.py
@@ -1,7 +1,21 @@
 from __future__ import annotations
 
-from textual.widgets import TabPane
+from clive.__private.ui.account_list_management.common.manage_accounts_tab_pane import ManageAccountsTabPane
 
 
-class WatchedAccounts(TabPane):
+class WatchedAccounts(ManageAccountsTabPane):
     """TabPane used to add and delete watched accounts."""
+
+    DEFAULT_CSS = """
+    WatchedAccounts {
+        #input-with-button {
+            CliveButton {
+                width: 10;
+                min-width: 10;
+            }
+        }
+    }
+    """
+
+    def __init__(self, title: str):
+        super().__init__(title=title, accounts_type="watched_accounts")
---
+++ clive/__private/validators/set_watched_account_validator.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Final
+
+from textual.validation import Function, ValidationResult
+
+from clive.__private.validators.account_name_validator import AccountNameValidator
+
+if TYPE_CHECKING:
+    from clive.__private.core.profile_data import ProfileData
+
+
+class SetWatchedAccountValidator(AccountNameValidator):
+    ACCOUNT_ALREADY_WATCHED_FAILURE: Final[str] = "This account is already being watched."
+    WATCH_SELF_FAILURE: Final[str] = "You cannot watch your own account."
+
+    def __init__(self, profile_data: ProfileData) -> None:
+        super().__init__()
+        self._profile_data = profile_data
+
+    def validate(self, value: str) -> ValidationResult:
+        super_result = super().validate(value)
+
+        validators = [
+            Function(self._validate_account_already_watched, self.ACCOUNT_ALREADY_WATCHED_FAILURE),
+            Function(self._validate_self_account_watched, self.WATCH_SELF_FAILURE),
+        ]
+
+        return ValidationResult.merge([super_result] + [validator.validate(value) for validator in validators])
+
+    def _validate_account_already_watched(self, value: str) -> bool:
+        return value not in [watched_account.name for watched_account in self._profile_data.watched_accounts]
+
+    def _validate_self_account_watched(self, value: str) -> bool:
+        if not self._profile_data.is_working_account_set():
+            return True
+
+        return value != self._profile_data.working_account.name
