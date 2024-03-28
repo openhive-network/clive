db51a70e0281a2e2e77aee4d443a864d83a1732b
Marek <marek@syncad.com>
2024-03-28 15:45:38 +0000

    Replace all query calls from app to screen

--- tests/clive-local-tools/clive_local_tools/tui/utils.py
+++ tests/clive-local-tools/clive_local_tools/tui/utils.py
@@ -14,7 +14,7 @@ if TYPE_CHECKING:
 def get_mode(app: CliveApp) -> str:
     """Do not call while onboarding process."""
     try:
-        widget = app.query_one("#mode-label", TitledLabel)
+        widget = app.screen.query_one("#mode-label", TitledLabel)
     except NoMatches as error:
         raise AssertionError("Mode couldn't be found. It is not available in the onboarding process.") from error
     return str(widget.value).strip()
@@ -25,12 +25,12 @@ def log_current_view(app: CliveApp, *, nodes: bool = False, source: str | None =
     source = f"{source}: " if source is not None else ""
     tt.logger.debug(f"{source}screen: {app.screen}, focused: {app.focused}")
     if nodes:
-        tt.logger.debug(f'nodes: {app.query("*").nodes}')
+        tt.logger.debug(f'nodes: {app.screen.query("*").nodes}')
 
 
 def get_profile_name(app: CliveApp) -> str:
     try:
-        widget = app.query_one("#profile-label", TitledLabel)
+        widget = app.screen.query_one("#profile-label", TitledLabel)
     except NoMatches as error:
         raise AssertionError("Profile couldn't be found. It is not available in the onboarding process.") from error
     return str(widget.value).strip()
