8494563e601fa6fae2d4dac7c4ad337544e136d4
Mateusz Żebrak <mzebrak@syncad.com>
2024-04-02 12:07:34 +0200

    Properly typehint TransferCommonOptions.amount parameter

--- clive/__private/cli/common/transfer_common_options.py
+++ clive/__private/cli/common/transfer_common_options.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
 import typer
 
@@ -6,9 +7,12 @@ from clive.__private.cli.common import options
 from clive.__private.cli.common.common_options_base import CommonOptionsBase
 from clive.__private.cli.common.options import liquid_amount_option
 
+if TYPE_CHECKING:
+    from clive.models import Asset
+
 
 @dataclass(kw_only=True)
 class TransferCommonOptions(CommonOptionsBase):
-    amount: str = liquid_amount_option
+    amount: "Asset.LiquidT" = liquid_amount_option
     memo: str = typer.Option("", help="The memo to attach to the transfer.")
     from_account: str = options.from_account_name_option
