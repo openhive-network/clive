d293d52e7cc268d86570aa38766c0df3cadc7ed0
Mateusz Żebrak <mzebrak@syncad.com>
2024-04-08 11:11:31 +0200

    Fix TUI tests conftest - correctly start on (in)active dashboard

--- tests/tui/conftest.py
+++ tests/tui/conftest.py
@@ -6,8 +6,6 @@ from typing import TYPE_CHECKING
 import pytest
 import test_tools as tt
 
-from clive.__private.core.commands.create_wallet import CreateWallet
-from clive.__private.core.commands.deactivate import Deactivate
 from clive.__private.core.keys.keys import PrivateKeyAliased
 from clive.__private.core.profile_data import ProfileData
 from clive.__private.core.world import TextualWorld
@@ -15,6 +13,7 @@ from clive.__private.storage.accounts import Account as WatchedAccount
 from clive.__private.storage.accounts import WorkingAccount
 from clive.__private.ui.app import Clive
 from clive.__private.ui.dashboard.dashboard_active import DashboardActive
+from clive.__private.ui.dashboard.dashboard_inactive import DashboardInactive
 from clive.core.url import Url
 from clive_local_tools.testnet_block_log import (
     get_alternate_chain_spec_path,
@@ -23,16 +22,16 @@ from clive_local_tools.testnet_block_log import (
     get_time_offset,
 )
 from clive_local_tools.testnet_block_log.constants import WATCHED_ACCOUNTS, WORKING_ACCOUNT
-from clive_local_tools.tui.activate import activate
 from clive_local_tools.tui.clive_quit import clive_quit
 from clive_local_tools.tui.constants import TUI_TESTS_PATCHED_NOTIFICATION_TIMEOUT
 from clive_local_tools.tui.textual_helpers import wait_for_screen
-from clive_local_tools.tui.utils import get_mode
 
 if TYPE_CHECKING:
     from collections.abc import AsyncIterator
 
     from clive_local_tools.tui.types import ClivePilot
+
+    PreparedTuiEnv = tuple[tt.RawNode, tt.Wallet, ClivePilot]
 
 
 @pytest.fixture(autouse=True)
@@ -40,7 +39,7 @@ def _patch_notification_timeout(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setattr(Clive, "notify", partialmethod(Clive.notify, timeout=TUI_TESTS_PATCHED_NOTIFICATION_TIMEOUT))
 
 
-def prepare_profile() -> None:
+def _prepare_profile() -> None:
     ProfileData(
         WORKING_ACCOUNT.name,
         working_account=WorkingAccount(name=WORKING_ACCOUNT.name),
@@ -48,15 +47,10 @@ def prepare_profile() -> None:
     ).save()
 
 
-async def prepare_wallet(world: TextualWorld) -> None:
-    password = await CreateWallet(
-        app_state=world.app_state,
-        beekeeper=world.beekeeper,
-        wallet=WORKING_ACCOUNT.name,
-        password=WORKING_ACCOUNT.name,
-    ).execute_with_result()
-
+async def _prepare_beekeeper_wallet(world: TextualWorld) -> None:
+    password = (await world.commands.create_wallet(password=WORKING_ACCOUNT.name)).result_or_raise
     tt.logger.info(f"password for {WORKING_ACCOUNT.name} is: `{password}`")
+
     world.profile_data.working_account.keys.add_to_import(
         PrivateKeyAliased(value=WORKING_ACCOUNT.private_key, alias=f"{WORKING_ACCOUNT.name}_key")
     )
@@ -64,15 +58,7 @@ async def prepare_wallet(world: TextualWorld) -> None:
 
 
 @pytest.fixture()
-async def world() -> AsyncIterator[TextualWorld]:
-    prepare_profile()
-
-    async with TextualWorld() as world:
-        yield world
-
-
-@pytest.fixture()
-async def prepared_env(world: TextualWorld) -> tuple[tt.RawNode, tt.Wallet, Clive]:
+def _node_with_wallet() -> tuple[tt.RawNode, tt.Wallet]:  # noqa: PT005 # not intended for direct usage
     config_lines = get_config().write_to_lines()
     block_log = get_block_log()
     alternate_chain_spec_path = get_alternate_chain_spec_path()
@@ -88,47 +74,55 @@ async def prepared_env(world: TextualWorld) -> tuple[tt.RawNode, tt.Wallet, Cliv
     account = wallet.api.get_account(WORKING_ACCOUNT.name)
     tt.logger.debug(f"working account: {account}")
 
+    return node, wallet
+
+
+@pytest.fixture()
+async def _world(  # noqa: PT005 # not intended for direct usage
+    _node_with_wallet: tuple[tt.RawNode, tt.Wallet],
+) -> AsyncIterator[TextualWorld]:
+    node, _ = _node_with_wallet
+
+    _prepare_profile()
+
+    async with TextualWorld() as world:
+        await world.node.set_address(Url.parse(node.http_endpoint.as_string()))
+        yield world
+
+
+@pytest.fixture()
+async def prepared_env(
+    _world: TextualWorld, _node_with_wallet: tuple[tt.RawNode, tt.Wallet]
+) -> AsyncIterator[PreparedTuiEnv]:
+    node, wallet = _node_with_wallet
+    world = _world
+
     app = Clive.app_instance()
-
     Clive.world = world
-    await prepare_wallet(world)
-    await world.node.set_address(Url.parse(node.http_endpoint.as_string()))
 
-    return node, wallet, app
-
-
-@pytest.fixture()
-async def prepared_tui_on_dashboard_inactive(
-    world: TextualWorld,
-    prepared_env: tuple[tt.RawNode, tt.Wallet, Clive],
-) -> AsyncIterator[tuple[tt.RawNode, tt.Wallet, ClivePilot]]:
-    node, wallet, app = prepared_env
-    await Deactivate(
-        app_state=world.app_state,
-        beekeeper=world.beekeeper,
-        wallet=world.profile_data.name,
-    ).execute()
+    pilot: ClivePilot
     async with app.run_test() as pilot:
-        yield node, wallet, pilot
-        await clive_quit(pilot)
+        await _prepare_beekeeper_wallet(world)
 
+        if world.app_state.is_active and isinstance(app.screen, DashboardInactive):
+            # beekeeper create wallet makes app active, we have to ensure correct dashboard to be displayed
+            await app.switch_screen("dashboard_active")
 
-@pytest.fixture()
-async def prepared_tui_on_dashboard_active(
-    world: TextualWorld,
-    prepared_env: tuple[tt.RawNode, tt.Wallet, Clive],
-) -> AsyncIterator[tuple[tt.RawNode, tt.Wallet, ClivePilot]]:
-    node, wallet, app = prepared_env
-    # Temporary code - waits until the problem with the application status is resolved (DashboardInactive vs DashboardActive)
-    await Deactivate(
-        app_state=world.app_state,
-        beekeeper=world.beekeeper,
-        wallet=world.profile_data.name,
-    ).execute()
-    async with app.run_test() as pilot:
-        await activate(pilot, app.world.profile_data.name)
-        # End of temporary code
-        assert get_mode(pilot.app) == "active", "Expected 'active' mode!"
         await wait_for_screen(pilot, DashboardActive)
+
         yield node, wallet, pilot
+
         await clive_quit(pilot)
+
+
+@pytest.fixture()
+async def prepared_tui_on_dashboard_inactive(prepared_env: PreparedTuiEnv) -> PreparedTuiEnv:
+    node, wallet, pilot = prepared_env
+    await pilot.pause()  # required, otherwise next call to self.app inside Commands will fail with NoActiveAppError
+    await pilot.app.world.commands.deactivate()  # we have to deactivate because by default we are in active state triggered by create_wallet
+    return node, wallet, pilot
+
+
+@pytest.fixture()
+async def prepared_tui_on_dashboard_active(prepared_env: PreparedTuiEnv) -> PreparedTuiEnv:
+    return prepared_env
