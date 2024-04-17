be9caf88cf699edd91f7360a1a8ce749dea181fc
Wieslaw Kedzierski <wkedzierski@syncad.com>
2024-04-17 10:37:00 +0200

    Add new aliases.

--- clive/models/aliased.py
+++ clive/models/aliased.py
@@ -6,6 +6,10 @@ from schemas.apis.database_api import FindProposals as SchemasFindProposals
 from schemas.apis.database_api import FindWitnesses as SchemasFindWitnesses
 from schemas.apis.database_api import (
     GetDynamicGlobalProperties,
+    GetFeedHistory,
+    GetHardforkProperties,
+    GetVersion,
+    GetWitnessSchedule,
     ListProposals,
     ListProposalVotes,
     ListWitnesses,
@@ -28,9 +32,11 @@ from schemas.apis.transaction_status_api import FindTransaction as SchemasFindTr
 from schemas.fields.assets import AssetHbdHF26, AssetHiveHF26, AssetVestsHF26
 from schemas.fields.assets._base import AssetBase as SchemasAssetBase
 from schemas.fields.basic import AccountName as SchemasAccountName
-from schemas.fields.compound import Proposal
+from schemas.fields.compound import HbdExchangeRate as SchemasHbdExchangeRate
+from schemas.fields.compound import Price, Proposal
 from schemas.fields.hex import Sha256
 from schemas.fields.hex import Signature as SchemasSignature
+from schemas.fields.hive_int import HiveInt as SchemasHiveInt
 from schemas.operation import Operation as SchemasBaseOperationType
 from schemas.operations import AnyOperation
 from schemas.operations.representation_types import Hf26OperationRepresentationType
@@ -38,39 +44,43 @@ from schemas.operations.virtual import AnyVirtualOperation
 from schemas.operations.virtual.representation_types import Hf26VirtualOperationRepresentationType
 from schemas.virtual_operation import VirtualOperation as SchemasBaseVirtualOperationType
 
-AssetBase = SchemasAssetBase
-Operation = AnyOperation
-VirtualOperation = AnyVirtualOperation
-OperationBaseClass = SchemasBaseOperationType
-VirtualOperationBaseClass = SchemasBaseVirtualOperationType
-OperationRepresentationType = Hf26OperationRepresentationType
-VirtualOperationRepresentationType = Hf26VirtualOperationRepresentationType
+AccountName = SchemasAccountName
 ApiOperationObject = Hf26ApiOperationObject
 ApiVirtualOperationObject = Hf26ApiVirtualOperationObject
-Signature = SchemasSignature
-AccountName = SchemasAccountName
-
-ProposalSchema = Proposal[AssetHbdHF26]
-ProposalsList = ListProposals
-ProposalVotes = ListProposalVotes
+AssetBase = SchemasAssetBase
+ChainIdSchema = Sha256
+ChangeRecoveryAccountRequest = ListChangeRecoveryAccountRequestsFundament
+CurrentPriceFeed = Price[AssetHiveHF26, AssetHbdHF26, AssetVestsHF26]
+DeclineVotingRightsRequest = ListDeclineVotingRightsRequestsFundament
+DynamicGlobalProperties = GetDynamicGlobalProperties
+FeedHistory = GetFeedHistory
+FindAccounts = SchemasFindAccounts
+FindProposals = SchemasFindProposals
+FindRcAccounts = SchemasFindRcAccounts[AssetVestsHF26]
 FindWitnesses = SchemasFindWitnesses
+HardforkProperties = GetHardforkProperties
+HbdExchangeRate = SchemasHbdExchangeRate[AssetHiveHF26, AssetHbdHF26]
+HiveInt = SchemasHiveInt
+Operation = AnyOperation
+OperationBaseClass = SchemasBaseOperationType
+OperationRepresentationType = Hf26OperationRepresentationType
+OwnerHistory = OwnerHistoriesFundament
+ProposalSchema = Proposal[AssetHbdHF26]
+ProposalVotes = ListProposalVotes
+ProposalsList = ListProposals
+RcAccount = SchemasRcAccount[AssetVestsHF26]
+Reputation = GetAccountReputationsFundament
+SavingsWithdrawals = SavingsWithdrawalsFundament[AssetHiveHF26, AssetHbdHF26]
+SchemasAccount = AccountItemFundament[AssetHiveHF26, AssetHbdHF26, AssetVestsHF26]
+Signature = SchemasSignature
+TransactionStatus = SchemasFindTransaction
+Version = GetVersion
+VestingDelegation = VestingDelegationsFundament
+VirtualOperation = AnyVirtualOperation
+VirtualOperationBaseClass = SchemasBaseVirtualOperationType
+VirtualOperationRepresentationType = Hf26VirtualOperationRepresentationType
+WithdrawRouteSchema = WithdrawVestingRoutesFundament
+Witness = WitnessesFundament[AssetHiveHF26, AssetHbdHF26]
+WitnessSchedule = GetWitnessSchedule
 WitnessVotes = ListWitnessVotes
 WitnessesList = ListWitnesses
-Reputation = GetAccountReputationsFundament
-OwnerHistory = OwnerHistoriesFundament
-FindRcAccounts = SchemasFindRcAccounts[AssetVestsHF26]
-SavingsWithdrawals = SavingsWithdrawalsFundament[AssetHiveHF26, AssetHbdHF26]
-Witness = WitnessesFundament[AssetHiveHF26, AssetHbdHF26]
-SchemasAccount = AccountItemFundament[AssetHiveHF26, AssetHbdHF26, AssetVestsHF26]
-RcAccount = SchemasRcAccount[AssetVestsHF26]
-DynamicGlobalProperties = GetDynamicGlobalProperties
-TransactionStatus = SchemasFindTransaction
-FindProposals = SchemasFindProposals
-FindAccounts = SchemasFindAccounts
-WithdrawRouteSchema = WithdrawVestingRoutesFundament
-VestingDelegation = VestingDelegationsFundament
-
-ChangeRecoveryAccountRequest = ListChangeRecoveryAccountRequestsFundament
-DeclineVotingRightsRequest = ListDeclineVotingRightsRequestsFundament
-
-ChainIdSchema = Sha256
