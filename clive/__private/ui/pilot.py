bb3a0805f979bfad6f6be5ad932a0eda546e356b
Mateusz Żebrak <mzebrak@syncad.com>
2024-04-19 08:49:10 +0200

    Override Pilot._wait_for_screen with our tiemout

--- clive/__private/ui/pilot.py
+++ clive/__private/ui/pilot.py
@@ -4,9 +4,14 @@ from typing import TYPE_CHECKING
 
 from textual.pilot import Pilot
 
+from clive_local_tools.tui.constants import TUI_TESTS_GENERAL_TIMEOUT
+
 if TYPE_CHECKING:
     from clive.__private.ui.app import Clive
 
 
 class ClivePilot(Pilot[int]):
     app: Clive
+
+    async def _wait_for_screen(self, timeout: float = TUI_TESTS_GENERAL_TIMEOUT) -> bool:
+        return await super()._wait_for_screen(timeout=timeout)
