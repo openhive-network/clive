4b80c7682d22d6a7c91dd195a7975b68b543a2b0
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-05-06 09:33:36 +0200

    Implementation of the fix alarm TUI details

---
+++ clive/__private/ui/alarms_representation/alarm_fix_details.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, Callable
+
+AlarmFixActionT = Callable[[], Any]
+
+
+@dataclass(kw_only=True)
+class AlarmFixDetails:
+    fix_info: str
+    fix_button_text: str = ""
+    fix_action_cb: AlarmFixActionT | None = None
+
+    @property
+    def fix_action_cb_ensure(self) -> AlarmFixActionT:
+        assert self.fix_action_cb is not None, "You are trying to fix an alarm using clive without passing fix action."
+        return self.fix_action_cb
