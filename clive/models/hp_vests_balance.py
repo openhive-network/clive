5fc95b512f6587b9c5b24a24b1527714d0cdfe90
Mateusz Żebrak <mzebrak@syncad.com>
2024-05-23 10:55:50 +0200

    Extract common model HpVestsBalance

---
+++ clive/models/hp_vests_balance.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from clive.__private.core.hive_vests_conversions import vests_to_hive
+
+if TYPE_CHECKING:
+    from clive.models import Asset
+    from clive.models.aliased import DynamicGlobalProperties
+
+
+@dataclass
+class HpVestsBalance:
+    """Class to store the balance of shares in HP and VESTS."""
+
+    hp_balance: Asset.Hive
+    vests_balance: Asset.Vests
+
+    @classmethod
+    def create(cls, vests_value: Asset.Vests, gdpo: DynamicGlobalProperties) -> HpVestsBalance:
+        return cls(hp_balance=vests_to_hive(vests_value, gdpo), vests_balance=vests_value)
