809faf9b3526162362f6f237cdc7304f8bd4cdc6
Marcin Sobczyk <msobczyk@syncad.com>
2024-06-05 14:16:46 +0200

    Store clive terminal size in constants module

--- tests/functional/cli/conftest.py
+++ tests/functional/cli/conftest.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
-from typing import Final
-
 import pytest
 from typer.testing import CliRunner
 
 from clive.__private.core.commands.create_wallet import CreateWallet
+from clive.__private.core.constants import TERMINAL_WIDTH
 from clive.__private.core.keys.keys import PrivateKeyAliased
 from clive.__private.core.profile_data import ProfileData
 from clive.__private.core.world import World
@@ -15,8 +14,6 @@ from clive.core.url import Url
 from clive_local_tools.cli.cli_tester import CLITester
 from clive_local_tools.data.constants import WORKING_ACCOUNT_KEY_ALIAS, WORKING_ACCOUNT_PASSWORD
 from clive_local_tools.testnet_block_log import WATCHED_ACCOUNTS_DATA, WORKING_ACCOUNT_DATA, run_node
-
-TEST_TERMINAL_WIDTH: Final[str] = "132"
 
 
 @pytest.fixture()
@@ -51,7 +48,7 @@ async def cli_tester(run_testnet: None) -> CLITester:  # noqa: ARG001
     # import cli after default profile is set, default values for --profile-name option are set during loading
     from clive.__private.cli.main import cli
 
-    env = {"COLUMNS": TEST_TERMINAL_WIDTH}
+    env = {"COLUMNS": f"{TERMINAL_WIDTH}"}
     runner = CliRunner(env=env)
 
     return CLITester(cli, runner)
