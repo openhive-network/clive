126dd69f0ce3c6c264c15f63aec91d83f42ed368
Mateusz Żebrak <mzebrak@syncad.com>
2024-06-05 09:54:17 +0200

    Use run node in CLI tests

--- tests/functional/cli/conftest.py
+++ tests/functional/cli/conftest.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import pytest
-import test_tools as tt
 from typer.testing import CliRunner
 
 from clive.__private.core.commands.create_wallet import CreateWallet
@@ -15,41 +14,11 @@ from clive_local_tools.cli.cli_tester import CLITester
 from clive_local_tools.data.constants import WORKING_ACCOUNT_KEY_ALIAS, WORKING_ACCOUNT_PASSWORD
 from clive_local_tools.testnet_block_log import WATCHED_ACCOUNTS_DATA, WORKING_ACCOUNT_DATA, run_node
 
+
 @pytest.fixture()
-async def cli_tester() -> CLITester:
-    """Will run init node, configure profile and return CliRunner from typer.testing module."""
-    init_node = tt.InitNode()
-    init_node.config.plugin.append("transaction_status_api")
-    init_node.config.plugin.append("account_history_api")
-    init_node.config.plugin.append("account_history_rocksdb")
-    init_node.config.plugin.append("reputation_api")
-    init_node.run()
-
-    cli_wallet = tt.Wallet(attach_to=init_node, additional_arguments=["--transaction-serialization", "hf26"])
-    cli_wallet.api.import_key(init_node.config.private_key[0])
-
-    with cli_wallet.in_single_transaction():
-        creator_account_name = CREATOR_ACCOUNT.name
-        for account in [WORKING_ACCOUNT, *WATCHED_ACCOUNTS]:
-            key = tt.PublicKey(account.name)
-            cli_wallet.api.create_account_with_keys(
-                creator_account_name,
-                account.name,
-                "{}",
-                key,
-                key,
-                key,
-                key,
-            )
-            cli_wallet.api.transfer(
-                creator_account_name, account.name, WORKING_ACCOUNT_HIVE_LIQUID_BALANCE.as_nai(), "memo"
-            )
-            cli_wallet.api.transfer_to_vesting(
-                creator_account_name, account.name, WORKING_ACCOUNT_VEST_BALANCE.as_nai()
-            )
-            cli_wallet.api.transfer(
-                creator_account_name, account.name, WORKING_ACCOUNT_HBD_LIQUID_BALANCE.as_nai(), "memo"
-            )
+async def run_testnet() -> None:
+    """Will run node from pregenerated block_log and prepare profile."""
+    node = run_node()
 
     ProfileData(
         WORKING_ACCOUNT_DATA.account.name,
@@ -65,12 +34,16 @@ async def cli_tester() -> CLITester:
             password=WORKING_ACCOUNT_PASSWORD,
         ).execute_with_result()
 
-        await world.node.set_address(Url.parse(init_node.http_endpoint.as_string()))
+        await world.node.set_address(Url.parse(node.http_endpoint.as_string()))
         world.profile_data.working_account.keys.add_to_import(
             PrivateKeyAliased(value=WORKING_ACCOUNT_DATA.account.private_key, alias=f"{WORKING_ACCOUNT_KEY_ALIAS}")
         )
         await world.commands.sync_data_with_beekeeper()
 
+
+@pytest.fixture()
+async def cli_tester(run_testnet: None) -> CLITester:  # noqa: ARG001
+    """Will return CliveTyper and CliRunner from typer.testing module.."""
     # import cli after default profile is set, default values for --profile-name option are set during loading
     from clive.__private.cli.main import cli
 
