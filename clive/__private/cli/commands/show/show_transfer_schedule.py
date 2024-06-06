ccec665dac0ea86e6437249d1d47ab5f813d4985
Wiesław Kędzierski <wkedzierski@syncad.com>
2024-06-06 10:24:43 +0200

    Remove 'type: ignore' and rename variable 'temp' to more verbose 'future_scheduled_transfers'.

--- clive/__private/cli/commands/show/show_transfer_schedule.py
+++ clive/__private/cli/commands/show/show_transfer_schedule.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from datetime import timedelta
-from typing import Final, overload
+from datetime import datetime, timedelta
+from typing import TYPE_CHECKING, Final, TypeVar
 
 from rich.columns import Columns
 from rich.console import Console, Group
@@ -15,15 +15,28 @@ from clive.__private.core.formatters.humanize import align_to_dot, humanize_asse
 from clive.__private.core.shorthand_timedelta import timedelta_to_shorthand_timedelta
 from clive.__private.storage.accounts import Account
 from clive.__private.storage.mock_database import NodeData
-from clive.models.aliased import TransferSchedule
 from clive.models.asset import Asset
 
+if TYPE_CHECKING:
+    from clive.models.aliased import TransferSchedule
 
-class FutureTransferSchedule(TransferSchedule):
-    possible_amount: Asset.Hive
+
+@dataclass
+class FutureTransferSchedule:
+    trigger_date: datetime
+    to: str
+    amount: Asset.Hive | Asset.Hbd
+    memo: str
+    recurrence: int
+    remaining_executions: int
+    pair_id: int
+    possible_amount: Asset.Hive | Asset.Hbd
+    from_: str
 
 
 FutureScheduledTransfers = list[FutureTransferSchedule]
+
+Sortable = TypeVar("Sortable", ScheduledTransfers, "FutureScheduledTransfers")
 
 ERROR_LACK_OF_FUNDS_MESSAGE_RAW: Final[str] = "Possible lack of funds."
 DEFAULT_FUTURE_DEEPTH: Final[int] = 10
@@ -39,8 +52,9 @@ class ShowTransferSchedule(WorldBasedCommand):
         (await self.world.commands.update_node_data(accounts=[account])).raise_if_error_occurred()
         data = account.data
 
-        wrapper = await self.world.commands.find_scheduled_transfers(account_name=self.account_name)
-        scheduled_transfers = wrapper.result_or_raise
+        scheduled_transfers = (
+            await self.world.commands.find_scheduled_transfers(account_name=self.account_name)
+        ).result_or_raise
 
         console = Console()
 
@@ -86,13 +100,13 @@ class ShowTransferSchedule(WorldBasedCommand):
             table_definitions.add_row(
                 scheduled_transfer.from_,
                 scheduled_transfer.to,
-                f"{scheduled_transfer.pair_id}",
-                f"{aligned_amounts[idx]}",
+                str(scheduled_transfer.pair_id),
+                str(aligned_amounts[idx]),
                 scheduled_transfer.memo,
                 humanize_datetime(scheduled_transfer.trigger_date),
                 timedelta_to_shorthand_timedelta(timedelta(hours=scheduled_transfer.recurrence)),
-                f"{scheduled_transfer.remaining_executions}",
-                f"{scheduled_transfer.consecutive_failures}",
+                str(scheduled_transfer.remaining_executions),
+                str(scheduled_transfer.consecutive_failures),
             )
         return table_definitions
 
@@ -124,7 +138,7 @@ class ShowTransferSchedule(WorldBasedCommand):
         possible_amount_aligned = align_to_dot(*possible_amount_to_align, center_to=possible_amount_column_name)
 
         for idx, future_scheduled_transfer in enumerate(future_scheduled_transfers):
-            possible_amount = (
+            possible_amount: Text | str = (
                 Text(f"{ERROR_LACK_OF_FUNDS_MESSAGE_RAW}", style="red", justify="center")
                 if future_scheduled_transfer.possible_amount == LACK_OF_FUNDS_AMOUNT
                 else possible_amount_aligned[idx]
@@ -132,11 +146,11 @@ class ShowTransferSchedule(WorldBasedCommand):
             table_upcoming.add_row(
                 future_scheduled_transfer.from_,
                 future_scheduled_transfer.to,
-                f"{future_scheduled_transfer.pair_id}",
+                str(future_scheduled_transfer.pair_id),
                 amount_aligned[idx],
-                possible_amount,  # type: ignore[arg-type]
+                possible_amount,
                 future_scheduled_transfer.memo,
-                f"{future_scheduled_transfer.trigger_date}",
+                str(future_scheduled_transfer.trigger_date),
                 timedelta_to_shorthand_timedelta(timedelta(hours=future_scheduled_transfer.recurrence)),
             )
         return table_upcoming
@@ -151,35 +165,23 @@ class ShowTransferSchedule(WorldBasedCommand):
         for scheduled_transfer in scheduled_transfers:
             for idx, remains in enumerate(range(min(scheduled_transfer.remaining_executions, deepth))):
                 amount = scheduled_transfer.amount * idx
-                temp = FutureTransferSchedule(  # type: ignore[call-arg]
-                    id_=scheduled_transfer.id_,
+                future_scheduled_transfer = FutureTransferSchedule(
                     trigger_date=scheduled_transfer.trigger_date + timedelta(hours=idx * scheduled_transfer.recurrence),
                     from_=scheduled_transfer.from_,
                     to=scheduled_transfer.to,
                     amount=scheduled_transfer.amount,
                     possible_amount=(
                         data.hive_balance - amount if data.hive_balance > amount else LACK_OF_FUNDS_AMOUNT
-                    ),  # type : ignore[call-arg]
+                    ),
                     memo=scheduled_transfer.memo,
                     recurrence=scheduled_transfer.recurrence,
-                    consecutive_failures=scheduled_transfer.consecutive_failures,
                     remaining_executions=remains,
                     pair_id=scheduled_transfer.pair_id,
                 )
-                future_scheduled_transfers.append(temp)
+                future_scheduled_transfers.append(future_scheduled_transfer)
         return self.get_sorted_by(future_scheduled_transfers, sort_by=[AllowedSorts.trigger_data])
 
-    @overload
-    def get_sorted_by(self, scheduled_transfers: ScheduledTransfers, sort_by: list[str]) -> ScheduledTransfers: ...  # type: ignore[overload-overlap]
-
-    @overload
-    def get_sorted_by(
-        self, scheduled_transfers: FutureScheduledTransfers, sort_by: list[str]
-    ) -> FutureScheduledTransfers: ...
-
-    def get_sorted_by(
-        self, scheduled_transfers: ScheduledTransfers | FutureScheduledTransfers, sort_by: list[str]
-    ) -> ScheduledTransfers | FutureScheduledTransfers:
+    def get_sorted_by(self, scheduled_transfers: Sortable, sort_by: list[str]) -> Sortable:
         import operator
 
         return sorted(scheduled_transfers, key=operator.attrgetter(*sort_by))
