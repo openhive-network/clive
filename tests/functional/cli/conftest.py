15c0fceca3fcb9577c7e2757dc406021c539e093
Marcin Sobczyk <msobczyk@syncad.com>
2024-06-06 10:28:03 +0200

    Split pytest fixture run_node
    Into parts with creating profile and beekeeper wallet allowing for
    override in some tests

--- tests/functional/cli/conftest.py
+++ tests/functional/cli/conftest.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import pytest
 from typer.testing import CliRunner
 
@@ -15,40 +17,57 @@ from clive_local_tools.cli.cli_tester import CLITester
 from clive_local_tools.data.constants import WORKING_ACCOUNT_KEY_ALIAS, WORKING_ACCOUNT_PASSWORD
 from clive_local_tools.testnet_block_log import WATCHED_ACCOUNTS_DATA, WORKING_ACCOUNT_DATA, run_node
 
+if TYPE_CHECKING:
+    import test_tools as tt
+
 
 @pytest.fixture()
-async def run_testnet() -> None:
-    """Will run node from pregenerated block_log and prepare profile."""
-    node = run_node()
-
-    ProfileData(
+async def prepare_profile() -> ProfileData:
+    profile_data = ProfileData(
         WORKING_ACCOUNT_DATA.account.name,
         working_account=WorkingAccount(name=WORKING_ACCOUNT_DATA.account.name),
         watched_accounts=[WatchedAccount(data.account.name) for data in WATCHED_ACCOUNTS_DATA],
-    ).save()
-
-    async with World(WORKING_ACCOUNT_DATA.account.name) as world:
-        await CreateWallet(
-            app_state=world.app_state,
-            beekeeper=world.beekeeper,
-            wallet=WORKING_ACCOUNT_DATA.account.name,
-            password=WORKING_ACCOUNT_PASSWORD,
-        ).execute_with_result()
-
-        await world.node.set_address(Url.parse(node.http_endpoint.as_string()))
-        world.profile_data.working_account.keys.add_to_import(
-            PrivateKeyAliased(value=WORKING_ACCOUNT_DATA.account.private_key, alias=f"{WORKING_ACCOUNT_KEY_ALIAS}")
-        )
-        await world.commands.sync_data_with_beekeeper()
+    )
+    profile_data.save()
+    return profile_data
 
 
 @pytest.fixture()
-async def cli_tester(run_testnet: None) -> CLITester:  # noqa: ARG001
+async def world(prepare_profile: ProfileData) -> World:  # noqa: ARG001
+    world = World(WORKING_ACCOUNT_DATA.account.name)
+    await world.setup()
+    return world
+
+
+@pytest.fixture()
+async def prepare_beekeeper_wallet(world: World) -> None:
+    await CreateWallet(
+        app_state=world.app_state,
+        beekeeper=world.beekeeper,
+        wallet=WORKING_ACCOUNT_DATA.account.name,
+        password=WORKING_ACCOUNT_PASSWORD,
+    ).execute_with_result()
+    world.profile_data.working_account.keys.add_to_import(
+        PrivateKeyAliased(value=WORKING_ACCOUNT_DATA.account.private_key, alias=f"{WORKING_ACCOUNT_KEY_ALIAS}")
+    )
+    await world.commands.sync_data_with_beekeeper()
+
+
+@pytest.fixture()
+async def run_testnet(world: World, prepare_beekeeper_wallet: None) -> tt.RawNode:  # noqa: ARG001
+    """Will run node from pregenerated block_log and prepare profile."""
+    node = run_node()
+    await world.node.set_address(Url.parse(node.http_endpoint.as_string()))
+    await world.close()
+    return node
+
+
+@pytest.fixture()
+async def cli_tester(run_testnet: tt.RawNode) -> CLITester:  # noqa: ARG001
     """Will return CliveTyper and CliRunner from typer.testing module.."""
     # import cli after default profile is set, default values for --profile-name option are set during loading
     from clive.__private.cli.main import cli
 
     env = {"COLUMNS": f"{TERMINAL_WIDTH}"}
     runner = CliRunner(env=env)
-
     return CLITester(cli, runner)
