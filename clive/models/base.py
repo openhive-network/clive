8cd1c3f0baa3754caac3027f5162be713135139a
Mateusz Żebrak <mzebrak@syncad.com>
2024-06-12 11:57:14 +0200

    Ignore pydantic-related RUF012

--- clive/models/base.py
+++ clive/models/base.py
@@ -10,4 +10,6 @@ from clive.__private.core.communication import CustomJSONEncoder
 class CliveBaseModel(BaseModel):
     class Config:
         allow_population_by_field_name = True
-        json_encoders = {datetime: lambda d: d.strftime(CustomJSONEncoder.TIME_FORMAT_WITH_MILLIS)}
+        json_encoders = {  # noqa: RUF012; pydantic convention
+            datetime: lambda d: d.strftime(CustomJSONEncoder.TIME_FORMAT_WITH_MILLIS)
+        }
