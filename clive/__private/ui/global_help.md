aabaf2c2cb366078f4acfa76c70128aa9578fcf3
Mateusz Żebrak <mzebrak@syncad.com>
2024-06-28 13:06:06 +0200

    WIP copy/paste demo onboarding and global help

--- clive/__private/ui/global_help.md
+++ clive/__private/ui/global_help.md
@@ -10,3 +10,14 @@
 | `Ctrl+X` | Quit                |
 | `Ctrl+S` | Screenshot          |
 |   `C`    | Clear notifications |
+
+## Select text, copy and paste inside Terminal:
+
+To select some text hold `Shift` while you click and drag.
+
+Copy/Paste action shortcuts depend on the environment (mainly terminal) in which Clive was launched. You may check:
+* on `Windows`: `Ctrl+C` and `Ctrl+V`
+* on `Linux`: `Ctrl+Shift+C` and `Ctrl+Shift+V`
+
+If none of the above works, you may also try `Ctrl+Insert` and `Shift+Insert`. \
+Otherwise, you should check this for your environment/terminal as it is often quite specific or configurable and we're unable to indicate a universal solution.
--- clive/__private/ui/shared/dedicated_form_screens/welcome_form_screen.py
+++ clive/__private/ui/shared/dedicated_form_screens/welcome_form_screen.py
@@ -12,6 +12,7 @@ from clive.__private.ui.shared.base_screen import BaseScreen
 from clive.__private.ui.shared.form_screen import FirstFormScreen
 from clive.__private.ui.widgets.clive_button import CliveButton
 from clive.__private.ui.widgets.dialog_container import DialogContainer
+from clive.__private.ui.widgets.section import Section
 
 if TYPE_CHECKING:
     from textual.app import ComposeResult
@@ -33,8 +34,43 @@ class WelcomeFormScreen(BaseScreen, FirstFormScreen[ContextT]):
         super().__init__(owner)
 
     def create_main_panel(self) -> ComposeResult:
+        system_color = "cyan"
+        shortcut_styling = "yellow italic"
+
+        title = "Select text, copy and paste inside Terminal"
+        select_desc = f"To select some text hold [{shortcut_styling}]Shift[/] while you click and drag."
+
+        copy_paste_desc = (
+            "Copy/Paste action shortcuts depend on the environment (mainly terminal)"
+            " in which Clive was launched. You may check:"
+        )
+
+        on_windows_text = (
+            f"  > On [{system_color}]Linux[/]:"
+            f" [{shortcut_styling}]Ctrl+Shift+C[/] / [{shortcut_styling}]Ctrl+Shift+V[/]"
+        )
+        on_llinux_text = (
+            f"  > On [{system_color}]Windows[/]: [{shortcut_styling}]Ctrl+C[/] / [{shortcut_styling}]Ctrl+V[/]"
+        )
+        last_hope_text = (
+            f"If none of the above works, you may also try"
+            f" [{shortcut_styling}]Ctrl+Insert[/] / [{shortcut_styling}]Shift+Insert[/]."
+        )
+        otherwise_text = (
+            "Otherwise, you should check this for your environment/terminal as it is often quite specific"
+            " or configurable and we're unable to indicate a universal solution."
+        )
+
         with DialogContainer("welcome"):
             yield WelcomeTitle(self.__title)
+            with Section(title):
+                yield Static(select_desc)
+                yield Static()
+                yield Static(copy_paste_desc)
+                yield Static(on_llinux_text)
+                yield Static(on_windows_text)
+                yield Static(last_hope_text)
+                yield Static(otherwise_text)
             yield CliveButton("Start!", id_="welcome-button-start")
 
     @on(CliveButton.Pressed, "#welcome-button-start")
