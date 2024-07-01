4d9c1197ba6ec7bfab366cb05d6591dc5e316fcb
Mateusz Kudela <mkudela@syncad.com>
2024-07-01 15:01:13 +0200

    Add copy/paste info OnboardingWelcomeScreen

--- clive/__private/ui/onboarding/onboarding.py
+++ clive/__private/ui/onboarding/onboarding.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Final
 
 from textual.binding import Binding
 
@@ -12,9 +12,15 @@ from clive.__private.ui.set_node_address.set_node_address import SetNodeAddressF
 from clive.__private.ui.shared.dedicated_form_screens.finish_form_screen import FinishFormScreen
 from clive.__private.ui.shared.dedicated_form_screens.welcome_form_screen import WelcomeFormScreen
 from clive.__private.ui.shared.form import Form, ScreenBuilder
+from clive.__private.ui.widgets.select_copy_paste_hint import SelectCopyPasteHint
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
+
+    from textual.app import ComposeResult
+
+
+SHORTCUT_STYLING: Final[str] = "yellow italic"
 
 
 class OnboardingWelcomeScreen(WelcomeFormScreen[ProfileData]):
@@ -22,6 +28,13 @@ class OnboardingWelcomeScreen(WelcomeFormScreen[ProfileData]):
         Binding("escape", "dummy", show=False),
         Binding("f1", "help", "Help"),  # help is a hidden global binding, but we want to show it here
     ]
+
+    def __init__(self, owner: Form[ProfileData], title: str) -> None:
+        super().__init__(owner, title)
+
+    def _content_after_title(self) -> ComposeResult:
+        title = "How to select, copy and paste text inside TUI app like Clive?"
+        yield SelectCopyPasteHint(title=title, system_color="cyan", shortcut_styling=SHORTCUT_STYLING)
 
 
 class OnboardingFinishScreen(FinishFormScreen[ProfileData]):
@@ -45,10 +58,10 @@ class Onboarding(Form[ProfileData]):
         yield NewKeyAliasForm
 
     def create_welcome_screen(self) -> ScreenBuilder[ProfileData]:
-        return lambda owner: OnboardingWelcomeScreen(
-            owner,
-            """Let's start onboarding!
-In any moment you can press the `[blue]F1[/]` button to see the help page.""",
+        return (
+            lambda owner,  # type: ignore[misc]
+            title=f"""Let's start onboarding! In any moment you can press
+            the [{SHORTCUT_STYLING}]F1[/] button to see the Help page.""": OnboardingWelcomeScreen(owner, title)
         )
 
     def create_finish_screen(self) -> ScreenBuilder[ProfileData]:
