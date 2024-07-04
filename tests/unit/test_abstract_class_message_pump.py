880559573fac5dc54ee75809beb388f9f4623468
Mateusz Żebrak <mzebrak@syncad.com>
2024-07-04 12:43:06 +0200

    Extend test for AbstracatClassMessagePump for test_instantiating_class_inheriting_from_abstract_class_with_abstract_method and checking in multi inheritance on actual Widget/Screen

--- tests/unit/test_abstract_class_message_pump.py
+++ tests/unit/test_abstract_class_message_pump.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 import pytest
+from textual.screen import Screen
+from textual.widget import Widget
 
 from clive.__private.abstract_class import AbstractClassMessagePump
 
@@ -25,9 +27,33 @@ class MockValidPlainSubclass(MockPlainAbstract):
     pass
 
 
+class AbstractWidget(Widget, AbstractClassMessagePump):
+    pass
+
+
+class AbstractScreen(Screen[None], AbstractClassMessagePump):
+    pass
+
+
 class MockValidSubclassWithAbstractMethod(MockAbstractWithAbstractMethod):
     def abstract_method(self) -> None:
         pass
+
+
+def test_textual_still_errors_with_abc_inheritance() -> None:
+    # ARRANGE
+    expected_error_message = (
+        "metaclass conflict: the metaclass of a derived class must be a"
+        " (non-strict) subclass of the metaclasses of all its bases"
+    )
+
+    # ACT & ASSERT
+    with pytest.raises(TypeError) as exception:
+
+        class InvalidAbstractScreen(Screen[None], ABC):  # type: ignore[misc]
+            pass
+
+    assert str(exception.value) == expected_error_message
 
 
 def test_instantiating_class_inheriting_from_abstract_class_with_abstract_method() -> None:
@@ -43,6 +69,8 @@ def test_instantiating_class_inheriting_from_abstract_class_with_abstract_method
     assert str(exception.value) == expected_error_message
 
 
-@pytest.mark.parametrize("cls", [MockValidPlainSubclass, MockValidSubclassWithAbstractMethod])
+@pytest.mark.parametrize(
+    "cls", [MockValidPlainSubclass, MockValidSubclassWithAbstractMethod, AbstractWidget, AbstractScreen]
+)
 def test_instantiating_valid_subclass(cls: type) -> None:
     cls()
