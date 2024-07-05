7ba5e8c742e8ee0c4ece85f7676229f4c1cd58ea
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-07-05 14:55:47 +0200

    Working account tab-pane is now only used to switch the working account
    Previously, it was possible to add / change a working account by input.
    Now the flow is different -> the user must first add the account to watched
    and then switch the working account.

--- clive/__private/ui/account_list_management/account_list_management.py
+++ clive/__private/ui/account_list_management/account_list_management.py
@@ -2,8 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from textual import on
 from textual.binding import Binding
+from textual.events import ScreenSuspend
 
+from clive.__private.ui.account_list_management.common.switch_working_account_container import (
+    SwitchWorkingAccountContainer,
+)
 from clive.__private.ui.account_list_management.known_accounts import KnownAccounts
 from clive.__private.ui.account_list_management.tracked_accounts import TrackedAccounts
 from clive.__private.ui.account_list_management.working_account import WorkingAccount
@@ -25,8 +30,12 @@ class AccountListManagement(BaseScreen):
     def create_main_panel(self) -> ComposeResult:
         with CliveTabbedContent():
             yield TrackedAccounts()
-            yield KnownAccounts()
             yield WorkingAccount()
+            yield KnownAccounts()
 
     def on_mount(self) -> None:
         self.app.trigger_profile_data_watchers()
+
+    @on(ScreenSuspend)
+    def on_suspended(self) -> None:
+        self.screen.query_one(SwitchWorkingAccountContainer).confirm_selected_working_account()
