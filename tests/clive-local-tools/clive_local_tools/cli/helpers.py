75bf0c9e35a681e4831bf1d181bf7f1c36ad8b19
Wiesław Kędzierski <wkedzierski@syncad.com>
2024-07-24 11:06:00 +0200

    Add get_prepared_recurrent_transfer_operation to helpers.

--- tests/clive-local-tools/clive_local_tools/cli/helpers.py
+++ tests/clive-local-tools/clive_local_tools/cli/helpers.py
@@ -9,7 +9,16 @@ from clive.__private.settings import clive_prefixed_envvar, safe_settings
 from clive_local_tools.helpers import get_transaction_id_from_output
 
 if TYPE_CHECKING:
+    import test_tools as tt
     from click.testing import Result
+
+from clive.__private.core.date_utils import timedelta_to_int_hours
+from clive.__private.core.shorthand_timedelta import shorthand_timedelta_to_timedelta
+from clive.__private.models.schemas import (
+    RecurrentTransferOperation,
+    RecurrentTransferPairIdExtension,
+    RecurrentTransferPairIdRepresentation,
+)
 
 
 def get_transaction_id_from_result(result: Result) -> str:
@@ -32,3 +41,31 @@ def run_clive_in_subprocess(command: list[str]) -> str:
         message = f"error executing command {command}:\n{output_string}"
         raise AssertionError(message) from error
     return completed_process.stdout.decode()
+
+
+def get_prepared_recurrent_transfer_operation(  # noqa: PLR0913
+    from_: str,
+    to: str,
+    amount: tt.Asset.HiveT | tt.Asset.HbdT,
+    memo: str,
+    pair_id: int,
+    recurrence: str,
+    executions: int,
+) -> RecurrentTransferOperation:
+    if pair_id != 0:
+        recurrent_transfer_extension = RecurrentTransferPairIdExtension(pair_id=pair_id)
+        representation = RecurrentTransferPairIdRepresentation(
+            type=recurrent_transfer_extension.get_name(), value=recurrent_transfer_extension
+        )
+        extensions = [representation.dict(by_alias=True)]
+    else:
+        extensions = []
+    return RecurrentTransferOperation(
+        from_=from_,
+        to=to,
+        amount=amount,
+        memo=memo,
+        recurrence=timedelta_to_int_hours(shorthand_timedelta_to_timedelta(recurrence)),
+        executions=executions,
+        extensions=extensions,
+    )
