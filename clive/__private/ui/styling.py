f0d8ecb9df2c986930e30f4b88e42b0d0547f6e0
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-07-29 15:05:49 +0200

    Implementation of the function to mark future functionalities

--- clive/__private/ui/styling.py
+++ clive/__private/ui/styling.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
-from typing import Final
+from typing import TYPE_CHECKING, Final
+
+if TYPE_CHECKING:
+    from rich.text import TextType
 
 SHORTCUT_STYLING: Final[str] = "yellow italic"
 SYSTEM_COLOR: Final[str] = "cyan"
+SOON_STYLING: Final[str] = "#FF8C00"
+SOON_LABEL: Final[str] = f"[{SOON_STYLING}]soon[/]"
 
 
 def colorize_shortcut(message: str) -> str:
@@ -12,3 +17,7 @@ def colorize_shortcut(message: str) -> str:
 
 def colorize_system_text(message: str) -> str:
     return f"[{SYSTEM_COLOR}]{message}[/]"
+
+
+def label_future_functionality(message: TextType) -> str:
+    return f"{message} {SOON_LABEL}"
