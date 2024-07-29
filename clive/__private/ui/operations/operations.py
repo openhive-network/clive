7180e0a042ac1e671ace4760584ff895e04ad70a
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-07-29 15:21:14 +0200

    Improve the indication that some operations have not yet been implemented

--- clive/__private/ui/operations/operations.py
+++ clive/__private/ui/operations/operations.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import inflection
 from textual import on
 from textual.binding import Binding
 from textual.widgets import TabPane
@@ -15,6 +14,7 @@ from clive.__private.ui.operations.cart_based_screen.cart_based_screen import Ca
 from clive.__private.ui.transaction_summary import TransactionSummaryFromFile
 from clive.__private.ui.widgets.buttons.clive_button import CliveButton
 from clive.__private.ui.widgets.clive_tabbed_content import CliveTabbedContent
+from clive.__private.ui.widgets.not_implemented_yet import NotImplementedYetButton, NotImplementedYetTabPane
 from clive.__private.ui.widgets.scrolling import ScrollablePart
 from clive.__private.ui.widgets.select_file import SelectFile
 
@@ -26,25 +26,9 @@ if TYPE_CHECKING:
 
 
 class OperationButton(CliveButton):
-    def __init__(
-        self,
-        operation_screen: type[OperationBaseScreen] | None,
-        *,
-        label: TextType | None = None,
-    ) -> None:
-        if operation_screen is None and label is None:
-            raise ValueError("Either `operation_screen` or `label` must be provided!")
-
-        if label is None:
-            assert operation_screen is not None
-            label = self.__humanize_class_name(operation_screen.__name__)
-
+    def __init__(self, label: TextType, operation_screen: type[OperationBaseScreen]) -> None:
         super().__init__(label)
         self.operation_screen = operation_screen
-
-    @staticmethod
-    def __humanize_class_name(class_name: str) -> str:
-        return inflection.humanize(inflection.underscore(class_name))
 
 
 class Operations(CartBasedScreen, CartBinding):
@@ -62,16 +46,16 @@ class Operations(CartBasedScreen, CartBinding):
     def create_left_panel(self) -> ComposeResult:
         with CliveTabbedContent(initial="financial"):
             with TabPane("Financial", id="financial"), ScrollablePart():
-                yield OperationButton(TransferToAccount, label="Transfer")
-                yield OperationButton(Savings, label="Saving")
-                yield OperationButton(HivePowerManagement, label="Hive power management")
-                yield OperationButton(None, label="Convert")
-            with TabPane("Social"), ScrollablePart():
-                yield OperationButton(None, label="Social operations")
+                yield OperationButton("Transfer", TransferToAccount)
+                yield OperationButton("Savings", Savings)
+                yield OperationButton("Hive power management", HivePowerManagement)
+                yield NotImplementedYetButton("Convert")
+            with NotImplementedYetTabPane("Social"), ScrollablePart():
+                yield NotImplementedYetButton("Social operations")
             with TabPane("Governance"), ScrollablePart():
-                yield OperationButton(Governance, label="Governance operations")
-            with TabPane("Account management"), ScrollablePart():
-                yield OperationButton(None, label="Account management operations")
+                yield OperationButton("Governance", Governance)
+            with NotImplementedYetTabPane("Account management"), ScrollablePart():
+                yield NotImplementedYetButton("Account management operations")
 
     def action_show_tab(self, tab: str) -> None:
         """Switch to a new tab."""
@@ -84,9 +68,6 @@ class Operations(CartBasedScreen, CartBinding):
     @on(OperationButton.Pressed)
     def push_operation_screen(self, event: OperationButton.Pressed) -> None:
         button: OperationButton = event.button  # type: ignore[assignment]
-        if not button.operation_screen:
-            self.notify("Not implemented yet!", severity="error")
-            return
         self.app.push_screen(button.operation_screen())
 
     @on(SelectFile.Saved)
