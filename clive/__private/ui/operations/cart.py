e20f58e172d5adf1640661be4da709b1759bd448
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-08-01 14:46:20 +0200

    Create base class for all dynamic widgets

--- clive/__private/ui/operations/cart.py
+++ clive/__private/ui/operations/cart.py
@@ -16,7 +16,7 @@ from clive.__private.ui.shared.base_screen import BaseScreen
 from clive.__private.ui.transaction_summary import TransactionSummaryFromCart
 from clive.__private.ui.widgets.buttons.clive_button import CliveButton
 from clive.__private.ui.widgets.clive_widget import CliveWidget
-from clive.__private.ui.widgets.dynamic_label import DynamicLabel
+from clive.__private.ui.widgets.dynamic_widgets.dynamic_label import DynamicLabel
 from clive.__private.ui.widgets.scrolling import ScrollablePart
 
 if TYPE_CHECKING:
---
+++ clive/__private/ui/widgets/dynamic_widgets/dynamic_label.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from textual.widgets import Label
+
+from clive.__private.ui.widgets.dynamic_widgets.dynamic_widget import (
+    DynamicWidget,
+    DynamicWidgetFirstTryCallbackType,
+    WatchLikeCallbackType,
+)
+
+if TYPE_CHECKING:
+    from textual.reactive import Reactable
+
+DynamicLabelCallbackType = WatchLikeCallbackType[str]
+
+
+class DynamicLabel(DynamicWidget[Label]):
+    def __init__(
+        self,
+        obj_to_watch: Reactable,
+        attribute_name: str,
+        callback: DynamicLabelCallbackType,
+        *,
+        first_try_callback: DynamicWidgetFirstTryCallbackType = lambda: True,
+        prefix: str = "",
+        init: bool = True,
+        id_: str | None = None,
+        classes: str | None = None,
+        shrink: bool = False,
+    ) -> None:
+        self._shrink = shrink
+        super().__init__(
+            obj_to_watch,
+            attribute_name,
+            callback,
+            first_try_callback=first_try_callback,
+            prefix=prefix,
+            init=init,
+            id_=id_,
+            classes=classes,
+        )
+
+    def update_widget_state(self, result: str) -> None:
+        if result != self._widget.renderable:
+            self._widget.update(f"{self._prefix}{result}")
+
+    def create_widget(self) -> Label:
+        return Label("loading...", shrink=self._shrink)
