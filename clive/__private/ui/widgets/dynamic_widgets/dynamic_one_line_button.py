da9183279c0de6086b2fd2b492f2ffb7611af619
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-08-02 08:49:37 +0200

    Implementation of the dynamic one line button

---
+++ clive/__private/ui/widgets/dynamic_widgets/dynamic_one_line_button.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from clive.__private.ui.widgets.buttons.one_line_button import OneLineButton
+from clive.__private.ui.widgets.dynamic_widgets.dynamic_widget import (
+    DynamicWidget,
+    DynamicWidgetCallbackType,
+    DynamicWidgetFirstTryCallbackType,
+)
+
+if TYPE_CHECKING:
+    from rich.text import TextType
+    from textual.reactive import Reactable
+    from textual.widgets._button import ButtonVariant
+
+
+class DynamicOneLineButton(DynamicWidget[OneLineButton]):
+    can_focus = False
+
+    def __init__(
+        self,
+        obj_to_watch: Reactable,
+        attribute_name: str,
+        callback: DynamicWidgetCallbackType,
+        *,
+        first_try_callback: DynamicWidgetFirstTryCallbackType = lambda: True,
+        variant: ButtonVariant = "primary",
+        prefix: str = "",
+        init: bool = True,
+        id_: str | None = None,
+        classes: str | None = None,
+    ) -> None:
+        self._variant = variant
+        super().__init__(
+            obj_to_watch,
+            attribute_name,
+            callback,
+            first_try_callback=first_try_callback,
+            prefix=prefix,
+            init=init,
+            id_=id_,
+            classes=classes,
+        )
+
+    def update_widget_label(self, result: str) -> None:
+        if result != self._widget.label:
+            self._widget.label = f"{self._prefix}{result}"
+
+    def create_widget(self) -> OneLineButton:
+        _button = OneLineButton("loading...", self._variant)
+        _button.can_focus = False
+        return _button
+
+    @property
+    def renderable(self) -> TextType:
+        return self._widget.label
