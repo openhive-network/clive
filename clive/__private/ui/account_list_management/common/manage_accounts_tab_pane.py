b59b984987ac79495d662cef3151ef41cc46a655
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-08-02 13:09:58 +0200

    Move the addition of a new account to a separate module, place it in one container
    This modification allows to reuse the input and add button, which is
    used to add a new account to the list of known/watched accounts.

--- clive/__private/ui/account_list_management/common/manage_accounts_tab_pane.py
+++ clive/__private/ui/account_list_management/common/manage_accounts_tab_pane.py
@@ -5,15 +5,13 @@ from typing import TYPE_CHECKING
 from textual import on
 from textual.widgets import TabPane
 
+from clive.__private.ui.account_list_management.common.add_account_container import AddAccountContainer
 from clive.__private.ui.account_list_management.common.manage_accounts_table import AccountsType, ManageAccountsTable
 from clive.__private.ui.get_css import get_css_from_relative_path
 from clive.__private.ui.widgets.buttons.clive_button import CliveButton
 from clive.__private.ui.widgets.clive_widget import CliveWidget
-from clive.__private.ui.widgets.inputs.account_name_input import AccountNameInput
 from clive.__private.ui.widgets.scrolling import ScrollablePart
-from clive.__private.ui.widgets.section import Section
-from clive.__private.validators.set_known_account_validator import SetKnownAccountValidator
-from clive.__private.validators.set_tracked_account_validator import SetTrackedAccountValidator
+from clive.__private.ui.widgets.section import SectionBody
 
 if TYPE_CHECKING:
     from textual.app import ComposeResult
@@ -26,49 +24,23 @@ class ManageAccountsTabPane(TabPane, CliveWidget):
 
     def __init__(self, title: str, accounts_type: AccountsType) -> None:
         super().__init__(title=title)
-        self._accounts_input = AccountNameInput(
-            required=False,
-            validators=(
-                SetTrackedAccountValidator(self.profile)
-                if accounts_type == "tracked_accounts"
-                else SetKnownAccountValidator(self.profile)
-            ),
-            ask_known_account=accounts_type != "known_accounts",
-        )
         self._accounts_type = accounts_type
+        self._add_account_container = AddAccountContainer(accounts_type)
 
     def compose(self) -> ComposeResult:
         with ScrollablePart():
-            with Section(f"{'Track' if self._accounts_type == 'tracked_accounts' else 'Add known'} account"):
-                yield self._accounts_input
-                yield CliveButton(
-                    "Add",
-                    variant="success",
-                    id_="save-account-button",
-                )
-
+            yield self._add_account_container
             yield ManageAccountsTable(self._accounts_type)
 
-    @on(CliveButton.Pressed, "#save-account-button")
-    async def save_account(self) -> None:
-        if not self._accounts_input.validate_passed():
-            return
+    def on_mount(self) -> None:
+        self._add_account_container.query_one(SectionBody).mount(
+            CliveButton(
+                "Track" if self._accounts_type == "tracked_accounts" else "Add",
+                variant="success",
+                id_="add-account-button",
+            )
+        )
 
-        account_name = self._accounts_input.value_or_error
-        wrapper = await self.commands.does_account_exists_in_node(account_name=account_name)
-        if wrapper.error_occurred:
-            self.notify(f"Failed to check if account {account_name} exists in the node.", severity="warning")
-            return
-
-        if not wrapper.result_or_raise:
-            self.notify(f"Account {account_name} does not exist in the node.", severity="warning")
-            return
-
-        if self._accounts_type == "tracked_accounts":
-            self.profile.accounts.add_tracked_account(account_name)
-        else:
-            self.profile.accounts.known.add(account_name)
-
-        self.app.trigger_profile_watchers()
-        self._accounts_input.input.clear()
-        self.app.update_alarms_data_asap()
+    @on(CliveButton.Pressed, "#add-account-button")
+    async def track_account(self) -> None:
+        await self._add_account_container.save_account()
