d0b38770218fcb55b18366fa9636d25b88eadd67
Mateusz Żebrak <mzebrak@syncad.com>
2024-08-06 09:23:56 +0200

    Fix `clive configure watched-account add` command resulting in error

--- clive/__private/cli/commands/configure/watched_account.py
+++ clive/__private/cli/commands/configure/watched_account.py
@@ -12,7 +12,7 @@ from clive.__private.validators.set_tracked_account_validator import SetTrackedA
 class AddWatchedAccount(ProfileBasedCommand):
     account_name: str
 
-    async def validate(self) -> None:
+    async def validate_inside_context_manager(self) -> None:
         result = SetTrackedAccountValidator(self.profile_data).validate(self.account_name)
         if not result.is_valid:
             raise CLIPrettyError(f"Can't use this account name: {humanize_validation_result(result)}", errno.EINVAL)
