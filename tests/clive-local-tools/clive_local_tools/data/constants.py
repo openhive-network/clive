e48fde16dcc735614d5108231cd6fc679a7105d9
Marcin Sobczyk <msobczyk@syncad.com>
2024-08-14 15:13:31 +0200

    Create complex authority for testnet example account

--- tests/clive-local-tools/clive_local_tools/data/constants.py
+++ tests/clive-local-tools/clive_local_tools/data/constants.py
@@ -2,7 +2,12 @@ from __future__ import annotations
 
 from typing import Final
 
-from clive_local_tools.testnet_block_log import ALT_WORKING_ACCOUNT1_DATA, WORKING_ACCOUNT_DATA
+from clive_local_tools.testnet_block_log import (
+    ALT_WORKING_ACCOUNT1_DATA,
+    ALT_WORKING_ACCOUNT2_DATA,
+    ALT_WORKING_ACCOUNT3_DATA,
+    WORKING_ACCOUNT_DATA,
+)
 
 TESTNET_CHAIN_ID: Final[str] = "18dcf0a285365fc58b71f18b3d3fec954aa0c141c44e4e5cb4cf777b9eab274e"
 
@@ -10,3 +15,7 @@ WORKING_ACCOUNT_PASSWORD: Final[str] = WORKING_ACCOUNT_DATA.account.name
 WORKING_ACCOUNT_KEY_ALIAS: Final[str] = f"{WORKING_ACCOUNT_DATA.account.name}_key"
 ALT_WORKING_ACCOUNT1_PASSWORD: Final[str] = ALT_WORKING_ACCOUNT1_DATA.account.name
 ALT_WORKING_ACCOUNT1_KEY_ALIAS: Final[str] = f"{ALT_WORKING_ACCOUNT1_DATA.account.name}_key"
+ALT_WORKING_ACCOUNT2_PASSWORD: Final[str] = ALT_WORKING_ACCOUNT2_DATA.account.name
+ALT_WORKING_ACCOUNT2_KEY_ALIAS: Final[str] = f"{ALT_WORKING_ACCOUNT2_DATA.account.name}_key"
+ALT_WORKING_ACCOUNT3_PASSWORD: Final[str] = ALT_WORKING_ACCOUNT3_DATA.account.name
+ALT_WORKING_ACCOUNT3_KEY_ALIAS: Final[str] = f"{ALT_WORKING_ACCOUNT3_DATA.account.name}_key"
