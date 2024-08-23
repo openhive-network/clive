dbad4439c1a7228f5324a8f5b7f884017444f06b
Mateusz Żebrak <mzebrak@syncad.com>
2024-08-23 12:23:48 +0200

    Make LiquidOperationChooseButton handle push_screen logic

--- clive/__private/ui/dashboard/liquid_navigation_screen.py
+++ clive/__private/ui/dashboard/liquid_navigation_screen.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Literal, cast
+from typing import TYPE_CHECKING, Callable, Literal
 
 from textual import on
 from textual.binding import Binding
@@ -19,6 +19,8 @@ if TYPE_CHECKING:
 
     from clive.models import Asset
 
+ActionType = Literal["transfer-to-account", "transfer-to-savings", "power-up"]
+
 
 def auto_switch_working_account(widget: CliveWidget, account: str | Account) -> None:
     if widget.profile.accounts.is_account_working(account):
@@ -32,11 +34,37 @@ class LiquidNavigationScreenContent(Vertical):
 
 
 class LiquidOperationChooseButton(OneLineButton):
-    def __init__(
-        self, label: str, operation: Literal["transfer-to-account", "transfer-to-savings", "power-up"]
-    ) -> None:
-        super().__init__(label=label, classes="liquid-operation-choose-button")
-        self.operation = operation
+    def __init__(self, label: str, action: ActionType, account: Account, asset_type: type[Asset.LiquidT]) -> None:
+        super().__init__(label=label)
+        self._action = action
+        self._account = account
+        self._asset_type = asset_type
+
+    @on(OneLineButton.Pressed)
+    def push_liquid_operation_screen(self) -> None:
+        action_map: dict[ActionType, Callable[[], None]] = {
+            "transfer-to-account": self._push_transfer_to_account_screen,
+            "transfer-to-savings": self._push_transfer_to_savings_screen,
+            "power-up": self._push_power_up_screen,
+        }
+
+        auto_switch_working_account(self, self._account)
+        action_map[self._action]()
+
+    def _push_transfer_to_account_screen(self) -> None:
+        from clive.__private.ui.operations.transfer_to_account.transfer_to_account import TransferToAccount
+
+        self.app.push_screen(TransferToAccount(default_asset_selected=self._asset_type))
+
+    def _push_transfer_to_savings_screen(self) -> None:
+        from clive.__private.ui.operations.savings_operations.savings_operations import Savings
+
+        self.app.push_screen(Savings(initial_tab="transfer-tab", default_asset_selected=self._asset_type))
+
+    def _push_power_up_screen(self) -> None:
+        from clive.__private.ui.operations.hive_power_management.hive_power_management import HivePowerManagement
+
+        self.app.push_screen(HivePowerManagement())
 
 
 class LiquidNavigationScreen(ModalScreen[None], CliveWidget):
@@ -50,33 +78,15 @@ class LiquidNavigationScreen(ModalScreen[None], CliveWidget):
 
     def compose(self) -> ComposeResult:
         with LiquidNavigationScreenContent(), Section("Choose liquid operation to perform"), Center():
-            yield LiquidOperationChooseButton("Transfer to account", operation="transfer-to-account")
-            yield LiquidOperationChooseButton("Transfer to savings", operation="transfer-to-savings")
-            yield LiquidOperationChooseButton("Power up", operation="power-up")
+            yield self._create_liquid_operation_choose_button("Transfer to account", "transfer-to-account")
+            yield self._create_liquid_operation_choose_button("Transfer to savings", "transfer-to-savings")
+            yield self._create_liquid_operation_choose_button("Power up", "power-up")
             yield OneLineButton("Cancel", id_="cancel-button", variant="error")
-
-    @on(LiquidOperationChooseButton.Pressed, ".liquid-operation-choose-button")
-    def push_liquid_operation_screen(self, event: LiquidOperationChooseButton.Pressed) -> None:
-        auto_switch_working_account(self, self._account)
-
-        button = cast(LiquidOperationChooseButton, event.button)
-        match button.operation:
-            case "transfer-to-account":
-                from clive.__private.ui.operations.transfer_to_account.transfer_to_account import TransferToAccount
-
-                self.app.push_screen(TransferToAccount(default_asset_selected=self._asset_type))
-            case "transfer-to-savings":
-                from clive.__private.ui.operations.savings_operations.savings_operations import Savings
-
-                self.app.push_screen(Savings(initial_tab="transfer-tab", default_asset_selected=self._asset_type))
-            case _:
-                from clive.__private.ui.operations.hive_power_management.hive_power_management import (
-                    HivePowerManagement,
-                )
-
-                self.app.push_screen(HivePowerManagement())
 
     @on(OneLineButton.Pressed, "#cancel-button")
     @on(Click)
     def action_cancel(self) -> None:
         self.app.pop_screen()
+
+    def _create_liquid_operation_choose_button(self, label: str, action: ActionType) -> LiquidOperationChooseButton:
+        return LiquidOperationChooseButton(label, action, self._account, self._asset_type)
