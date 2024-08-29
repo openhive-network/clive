7e0e7bdf553c11175b36332c47534ab6141250cf
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-08-29 09:51:29 +0200

    Apply new base dialog in switch_wrking_account

--- clive/__private/ui/dialogs/switch_working_account_dialog.py
+++ clive/__private/ui/dialogs/switch_working_account_dialog.py
@@ -4,13 +4,10 @@ from typing import TYPE_CHECKING
 
 from textual import on
 from textual.binding import Binding
-from textual.containers import Horizontal, Vertical
-from textual.screen import ModalScreen
 
+from clive.__private.ui.dialogs.clive_base_dialogs import CliveActionDialog
 from clive.__private.ui.get_css import get_relative_css_path
 from clive.__private.ui.widgets.account_managament_reference import AccountManagementReference
-from clive.__private.ui.widgets.buttons.cancel_button import CancelButton
-from clive.__private.ui.widgets.buttons.one_line_button import OneLineButton
 from clive.__private.ui.widgets.switch_working_account_container import (
     SwitchWorkingAccountContainer,
 )
@@ -19,33 +16,20 @@ if TYPE_CHECKING:
     from textual.app import ComposeResult
 
 
-class SwitchWorkingAccountDialogContent(Vertical):
-    BORDER_TITLE = "Switch working account"
-
-
-class SwitchWorkingAccountDialog(ModalScreen[None]):
+class SwitchWorkingAccountDialog(CliveActionDialog):
     CSS_PATH = [get_relative_css_path(__file__)]
     BINDINGS = [Binding("escape,f3", "cancel", "Quit")]
-
     AUTO_FOCUS = "RadioSet"
 
     def __init__(self) -> None:
-        super().__init__()
+        super().__init__(border_title="Switch working account")
         self._switch_working_account_container = SwitchWorkingAccountContainer(show_title=False)
 
-    def compose(self) -> ComposeResult:
-        with SwitchWorkingAccountDialogContent():
-            yield AccountManagementReference()
-            yield self._switch_working_account_container
-            with Horizontal(id="buttons-container"):
-                yield OneLineButton("Confirm", variant="success", id_="confirm-button")
-                yield CancelButton()
+    def create_dialog_content(self) -> ComposeResult:
+        yield AccountManagementReference()
+        yield self._switch_working_account_container
 
-    @on(OneLineButton.Pressed, "#confirm-button")
+    @on(CliveActionDialog.Confirmed)
     def confirm_selected_working_account(self) -> None:
         self._switch_working_account_container.confirm_selected_working_account()
-        self.app.pop_screen()
-
-    @on(CancelButton.Pressed, "#cancel-button")
-    def action_cancel(self) -> None:
         self.app.pop_screen()
