5e2c8f5d07bb2fb844ec6265f0469500c71b9e0f
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-09-09 12:20:29 +0200

    Remove the switch working account tab, as we can switch the working account from the header

--- clive/__private/ui/screens/config/account_management/account_management.py
+++ clive/__private/ui/screens/config/account_management/account_management.py
@@ -2,19 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from textual import on
 from textual.binding import Binding
-from textual.events import ScreenSuspend
 
 from clive.__private.ui.get_css import get_relative_css_path
 from clive.__private.ui.screens.base_screen import BaseScreen
 from clive.__private.ui.screens.config.account_management.known_accounts import KnownAccounts
-from clive.__private.ui.screens.config.account_management.switch_working_account import SwitchWorkingAccount
 from clive.__private.ui.screens.config.account_management.tracked_accounts import TrackedAccounts
 from clive.__private.ui.widgets.clive_basic import CliveTabbedContent
-from clive.__private.ui.widgets.switch_working_account_container import (
-    SwitchWorkingAccountContainer,
-)
 
 if TYPE_CHECKING:
     from textual.app import ComposeResult
@@ -30,13 +24,7 @@ class AccountManagement(BaseScreen):
     def create_main_panel(self) -> ComposeResult:
         with CliveTabbedContent():
             yield TrackedAccounts()
-            yield SwitchWorkingAccount()
             yield KnownAccounts()
 
     def on_mount(self) -> None:
         self.app.trigger_profile_watchers()
-
-    @on(CliveTabbedContent.TabActivated)
-    @on(ScreenSuspend)
-    def _confirm_selected_working_account(self) -> None:
-        self.screen.query_one(SwitchWorkingAccountContainer).confirm_selected_working_account()
