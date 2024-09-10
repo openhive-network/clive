6ea10a24270e556540329d8246c2e0f50a3c8b31
Mateusz Żebrak <mzebrak@syncad.com>
2024-09-10 14:04:29 +0200

    Get rid of unused NodeUrlHighlighter

--- clive/__private/ui/screens/config/set_node_address/set_node_address.py
+++ clive/__private/ui/screens/config/set_node_address/set_node_address.py
@@ -1,29 +1,24 @@
 from __future__ import annotations
 
 from abc import ABC
-from typing import TYPE_CHECKING, Any, Final
+from typing import TYPE_CHECKING, Any
 
-from rich.highlighter import Highlighter
 from textual import on
 from textual.binding import Binding
 from textual.containers import Container
 from textual.widgets import Select, Static
 from textual.widgets._select import NoSelection
 
-from clive.__private.core.communication import Communication
-from clive.__private.core.date_utils import utc_now
 from clive.__private.core.url import Url
-from clive.__private.models.schemas import JSONRPCRequest
 from clive.__private.ui.clive_widget import CliveWidget
 from clive.__private.ui.get_css import get_relative_css_path
 from clive.__private.ui.screens.base_screen import BaseScreen
 from clive.__private.ui.widgets.buttons.clive_button import CliveButton
 from clive.__private.ui.widgets.section import SectionScrollable
-from clive.exceptions import CommunicationError, NodeAddressError
+from clive.exceptions import NodeAddressError
 
 if TYPE_CHECKING:
     from rich.console import RenderableType
-    from rich.text import Text
     from textual.app import ComposeResult
 
 
@@ -50,36 +45,6 @@ class NodesList(Container, CliveWidget):
         yield Static("Please select the node you want to connect to from the predefined list below.")
         with self.prevent(NodeSelector.Changed):
             yield NodeSelector()
-
-
-class NodeUrlHighlighter(Highlighter):
-    def __init__(self) -> None:
-        self.__last_highlight_time = utc_now()
-        self.__last_style = "white"
-        super().__init__()
-
-    def __check_and_update_highlight_period(self) -> bool:
-        highlight_period: Final[int] = 1
-        now = utc_now()
-        if (now - self.__last_highlight_time).seconds >= highlight_period:
-            self.__last_highlight_time = now
-            return True
-        return False
-
-    def is_valid_url(self, url: str) -> bool:
-        try:
-            Communication().request(url, data=JSONRPCRequest(method="database_api.get_config"))
-        except CommunicationError:
-            return False
-        return True
-
-    def highlight(self, text: Text) -> None:
-        if self.__check_and_update_highlight_period():
-            if self.is_valid_url(str(text)):
-                self.__last_style = "green"
-            else:
-                self.__last_style = "red"
-        text.stylize(self.__last_style)
 
 
 class SetNodeAddressBase(BaseScreen, ABC):
