c925a1ea19c13173d9fafe74852cd2da808292d2
Mateusz Żebrak <mzebrak@syncad.com>
2024-09-11 15:58:43 +0200

    Create a help group for operation common options

--- clive/__private/cli/common/operation_common_options.py
+++ clive/__private/cli/common/operation_common_options.py
@@ -1,21 +1,31 @@
 from dataclasses import dataclass
+from functools import partial
 from typing import Optional
 
 import typer
 
 from clive.__private.cli.common.common_options_base import CommonOptionsBase
-from clive.__private.cli.common.parameters import options
+from clive.__private.cli.common.parameters import modified_param, options
+from clive.__private.core.constants.cli import OPERATION_COMMON_OPTIONS_PANEL_TITLE
+
+operation_common_option = partial(modified_param, rich_help_panel=OPERATION_COMMON_OPTIONS_PANEL_TITLE)
 
 
 @dataclass(kw_only=True)
 class OperationCommonOptions(CommonOptionsBase):
-    profile_name: str = options.profile_name
-    password: Optional[str] = options.password_optional
-    sign: Optional[str] = typer.Option(None, help="Key alias to sign the transaction with.", show_default=False)
-    beekeeper_remote: Optional[str] = options.beekeeper_remote
-    broadcast: bool = typer.Option(default=True, help="Whether broadcast the transaction. (i.e. dry-run)")
-    save_file: Optional[str] = typer.Option(
-        None,
-        help="The file to save the transaction to (format is determined by file extension - .bin or .json).",
-        show_default=False,
+    profile_name: str = operation_common_option(options.profile_name)
+    password: Optional[str] = operation_common_option(options.password_optional)
+    sign: Optional[str] = operation_common_option(
+        typer.Option(None, help="Key alias to sign the transaction with.", show_default=False)
+    )
+    beekeeper_remote: Optional[str] = operation_common_option(options.beekeeper_remote)
+    broadcast: bool = operation_common_option(
+        typer.Option(default=True, help="Whether broadcast the transaction. (i.e. dry-run)")
+    )
+    save_file: Optional[str] = operation_common_option(
+        typer.Option(
+            None,
+            help="The file to save the transaction to (format is determined by file extension - .bin or .json).",
+            show_default=False,
+        )
     )
--- clive/__private/core/constants/cli.py
+++ clive/__private/core/constants/cli.py
@@ -9,3 +9,5 @@ REQUIRED_AS_ARG_OR_OPTION: Final[str] = "required as argument or option"
 LOOK_INTO_ARGUMENT_OPTION_HELP: Final[str] = (
     "For more help look into argument description. This option takes precedence over positional argument."
 )
+
+OPERATION_COMMON_OPTIONS_PANEL_TITLE: Final[str] = "Operation common options"
