22e7327bd4d3443ae66490c0a91c2913cb2d69fb
Mateusz Żebrak <mzebrak@syncad.com>
2024-09-11 12:37:47 +0200

    Make it possible for set_chain_id to pass param as argument or option

--- clive/__private/cli/configure/chain_id.py
+++ clive/__private/cli/configure/chain_id.py
@@ -1,29 +1,37 @@
+from typing import Optional
+
 import typer
 
 from clive.__private.cli.clive_typer import CliveTyper
+from clive.__private.cli.common.ensure_single_value import ensure_single_value
+from clive.__private.cli.common.parameters.utils import make_argument_related_option
 from clive.__private.cli.common.profile_common_options import ProfileCommonOptions
+from clive.__private.core.constants.cli import REQUIRED_AS_ARG_OR_OPTION
 
 chain_id = CliveTyper(name="chain-id", help="Manage the chain ID for the profile.")
+
+_chain_id_argument = typer.Argument(
+    None,
+    help=(f"The chain ID to use when signing the transaction. ({REQUIRED_AS_ARG_OR_OPTION})"),
+    show_default=False,
+)
 
 
 @chain_id.command(name="set", common_options=[ProfileCommonOptions])
 async def set_chain_id(
     ctx: typer.Context,  # noqa: ARG001
-    chain_id_: str = typer.Option(
-        ...,
-        "--chain-id",
-        help=(
-            "The chain ID to use when signing the transaction. (if not provided, the one from node get_config "
-            "api will be set)"
-        ),
-        show_default=False,
-    ),
+    chain_id: Optional[str] = _chain_id_argument,
+    chain_id_option: Optional[str] = make_argument_related_option("--chain-id"),
 ) -> None:
-    """Set/change the chain ID for the profile."""
+    """
+    Set/change the chain ID for the profile.
+
+    If not set, the one from node get_config api will be retrieved and set.
+    """
     from clive.__private.cli.commands.configure.chain_id import SetChainId
 
     common = ProfileCommonOptions.get_instance()
-    await SetChainId(**common.as_dict(), chain_id=chain_id_).run()
+    await SetChainId(**common.as_dict(), chain_id=ensure_single_value(chain_id, chain_id_option, "chain-id")).run()
 
 
 @chain_id.command(name="unset", common_options=[ProfileCommonOptions])
