cf55ffd3f8a1870d2593a8acc0ef8b415f6b5dbe
Mateusz Żebrak <mzebrak@syncad.com>
2024-09-11 13:38:46 +0200

    Make it possible for set_working_account, unset_working_account  pass param as argument or option

--- clive/__private/cli/configure/working_account.py
+++ clive/__private/cli/configure/working_account.py
@@ -1,34 +1,53 @@
+from typing import Optional
+
 import typer
 
 from clive.__private.cli.clive_typer import CliveTyper
-from clive.__private.cli.common import modified_param, options
+from clive.__private.cli.common import modified_param
+from clive.__private.cli.common.ensure_single_value import ensure_single_value_account_name
+from clive.__private.cli.common.parameters import argument_related_options
 from clive.__private.cli.common.profile_common_options import ProfileCommonOptions
+from clive.__private.core.constants.cli import REQUIRED_AS_ARG_OR_OPTION
 
 working_account = CliveTyper(name="working-account", help="Manage your working account.")
+
+_account_name_set_argument = typer.Argument(
+    None,
+    help=f"The name of the account to set as working. ({REQUIRED_AS_ARG_OR_OPTION})",
+    show_default=False,
+)
 
 
 @working_account.command(name="set", common_options=[ProfileCommonOptions])
 async def set_working_account(
     ctx: typer.Context,  # noqa: ARG001
-    account_name: str = typer.Option(..., help="The name of the account to set.", show_default=False),
+    account_name: Optional[str] = _account_name_set_argument,
+    account_name_option: Optional[str] = argument_related_options.account_name,
 ) -> None:
     """Set the working account."""
     from clive.__private.cli.commands.configure.working_account import SetWorkingAccount
 
     common = ProfileCommonOptions.get_instance()
-    await SetWorkingAccount(**common.as_dict(), account_name=account_name).run()
+    await SetWorkingAccount(
+        **common.as_dict(), account_name=ensure_single_value_account_name(account_name, account_name_option)
+    ).run()
 
 
-working_account_name_to_remove_option = modified_param(options.account_name, help="The name of the account to unset.")
+_account_name_remove_argument = modified_param(
+    _account_name_set_argument, help=f"The name of the account to unset. ({REQUIRED_AS_ARG_OR_OPTION})"
+)
 
 
 @working_account.command(name="unset", common_options=[ProfileCommonOptions])
 async def unset_working_account(
     ctx: typer.Context,  # noqa: ARG001
-    account_name: str = working_account_name_to_remove_option,
+    account_name: str = _account_name_remove_argument,
+    account_name_option: Optional[str] = argument_related_options.account_name,
 ) -> None:
     """Unset the working account."""
     from clive.__private.cli.commands.configure.working_account import UnsetWorkingAccount
 
     common = ProfileCommonOptions.get_instance()
-    await UnsetWorkingAccount(**common.as_dict(), account_name=account_name).run()
+    await UnsetWorkingAccount(
+        **common.as_dict(), account_name=ensure_single_value_account_name(account_name, account_name_option)
+    ).run()
