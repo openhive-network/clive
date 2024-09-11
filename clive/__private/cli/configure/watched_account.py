4e6533708532ced5a83afd7e141b3754b0bc1cd7
Mateusz Żebrak <mzebrak@syncad.com>
2024-09-11 13:30:41 +0200

    Make it possible for add_watched_account, remove_watched_account  pass param as argument or option

--- clive/__private/cli/configure/watched_account.py
+++ clive/__private/cli/configure/watched_account.py
@@ -1,30 +1,50 @@
+from typing import Optional
+
 import typer
 
 from clive.__private.cli.clive_typer import CliveTyper
+from clive.__private.cli.common import argument_related_options, modified_param
+from clive.__private.cli.common.ensure_single_value import ensure_single_value_account_name
 from clive.__private.cli.common.profile_common_options import ProfileCommonOptions
+from clive.__private.core.constants.cli import REQUIRED_AS_ARG_OR_OPTION
 
 watched_account = CliveTyper(name="watched-account", help="Manage your watched account(s).")
+
+_account_name_add_argument = typer.Argument(
+    None, help=f"The name of the watched account to add. ({REQUIRED_AS_ARG_OR_OPTION})", show_default=False
+)
 
 
 @watched_account.command(name="add", common_options=[ProfileCommonOptions])
 async def add_watched_account(
     ctx: typer.Context,  # noqa: ARG001
-    account_name: str = typer.Option(..., help="The name of the watched account to add.", show_default=False),
+    account_name: Optional[str] = _account_name_add_argument,
+    account_name_option: Optional[str] = argument_related_options.account_name,
 ) -> None:
     """Add an account to the watched accounts."""
     from clive.__private.cli.commands.configure.watched_account import AddWatchedAccount
 
     common = ProfileCommonOptions.get_instance()
-    await AddWatchedAccount(**common.as_dict(), account_name=account_name).run()
+    await AddWatchedAccount(
+        **common.as_dict(), account_name=ensure_single_value_account_name(account_name, account_name_option)
+    ).run()
+
+
+_account_name_remove_argument = modified_param(
+    _account_name_add_argument, help=f"The name of the watched account to remove. ({REQUIRED_AS_ARG_OR_OPTION})"
+)
 
 
 @watched_account.command(name="remove", common_options=[ProfileCommonOptions])
 async def remove_watched_account(
     ctx: typer.Context,  # noqa: ARG001
-    account_name: str = typer.Option(..., help="The name of the watched account to remove.", show_default=False),
+    account_name: Optional[str] = _account_name_remove_argument,
+    account_name_option: Optional[str] = argument_related_options.account_name,
 ) -> None:
     """Remove an account from the watched accounts."""
     from clive.__private.cli.commands.configure.watched_account import RemoveWatchedAccount
 
     common = ProfileCommonOptions.get_instance()
-    await RemoveWatchedAccount(**common.as_dict(), account_name=account_name).run()
+    await RemoveWatchedAccount(
+        **common.as_dict(), account_name=ensure_single_value_account_name(account_name, account_name_option)
+    ).run()
