29a41f74e87c477eb338691286158448baf8b956
Wiesław Kędzierski <wkedzierski@syncad.com>
2024-09-12 11:24:50 +0200

    Add tests for clive process transfer and clive configure key add/remove commands and for session token.

---
+++ tests/functional/beekeeper/env_session_token/test_env_session_token.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from clive.__private.core.beekeeper import Beekeeper
+    from clive_local_tools.data.types import SessionTokenContextT
+
+
+async def test_env_beekeeper_session_token(
+    beekeeper: Beekeeper,
+    session_token_context: SessionTokenContextT,
+) -> None:
+    """Check if beekeeper will use session token provided by CLIVE_BEEKEEPER_SESSION_TOKEN."""
+    # ARRANGE
+    token = (
+        await beekeeper.api.create_session(
+            notifications_endpoint=beekeeper.notification_server_http_endpoint.as_string(with_protocol=False),
+            salt="salt",
+        )
+    ).token
+
+    assert token is not None, "There should be new token."
+    assert token != beekeeper.token, "New token, should be different than beekeepers current token."
+
+    # ACT & ASSERT
+    with session_token_context(token):
+        assert token == beekeeper.token, "New token should be used by beekeeper."
+    assert token != beekeeper.token, "Again, new token, should be different than beekeepers current token."
---
+++ tests/functional/cli/process/test_process_transfer.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Final
+
+import pytest
+
+from clive.__private.cli.exceptions import BEEKEEPER_PASSWORD_OR_SESSION_TOKEN_MUST_BE_SET_MESSAGE
+from clive_local_tools.checkers import assert_operations_placed_in_blockchain, assert_transaction_in_blockchain
+from clive_local_tools.cli.exceptions import CLITestCommandError
+from clive_local_tools.data.constants import WORKING_ACCOUNT_KEY_ALIAS, WORKING_ACCOUNT_PASSWORD
+from clive_local_tools.testnet_block_log.constants import WATCHED_ACCOUNTS_DATA, WORKING_ACCOUNT_NAME
+from schemas.operations import TransferOperation
+
+if TYPE_CHECKING:
+    from clive_local_tools.cli.cli_tester import CLITester
+
+import test_tools as tt
+
+RECEIVER: Final[str] = WATCHED_ACCOUNTS_DATA[0].account.name
+AMOUNT: Final[tt.Asset.HiveT] = tt.Asset.Hive(1)
+MEMO: Final[str] = ""
+
+
+async def test_process_transfer(
+    node: tt.RawNode,
+    cli_tester: CLITester,
+) -> None:
+    """Check clive process transfer command."""
+    # ARRANGE
+    operation = TransferOperation(
+        from_=WORKING_ACCOUNT_NAME,
+        to=RECEIVER,
+        amount=AMOUNT,
+        memo=MEMO,
+    )
+
+    # ACT
+    result = cli_tester.process_transfer(
+        from_=WORKING_ACCOUNT_NAME,
+        amount=tt.Asset.Hive(1),
+        to=RECEIVER,
+        sign=WORKING_ACCOUNT_KEY_ALIAS,
+        password=WORKING_ACCOUNT_PASSWORD,
+        memo="",
+    )
+
+    # ASSERT
+    assert_transaction_in_blockchain(node, result)
+    assert_operations_placed_in_blockchain(node, result, operation)
+
+
+async def test_process_transfer_required_password(
+    cli_tester: CLITester,
+) -> None:
+    """Check if clive process transfer requires password."""
+    # ARRANGE
+    message = BEEKEEPER_PASSWORD_OR_SESSION_TOKEN_MUST_BE_SET_MESSAGE
+
+    #  ACT & ASSERT
+    with pytest.raises(CLITestCommandError, match=message):
+        cli_tester.process_transfer(
+            from_=WORKING_ACCOUNT_NAME,
+            amount=tt.Asset.Hive(1),
+            to=RECEIVER,
+            sign=WORKING_ACCOUNT_KEY_ALIAS,
+        )
