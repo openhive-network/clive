e49f39554cf3ab4de959df09e92b0c68be78605e
Wiesław Kędzierski <wkedzierski@syncad.com>
2024-09-16 20:03:21 +0200

    Add BEEKEEPER_SESSION_TOKEN_ENV_NAME to constants.py

--- tests/clive-local-tools/clive_local_tools/data/constants.py
+++ tests/clive-local-tools/clive_local_tools/data/constants.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import Final
 
+from clive.__private.core.constants.setting_identifiers import BEEKEEPER_SESSION_TOKEN
+from clive.__private.settings import clive_prefixed_envvar
 from clive_local_tools.testnet_block_log import ALT_WORKING_ACCOUNT1_DATA, WORKING_ACCOUNT_DATA
 
 TESTNET_CHAIN_ID: Final[str] = "18dcf0a285365fc58b71f18b3d3fec954aa0c141c44e4e5cb4cf777b9eab274e"
@@ -10,3 +12,4 @@ WORKING_ACCOUNT_PASSWORD: Final[str] = WORKING_ACCOUNT_DATA.account.name
 WORKING_ACCOUNT_KEY_ALIAS: Final[str] = f"{WORKING_ACCOUNT_DATA.account.name}_key"
 ALT_WORKING_ACCOUNT1_PASSWORD: Final[str] = ALT_WORKING_ACCOUNT1_DATA.account.name
 ALT_WORKING_ACCOUNT1_KEY_ALIAS: Final[str] = f"{ALT_WORKING_ACCOUNT1_DATA.account.name}_key"
+BEEKEEPER_SESSION_TOKEN_ENV_NAME: Final[str] = clive_prefixed_envvar(BEEKEEPER_SESSION_TOKEN)
