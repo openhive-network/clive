d880563cc7ccaad34c85fb35247e8dcae4379cd9
Mateusz Kudela <mkudela@syncad.com>
2024-09-24 16:05:32 +0200

    Remove popping screen after adding operation to cart (in some screens)
    Also add notification

--- clive/__private/ui/screens/operations/operation_summary/operation_summary.py
+++ clive/__private/ui/screens/operations/operation_summary/operation_summary.py
@@ -24,6 +24,8 @@ class OperationSummary(OperationBaseScreen, OperationActionBindings, AbstractCla
 
     CSS_PATH = [get_relative_css_path(__file__)]
 
+    ADD_TO_CART_POP_SCREEN_MODE = True
+
     SECTION_TITLE: ClassVar[str] = "Operation summary"
 
     ALLOW_THE_SAME_OPERATION_IN_CART_MULTIPLE_TIMES = False
