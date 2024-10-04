da1441baa8adf7551b80e02de80f569394803a12
Wiesław Kędzierski <wkedzierski@syncad.com>
2024-10-04 14:12:26 +0200

    Introduce base.sh.template and use it as base for start_clive.sh and start_clive_cli.

--- scripts/ci-helpers/generate_startup_scripts.sh
+++ scripts/ci-helpers/generate_startup_scripts.sh
@@ -8,5 +8,13 @@ IMAGE_NAME=${1:?"Missing arg #1 to specify built docker image name"}
 
 ESCAPED_IMAGE_NAME=${IMAGE_NAME//\//\\/}
 
-cp -f "${SCRIPTPATH}/../start_clive.sh.template" "${SCRIPTPATH}/../start_clive.sh"
-sed -i -e "s/<IMG_PLACEHOLDER>/${ESCAPED_IMAGE_NAME}/g" "${SCRIPTPATH}/../start_clive.sh"
+cp -f "${SCRIPTPATH}/../templates/base.sh.template" "${SCRIPTPATH}/../templates/base.sh"
+sed -i -e "s/<IMG_PLACEHOLDER>/${ESCAPED_IMAGE_NAME}/g" "${SCRIPTPATH}/../templates/base.sh"
+
+template_files=("start_clive.sh.template" "start_clive_cli.sh.template")
+for template_file in "${template_files[@]}"; do
+  file="${template_file%.template}"
+  sed "/BASE_PLACEHOLDER/{r ${SCRIPTPATH}/../templates/base.sh
+  d}" "${SCRIPTPATH}/../templates/${template_file}" > "${SCRIPTPATH}/../${file}"
+  chmod +x "${SCRIPTPATH}/../${file}"
+done
