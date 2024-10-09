759956241fbb0185155c6dcd21297204d3d1840b
Wiesław Kędzierski <wkedzierski@syncad.com>
2024-10-09 10:35:43 +0200

    Introduce base.sh.template and use it as base for start_clive.sh and start_clive_cli.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -33,6 +33,7 @@ print_help() {
   echo "An entrypoint script for the clive docker container."
   echo "OPTIONS:"
   echo "  --cli                                 Launch clive in the interactive CLI mode (default is TUI)"
+  echo "  --exec PATH_TO_FILE                   Path to bash script to be executed."
   echo "  -h / --help                           Display this help screen and exit"
   echo
 }
@@ -47,7 +48,21 @@ wait_for_testnet() {
   echo "Testnet node is ready to use"
 }
 
+execute_passed_script(){
+  if [ -n "${FILE_TO_EXECUTE:-}" ]; then
+      if [ -f "${FILE_TO_EXECUTE}" ]; then
+          echo "Executing file: ${FILE_TO_EXECUTE}"
+          # shellcheck disable=SC1090
+          source "./${FILE_TO_EXECUTE}"
+      else
+          echo "Error: ${FILE_TO_EXECUTE} does not exist or is not a file."
+          exit 1
+      fi
+  fi
+}
+
 launch_cli() {
+  execute_passed_script
   clive --install-completion >/dev/null 2>&1
   clive beekeeper spawn # Spawn the beekeeper so commands that require it don't have to do it every time
   bash
@@ -60,6 +75,10 @@ while [ $# -gt 0 ]; do
   case "$1" in
   --cli)
     INTERACTIVE_CLI_MODE=1
+    ;;
+  --exec)
+    shift
+    FILE_TO_EXECUTE="$1"
     ;;
   -h | --help)
     print_help
