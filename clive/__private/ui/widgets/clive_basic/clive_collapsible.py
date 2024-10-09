ef7f83a3d66e53d8b5d552028305a407b5dc91f2
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-10-09 14:05:53 +0200

    Change collapsed symbol in the clive collapsible

--- clive/__private/ui/widgets/clive_basic/clive_collapsible.py
+++ clive/__private/ui/widgets/clive_basic/clive_collapsible.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from textual.widgets import Collapsible
+
+if TYPE_CHECKING:
+    from textual.widget import Widget
 
 
 class CliveCollapsible(Collapsible):
@@ -19,3 +24,27 @@ class CliveCollapsible(Collapsible):
         }
     }
     """
+
+    def __init__(
+        self,
+        *children: Widget,
+        title: str = "Toggle",
+        collapsed: bool = True,
+        collapsed_symbol: str = "▲",
+        expanded_symbol: str = "▼",
+        name: str | None = None,
+        id_: str | None = None,
+        classes: str | None = None,
+        disabled: bool = False,
+    ) -> None:
+        super().__init__(
+            *children,
+            title=title,
+            collapsed=collapsed,
+            collapsed_symbol=collapsed_symbol,
+            expanded_symbol=expanded_symbol,
+            name=name,
+            id=id_,
+            classes=classes,
+            disabled=disabled,
+        )
