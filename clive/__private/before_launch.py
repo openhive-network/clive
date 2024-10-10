8021f76558448ac17e11f471fa2a372f6fc536ef
Mateusz Żebrak <mzebrak@syncad.com>
2024-10-10 09:33:45 +0200

    Extract functons from prepare_before_launch

--- clive/__private/before_launch.py
+++ clive/__private/before_launch.py
@@ -9,15 +9,25 @@ from clive.__private.settings import safe_settings, settings
 from clive.dev import is_in_dev_mode
 
 
+def _create_clive_data_directory() -> None:
+    Path(safe_settings.data_path).mkdir(parents=True, exist_ok=True)
+
+
+def _initialize_user_settings() -> None:
+    user_settings_path = Path(safe_settings.data_path) / "settings.toml"
+    if not user_settings_path.is_file():
+        shutil.copy(ROOT_DIRECTORY.parent / "settings.toml", user_settings_path)
+
+
+def _log_in_dev_mode() -> None:
+    if is_in_dev_mode():
+        from rich.pretty import pretty_repr
+
+        logger.warning("Running in development mode.")
+        logger.debug(f"settings:\n{pretty_repr(settings.as_dict())}")
+
+
 def prepare_before_launch(*, enable_textual_logger: bool = True, enable_stream_handlers: bool = False) -> None:
-    def _create_clive_data_directory() -> None:
-        Path(safe_settings.data_path).mkdir(parents=True, exist_ok=True)
-
-    def _copy_settings() -> None:
-        user_settings_path = Path(safe_settings.data_path) / "settings.toml"
-        if not user_settings_path.is_file():
-            shutil.copy(ROOT_DIRECTORY.parent / "settings.toml", user_settings_path)
-
     if is_in_dev_mode():
         # logger also refers to settings, so we need to set it before logger setup
         settings.setenv("dev")
@@ -27,10 +37,5 @@ def prepare_before_launch(*, enable_textual_logger: bool = True, enable_stream_h
     logger.setup(enable_textual=enable_textual_logger, enable_stream_handlers=enable_stream_handlers)
 
     _create_clive_data_directory()
-    _copy_settings()
-
-    if is_in_dev_mode():
-        from rich.pretty import pretty_repr
-
-        logger.warning("Running in development mode.")
-        logger.debug(f"settings:\n{pretty_repr(settings.as_dict())}")
+    _initialize_user_settings()
+    _log_in_dev_mode()
