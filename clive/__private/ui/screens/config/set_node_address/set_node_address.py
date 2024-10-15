153c938e4323fe6f9efdf317bdacdf3271f551aa
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-10-15 14:41:55 +0200

    Refactor set_node_address

--- clive/__private/ui/screens/config/set_node_address/set_node_address.py
+++ clive/__private/ui/screens/config/set_node_address/set_node_address.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 from textual import on
 from textual.binding import Binding
@@ -13,7 +13,6 @@ from clive.__private.core.url import Url
 from clive.__private.ui.clive_widget import CliveWidget
 from clive.__private.ui.get_css import get_relative_css_path
 from clive.__private.ui.screens.base_screen import BaseScreen
-from clive.__private.ui.widgets.buttons import CliveButton
 from clive.__private.ui.widgets.section import SectionScrollable
 from clive.exceptions import NodeAddressError
 
@@ -46,31 +45,12 @@ class NodesList(Container, CliveWidget):
         with self.prevent(NodeSelector.Changed):
             yield NodeSelector()
 
-
-class SetNodeAddressBase(BaseScreen, ABC):
-    CSS_PATH = [get_relative_css_path(__file__)]
-
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-
-        self.__selected_node = SelectedNodeAddress()
-        self.__nodes_list = NodesList()
-
-    def create_main_panel(self) -> ComposeResult:
-        # Section is focusable bcs it's not possible to use scrolling via keyboard when Select is focused
-        with SectionScrollable("Set node address", focusable=True):
-            yield self.__selected_node
-            yield self.__nodes_list
-
     async def _valid_and_save_address(self) -> None:
         address = self.query_one(NodeSelector).value
         assert not isinstance(address, NoSelection), "No node was selected."
         await self.node.set_address(address)
         self.app.trigger_node_watchers()
-        self.__selected_node.refresh()
 
-    @on(NodeSelector.Changed)
-    @on(CliveButton.Pressed, "#save-node-address-button")
     async def save_node_address_with_gui_support(self) -> None:
         try:
             await self._valid_and_save_address()
@@ -81,6 +61,21 @@ class SetNodeAddressBase(BaseScreen, ABC):
             )
         else:
             self.notify(f"Node address set to `{self.node.address}`.")
+
+
+class SetNodeAddressBase(BaseScreen, ABC):
+    CSS_PATH = [get_relative_css_path(__file__)]
+
+    def create_main_panel(self) -> ComposeResult:
+        # Section is focusable bcs it's not possible to use scrolling via keyboard when Select is focused
+        with SectionScrollable("Set node address", focusable=True):
+            yield SelectedNodeAddress()
+            yield NodesList()
+
+    @on(NodeSelector.Changed)
+    async def update_selected_node_address(self) -> None:
+        self.query_one(SelectedNodeAddress).refresh()
+        await self.query_one(NodesList).save_node_address_with_gui_support()
 
 
 class SetNodeAddress(SetNodeAddressBase):
