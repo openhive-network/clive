fed7ded1d1ef26db384866b17a29c4fb4fb65cad
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-10-22 14:21:11 +0200

    Implementation of the unlock screen

---
+++ clive/__private/core/constants/tui/messages.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from typing import Final
+
+from clive.__private.ui.styling import colorize_shortcut
+
+PRESS_HELP_MESSAGE: Final[str] = (
+    f"In any moment you can press the {colorize_shortcut('F1')} button to see the Help page."
+)
