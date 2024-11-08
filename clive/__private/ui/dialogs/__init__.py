0aa6b6d7d199974470221c23d937261218c79685
Mateusz Kudela <mkudela@syncad.com>
2024-11-08 14:51:33 +0100

    Add ConfirmInvalidateSignaturesDialog

--- clive/__private/ui/dialogs/__init__.py
+++ clive/__private/ui/dialogs/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from clive.__private.ui.dialogs.add_tracked_account_dialog import AddTrackedAccountDialog
 from clive.__private.ui.dialogs.alarm_info_dialog import AlarmInfoDialog
+from clive.__private.ui.dialogs.confirm_invalidate_signatures_dialog import ConfirmInvalidateSignaturesDialog
 from clive.__private.ui.dialogs.mark_alarm_as_harmless_dialog import MarkAlarmAsHarmlessDialog
 from clive.__private.ui.dialogs.remove_key_alias_dialog import RemoveKeyAliasDialog
 from clive.__private.ui.dialogs.switch_working_account_dialog import SwitchWorkingAccountDialog
@@ -10,6 +11,7 @@ from clive.__private.ui.dialogs.witness_details_dialog import WitnessDetailsDial
 __all__ = [
     "AddTrackedAccountDialog",
     "AlarmInfoDialog",
+    "ConfirmInvalidateSignaturesDialog",
     "MarkAlarmAsHarmlessDialog",
     "RemoveKeyAliasDialog",
     "SwitchWorkingAccountDialog",
