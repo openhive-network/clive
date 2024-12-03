2b66f70ec2d1409c3d87179cbdcfa4354a34f27e
Dawid Nocinski <nocinskidawid@gmail.com>
2024-12-03 13:12:58 +0100

    Add FinalizeTransactionButton module

---
+++ clive/__private/ui/widgets/buttons/finalize_transaction_button.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from clive.__private.core.constants.tui.bindings import FINALIZE_TRANSACTION_BINDING_KEY
+from clive.__private.ui.widgets.buttons.clive_button import CliveButton
+from clive.__private.ui.widgets.buttons.one_line_button import OneLineButton
+
+
+class FinalizeTransactionButton(CliveButton):
+    DEFAULT_CSS = """
+    FinalizeTransactionButton {
+        width: 29;
+    }
+    """
+
+    class Pressed(CliveButton.Pressed):
+        """Used to identify exactly that FinalizeTransactionButton was pressed."""
+
+    def __init__(self) -> None:
+        super().__init__(
+            f"Finalize transaction ({FINALIZE_TRANSACTION_BINDING_KEY.upper()})",
+            variant="success",
+            id_="finalize-button",
+        )
+
+
+class FinalizeTransactionOneLineButton(OneLineButton, FinalizeTransactionButton):
+    class Pressed(FinalizeTransactionButton.Pressed):
+        """Message sent when FinalizeTransactionOneLineButton is pressed."""
