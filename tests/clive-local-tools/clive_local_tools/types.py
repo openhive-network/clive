cf479b7b29256d53ad70bd07fdff888cdc1a4256
Marcin Sobczyk <msobczyk@syncad.com>
2024-12-04 16:37:06 +0100

    Squash tests for beekeeper session token with other tests

--- tests/clive-local-tools/clive_local_tools/types.py
+++ tests/clive-local-tools/clive_local_tools/types.py
@@ -7,7 +7,6 @@ from clive_local_tools.data.models import WalletInfo
 if TYPE_CHECKING:
     from collections.abc import Coroutine
 
-    from clive_local_tools.cli.cli_tester import CLITester
 
 Wallets = list[WalletInfo]
 
@@ -20,7 +19,3 @@ class SetupWalletsFactory(Protocol):
 
 EnvContextFactory = Callable[[str | None], ContextManager[None]]
 GenericEnvContextFactory = Callable[[str], EnvContextFactory]
-
-
-class CLITesterWithSessionFactory(Protocol):
-    def __call__(self, *, unlocked: bool) -> CLITester: ...
