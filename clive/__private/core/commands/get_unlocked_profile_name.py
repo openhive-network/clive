f10f740b54c3633ba3b62f6b4dda0a2825c06dbb
Marcin Sobczyk <msobczyk@syncad.com>
2024-12-05 16:06:54 +0100

    Add core command getting unlocked profile name

---
+++ clive/__private/core/commands/get_unlocked_profile_name.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Literal, TypeAlias
+
+from clive.__private.core.commands.abc.command_with_result import CommandWithResult
+from clive.__private.core.commands.exceptions import MultipleProfilesUnlockedError, NoProfileUnlockedError
+
+if TYPE_CHECKING:
+    from clive.__private.core.beekeeper.handle import Beekeeper
+
+
+WalletStatus: TypeAlias = Literal["all", "locked", "unlocked"]
+
+
+@dataclass(kw_only=True)
+class GetUnlockedProfileName(CommandWithResult[str]):
+    beekeeper: Beekeeper
+
+    async def _execute(self) -> None:
+        wallets = (await self.beekeeper.api.list_wallets()).wallets
+        unlocked = [wallet.name for wallet in wallets if wallet.unlocked]
+        if len(unlocked) > 1:
+            raise MultipleProfilesUnlockedError(self)
+        if len(unlocked) < 1:
+            raise NoProfileUnlockedError(self)
+        self._result = unlocked[0]
