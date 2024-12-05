f880b418402ddd16987f229f51bd4c77a55868f9
Marcin Sobczyk <msobczyk@syncad.com>
2024-12-05 16:06:54 +0100

    Add core command getting unlocked profile name

--- clive/__private/core/commands/exceptions.py
+++ clive/__private/core/commands/exceptions.py
@@ -6,3 +6,13 @@ from clive.__private.core.commands.abc.command import Command, CommandError
 class WalletNotFoundError(CommandError):
     def __init__(self, command: Command, wallet_name: str) -> None:
         super().__init__(command, f"Wallet `{wallet_name}` not found on the beekeeper.")
+
+
+class MultipleProfilesUnlockedError(CommandError):
+    def __init__(self, command: Command) -> None:
+        super().__init__(command, "Multiple profiles are unlocked on the beekeeper.")
+
+
+class NoProfileUnlockedError(CommandError):
+    def __init__(self, command: Command) -> None:
+        super().__init__(command, "There is no unlocked profile the beekeeper.")
