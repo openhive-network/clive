b59d8bda78284df079d7489722e925d6e37cd903
Marcin Sobczyk <msobczyk@syncad.com>
2024-12-06 12:29:05 +0100

    Remove --profile-name option from show and configure commands
    those parameters will remain only in clive configure profile remove,
    clive configure profile add, clive unlock

--- clive/__private/cli/common/parameters/get_default.py
+++ clive/__private/cli/common/parameters/get_default.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import contextlib
-from typing import Any
 
 from clive.__private.cli.completion import is_tab_completion_active
 
@@ -23,11 +22,3 @@ def get_default_beekeeper_remote() -> str | None:
         address = Beekeeper.get_remote_address_from_settings() or Beekeeper.get_remote_address_from_connection_file()
         return str(address) if address else None
     return None
-
-
-def get_default_or_make_required(value: Any) -> Any:  # noqa: ANN401
-    return ... if value is None else value
-
-
-def get_default_or_make_optional(value: Any) -> Any:  # noqa: ANN401
-    return value if value else None
