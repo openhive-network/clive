c533774c5f1a7d72a04cb0389ca7e36f32904952
Wiesław Kędzierski <wkedzierski@syncad.com>
2024-12-09 13:04:26 +0100

    Avoid running clive TUI while in env set by start_clive_cli.sh
    We assume that user will not set CLIVE_BEEKEEPEER__SESSION_TOKEN by
    himself. But it will be set by start_clive_cli.sh
    
    Clive TUI will not get notifications from beekeeper, while env var
    CLIVE_BEEKEEPER__SESSION_TOKEN is set. It because, it does not own the
    notification server endpoint that it is needed while creating session
    token.

--- clive/__private/run_tui.py
+++ clive/__private/run_tui.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import warnings
 
+import typer
+
 
 def _hide_never_awaited_warnings_in_non_dev_mode() -> None:
     from clive.dev import is_in_dev_mode
@@ -10,9 +12,28 @@ def _hide_never_awaited_warnings_in_non_dev_mode() -> None:
         warnings.filterwarnings("ignore", message=".* was never awaited")
 
 
+def _check_for_beekeeper_session_token_set() -> None:
+    import sys
+
+    from clive.__private.settings import safe_settings
+
+    if safe_settings.beekeeper.is_session_token_set:
+        # We assume that user will not set CLIVE_BEEKEEPEER__SESSION_TOKEN by himself.
+        # But it will be set by using start_clive_cli.sh
+        #
+        message = (
+            "You cannot run Clive TUI directly from the start_clive_cli.sh script.\n"
+            "Please close it, and then use start_clive.sh script instead."
+        )
+        typer.echo(message)
+        sys.exit(1)
+
+
 def run_tui() -> None:
     from clive.__private.before_launch import prepare_before_launch
     from clive.__private.ui.app import Clive
+
+    _check_for_beekeeper_session_token_set()
 
     _hide_never_awaited_warnings_in_non_dev_mode()
     prepare_before_launch()
