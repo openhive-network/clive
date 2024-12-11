47d9bb429192d5f115cc117bbbc7c88084a7edcb
Mateusz Kudela <mkudela@syncad.com>
2024-12-11 10:19:04 +0100

    Add MarkAlarmAsHarmlessDialog

--- clive/__private/ui/dialogs/__init__.py
+++ clive/__private/ui/dialogs/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from clive.__private.ui.dialogs.add_tracked_account_dialog import AddTrackedAccountDialog
 from clive.__private.ui.dialogs.alarm_info_dialog import AlarmInfoDialog
+from clive.__private.ui.dialogs.mark_alarm_as_harmless_dialog import MarkAlarmAsHarmlessDialog
 from clive.__private.ui.dialogs.remove_key_alias_dialog import RemoveKeyAliasDialog
 from clive.__private.ui.dialogs.switch_working_account_dialog import SwitchWorkingAccountDialog
 from clive.__private.ui.dialogs.witness_details_dialog import WitnessDetailsDialog
@@ -9,6 +10,7 @@ from clive.__private.ui.dialogs.witness_details_dialog import WitnessDetailsDial
 __all__ = [
     "AddTrackedAccountDialog",
     "AlarmInfoDialog",
+    "MarkAlarmAsHarmlessDialog",
     "RemoveKeyAliasDialog",
     "SwitchWorkingAccountDialog",
     "WitnessDetailsDialog",
---
+++ clive/__private/ui/dialogs/mark_alarm_as_harmless_dialog.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from textual import on
+
+from clive.__private.ui.dialogs.confirm_action_dialog import ConfirmActionDialog
+
+if TYPE_CHECKING:
+    from clive.__private.core.alarms.alarm import AnyAlarm
+
+
+class MarkAlarmAsHarmlessDialog(ConfirmActionDialog):
+    """Dialog to confirm if the user wants to mark alarm as harmless."""
+
+    def __init__(self, alarm: AnyAlarm) -> None:
+        self._alarm = alarm
+        super().__init__(
+            border_title="Marking the alarm as harmless",
+            confirm_question=(
+                f"You are about to mark `{self.alarm_info}` alarm as harmless.\nAre you sure you want to proceed?"
+            ),
+        )
+
+    @property
+    def alarm_info(self) -> str:
+        return self._alarm.get_alarm_basic_info()
+
+    @on(ConfirmActionDialog.Confirmed)
+    def mark_alarm_as_harmless(self) -> None:
+        self._alarm.is_harmless = True
+        self.notify(f"Alarm `{self.alarm_info}` was marked as harmless.")
+        self.app.trigger_profile_watchers()
+        self.app.pop_screen()
