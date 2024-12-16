be844aca9931812131a51c9cd8e599e9cb82379e
Mateusz Kudela <mkudela@syncad.com>
2024-12-16 12:44:40 +0100

    Add some bindings to constants

--- clive/__private/core/constants/tui/bindings.py
+++ clive/__private/core/constants/tui/bindings.py
@@ -4,3 +4,7 @@ from typing import Final
 
 NEXT_SCREEN_BINDING_KEY: Final[str] = "ctrl+n"
 PREVIOUS_SCREEN_BINDING_KEY: Final[str] = "ctrl+p"
+_F2_BINDING_KEY: Final[str] = "f2"
+ADD_TO_CART_BINDING_KEY: Final[str] = _F2_BINDING_KEY
+SELECT_FILE_BINDING_KEY: Final[str] = _F2_BINDING_KEY
+MANAGE_KEY_ALIASES_BINDING_KEY: Final[str] = _F2_BINDING_KEY
