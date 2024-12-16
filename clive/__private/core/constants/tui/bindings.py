5dccb509e64b4259392ff3ffea10f7763018a0ba
Mateusz Kudela <mkudela@syncad.com>
2024-12-16 12:44:40 +0100

    Move `f2` bindings to the constants package

--- clive/__private/core/constants/tui/bindings.py
+++ clive/__private/core/constants/tui/bindings.py
@@ -5,3 +5,7 @@ from typing import Final
 NEXT_SCREEN_BINDING_KEY: Final[str] = "ctrl+n"
 PREVIOUS_SCREEN_BINDING_KEY: Final[str] = "ctrl+p"
 FINALIZE_TRANSACTION_BINDING_KEY: Final[str] = "f6"
+_F2_BINDING_KEY: Final[str] = "f2"
+ADD_TO_CART_BINDING_KEY: Final[str] = _F2_BINDING_KEY
+SELECT_FILE_BINDING_KEY: Final[str] = _F2_BINDING_KEY
+MANAGE_KEY_ALIASES_BINDING_KEY: Final[str] = _F2_BINDING_KEY
