8b8ae639d057f47435cf2d8989630420e5fdc58e
Jakub Ziebinski <ziebinskijakub@gmail.com>
2025-01-21 13:18:45 +0100

    Enable command palette

--- clive/__private/ui/app.py
+++ clive/__private/ui/app.py
@@ -50,7 +50,7 @@ class Clive(App[int]):
 
     AUTO_FOCUS = "*"
 
-    ENABLE_COMMAND_PALETTE = False
+    ENABLE_COMMAND_PALETTE = True
 
     BINDINGS = [
         Binding("ctrl+s", "app.screenshot()", "Screenshot", show=False),
