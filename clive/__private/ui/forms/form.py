5ab82f7cbb9e0aea7657f8808552109ee9f21003
Mateusz Żebrak <mzebrak@syncad.com>
2025-01-24 14:29:48 +0100

    Rename misleading screens -> screen_types in Form

--- clive/__private/ui/forms/form.py
+++ clive/__private/ui/forms/form.py
@@ -19,20 +19,20 @@ class Form(CliveScreen):
 
     def __init__(self) -> None:
         self._current_screen_index = 0
-        self._screens: list[type[FormScreenBase]] = [*list(self.register_screen_builders())]
-        self._skipped_screens: set[type[FormScreenBase]] = set()
-        assert len(self._screens) >= self.MINIMUM_SCREEN_COUNT, "Form must have at least 2 screens"
+        self._screen_types: list[type[FormScreenBase]] = [*list(self.register_screen_builders())]
+        self._skipped_screen_types: set[type[FormScreenBase]] = set()
+        assert len(self._screen_types) >= self.MINIMUM_SCREEN_COUNT, "Form must have at least 2 screens"
         self._post_actions = Queue[PostAction]()
 
         super().__init__()
 
     @property
-    def screens(self) -> list[type[FormScreenBase]]:
-        return self._screens
+    def screens_types(self) -> list[type[FormScreenBase]]:
+        return self._screen_types
 
     @property
-    def current_screen(self) -> type[FormScreenBase]:
-        return self._screens[self._current_screen_index]
+    def current_screen_type(self) -> type[FormScreenBase]:
+        return self._screen_types[self._current_screen_index]
 
     async def on_mount(self) -> None:
         assert self._current_screen_index == 0
@@ -55,7 +55,7 @@ class Form(CliveScreen):
         self._current_screen_index += 1
 
         if self._is_current_screen_to_skip():
-            self._skipped_screens.add(self.current_screen)
+            self._skipped_screen_types.add(self.current_screen_type)
             self.action_next_screen()
             return
 
@@ -68,7 +68,7 @@ class Form(CliveScreen):
         self._current_screen_index -= 1
 
         if self._is_current_screen_skipped():
-            self._skipped_screens.discard(self.current_screen)
+            self._skipped_screen_types.discard(self.current_screen_type)
             self.action_previous_screen()
             return
 
@@ -76,16 +76,16 @@ class Form(CliveScreen):
         self.app.pop_screen()
 
     def _is_current_screen_to_skip(self) -> bool:
-        return self.current_screen in self._skip_during_push_screen()
+        return self.current_screen_type in self._skip_during_push_screen()
 
     def _is_current_screen_skipped(self) -> bool:
-        return self.current_screen in self._skipped_screens
+        return self.current_screen_type in self._skipped_screen_types
 
     def _push_current_screen(self) -> None:
-        self.app.push_screen(self.current_screen(self))
+        self.app.push_screen(self.current_screen_type(self))
 
     def _check_valid_range(self, proposed_idx: int) -> bool:
-        return (proposed_idx >= 0) and (proposed_idx < len(self._screens))
+        return (proposed_idx >= 0) and (proposed_idx < len(self._screen_types))
 
     @abstractmethod
     def register_screen_builders(self) -> Iterator[type[FormScreenBase]]:
