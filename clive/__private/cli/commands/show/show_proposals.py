d1bf52764f6c36a145aa5675abbe6d8b2ea742f1
Wiesław Kędzierski <wkedzierski@syncad.com>
2025-01-27 12:01:08 +0100

    Add number of page for clive show proposals command.

--- clive/__private/cli/commands/show/show_proposals.py
+++ clive/__private/cli/commands/show/show_proposals.py
@@ -5,6 +5,7 @@ from rich.console import Console
 from rich.table import Table
 
 from clive.__private.cli.commands.abc.world_based_command import WorldBasedCommand
+from clive.__private.cli.table_pagination_info import add_pagination_info_if_needed
 from clive.__private.core.commands.data_retrieval.proposals_data import ProposalsDataRetrieval
 from clive.__private.core.formatters.humanize import humanize_bool
 
@@ -61,4 +62,8 @@ class ShowProposals(WorldBasedCommand):
                 f"{proposal.pretty_end_date}",
             )
         console = Console()
-        console.print(table)
+        console.print(
+            add_pagination_info_if_needed(
+                table=table, page_no=self.page_no, page_size=self.page_size, all_entries=len(proposals_data.proposals)
+            )
+        )
