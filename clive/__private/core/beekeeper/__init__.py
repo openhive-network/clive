d815e73cbaf9ec7cfa678c31a5e6c95c49aeec0f
kmochocki <kmochocki@syncad.com>
2025-01-27 11:03:40 +0000

    Remove redundancy between clive and helpy/beekeepy

--- clive/__private/core/beekeeper/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .handle import Beekeeper
-
-__all__ = ["Beekeeper"]
--- tests/functional/beekeeper/pattern_tests/patterns/generate_config_pattern.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from pathlib import Path
-
-from clive.__private.core.beekeeper import Beekeeper
-
-if __name__ == "__main__":
-    config_pattern_file = Path(__file__).parent / "config.ini"
-    Beekeeper().generate_beekeepers_default_config(destination=config_pattern_file)
