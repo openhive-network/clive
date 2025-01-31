9733c4f0f9717883008542cb05c7cfcfb11d9e0a
Mateusz Żebrak <mzebrak@syncad.com>
2025-01-31 15:27:27 +0100

    fixup! Use beekeepey in conrmal commands

--- clive/__private/core/commands/lock.py
+++ clive/__private/core/commands/lock.py
@@ -3,18 +3,15 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
-from clive.__private.core.commands.abc.command import Command
+from clive.__private.core.commands.abc.command_in_unlocked import CommandInUnlocked
 
 if TYPE_CHECKING:
-    from beekeepy import AsyncUnlockedWallet
-
     from clive.__private.core.app_state import AppState
 
 
 @dataclass(kw_only=True)
-class Lock(Command):
+class Lock(CommandInUnlocked):
     app_state: AppState
-    unlocked_wallet: AsyncUnlockedWallet
 
     async def _execute(self) -> None:
         await self.unlocked_wallet.lock()
