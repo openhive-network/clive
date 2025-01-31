c68c9fd4ac19f979f2766db22ad5513bc543c304
Mateusz Żebrak <mzebrak@syncad.com>
2025-01-31 08:52:23 +0100

    Properly handle CommandRequiresUnlockedModeError in TUI
    This could happen with the new mechanism of determining if clive should
    be locked/unlocked. Because we no longer receive notification, but we
    ask beekeeper ourselfs, there might be a time window between 2 queries
    in which user makes action that requires unlocked mode and Clive would
    crash.
    
    See: https://gitlab.syncad.com/hive/clive/-/merge_requests/553#note_195770

---
+++ clive/__private/core/error_handlers/tui_error_handler.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import Any, Callable, TypeGuard
+
+from clive.__private.core.clive_import import get_clive
+from clive.__private.core.commands.abc.command_in_unlocked import CommandRequiresUnlockedModeError
+from clive.__private.core.error_handlers.abc.error_handler_context_manager import (
+    ErrorHandlerContextManager,
+    ResultNotAvailable,
+)
+
+Action = Callable[[], Any]
+
+
+class TUIErrorHandler(ErrorHandlerContextManager[Exception]):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._exception_action_map: dict[type[Exception], Action] = {
+            CommandRequiresUnlockedModeError: self._switch_to_locked_mode,
+        }
+
+        self._app = get_clive().app_instance()
+        self._action: Action | None = None
+
+    def _is_exception_to_catch(self, error: Exception) -> TypeGuard[Exception]:
+        for exception_type, action in self._exception_action_map.items():
+            if isinstance(error, exception_type):
+                self._action = action
+                return True
+        return False
+
+    def _handle_error(self, error: Exception) -> ResultNotAvailable:
+        if self._action is not None:
+            self._action()
+        return ResultNotAvailable(error)
+
+    def _switch_to_locked_mode(self) -> None:
+        self._app.world.app_state.lock()
