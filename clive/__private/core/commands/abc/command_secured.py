398387606a58854fc5762ebaeca722787ee30774
Mateusz Żebrak <mzebrak@syncad.com>
2025-02-03 09:35:48 +0100

    fixup! Use beekeepey in conrmal commands

--- clive/__private/core/commands/abc/command_secured.py
+++ clive/__private/core/commands/abc/command_secured.py
@@ -8,6 +8,10 @@ from clive.__private.core.commands.abc.command import Command
 
 @dataclass(kw_only=True)
 class CommandPasswordSecured(Command, ABC):
-    """A command that requires a password to proceed."""
+    """
+    A command that requires a password to proceed.
+
+    In case the password is invalid, there could be `beekeepy.exceptions.InvalidPasswordError` raised.
+    """
 
     password: str
