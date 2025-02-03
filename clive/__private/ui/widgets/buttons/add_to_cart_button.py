3c3c8e0e61d3c6e32cab705bb977a8f6c25f9346
Mateusz Kudela <mkudela@syncad.com>
2025-02-03 13:50:26 +0100

    Introduce ADD_OPERATION_TO_CART_BINDING_KEY

--- clive/__private/ui/widgets/buttons/add_to_cart_button.py
+++ clive/__private/ui/widgets/buttons/add_to_cart_button.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from clive.__private.core.constants.tui.bindings import ADD_OPERATION_TO_CART_BINDING_KEY
 from clive.__private.ui.widgets.buttons.clive_button import CliveButton
 from clive.__private.ui.widgets.buttons.one_line_button import OneLineButton
 
@@ -15,7 +16,9 @@ class AddToCartButton(CliveButton):
         """Message send when AddToCartButton is pressed."""
 
     def __init__(self) -> None:
-        super().__init__("Add to cart (F2)", id_="add-to-cart-button", variant="success")
+        super().__init__(
+            f"Add to cart ({ADD_OPERATION_TO_CART_BINDING_KEY.upper()})", id_="add-to-cart-button", variant="success"
+        )
 
 
 class AddToCartOneLineButton(OneLineButton, AddToCartButton):
