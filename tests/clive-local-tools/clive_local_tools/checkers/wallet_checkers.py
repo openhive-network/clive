64caafdfe433bb9144db68a651c1577446503cdf
Mateusz Żebrak <mzebrak@syncad.com>
2025-02-03 13:40:39 +0100

    fixup! Adjust tests to new implementation
    Refactor assert_wallets_locked/assert_wallet_unlocked

--- tests/clive-local-tools/clive_local_tools/checkers/wallet_checkers.py
+++ tests/clive-local-tools/clive_local_tools/checkers/wallet_checkers.py
@@ -2,17 +2,19 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from clive.__private.core.commands.is_wallet_unlocked import IsWalletUnlocked
+
 if TYPE_CHECKING:
     from beekeepy import AsyncBeekeeper
 
 
 async def assert_wallets_locked(beekeeper: AsyncBeekeeper) -> None:
     wallets = await (await beekeeper.session).wallets
-    locked_wallets = [(await w.unlocked) is None for w in wallets]
+    locked_wallets = [not await IsWalletUnlocked(wallet=wallet).execute_with_result() for wallet in wallets]
     assert all(locked_wallets), "All wallets should be locked."
 
 
 async def assert_wallet_unlocked(beekeeper: AsyncBeekeeper, wallet_name: str) -> None:
-    wallets = await (await beekeeper.session).wallets
-    unlocked_wallets = [((wallet.name == wallet_name) and ((await wallet.unlocked) is not None)) for wallet in wallets]
-    assert any(unlocked_wallets), f"Wallet {wallet_name} should be unlocked."
+    unlocked_wallets = await (await beekeeper.session).wallets_unlocked
+    unlocked_wallet_names = [wallet.name for wallet in unlocked_wallets]
+    assert wallet_name in unlocked_wallet_names, f"Wallet `{wallet_name}` should be unlocked."
