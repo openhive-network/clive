6886663a026994e0ff88aee80a031799bc805a78
Mateusz Żebrak <mzebrak@syncad.com>
2025-02-04 10:20:11 +0100

    Logging improvement - add LOG_LEVEL_1ST_PARTY
    So we can have 2 filters - not only for 3RD party packages, but also
    our known 1ST party packages like helpy and beekeepy.

--- clive/__private/core/constants/env.py
+++ clive/__private/core/constants/env.py
@@ -7,3 +7,12 @@ from typing import Final
 ROOT_DIRECTORY: Final[Path] = Path(__file__).parent.parent.parent.parent
 LAUNCH_TIME: Final[datetime] = datetime.now()  # noqa: DTZ005; we want to use the local timezone
 ENVVAR_PREFIX: Final[str] = "CLIVE"
+
+KNOWN_FIRST_PARTY_PACKAGES: Final[list[str]] = [
+    "beekeepy",
+    "clive_local_tools",
+    "helpy",
+    "schemas",
+    "test_tools",
+    "wax",
+]
