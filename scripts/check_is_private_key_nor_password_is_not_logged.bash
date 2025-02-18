c41fda9211ba3e855fe1096440211827f11ee8bc
Mateusz Żebrak <mzebrak@syncad.com>
2025-02-18 18:33:15 +0100

    Refactor check_is_private_key_nor_password_is_not_logged - extract exclude_patterns local list

--- scripts/check_is_private_key_nor_password_is_not_logged.bash
+++ scripts/check_is_private_key_nor_password_is_not_logged.bash
@@ -3,12 +3,17 @@
 set -euo pipefail
 
 function find_password_private_keys() {
+     local exclude_patterns=(
+        "Error in response from url"
+        "Problem occurred during communication with"
+        "CI_"
+    )
+
     find . -path "*/clive/*/latest.log" -print0 |
     xargs -0 \
     grep --with-filename --line-number --ignore-case --word-regexp  --extended-regexp \
         '(pass(word)?|[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{51})' |
-    grep "$@" --invert-match --extended-regexp \
-        'Error in response from url|Problem occurred during communication with|CI_' || true
+    grep --invert-match --extended-regexp "$(IFS='|'; echo "${exclude_patterns[*]}")" || true
 }
 
 amount_of_occurrences=$(find_password_private_keys --count)
