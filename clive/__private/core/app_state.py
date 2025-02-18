a5ba6b05502c2008f8058234d6e5299bce8d1b23
Mateusz Żebrak <mzebrak@syncad.com>
2025-02-18 11:33:55 +0100

    Clear wallets when locking

--- clive/__private/core/app_state.py
+++ clive/__private/core/app_state.py
@@ -42,6 +42,7 @@ class AppState:
             return
 
         self._is_unlocked = False
+        self.world.wallets.clear_wallets()
         self.world.on_going_into_locked_mode(source)
         logger.info("Mode switched to LOCKED.")
 
