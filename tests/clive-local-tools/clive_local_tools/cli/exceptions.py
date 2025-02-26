60a4a0312c2d2305f1afb1523ea4ed6eb50ccbed
Mateusz Żebrak <mzebrak@syncad.com>
2025-02-26 12:11:34 +0100

    Improve CLITestCommandError message by including traceback

--- tests/clive-local-tools/clive_local_tools/cli/exceptions.py
+++ tests/clive-local-tools/clive_local_tools/cli/exceptions.py
@@ -1,16 +1,22 @@
 from __future__ import annotations
 
+import traceback
 from typing import TYPE_CHECKING, TypeAlias, get_args
 
 if TYPE_CHECKING:
+    from types import TracebackType
+
     from click.testing import Result
 
 
 class CLITestCommandError(AssertionError):
     def __init__(self, command: list[str], exit_code: int, stdout: str, result: Result) -> None:
-        message = f"command {command} failed because of {exit_code=}. Output:\n{stdout}"
+        message = f"Command {command} failed because of {exit_code=}.\n\nOutput:\n{stdout}"
         if result.exception:
-            message += f"\nException occurred:\n{result.exception!r}"
+            assert result.exc_info is not None, "exc_info should be set when exception is set."
+            tb: TracebackType = result.exc_info[2]
+            tb_formatted = "".join(traceback.format_tb(tb))
+            message += f"\nException:\n{result.exception!r}\n\nTraceback:\n{tb_formatted}"
         super().__init__(message)
         self.command = command
         self.exit_code = exit_code
