8c0e58c19fc22c4f69e5cbd1a2590419b5c7d96c
Wiesław Kędzierski <wkedzierski@syncad.com>
2025-02-26 09:31:51 +0100

    Use cli_profile_checker fixture in tests.

--- tests/functional/cli/configure/test_configure_tracked_account.py
+++ tests/functional/cli/configure/test_configure_tracked_account.py
@@ -4,7 +4,6 @@ from typing import TYPE_CHECKING, Final
 
 import pytest
 
-from clive_local_tools.checkers.profile_accounts_checker import ProfileAccountsChecker
 from clive_local_tools.cli.exceptions import CLITestCommandError
 from clive_local_tools.testnet_block_log.constants import (
     ALT_WORKING_ACCOUNT1_NAME,
@@ -13,25 +12,26 @@ from clive_local_tools.testnet_block_log.constants import (
 )
 
 if TYPE_CHECKING:
+    from clive_local_tools.checkers.profile_accounts_checker import ProfileAccountsChecker
     from clive_local_tools.cli.cli_tester import CLITester
 
 PROFILE_NAME: Final[str] = WORKING_ACCOUNT_NAME
 ACCOUNT_TO_REMOVE: Final[str] = WATCHED_ACCOUNTS_NAMES[0]
 
 
-async def test_configure_tracked_account_add(cli_tester: CLITester) -> None:
+async def test_configure_tracked_account_add(
+    cli_tester: CLITester, cli_profile_checker: ProfileAccountsChecker
+) -> None:
     """Check clive configure tracked-account add command."""
     # ARRANGE
     account_to_add = ALT_WORKING_ACCOUNT1_NAME
-    profile_name = cli_tester.world.profile.name
-    profile_checker = ProfileAccountsChecker(profile_name, cli_tester.world.wallets._content)
 
     # ACT
     cli_tester.configure_tracked_account_add(account_name=account_to_add)
 
     # ASSERT
-    await profile_checker.assert_in_tracked_accounts(account_names=[account_to_add])
-    await profile_checker.assert_in_known_accounts(account_names=[account_to_add])
+    await cli_profile_checker.assert_in_tracked_accounts(account_names=[account_to_add])
+    await cli_profile_checker.assert_in_known_accounts(account_names=[account_to_add])
 
 
 async def test_configure_tracked_account_add_already_tracked_account(cli_tester: CLITester) -> None:
@@ -48,29 +48,28 @@ async def test_configure_tracked_account_add_already_tracked_account(cli_tester:
         cli_tester.configure_tracked_account_add(account_name=account_to_add)
 
 
-async def test_configure_tracked_account_remove(cli_tester: CLITester) -> None:
+async def test_configure_tracked_account_remove(
+    cli_tester: CLITester, cli_profile_checker: ProfileAccountsChecker
+) -> None:
     """Check clive configure tracked-account remove command."""
     # ARRANGE
-    profile_name = cli_tester.world.profile.name
-    profile_checker = ProfileAccountsChecker(profile_name, cli_tester.world.wallets._content)
-
     # ACT
-    await profile_checker.assert_in_tracked_accounts(account_names=[ACCOUNT_TO_REMOVE])
+    await cli_profile_checker.assert_in_tracked_accounts(account_names=[ACCOUNT_TO_REMOVE])
     cli_tester.configure_tracked_account_remove(account_name=ACCOUNT_TO_REMOVE)
 
     # ASSERT
-    await profile_checker.assert_not_in_tracked_accounts(account_names=[ACCOUNT_TO_REMOVE])
+    await cli_profile_checker.assert_not_in_tracked_accounts(account_names=[ACCOUNT_TO_REMOVE])
 
 
-async def test_configure_tracked_account_remove_with_already_removed_account(cli_tester: CLITester) -> None:
+async def test_configure_tracked_account_remove_with_already_removed_account(
+    cli_tester: CLITester, cli_profile_checker: ProfileAccountsChecker
+) -> None:
     """Check clive configure tracked-account remove command with already removed account."""
     # ARRANGE
     message = f"Account {ACCOUNT_TO_REMOVE} not found."
-    profile_name = cli_tester.world.profile.name
-    profile_checker = ProfileAccountsChecker(profile_name, cli_tester.world.wallets._content)
 
     # ACT
-    await profile_checker.assert_in_tracked_accounts(account_names=[ACCOUNT_TO_REMOVE])
+    await cli_profile_checker.assert_in_tracked_accounts(account_names=[ACCOUNT_TO_REMOVE])
     cli_tester.configure_tracked_account_remove(account_name=ACCOUNT_TO_REMOVE)
 
     # ASSERT
--- tests/functional/cli/configure/test_configure_working_account.py
+++ tests/functional/cli/configure/test_configure_working_account.py
@@ -4,30 +4,30 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from clive_local_tools.checkers.profile_accounts_checker import ProfileAccountsChecker
 from clive_local_tools.cli.exceptions import CLITestCommandError
 from clive_local_tools.testnet_block_log.constants import WATCHED_ACCOUNTS_NAMES, WORKING_ACCOUNT_NAME
 
 if TYPE_CHECKING:
+    from clive_local_tools.checkers.profile_accounts_checker import ProfileAccountsChecker
     from clive_local_tools.cli.cli_tester import CLITester
 
 
-async def test_configure_working_account_switch(cli_tester: CLITester) -> None:
+async def test_configure_working_account_switch(
+    cli_tester: CLITester, cli_profile_checker: ProfileAccountsChecker
+) -> None:
     """Check clive configure working-account switch command."""
     # ARRANGE
-    profile_name = cli_tester.world.profile.name
-    profile_account_checker = ProfileAccountsChecker(profile_name, cli_tester.world.wallets._content)
     account_to_switch = WATCHED_ACCOUNTS_NAMES[0]
 
     # ACT
-    await profile_account_checker.assert_working_account(working_account=WORKING_ACCOUNT_NAME)
-    await profile_account_checker.assert_in_tracked_accounts(account_names=[account_to_switch])
+    await cli_profile_checker.assert_working_account(working_account=WORKING_ACCOUNT_NAME)
+    await cli_profile_checker.assert_in_tracked_accounts(account_names=[account_to_switch])
 
     cli_tester.configure_working_account_switch(account_name=account_to_switch)
 
     # ASSERT
-    await profile_account_checker.assert_working_account(working_account=account_to_switch)
-    await profile_account_checker.assert_in_tracked_accounts(account_names=[account_to_switch])
+    await cli_profile_checker.assert_working_account(working_account=account_to_switch)
+    await cli_profile_checker.assert_in_tracked_accounts(account_names=[account_to_switch])
 
 
 async def test_configure_working_account_switch_same_account(cli_tester: CLITester) -> None:
