664326b06afc6b57b51ad09436c6628c0441e5ce
Mateusz Żebrak <mzebrak@syncad.com>
2025-03-04 09:21:45 +0100

    Add workaround for CanFocusWithScrollbarsOnly missing focus

--- clive/__private/ui/widgets/scrolling.py
+++ clive/__private/ui/widgets/scrolling.py
@@ -25,6 +25,17 @@ class CanFocusWithScrollbarsOnly(CliveWidget, AbstractClassMessagePump):
     def __enable_focus_only_when_scrollbar_is_active(self) -> None:
         self.can_focus = any(self.scrollbars_enabled)
 
+        # START WORKAROUND FOR https://github.com/Textualize/textual/issues/5605>>>
+        # based on:
+        # https://github.com/Textualize/textual/blob/dd36b696ecf2c3f7ae75b1f63671ecab1c8f04cb/src/textual/screen.py#L1324
+        auto_focus = self.app.AUTO_FOCUS if self.screen.AUTO_FOCUS is None else self.screen.AUTO_FOCUS
+        if auto_focus and self.app.focused is None:
+            for widget in self.screen.query(auto_focus):
+                if widget.focusable:
+                    self.screen.set_focus(widget)
+                    break
+        # <<<END WORKAROUND
+
 
 class ScrollablePartFocusable(VerticalScroll, CanFocusWithScrollbarsOnly):
     """Scrollable part of screens that does not contain elements to focus e.g.: Cart."""
