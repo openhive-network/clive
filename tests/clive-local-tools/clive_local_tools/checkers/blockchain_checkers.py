f0cc976c8004f7d745105d5500e0af9d08161295
Radek Masłowski <rmaslowski@syncad.com>
2025-03-12 11:50:38 +0100

    Implement new schemas to clive tests

--- tests/clive-local-tools/clive_local_tools/checkers/blockchain_checkers.py
+++ tests/clive-local-tools/clive_local_tools/checkers/blockchain_checkers.py
@@ -3,16 +3,15 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pytest
-
-if TYPE_CHECKING:
-    import test_tools as tt
-
-    from clive.__private.models.schemas import OperationUnion, RepresentationBase
-
 from beekeepy.exceptions import ErrorInResponseError
 from click.testing import Result
 
 from clive_local_tools.helpers import get_transaction_id_from_output
+
+if TYPE_CHECKING:
+    import test_tools as tt
+
+    from clive.__private.models.schemas import OperationUnion, TransactionInBlockchain
 
 
 def _ensure_transaction_id(trx_id_or_result: Result | str) -> str:
@@ -42,14 +41,14 @@ def assert_operations_placed_in_blockchain(
 ) -> None:
     assert_transaction_in_blockchain(node, trx_id_or_result, wait_for_the_next_block=wait_for_the_next_block)
     transaction_id = _ensure_transaction_id(trx_id_or_result)
-    transaction = node.api.account_history.get_transaction(
+    transaction: TransactionInBlockchain = node.api.account_history.get_transaction(
         id=transaction_id,
         include_reversible=True,  # type: ignore[call-arg] # TODO: id -> id_ after helpy bug fixed
     )
     operations_to_check = list(expected_operations)
+
     for operation_representation in transaction.operations:
-        _operation_representation: RepresentationBase[OperationUnion] = operation_representation
-        operation = _operation_representation.value
+        operation = operation_representation.value
         if operation in operations_to_check:
             operations_to_check.remove(operation)
 
--- tests/clive-local-tools/clive_local_tools/tui/choose_asset_token.py
+++ tests/clive-local-tools/clive_local_tools/tui/choose_asset_token.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from clive.__private.models.asset import Asset, AssetFactoryHolder
+from clive.__private.models.asset import Asset, AssetFactoryHolderHbd, AssetFactoryHolderHive, AssetFactoryHolderVests
 from clive.__private.ui.widgets.currency_selector.currency_selector_liquid import CurrencySelectorLiquid
 
 from .checkers import assert_is_focused
@@ -16,9 +16,9 @@ async def choose_asset_token(pilot: ClivePilot, asset_token: LiquidAssetToken) -
     if asset_token == "HBD":
         await pilot.press("down", "down", "enter")
     selected_asset = pilot.app.screen.query_exactly_one(CurrencySelectorLiquid).value
-    assert isinstance(selected_asset, AssetFactoryHolder), (
-        f"Expected 'AssetFactoryHolder', current is {type(selected_asset)}."
-    )
+    assert isinstance(
+        selected_asset, AssetFactoryHolderHive | AssetFactoryHolderHbd | AssetFactoryHolderVests
+    ), f"Expected 'AssetFactoryHolder', current is {type(selected_asset)}."
     selected_asset_cls = selected_asset.asset_cls
     selected_asset_name = Asset.get_symbol(selected_asset_cls)
     assert selected_asset_name == asset_token, (
