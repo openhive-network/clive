1aaf2c189d8ffa736a679851f97008169adb3cd7
Jakub Ziebinski <ziebinskijakub@gmail.com>
2025-03-25 12:33:58 +0100

    Change size of search and clear buttons on the witness tab to the one line

--- clive/__private/ui/widgets/buttons/search_operation_buttons.py
+++ clive/__private/ui/widgets/buttons/search_operation_buttons.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from clive.__private.ui.widgets.buttons.clive_button import CliveButton
+from clive.__private.ui.widgets.buttons.one_line_button import OneLineButton
 
 
 class SearchButton(CliveButton):
@@ -11,9 +12,23 @@ class SearchButton(CliveButton):
         super().__init__(label=label, id_=id_, variant="success")
 
 
+class SearchOneLineButton(OneLineButton, SearchButton):
+    """OneLineButton with SearchButton behavior."""
+
+    class Pressed(OneLineButton.Pressed):
+        """Message send when SearchOneLineButton is pressed."""
+
+
 class ClearButton(CliveButton):
     class Pressed(CliveButton.Pressed):
         """Message send when ClearButton is pressed."""
 
     def __init__(self, label: str = "Clear", id_: str = "clear-button") -> None:
         super().__init__(label=label, id_=id_, variant="error")
+
+
+class ClearOneLineButton(OneLineButton, ClearButton):
+    """OneLineButton with ClearButton behavior."""
+
+    class Pressed(OneLineButton.Pressed):
+        """Message send when ClearOneLineButton is pressed."""
