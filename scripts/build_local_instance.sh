85279741af1d977a7186f82dcd00cebff7686b8a
Mateusz Żebrak <mzebrak@syncad.com>
2025-03-31 10:23:06 +0200

    Update build_local_instance.sh to latest changes in registry

--- scripts/build_local_instance.sh
+++ scripts/build_local_instance.sh
@@ -10,14 +10,19 @@ IMAGE_TAG="local"
 SRC_DIR="${SCRIPTSDIR}/../"
 REGISTRY_URL="registry.gitlab.syncad.com/hive/clive"
 
-# EDIT THESE VARIABLES
-#    IMAGE could be get from prepare_hived_image job like: https://gitlab.syncad.com/hive/clive/-/jobs/787089#L342
-IMAGE="registry.gitlab.syncad.com/hive/hive/testnet-base_instance:fe0084e"
-CLIVE_VERSION="1.27.5.2"
-BUILD_TESTNET=1
+# >>> EDIT THESE VARIABLES
 
-HIVED_SOURCE_IMAGE="${IMAGE}"
-BASE_IMAGE="mwalbeck/python-poetry:1.7-3.10"
+#    HIVED_SOURCE_IMAGE could be get from prepare_mainnet_hived_image or prepare_testnet_hived_image job like: https://gitlab.syncad.com/hive/clive/-/jobs/787089#L342 Depends if building testnet - can be base_instance or testnet-base_instance
+HIVED_SOURCE_IMAGE="registry.gitlab.syncad.com/hive/hive/testnet-base_instance:8a42333b"
+
+#    BASE_IMAGE is the image on which clive image will be built. Depends if building testnet - can be either https://gitlab.syncad.com/hive/clive/container_registry/682 or https://gitlab.syncad.com/hive/clive/container_registry/684
+BASE_IMAGE="registry.gitlab.syncad.com/hive/clive/clive-testnet-base-image@sha256:d38883407b5860a89e7491b1de4153c516dc5ae87ca9988fba0ed0400e97f98f"
+
+#   CLIVE_VERSION is the version of clive which will be available in the image, Clive won't be built from sources but .whl file will be used to install it.
+CLIVE_VERSION="1.27.5.21"
+
+BUILD_TESTNET=1
+# <<<
 
 BUILD_INSTANCE_PATH=$(realpath "${SCRIPTSDIR}/ci-helpers/")
 cd "${BUILD_INSTANCE_PATH}"
