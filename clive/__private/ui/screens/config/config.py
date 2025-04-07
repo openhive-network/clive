6359eb404170e909c4ddbc8b2368ab6648c0b4ce
Marcin Sobczyk <msobczyk@syncad.com>
2025-04-07 15:53:38 +0200

    Adjust the look of Config screen - OneLineButton, Section and BigTitle

--- clive/__private/ui/screens/config/config.py
+++ clive/__private/ui/screens/config/config.py
@@ -4,15 +4,15 @@ from typing import TYPE_CHECKING
 
 from textual import on
 from textual.binding import Binding
-from textual.widgets import Static
 
 from clive.__private.ui.get_css import get_relative_css_path
 from clive.__private.ui.screens.base_screen import BaseScreen
 from clive.__private.ui.screens.config.account_management.account_management import AccountManagement
 from clive.__private.ui.screens.config.manage_key_aliases import ManageKeyAliases
 from clive.__private.ui.screens.config.switch_node_address import SwitchNodeAddress
-from clive.__private.ui.widgets.buttons import CliveButton
-from clive.__private.ui.widgets.dialog_container import DialogContainer
+from clive.__private.ui.widgets.big_title import BigTitle
+from clive.__private.ui.widgets.buttons import OneLineButton
+from clive.__private.ui.widgets.section import Section
 
 if TYPE_CHECKING:
     from textual.app import ComposeResult
@@ -26,20 +26,20 @@ class Config(BaseScreen):
     ]
 
     def create_main_panel(self) -> ComposeResult:
-        with DialogContainer():
-            yield Static("Please choose which configuration you would like to make:", id="hint")
-            yield CliveButton("Select node", id_="select-node")
-            yield CliveButton("Manage key aliases", id_="manage-key-aliases")
-            yield CliveButton("Accounts management", id_="account-list-management")
+        yield BigTitle("Configuration")
+        with Section(title="Please choose which configuration you would like to make"):
+            yield OneLineButton("Select node", id_="select-node")
+            yield OneLineButton("Manage key aliases", id_="manage-key-aliases")
+            yield OneLineButton("Accounts management", id_="account-list-management")
 
-    @on(CliveButton.Pressed, "#select-node")
+    @on(OneLineButton.Pressed, "#select-node")
     def push_set_node_address_screen(self) -> None:
         self.app.push_screen(SwitchNodeAddress())
 
-    @on(CliveButton.Pressed, "#account-list-management")
+    @on(OneLineButton.Pressed, "#account-list-management")
     def push_account_list_management_screen(self) -> None:
         self.app.push_screen(AccountManagement())
 
-    @on(CliveButton.Pressed, "#manage-key-aliases")
+    @on(OneLineButton.Pressed, "#manage-key-aliases")
     async def push_manage_key_aliases_screen(self) -> None:
         await self.app.push_screen(ManageKeyAliases())
