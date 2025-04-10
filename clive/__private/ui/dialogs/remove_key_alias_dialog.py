c6675669c0efcb7c0a60bdc73c5ee97fae8691b2
Mateusz Kudela <mkudela@syncad.com>
2025-04-10 09:29:18 +0200

    Add support for PublicKey (without alias) in RemoveKeyAliasDialog

--- clive/__private/ui/dialogs/remove_key_alias_dialog.py
+++ clive/__private/ui/dialogs/remove_key_alias_dialog.py
@@ -1,22 +1,26 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
+from clive.__private.core.keys import PublicKeyAliased
 from clive.__private.ui.dialogs.confirm_action_dialog import ConfirmActionDialog
-
-if TYPE_CHECKING:
-    from clive.__private.core.keys import PublicKeyAliased
+from wax.models.basic import PublicKey
 
 
 class RemoveKeyAliasDialog(ConfirmActionDialog):
     """Dialog to confirm if the user wants to remove key alias from profile."""
 
-    def __init__(self, public_key: PublicKeyAliased) -> None:
+    def __init__(self, public_key: PublicKey | PublicKeyAliased) -> None:
+        if isinstance(public_key, PublicKey):
+            # find corresponding aliased key for given value:
+            for aliased_key in self.profile.keys:
+                if aliased_key.value == public_key:
+                    public_key = aliased_key
+
+        assert isinstance(public_key, PublicKeyAliased), "Public key has to be in PublicKeyAliased type at this point!"
         self._public_key = public_key
         super().__init__(
             border_title="Removing the key alias",
             confirm_question=(
-                f"You are about to remove a `{self.key_alias}` key alias from the profile.\n"
+                f"You are about to remove key {self._public_key.value} aliased `{self.key_alias}` from the profile.\n"
                 f"Are you sure you want to proceed?"
             ),
         )
