f6869d6cd4a3fe799792b1c1a06667bb050ab92e
Marcin Sobczyk <msobczyk@syncad.com>
2025-04-11 14:13:10 +0200

    Don't show bindings in footer except for governance operations

--- clive/__private/ui/screens/operations/bindings/operation_action_bindings.py
+++ clive/__private/ui/screens/operations/bindings/operation_action_bindings.py
@@ -43,8 +43,8 @@ class OperationActionBindings(CliveWidget, AbstractClassMessagePump):
     """Class to provide access to methods related with operations to not just screens."""
 
     BINDINGS = [
-        Binding(ADD_OPERATION_TO_CART_BINDING_KEY, "add_to_cart", "Add to cart"),
-        Binding(FINALIZE_TRANSACTION_BINDING_KEY, "finalize_transaction", "Finalize transaction"),
+        Binding(ADD_OPERATION_TO_CART_BINDING_KEY, "add_to_cart", "Add to cart", show=False),
+        Binding(FINALIZE_TRANSACTION_BINDING_KEY, "finalize_transaction", "Finalize transaction", show=False),
     ]
     ALLOW_THE_SAME_OPERATION_IN_CART_MULTIPLE_TIMES: ClassVar[bool] = True
     POP_SCREEN_AFTER_ADDING_TO_CART: ClassVar[bool] = False
--- clive/__private/ui/screens/operations/governance_operations/common_governance/governance_tab_pane.py
+++ clive/__private/ui/screens/operations/governance_operations/common_governance/governance_tab_pane.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
 from textual import on
+from textual.binding import Binding
 from textual.widgets import TabPane
 
+from clive.__private.core.constants.tui.bindings import (
+    ADD_OPERATION_TO_CART_BINDING_KEY,
+    FINALIZE_TRANSACTION_BINDING_KEY,
+)
 from clive.__private.ui.get_css import get_css_from_relative_path
 from clive.__private.ui.screens.operations.bindings import OperationActionBindings
 from clive.__private.ui.screens.operations.governance_operations.common_governance.governance_actions import (
@@ -15,6 +20,11 @@ from clive.__private.ui.screens.operations.governance_operations.common_governan
 
 class GovernanceTabPane(TabPane, OperationActionBindings):
     """TabPane with operation bindings and mechanism to handle with message to mount/unmount action."""
+
+    BINDINGS = [
+        Binding(ADD_OPERATION_TO_CART_BINDING_KEY, "add_to_cart", "Add to cart"),
+        Binding(FINALIZE_TRANSACTION_BINDING_KEY, "finalize_transaction", "Finalize transaction"),
+    ]
 
     DEFAULT_CSS = get_css_from_relative_path(__file__)
     POP_SCREEN_AFTER_ADDING_TO_CART = True
