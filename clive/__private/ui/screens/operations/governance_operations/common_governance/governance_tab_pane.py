8c6fd403763878f84cfcbcdb5b4b8bb891080883
Jakub Ziebinski <jziebinski@gmail.com>
2025-04-11 09:32:39 +0000

    Restore/reset governance table and actions on screen resumed

--- clive/__private/ui/screens/operations/governance_operations/common_governance/governance_tab_pane.py
+++ clive/__private/ui/screens/operations/governance_operations/common_governance/governance_tab_pane.py
@@ -4,10 +4,12 @@ from textual import on
 from textual.widgets import TabPane
 
 from clive.__private.ui.get_css import get_css_from_relative_path
+from clive.__private.ui.screens.operation_base_screen import OperationBaseScreen
 from clive.__private.ui.screens.operations.governance_operations.common_governance.governance_actions import (
     GovernanceActions,
 )
 from clive.__private.ui.screens.operations.governance_operations.common_governance.governance_table import (
+    GovernanceTable,
     GovernanceTableRow,
 )
 
@@ -27,3 +29,15 @@ class GovernanceTabPane(TabPane):
         else:
             await actions.remove_row(identifier=event.action_identifier, vote=event.vote)
             actions.remove_operation_from_cart(event.action_identifier, vote=event.vote)
+
+    @on(OperationBaseScreen.Resumed)
+    async def restore_actions_and_table(self) -> None:
+        """
+        Restore actions and table after screen is resumed.
+
+        This is needed because user can go to cart and then back to governance screen.
+        In cart user might remove some actions and we need to update their status.
+        """
+        with self.app.batch_update():
+            await self.query_exactly_one(GovernanceTable).reset_page()  # type: ignore[type-abstract]
+            await self.query_exactly_one(GovernanceActions).rebuild()  # type: ignore[type-abstract]
