597d2cf0abb23e96f0f8f7524acabec735eeee17
Radek Masłowski <rmaslowski@syncad.com>
2025-04-22 06:34:16 +0000

    fixup! Test commit

--- clive/__private/core/alarms/alarm_identifier.py
+++ clive/__private/core/alarms/alarm_identifier.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 
-from abc import ABC
-
 from clive.__private.models.schemas import HiveDateTime  # noqa: TCH001
 from schemas.clive.base import CliveBaseModel
 
 
-class AlarmIdentifier(CliveBaseModel, ABC):
+class AlarmIdentifier(CliveBaseModel):
     """Base class for alarm identifiers."""
 
 
