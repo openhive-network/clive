2bc8ab617dfc4ac0f328323cbce49822b2a44e7f
Mateusz Żebrak <mzebrak@syncad.com>
2025-04-24 09:57:56 +0200

    Add missing Pressed overrides in one_line_button.py

--- clive/__private/ui/widgets/buttons/one_line_button.py
+++ clive/__private/ui/widgets/buttons/one_line_button.py
@@ -6,6 +6,9 @@ from clive.__private.ui.widgets.buttons.clive_button import CliveButton
 class OneLineButton(CliveButton):
     """Button that is without border around it, so it can be used in one line."""
 
+    class Pressed(CliveButton.Pressed):
+        """Used to identify exactly that OneLineButton was pressed."""
+
     DEFAULT_CSS = """
     OneLineButton {
         border: none !important;
@@ -19,3 +22,6 @@ class OneLineButton(CliveButton):
 
 class OneLineButtonUnfocusable(OneLineButton, can_focus=False):
     """Unfocusable version of `OneLineButton`."""
+
+    class Pressed(OneLineButton.Pressed):
+        """Used to identify exactly that OneLineButtonUnfocusable was pressed."""
