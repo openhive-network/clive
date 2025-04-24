dcbd02945fafdf884272033d2d34378d1a460be2
Wiesław Kędzierski <wkedzierski@syncad.com>
2025-04-24 09:29:00 +0000

    Add BAD_ACCOUNT_IN_LOADED_TRANSACTION_MESSAGE and ERROR_BAD_ACCOUNT_IN_LOADED_TRANSACTION_MESSAGE to tui/messages.py.

--- clive/__private/core/constants/tui/messages.py
+++ clive/__private/core/constants/tui/messages.py
@@ -7,3 +7,11 @@ from clive.__private.ui.styling import colorize_shortcut
 PRESS_HELP_MESSAGE: Final[str] = (
     f"In any moment you can press the {colorize_shortcut('F1')} button to see the Help page."
 )
+
+BAD_ACCOUNT_IN_LOADED_TRANSACTION_MESSAGE: Final[str] = (
+    "The transaction contains an operation to a bad account that is not present in the list of known accounts."
+)
+
+ERROR_BAD_ACCOUNT_IN_LOADED_TRANSACTION_MESSAGE: Final[str] = (
+    f"You cannot load the transaction from a file.\n{BAD_ACCOUNT_IN_LOADED_TRANSACTION_MESSAGE}"
+)
