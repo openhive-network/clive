357892292658cc4bd65082df2074937d522e6b7a
Wiesław Kędzierski <wkedzierski@syncad.com>
2025-04-24 08:04:46 +0000

    Add PotentialBadAccountCollector.

---
+++ clive/__private/visitors/operation/potential_bad_account_visitor.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from clive.__private.visitors.operation.financial_operations_account_visitor import FinancialOperationsAccountCollector
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+
+class PotentialBadAccountCollector(FinancialOperationsAccountCollector):
+    """Collects accounts that could potentially be bad basing on the operations that are made to them."""
+
+    def get_bad_accounts(self, bad_accounts: Iterable[str]) -> list[str]:
+        return [account for account in self.accounts if account in bad_accounts]
