ef813b8c8ec98ec0edadeb8d8ffb4e1497a80d11
Mateusz Żebrak <mzebrak@syncad.com>
2025-04-25 09:09:30 +0200

    Refactor v1 and v2 ProfileStorageModel

--- clive/__private/storage/migrations/v2.py
+++ clive/__private/storage/migrations/v2.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Self
+
 from clive.__private.core.constants.tui.themes import DEFAULT_THEME
 from clive.__private.storage.migrations import v1
 
@@ -7,6 +9,6 @@ from clive.__private.storage.migrations import v1
 class ProfileStorageModel(v1.ProfileStorageModel):
     tui_theme: str = DEFAULT_THEME
 
-    @staticmethod
-    def upgrade(old: v1.ProfileStorageModel) -> ProfileStorageModel:  # type: ignore[override]  # should always take previous model
-        return ProfileStorageModel(tui_theme=DEFAULT_THEME, **old.dict())
+    @classmethod
+    def upgrade(cls, old: v1.ProfileStorageModel) -> Self:  # type: ignore[override]  # should always take previous model
+        return cls(**old.dict())
