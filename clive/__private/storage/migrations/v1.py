29346bf32e115899f5a77e81e4166e9a7bfc10a6
Mateusz Żebrak <mzebrak@syncad.com>
2025-04-25 09:09:30 +0200

    Refactor v1 and v2 ProfileStorageModel

--- clive/__private/storage/migrations/v1.py
+++ clive/__private/storage/migrations/v1.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path  # noqa: TC003
-from typing import ClassVar, TypeAlias
+from typing import ClassVar, Self, TypeAlias
 
 from clive.__private.models.base import CliveBaseModel
 from clive.__private.models.schemas import Transaction
@@ -18,30 +18,23 @@ class ProfileStorageModel(v0.ProfileStorageModel):
 
     TransactionStorageModel: ClassVar[TypeAlias] = TransactionStorageModel
 
-    @staticmethod
-    def upgrade(old: v0.ProfileStorageModel) -> ProfileStorageModel:
-        transaction = (
+    @classmethod
+    def upgrade(cls, old: v0.ProfileStorageModel) -> Self:
+        old_transaction = old.transaction
+        new_transaction = (
             TransactionStorageModel(
                 transaction_core=Transaction(
-                    ref_block_num=old.transaction.transaction_core.ref_block_num,
-                    ref_block_prefix=old.transaction.transaction_core.ref_block_prefix,
-                    expiration=old.transaction.transaction_core.expiration,
-                    extensions=old.transaction.transaction_core.extensions,
-                    signatures=old.transaction.transaction_core.signatures,
-                    operations=old.transaction.transaction_core.operations,
+                    **old_transaction.transaction_core.dict(),
                 ),
-                transaction_file_path=old.transaction.transaction_file_path,
+                transaction_file_path=old_transaction.transaction_file_path,
             )
-            if old.transaction
+            if old_transaction is not None
             else None
         )
-        return ProfileStorageModel(
-            name=old.name,
-            working_account=old.working_account,
-            tracked_accounts=old.tracked_accounts,
-            known_accounts=old.known_accounts,
-            key_aliases=old.key_aliases,
-            transaction=transaction,
-            chain_id=old.chain_id,
-            node_address=old.node_address,
+
+        old_dict = old.dict()
+        old_dict.pop("transaction")
+        return cls(
+            **old_dict,
+            transaction=new_transaction,
         )
