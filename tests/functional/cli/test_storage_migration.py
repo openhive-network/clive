c26f00d8fcabb2e4e9387c90585a440bf526e0c6
Mateusz Żebrak <mzebrak@syncad.com>
2025-04-29 10:24:04 +0200

    Remove reduntant assert_no_exit_code_error

--- tests/functional/cli/test_storage_migration.py
+++ tests/functional/cli/test_storage_migration.py
@@ -5,7 +5,6 @@ from typing import TYPE_CHECKING, Final
 import pytest
 
 from clive.__private.settings import safe_settings
-from clive_local_tools.cli.checkers import assert_no_exit_code_error
 from clive_local_tools.data.constants import ALT_WORKING_ACCOUNT1_PASSWORD
 from clive_local_tools.storage_migration.helpers import copy_profile_without_alarms_and_operations
 from clive_local_tools.testnet_block_log import ALT_WORKING_ACCOUNT1_NAME, WATCHED_ACCOUNTS_NAMES
@@ -24,11 +23,8 @@ OTHER_ACCOUNT: Final[str] = WATCHED_ACCOUNTS_NAMES[0]
 
 
 async def test_unlock_old_profile(cli_tester_locked: CLITester) -> None:
-    # ACT
-    result = cli_tester_locked.unlock(profile_name=PROFILE_NAME, password_stdin=ALT_WORKING_ACCOUNT1_PASSWORD)
-
-    # ASSERT
-    assert_no_exit_code_error(result)
+    # ACT & ASSERT
+    cli_tester_locked.unlock(profile_name=PROFILE_NAME, password_stdin=ALT_WORKING_ACCOUNT1_PASSWORD)
 
 
 async def test_load_migrated_profile(cli_tester_locked: CLITester) -> None:
@@ -38,7 +34,6 @@ async def test_load_migrated_profile(cli_tester_locked: CLITester) -> None:
 
     # ACT & ASSERT
     result = cli_tester.show_profile()
-    assert_no_exit_code_error(result)
     assert f"Profile name: {PROFILE_NAME}" in result.output, "profile is not loaded"
 
 
@@ -48,12 +43,10 @@ async def test_save_changes_in_migrated_profile(cli_tester_locked: CLITester) ->
     cli_tester = cli_tester_locked
 
     # ACT & ASSERT
-    result = cli_tester.configure_tracked_account_add(account_name=OTHER_ACCOUNT)
-    assert_no_exit_code_error(result)
+    cli_tester.configure_tracked_account_add(account_name=OTHER_ACCOUNT)
     result = cli_tester.show_profile()
     assert OTHER_ACCOUNT in result.output, f"{OTHER_ACCOUNT} should be added to tracked accounts"
 
-    result = cli_tester.configure_tracked_account_remove(account_name=OTHER_ACCOUNT)
-    assert_no_exit_code_error(result)
+    cli_tester.configure_tracked_account_remove(account_name=OTHER_ACCOUNT)
     result = cli_tester.show_profile()
     assert OTHER_ACCOUNT in result.output, f"{OTHER_ACCOUNT} should be removed from tracked accounts"
