b7e24749594411e5b6ce13a158b647604e6ea733
Marcin Sobczyk <msobczyk@syncad.com>
2025-05-05 10:32:00 +0000

    Cleanup of tests for storage revision from old unused code

--- tests/unit/storage/test_storage_revision.py
+++ tests/unit/storage/test_storage_revision.py
@@ -2,32 +2,11 @@ from __future__ import annotations
 
 from typing import Final
 
-from beekeepy import AsyncBeekeeper
-
-from clive.__private.core.commands.create_profile_wallets import CreateProfileWallets
-from clive.__private.core.commands.save_profile import SaveProfile
-from clive.__private.core.profile import Profile
-from clive.__private.settings import safe_settings
 from clive.__private.storage import ProfileStorageModel
 from clive.__private.storage.storage_history import StorageHistory
 
 REVISIONS: Final[list[str]] = ["ffc97b51", "a721f943", "9c46df0c"]
 LATEST_REVISION: Final[str] = REVISIONS[-1]
-
-FIRST_PROFILE_NAME: Final[str] = "first"
-
-
-async def create_and_save_profile(profile_name: str) -> None:
-    async with await AsyncBeekeeper.factory(settings=safe_settings.beekeeper.settings_local_factory()) as beekeeper:
-        result = await CreateProfileWallets(
-            session=await beekeeper.session, profile_name=profile_name, password=profile_name
-        ).execute_with_result()
-        profile = Profile.create(profile_name)
-        await SaveProfile(
-            profile=profile,
-            unlocked_wallet=result.unlocked_user_wallet,
-            unlocked_encryption_wallet=result.unlocked_encryption_wallet,
-        ).execute()
 
 
 def test_storage_revision_doesnt_changed_for_latest_version() -> None:
--- tests/unit/storage/test_storage_structure.py
+++ tests/unit/storage/test_storage_structure.py
@@ -1,14 +1,33 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Final
 
 import pytest
+from beekeepy import AsyncBeekeeper
 
+from clive.__private.core.commands.create_profile_wallets import CreateProfileWallets
+from clive.__private.core.commands.save_profile import SaveProfile
+from clive.__private.core.profile import Profile
+from clive.__private.settings import safe_settings
 from clive.__private.storage.service import PersistentStorageService
-from tests.unit.storage.test_storage_revision import FIRST_PROFILE_NAME, create_and_save_profile
 
 if TYPE_CHECKING:
     from pathlib import Path
+
+FIRST_PROFILE_NAME: Final[str] = "first"
+
+
+async def _create_and_save_profile(profile_name: str) -> None:
+    async with await AsyncBeekeeper.factory(settings=safe_settings.beekeeper.settings_local_factory()) as beekeeper:
+        result = await CreateProfileWallets(
+            session=await beekeeper.session, profile_name=profile_name, password=profile_name
+        ).execute_with_result()
+        profile = Profile.create(profile_name)
+        await SaveProfile(
+            profile=profile,
+            unlocked_wallet=result.unlocked_user_wallet,
+            unlocked_encryption_wallet=result.unlocked_encryption_wallet,
+        ).execute()
 
 
 def _create_stub_profile_file(file_name: str) -> Path:
@@ -27,7 +46,7 @@ async def test_storage_dir_contains_expected_files() -> None:
 
     # ACT
     # saving a profile will cause persisting storage data to be saved
-    await create_and_save_profile(FIRST_PROFILE_NAME)
+    await _create_and_save_profile(FIRST_PROFILE_NAME)
 
     # ASSERT
     assert storage_data_dir.is_dir(), "Storage data path is not a directory or is missing."
