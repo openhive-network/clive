dcff89d7cdc97050191c3a78930bf3eb63f81ccb
Marcin Sobczyk <msobczyk@syncad.com>
2025-05-06 08:28:01 +0000

    Assert that migrated profile is saved in cli tests for migration

--- tests/functional/cli/test_storage_migration.py
+++ tests/functional/cli/test_storage_migration.py
@@ -5,6 +5,9 @@ from typing import TYPE_CHECKING, Final
 import pytest
 
 from clive.__private.settings import safe_settings
+from clive.__private.storage.current_model import ProfileStorageModel
+from clive.__private.storage.migrations import v0
+from clive.__private.storage.service import PersistentStorageService
 from clive_local_tools.data.constants import ALT_WORKING_ACCOUNT1_PASSWORD
 from clive_local_tools.storage_migration.helpers import copy_profile_without_alarms_and_operations
 from clive_local_tools.testnet_block_log import ALT_WORKING_ACCOUNT1_NAME, WATCHED_ACCOUNTS_NAMES
@@ -23,6 +26,13 @@ PROFILE_PASSWORD: Final[str] = ALT_WORKING_ACCOUNT1_PASSWORD
 OTHER_ACCOUNT: Final[str] = WATCHED_ACCOUNTS_NAMES[0]
 
 
+def _get_profile_file_version(profile_name: str) -> int:
+    paths = PersistentStorageService._get_filepaths(profile_name=PROFILE_NAME, file_type="profile")
+    assert len(paths) == 1, f"Profile {profile_name} should have exactly one file"
+    info = next(iter(paths))
+    return info.version()
+
+
 async def test_unlock_old_profile(cli_tester_locked: CLITester) -> None:
     # ACT & ASSERT
     cli_tester_locked.unlock(profile_name=PROFILE_NAME, password_stdin=PROFILE_PASSWORD)
@@ -32,10 +42,16 @@ async def test_load_migrated_profile(cli_tester_locked: CLITester) -> None:
     # ARRANGE
     cli_tester_locked.unlock(profile_name=PROFILE_NAME, password_stdin=PROFILE_PASSWORD)
     cli_tester = cli_tester_locked
+    assert _get_profile_file_version(PROFILE_NAME) == v0.ProfileStorageModel.get_this_version(), (
+        f"before migration profile should be stored in version {v0.ProfileStorageModel.get_this_version()}"
+    )
 
     # ACT & ASSERT
     result = cli_tester.show_profile()
     assert f"Profile name: {PROFILE_NAME}" in result.output, "profile is not loaded"
+    assert _get_profile_file_version(PROFILE_NAME) == ProfileStorageModel.get_this_version(), (
+        f"after migration profile should be stored in version {ProfileStorageModel.get_this_version()}"
+    )
 
 
 async def test_save_changes_in_migrated_profile(cli_tester_locked: CLITester) -> None:
