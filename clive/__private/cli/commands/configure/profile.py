15d4e56f2930ba62476ed94a6e4224bff3fe218d
Marcin Sobczyk <msobczyk@syncad.com>
2025-05-07 11:47:52 +0000

    If we are deleting in cli unlocked profile then lock it

--- clive/__private/cli/commands/configure/profile.py
+++ clive/__private/cli/commands/configure/profile.py
@@ -5,6 +5,8 @@ import sys
 from dataclasses import dataclass
 from getpass import getpass
 
+import typer
+from beekeepy import AsyncBeekeeper
 from beekeepy.exceptions import CommunicationError
 
 from clive.__private.cli.commands.abc.beekeeper_based_command import BeekeeperBasedCommand
@@ -15,9 +17,12 @@ from clive.__private.cli.exceptions import (
     CLIPrettyError,
 )
 from clive.__private.core.commands.create_profile_wallets import CreateProfileWallets
+from clive.__private.core.commands.get_wallet_names import GetWalletNames
+from clive.__private.core.commands.lock import Lock
 from clive.__private.core.commands.save_profile import SaveProfile
 from clive.__private.core.formatters.humanize import humanize_validation_result
 from clive.__private.core.profile import Profile
+from clive.__private.settings import safe_settings
 from clive.__private.storage.service import MultipleProfileVersionsError
 from clive.__private.validators.profile_name_validator import ProfileNameValidator
 from clive.__private.validators.set_password_validator import SetPasswordValidator
@@ -101,5 +106,19 @@ class DeleteProfile(ExternalCLICommand):
     async def _run(self) -> None:
         try:
             Profile.delete_by_name(self.profile_name, force=self.force)
+            await self._lock_remote_beekeeper()
         except MultipleProfileVersionsError as error:
             raise CLIMultipleProfileVersionsError(self.profile_name) from error
+
+    async def _lock_remote_beekeeper(self) -> None:
+        if safe_settings.beekeeper.is_remote_address_set:
+            async with await AsyncBeekeeper.remote_factory(
+                url_or_settings=safe_settings.beekeeper.settings_remote_factory()
+            ) as beekeeper:
+                session = await beekeeper.session
+                unlocked_wallets = await GetWalletNames(
+                    session=session, filter_by_status="unlocked"
+                ).execute_with_result()
+                if self.profile_name in unlocked_wallets:
+                    await Lock(session=session).execute()
+                    typer.echo("Locked beekeeper session.")
