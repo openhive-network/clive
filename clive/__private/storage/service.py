342a7c484c3b1362e9ede2ac41885973b1ad4381
Marcin Sobczyk <msobczyk@syncad.com>
2025-05-08 12:24:59 +0000

    Refactor delete_profile in PersistentStorageService

--- clive/__private/storage/service.py
+++ clive/__private/storage/service.py
@@ -156,12 +156,16 @@ class PersistentStorageService:
             MultipleProfileVersionsError: If multiple versions / back-ups of profile exist and force is False.
         """
         filepaths = cls._get_filepaths(profile_name, file_type="all", include_impossible_to_load=True)
-        valid_profile_filepaths = {filepath for filepath in filepaths if not filepath.is_backup}
+        only_backups_found = filepaths and all(filepath.is_backup for filepath in filepaths)
         num_of_files_to_remove = len(filepaths)
-        if num_of_files_to_remove == 0 or (len(valid_profile_filepaths) == 0 and not force):
+
+        if num_of_files_to_remove == 0:
             raise ProfileDoesNotExistsError(profile_name)
-        if num_of_files_to_remove > 1 and not force:
-            raise MultipleProfileVersionsError(profile_name)
+        if not force:
+            if only_backups_found:
+                raise ProfileDoesNotExistsError(profile_name)
+            if num_of_files_to_remove > 1:
+                raise MultipleProfileVersionsError(profile_name)
 
         cls._delete_legacy_profile_data(profile_name)
         profile_dir = cls.get_profile_directory(profile_name)
