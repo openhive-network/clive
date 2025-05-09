0bab3d227f793c6230084f0a6d8d95a6c1c1bff9
Marcin Sobczyk <msobczyk@syncad.com>
2025-05-09 10:35:08 +0000

    fixup! Add tests that will search for bad account in operations in transaction.

--- tests/functional/cli/accounts_validation/test_bad_and_known_accounts_validation.py
+++ tests/functional/cli/accounts_validation/test_bad_and_known_accounts_validation.py
@@ -15,6 +15,7 @@ from clive_local_tools.testnet_block_log.constants import KNOWN_ACCOUNTS, UNKNOW
 
 if TYPE_CHECKING:
     from collections.abc import Callable
+    from pathlib import Path
 
     from clive_local_tools.cli.cli_tester import CLITester
 
@@ -30,121 +31,187 @@ VALIDATION_RECEIVERS: Final[list[str]] = [UNKNOWN_ACCOUNT, KNOWN_ACCOUNT, BAD_AC
 VALIDATION_IDS: Final[list[str]] = ["unknown_account", "known_account", "bad_account"]
 
 
-def _assert_operation_to_bad_account_fails(send_operation_cb: Callable[[], None]) -> None:
+@pytest.fixture(params=["broadcast", "save_file"])
+def save_file_path_or_none(tmp_path: Path, request: pytest.FixtureRequest) -> Path | None:
+    if request.param == "broadcast":
+        return None
+    return tmp_path / "trx.json"
+
+
+def _assert_operation_to_bad_account_fails(perform_operation_cb: Callable[[], None]) -> None:
     with pytest.raises(CLITestCommandError) as error:
-        send_operation_cb()
+        perform_operation_cb()
     assert_output_contains(EXPECTED_BAD_ACCOUNT_ERROR_MESSAGE, error.value.stdout)
 
 
-def _assert_operation_to_unknown_account_fails(send_operation_cb: Callable[[], None]) -> None:
+def _assert_operation_to_unknown_account_fails(perform_operation_cb: Callable[[], None]) -> None:
     with pytest.raises(CLITestCommandError) as error:
-        send_operation_cb()
+        perform_operation_cb()
     assert_output_contains(EXPECTED_UNKNOWN_ACCOUNT_ERROR_MSG, error.value.stdout)
 
 
-def _assert_validation_of_accounts(send_operation_cb: Callable[[], None], receiver: str) -> None:
+def _assert_validation_of_accounts(perform_operation_cb: Callable[[], None], receiver: str) -> None:
     if receiver == UNKNOWN_ACCOUNT:
-        _assert_operation_to_unknown_account_fails(send_operation_cb)
+        _assert_operation_to_unknown_account_fails(perform_operation_cb)
     elif receiver == BAD_ACCOUNT:
-        _assert_operation_to_bad_account_fails(send_operation_cb)
+        _assert_operation_to_bad_account_fails(perform_operation_cb)
     else:
-        send_operation_cb()
+        perform_operation_cb()
 
 
 @pytest.mark.parametrize("receiver", VALIDATION_RECEIVERS, ids=VALIDATION_IDS)
-async def test_validation_of_delegate_vesting_shares(cli_tester: CLITester, receiver: str) -> None:
+async def test_validation_of_delegate_vesting_shares(
+    cli_tester: CLITester, receiver: str, save_file_path_or_none: Path | None
+) -> None:
     # ARRANGE
-    def send_operation() -> None:
-        cli_tester.process_delegations_set(delegatee=receiver, amount=AMOUNT, sign=WORKING_ACCOUNT_KEY_ALIAS)
+    def perform_operation() -> None:
+        cli_tester.process_delegations_set(
+            delegatee=receiver,
+            amount=AMOUNT,
+            sign=WORKING_ACCOUNT_KEY_ALIAS,
+            broadcast=True if save_file_path_or_none is None else None,
+            save_file=save_file_path_or_none if save_file_path_or_none is not None else None,
+        )
 
     # ACT & ASSERT
-    _assert_validation_of_accounts(send_operation, receiver)
+    _assert_validation_of_accounts(perform_operation, receiver)
 
 
 @pytest.mark.parametrize("receiver", VALIDATION_RECEIVERS, ids=VALIDATION_IDS)
-async def test_validation_of_setting_withdrawal_routes(cli_tester: CLITester, receiver: str) -> None:
+async def test_validation_of_setting_withdrawal_routes(
+    cli_tester: CLITester, receiver: str, save_file_path_or_none: Path | None
+) -> None:
     # ARRANGE
-    def send_operation() -> None:
+    def perform_operation() -> None:
         percent: int = 30
-        cli_tester.process_withdraw_routes_set(to=receiver, percent=percent, sign=WORKING_ACCOUNT_KEY_ALIAS)
+        cli_tester.process_withdraw_routes_set(
+            to=receiver,
+            percent=percent,
+            sign=WORKING_ACCOUNT_KEY_ALIAS,
+            broadcast=True if save_file_path_or_none is None else None,
+            save_file=save_file_path_or_none if save_file_path_or_none is not None else None,
+        )
 
     # ACT & ASSERT
-    _assert_validation_of_accounts(send_operation, receiver)
+    _assert_validation_of_accounts(perform_operation, receiver)
 
 
 @pytest.mark.parametrize("receiver", VALIDATION_RECEIVERS, ids=VALIDATION_IDS)
-async def test_validation_of_savings_withdrawal(cli_tester: CLITester, receiver: str) -> None:
+async def test_validation_of_savings_withdrawal(
+    cli_tester: CLITester, receiver: str, save_file_path_or_none: Path | None
+) -> None:
     # ARRANGE
-    def send_operation() -> None:
-        cli_tester.process_savings_withdrawal(to=receiver, amount=AMOUNT, sign=WORKING_ACCOUNT_KEY_ALIAS)
+    def perform_operation() -> None:
+        cli_tester.process_savings_withdrawal(
+            to=receiver,
+            amount=AMOUNT,
+            sign=WORKING_ACCOUNT_KEY_ALIAS,
+            broadcast=True if save_file_path_or_none is None else None,
+            save_file=save_file_path_or_none if save_file_path_or_none is not None else None,
+        )
 
     # ACT & ASSERT
-    _assert_validation_of_accounts(send_operation, receiver)
+    _assert_validation_of_accounts(perform_operation, receiver)
 
 
 @pytest.mark.parametrize("receiver", VALIDATION_RECEIVERS, ids=VALIDATION_IDS)
-async def test_validation_of_deposing_savings(cli_tester: CLITester, receiver: str) -> None:
+async def test_validation_of_deposing_savings(
+    cli_tester: CLITester, receiver: str, save_file_path_or_none: Path | None
+) -> None:
     # ARRANGE
-    def send_operation() -> None:
-        cli_tester.process_savings_deposit(to=receiver, amount=AMOUNT, sign=WORKING_ACCOUNT_KEY_ALIAS)
+    def perform_operation() -> None:
+        cli_tester.process_savings_deposit(
+            to=receiver,
+            amount=AMOUNT,
+            sign=WORKING_ACCOUNT_KEY_ALIAS,
+            broadcast=True if save_file_path_or_none is None else None,
+            save_file=save_file_path_or_none if save_file_path_or_none is not None else None,
+        )
 
     # ACT & ASSERT
-    _assert_validation_of_accounts(send_operation, receiver)
+    _assert_validation_of_accounts(perform_operation, receiver)
 
 
 @pytest.mark.parametrize("receiver", VALIDATION_RECEIVERS, ids=VALIDATION_IDS)
-async def test_validation_of_powering_up(cli_tester: CLITester, receiver: str) -> None:
+async def test_validation_of_powering_up(
+    cli_tester: CLITester, receiver: str, save_file_path_or_none: Path | None
+) -> None:
     # ARRANGE
-    def send_operation() -> None:
-        cli_tester.process_power_up(amount=AMOUNT, to=receiver, sign=WORKING_ACCOUNT_KEY_ALIAS)
+    def perform_operation() -> None:
+        cli_tester.process_power_up(
+            amount=AMOUNT,
+            to=receiver,
+            sign=WORKING_ACCOUNT_KEY_ALIAS,
+            broadcast=True if save_file_path_or_none is None else None,
+            save_file=save_file_path_or_none if save_file_path_or_none is not None else None,
+        )
 
     # ACT & ASSERT
-    _assert_validation_of_accounts(send_operation, receiver)
+    _assert_validation_of_accounts(perform_operation, receiver)
 
 
 async def test_validation_of_powering_up_to_self(cli_tester: CLITester) -> None:
     # ARRANGE
-    def send_operation() -> None:
+    def perform_operation() -> None:
         cli_tester.process_power_up(amount=AMOUNT, sign=WORKING_ACCOUNT_KEY_ALIAS)
 
     # ACT & ASSERT
-    _assert_validation_of_accounts(send_operation, WORKING_ACCOUNT_NAME)
+    _assert_validation_of_accounts(perform_operation, WORKING_ACCOUNT_NAME)
 
 
 @pytest.mark.parametrize("receiver", VALIDATION_RECEIVERS, ids=VALIDATION_IDS)
-async def test_validation_of_transfer(cli_tester: CLITester, receiver: str) -> None:
+async def test_validation_of_transfer(
+    cli_tester: CLITester, receiver: str, save_file_path_or_none: Path | None
+) -> None:
     # ARRANGE
-    def send_operation() -> None:
-        cli_tester.process_transfer(to=receiver, amount=AMOUNT, sign=WORKING_ACCOUNT_KEY_ALIAS)
+    def perform_operation() -> None:
+        cli_tester.process_transfer(
+            to=receiver,
+            amount=AMOUNT,
+            sign=WORKING_ACCOUNT_KEY_ALIAS,
+            broadcast=True if save_file_path_or_none is None else None,
+            save_file=save_file_path_or_none if save_file_path_or_none is not None else None,
+        )
 
     # ACT & ASSERT
-    _assert_validation_of_accounts(send_operation, receiver)
+    _assert_validation_of_accounts(perform_operation, receiver)
 
 
 @pytest.mark.parametrize("receiver", VALIDATION_RECEIVERS, ids=VALIDATION_IDS)
-async def test_validation_of_setting_proxy(cli_tester: CLITester, receiver: str) -> None:
+async def test_validation_of_setting_proxy(
+    cli_tester: CLITester, receiver: str, save_file_path_or_none: Path | None
+) -> None:
     # ARRANGE
-    def send_operation() -> None:
-        cli_tester.process_proxy_set(proxy=receiver, sign=WORKING_ACCOUNT_KEY_ALIAS)
+    def perform_operation() -> None:
+        cli_tester.process_proxy_set(
+            proxy=receiver,
+            sign=WORKING_ACCOUNT_KEY_ALIAS,
+            broadcast=True if save_file_path_or_none is None else None,
+            save_file=save_file_path_or_none if save_file_path_or_none is not None else None,
+        )
 
     # ACT & ASSERT
-    _assert_validation_of_accounts(send_operation, receiver)
+    _assert_validation_of_accounts(perform_operation, receiver)
 
 
 @pytest.mark.parametrize("receiver", VALIDATION_RECEIVERS, ids=VALIDATION_IDS)
-async def test_validation_of_creating_scheduled_transfer(cli_tester: CLITester, receiver: str) -> None:
+async def test_validation_of_creating_scheduled_transfer(
+    cli_tester: CLITester, receiver: str, save_file_path_or_none: Path | None
+) -> None:
     # ARRANGE
-    def send_operation() -> None:
+    def perform_operation() -> None:
         cli_tester.process_transfer_schedule_create(
             to=receiver,
             amount=tt.Asset.Hive(1),
             sign=WORKING_ACCOUNT_KEY_ALIAS,
             repeat=2,
             frequency="24h",
+            broadcast=True if save_file_path_or_none is None else None,
+            save_file=save_file_path_or_none if save_file_path_or_none is not None else None,
         )
 
     # ACT & ASSERT
-    _assert_validation_of_accounts(send_operation, receiver)
+    _assert_validation_of_accounts(perform_operation, receiver)
 
 
 async def test_no_validation_of_removing_withdraw_routes_to_account_that_was_known(
