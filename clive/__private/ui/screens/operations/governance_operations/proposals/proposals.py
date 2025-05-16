3a46a10823ee286ed77d117476d072cf7a6844a4
Jakub Ziebinski <jziebinski@gmail.com>
2025-05-16 13:33:50 +0000

    Do not add proposal id to the operation immediately
    The method must check all the operations in the cart before
    adding a proposal id to any operation.

--- clive/__private/ui/screens/operations/governance_operations/proposals/proposals.py
+++ clive/__private/ui/screens/operations/governance_operations/proposals/proposals.py
@@ -170,6 +170,59 @@ class ProposalsActions(GovernanceActions[UpdateProposalVotesOperation]):
     def create_action_row(self, identifier: str, *, vote: bool) -> GovernanceActionRow:
         return ProposalActionRow(identifier, vote=vote)
 
+    def add_operation_to_cart(self, identifier: str, *, vote: bool = False) -> None:
+        """
+        Add / update proposal votes operation to / in the cart.
+
+        If the proposal ID is already in the cart, nothing will be done.
+        If an operation with a free slot is in the cart, it will be added to the operation.
+        If an operation with such a proposal ID is NOT in the cart, a new operation will be created and added.
+        """
+
+        def append_operation(operation: UpdateProposalVotesOperation) -> None:
+            operation.proposal_ids.append(proposal_id)  # type: ignore[arg-type]
+            operation.proposal_ids.sort()  # proposal id's must be sorted
+
+        proposal_id = int(identifier)
+        op_to_append: UpdateProposalVotesOperation | None = None  # If None -> new operation will be created
+
+        for op in self.profile.transaction:
+            if not isinstance(op, UpdateProposalVotesOperation):
+                continue
+
+            if proposal_id in op.proposal_ids:
+                return
+
+            if op.approve == vote and len(op.proposal_ids) < MAX_NUMBER_OF_PROPOSAL_IDS_IN_SINGLE_OPERATION:
+                op_to_append = op  # Do not attach immediately, because we have to check whether there is an
+                # another operation with such a proposal id. If so, the function will be stopped by if above.
+
+        if op_to_append is not None:
+            append_operation(op_to_append)
+            return
+
+        self.profile.transaction.add_operation(
+            UpdateProposalVotesOperation(
+                voter=self.profile.accounts.working.name,
+                proposal_ids=[proposal_id],
+                approve=vote,
+            )
+        )
+
+    def remove_operation_from_cart(self, identifier: str, *, vote: bool = False) -> None:
+        proposal_id = int(identifier)
+
+        for op in self.profile.transaction:
+            if not isinstance(op, UpdateProposalVotesOperation):
+                continue
+
+            if op.approve == vote and proposal_id in op.proposal_ids:
+                if len(op.proposal_ids) == 1:
+                    self.profile.transaction.remove_operation(op)
+                    return
+
+                op.proposal_ids.remove(proposal_id)  # type: ignore[arg-type]
+
 
 class ProposalsList(GovernanceListWidget[ProposalData]):
     def _create_row(self, data: ProposalData, *, even: bool = False) -> Proposal:
