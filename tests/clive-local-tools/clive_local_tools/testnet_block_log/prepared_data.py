97d92627bc76a844c45ac776c92a4763afc78a9a
Radek Masłowski <rmaslowski@syncad.com>
2025-05-19 10:24:47 +0000

    fixup! Implement new schemas to clive tests

--- tests/clive-local-tools/clive_local_tools/testnet_block_log/prepared_data.py
+++ tests/clive-local-tools/clive_local_tools/testnet_block_log/prepared_data.py
@@ -6,8 +6,6 @@ import test_tools as tt
 from test_tools import BlockLog
 from test_tools.__private.node_config import NodeConfig
 
-from clive.__private.models.schemas import get_hf26_decoder
-
 
 def get_alternate_chain_spec_path() -> Path:
     return Path(__file__).parent.absolute() / "alternate-chain-spec.json"
@@ -34,9 +32,7 @@ def get_time_offset() -> str:
 def run_node(webserver_http_endpoint: str | None = None, *, use_faketime: bool = False) -> tt.RawNode:
     config_lines = get_config().write_to_lines()
     block_log = get_block_log()
-    alternate_chain_spec = tt.AlternateChainSpecs.parse_file(
-        path=get_alternate_chain_spec_path(), decoder_factory=get_hf26_decoder
-    )
+    alternate_chain_spec = tt.AlternateChainSpecs.parse_file(path=get_alternate_chain_spec_path())
     time_offset = get_time_offset() if use_faketime else None
 
     node = tt.RawNode()
