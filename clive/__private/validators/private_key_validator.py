7bf03aa2a0ce7def378f0d9a919ce0d998da26be
Mateusz Kudela <mkudela@syncad.com>
2025-05-20 12:29:47 +0000

    Improve PrivateKeyValidator

--- clive/__private/validators/private_key_validator.py
+++ clive/__private/validators/private_key_validator.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from textual.validation import ValidationResult, Validator
+from textual.validation import Function, ValidationResult, Validator
 
 from clive.__private.core.iwax import calculate_public_key
 from clive.__private.core.keys import PrivateKey, PublicKey
@@ -10,21 +10,23 @@ class PrivateKeyValidator(Validator):
     INVALID_PRIVATE_KEY_FAILURE_DESCRIPTION: str = "Invalid private key format."
     PRIVATE_KEY_NOT_MATCHED_TO_PUBLIC_FAILURE_DESCRIPTION: str = "Does not match the expected public key."
 
-    def __init__(self, public_key_to_match: str | PublicKey | None = None) -> None:
+    def __init__(self, public_key_to_match: PublicKey | None = None) -> None:
         super().__init__()
         self._public_key_to_match = public_key_to_match
 
     def validate(self, value: str) -> ValidationResult:
-        if PrivateKey.is_valid(value):
-            if self._public_key_to_match:
-                self._public_key_to_match = (
-                    self._public_key_to_match
-                    if isinstance(self._public_key_to_match, str)
-                    else self._public_key_to_match.value
-                )
-                if self._public_key_to_match == calculate_public_key(value).value:
-                    return self.success()
-                return self.failure(self.PRIVATE_KEY_NOT_MATCHED_TO_PUBLIC_FAILURE_DESCRIPTION)
-            return self.success()
+        validators = [
+            Function(self._validate_private_key, self.INVALID_PRIVATE_KEY_FAILURE_DESCRIPTION),
+            Function(self._validate_matching_key, self.PRIVATE_KEY_NOT_MATCHED_TO_PUBLIC_FAILURE_DESCRIPTION),
+        ]
+        return ValidationResult.merge([validator.validate(value) for validator in validators])
 
-        return self.failure(self.INVALID_PRIVATE_KEY_FAILURE_DESCRIPTION, value)
+    def _validate_matching_key(self, value: str) -> bool:
+        if not self._public_key_to_match:
+            return True
+        if not PrivateKey.is_valid(value):
+            return False
+        return self._public_key_to_match == calculate_public_key(value)
+
+    def _validate_private_key(self, value: str) -> bool:
+        return PrivateKey.is_valid(value)
