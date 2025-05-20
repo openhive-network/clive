08aae10fd23c33a9fef8c25c585e4d45920c9438
Mateusz Kudela <mkudela@syncad.com>
2025-05-20 12:29:47 +0000

    Improve PrivateKeyValidator

--- clive/__private/validators/private_key_validator.py
+++ clive/__private/validators/private_key_validator.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from textual.validation import ValidationResult, Validator
+from textual.validation import Function, ValidationResult, Validator
 
 from clive.__private.core.iwax import calculate_public_key
 from clive.__private.core.keys import PrivateKey, PublicKey
@@ -15,16 +15,28 @@ class PrivateKeyValidator(Validator):
         self._public_key_to_match = public_key_to_match
 
     def validate(self, value: str) -> ValidationResult:
-        if PrivateKey.is_valid(value):
-            if self._public_key_to_match:
-                self._public_key_to_match = (
-                    self._public_key_to_match
-                    if isinstance(self._public_key_to_match, str)
-                    else self._public_key_to_match.value
-                )
-                if self._public_key_to_match == calculate_public_key(value).value:
-                    return self.success()
-                return self.failure(self.PRIVATE_KEY_NOT_MATCHED_TO_PUBLIC_FAILURE_DESCRIPTION)
-            return self.success()
+        valid_private_key_result = Function(
+            self._validate_private_key, self.INVALID_PRIVATE_KEY_FAILURE_DESCRIPTION
+        ).validate(value)
+        validation_results = [valid_private_key_result]
 
-        return self.failure(self.INVALID_PRIVATE_KEY_FAILURE_DESCRIPTION, value)
+        if valid_private_key_result.is_valid:
+            public_key_matched_result = Function(
+                self._validate_matching_keys, self.PRIVATE_KEY_NOT_MATCHED_TO_PUBLIC_FAILURE_DESCRIPTION
+            ).validate(value)
+            validation_results.append(public_key_matched_result)
+
+        return ValidationResult.merge(validation_results)
+
+    def _validate_matching_keys(self, value: str) -> bool:
+        if self._public_key_to_match:
+            key_to_compare = (
+                self._public_key_to_match
+                if isinstance(self._public_key_to_match, str)
+                else self._public_key_to_match.value
+            )
+            return key_to_compare == calculate_public_key(value).value
+        return True
+
+    def _validate_private_key(self, value: str) -> bool:
+        return PrivateKey.is_valid(value)
