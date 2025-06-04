a0284e3f47329908de5b7c5d02664fe49992c99e
Radek Masłowski <rmaslowski@syncad.com>
2025-06-04 07:40:06 +0000

    wip

--- tests/clive-local-tools/clive_local_tools/storage_migration/regenerate_prepared_profiles.py
+++ tests/clive-local-tools/clive_local_tools/storage_migration/regenerate_prepared_profiles.py
@@ -127,7 +127,7 @@ async def _main() -> None:
     with copy_profile_files_from_tmp_dir("without_alarms_and_operations"):
         async with prepare_encryption_service() as encryption_service:
             profile_model = create_model_from_scratch()
-            encrypted = await encryption_service.encrypt(profile_model.json(indent=4))
+            encrypted = await encryption_service.encrypt(profile_model.json())
             save_encrypted_profile(encrypted)
 
     with copy_profile_files_from_tmp_dir("with_alarms"):
@@ -142,7 +142,7 @@ async def _main() -> None:
                     ),
                 )
             ]
-            encrypted = await encryption_service.encrypt(profile_model.json(indent=4))
+            encrypted = await encryption_service.encrypt(profile_model.json())
             save_encrypted_profile(encrypted)
 
     with copy_profile_files_from_tmp_dir("with_operations"):
@@ -154,7 +154,7 @@ async def _main() -> None:
                 ),
                 transaction_file_path=Path("example/path"),
             )
-            encrypted = await encryption_service.encrypt(profile_model.json(indent=4))
+            encrypted = await encryption_service.encrypt(profile_model.json())
             save_encrypted_profile(encrypted)
 
 
