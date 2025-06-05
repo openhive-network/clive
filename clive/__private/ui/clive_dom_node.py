d4c2c79aa2babba1a3b3041ea75553a5aa3c7399
Mateusz Żebrak <mzebrak@syncad.com>
2025-06-05 09:05:37 +0200

    Move proxy properties to lower of CliveDOMNode instead of CliveWidget

--- clive/__private/ui/clive_dom_node.py
+++ clive/__private/ui/clive_dom_node.py
@@ -5,10 +5,41 @@ from typing import TYPE_CHECKING, cast
 from textual.dom import DOMNode
 
 if TYPE_CHECKING:
+    from clive.__private.core.app_state import AppState
+    from clive.__private.core.commands.commands import TUICommands
+    from clive.__private.core.node import Node
+    from clive.__private.core.profile import Profile
+    from clive.__private.core.world import TUIWorld
     from clive.__private.ui.app import Clive
 
 
 class CliveDOMNode(DOMNode):
+    """
+    An ordinary textual DOMNode that also knows what type of application it belongs to.
+
+    Inspired by: https://github.com/Textualize/textual/discussions/1099#discussioncomment-4049612
+    """
+
     @property
     def app(self) -> Clive:  # type: ignore[override]
         return cast("Clive", super().app)
+
+    @property
+    def world(self) -> TUIWorld:
+        return self.app.world
+
+    @property
+    def profile(self) -> Profile:
+        return self.world.profile
+
+    @property
+    def app_state(self) -> AppState:
+        return self.world.app_state
+
+    @property
+    def commands(self) -> TUICommands:
+        return self.world.commands
+
+    @property
+    def node(self) -> Node:
+        return self.world.node
