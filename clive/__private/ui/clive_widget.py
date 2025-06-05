cf5bb21aee49effd4199e54e56d41e891a5fe99b
Mateusz Żebrak <mzebrak@syncad.com>
2025-06-05 09:05:37 +0200

    Move proxy properties to lower of CliveDOMNode instead of CliveWidget

--- clive/__private/ui/clive_widget.py
+++ clive/__private/ui/clive_widget.py
@@ -9,40 +9,8 @@ from clive.__private.ui.clive_dom_node import CliveDOMNode
 if TYPE_CHECKING:
     from textual.binding import Binding
 
-    from clive.__private.core.app_state import AppState
-    from clive.__private.core.commands.commands import TUICommands
-    from clive.__private.core.node import Node
-    from clive.__private.core.profile import Profile
-    from clive.__private.core.world import TUIWorld
-
 
 class CliveWidget(CliveDOMNode, Widget):
-    """
-    An ordinary textual widget that also knows what type of application it belongs to.
-
-    Inspired by: https://github.com/Textualize/textual/discussions/1099#discussioncomment-4049612
-    """
-
-    @property
-    def world(self) -> TUIWorld:
-        return self.app.world
-
-    @property
-    def profile(self) -> Profile:
-        return self.world.profile
-
-    @property
-    def app_state(self) -> AppState:
-        return self.world.app_state
-
-    @property
-    def commands(self) -> TUICommands:
-        return self.world.commands
-
-    @property
-    def node(self) -> Node:
-        return self.world.node
-
     def bind(self, binding: Binding) -> None:
         """
         Bind a key to an action.
