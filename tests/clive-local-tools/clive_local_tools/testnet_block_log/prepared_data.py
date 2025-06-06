a3d9d16b7b874222ba6a65145224f45247753bcc
Mateusz Żebrak <mzebrak@syncad.com>
2025-06-06 10:57:25 +0200

    Refactor prepared_data.py

--- tests/clive-local-tools/clive_local_tools/testnet_block_log/prepared_data.py
+++ tests/clive-local-tools/clive_local_tools/testnet_block_log/prepared_data.py
@@ -4,44 +4,46 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Final
 
 import test_tools as tt
-from test_tools import BlockLog
 
 if TYPE_CHECKING:
     from datetime import timedelta
 
     from beekeepy.interfaces import HttpUrl
 
+CONFIG_FILE_DIR: Final[Path] = Path(__file__).parent
+ALTERNATE_CHAIN_SPECS_DIR: Final[Path] = CONFIG_FILE_DIR
+BLOCK_LOG_DIR: Final[Path] = CONFIG_FILE_DIR / "blockchain"
+
 EXTRA_TIME_SHIFT_FOR_GOVERNANCE: Final[timedelta] = tt.Time.days(1)
 
 
-def get_alternate_chain_spec_path() -> Path:
-    return Path(__file__).parent.absolute() / "alternate-chain-spec.json"
+def get_config(webserver_http_endpoint: HttpUrl | None = None) -> tt.NodeConfig:
+    config = tt.NodeConfig.from_path(CONFIG_FILE_DIR)
+    if webserver_http_endpoint is not None:
+        config.webserver_http_endpoint = webserver_http_endpoint
+    return config
 
 
-def get_block_log() -> BlockLog:
-    path = Path(__file__).parent.absolute() / "blockchain"
-    return BlockLog(path)
+def get_alternate_chain_spec() -> tt.AlternateChainSpecs:
+    return tt.AlternateChainSpecs.parse_file(ALTERNATE_CHAIN_SPECS_DIR)
 
 
-def get_config() -> tt.NodeConfig:
-    path = Path(__file__).parent.absolute() / "config.ini"
-    return tt.NodeConfig.from_path(path)
+def get_block_log() -> tt.BlockLog:
+    return tt.BlockLog(BLOCK_LOG_DIR)
 
 
-def get_time_control(block_log: BlockLog) -> tt.StartTimeControl:
+def get_time_control(block_log: tt.BlockLog) -> tt.StartTimeControl:
     block_log_time = block_log.get_head_block_time()
     return tt.StartTimeControl(start_time=block_log_time + EXTRA_TIME_SHIFT_FOR_GOVERNANCE)
 
 
 def run_node(webserver_http_endpoint: HttpUrl | None = None) -> tt.RawNode:
-    config = get_config()
+    config = get_config(webserver_http_endpoint)
+    alternate_chain_spec = get_alternate_chain_spec()
     block_log = get_block_log()
-    alternate_chain_spec = tt.AlternateChainSpecs.parse_file(get_alternate_chain_spec_path())
     time_control = get_time_control(block_log)
 
     node = tt.RawNode()
     node.config.load(config)
-    if webserver_http_endpoint is not None:
-        node.config.webserver_http_endpoint = webserver_http_endpoint
     node.run(replay_from=block_log, time_control=time_control, alternate_chain_specs=alternate_chain_spec)
     return node
