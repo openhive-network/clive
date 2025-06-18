3dfeef8394966c4a23dfd744164b45da7f493e72
Mateusz Żebrak <mzebrak@syncad.com>
2025-06-18 12:49:23 +0200

    WIP DO NOT MERGE modified test-tools BlockLog.get_head_block_time logs

---
+++ test_tools/__private/account.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from test_tools.__private.key_generator import KeyGenerator, KeyGeneratorItem
+
+if TYPE_CHECKING:
+    from schemas.fields.basic import PrivateKey as PrivateKeyType
+    from schemas.fields.basic import PublicKey as PublicKeyType
+
+
+class Account:
+    def __init__(self, name_or_generated_item: str | KeyGeneratorItem, secret: str = "secret") -> None:
+        self.__secret = secret
+        self.__private_key: PrivateKeyType | None = None
+        self.__public_key: PublicKeyType | None = None
+        self.__name: str = ""
+
+        if not isinstance(name_or_generated_item, str):
+            self.__name = name_or_generated_item.account_name
+            self.__private_key = name_or_generated_item.private_key
+            self.__public_key = name_or_generated_item.public_key
+        else:
+            self.__name = name_or_generated_item
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def secret(self) -> str:
+        return self.__secret
+
+    @property
+    def private_key(self) -> PrivateKeyType:
+        if self.__private_key is None:
+            self.__generate_keys()
+        assert self.__private_key is not None  # mypy check
+        return self.__private_key
+
+    @property
+    def public_key(self) -> PublicKeyType:
+        if self.__public_key is None:
+            self.__generate_keys()
+        assert self.__public_key is not None  # mypy check
+        return self.__public_key
+
+    @staticmethod
+    def create_multiple(
+        number_of_accounts: int, name_base: str = "account", *, secret: str = "secret"
+    ) -> list[Account]:
+        return [
+            Account(generated, secret=secret)
+            for generated in KeyGenerator.generate_keys(name_base, number_of_accounts=number_of_accounts, secret=secret)
+        ]
+
+    def __generate_keys(self) -> None:
+        keys = KeyGenerator.generate_keys(self.__name, secret=self.__secret)[0]
+        self.__private_key = keys.private_key
+        self.__public_key = keys.public_key
+
+    def __eq__(self, __value: object) -> bool:
+        assert isinstance(__value, Account)
+        return (self.name, self.secret) == (__value.name, __value.secret)
+
+    def __hash__(self) -> int:
+        return hash(f"{self.name} {self.secret}")
+
+
+def PublicKey(account_name: str, *, secret: str = "secret") -> PublicKeyType:  # noqa: N802
+    return Account(account_name, secret=secret).public_key
+
+
+def PrivateKey(account_name: str, *, secret: str = "secret") -> PrivateKeyType:  # noqa: N802
+    return Account(account_name, secret=secret).private_key
---
+++ test_tools/__private/communication.py
@@ -0,0 +1,108 @@
+# mypy: ignore-errors
+# ruff: noqa
+# file for deletion after cli_wallet deprecation
+from collections.abc import Callable
+from datetime import datetime
+import json
+import time
+from typing import Any, Callable
+
+import requests
+
+from schemas.fields.assets import AssetBase
+from schemas.fields.json_string import JsonString
+from schemas._preconfigured_base_model import PreconfiguredBaseModel
+from schemas.operations.representations import LegacyRepresentation
+from beekeepy.exceptions import UnableToAcquireDatabaseLockError, UnableToAcquireForkdbLockError, CommunicationError
+from loguru import logger
+from beekeepy.interfaces import HttpUrl
+from wax.helpy import Time
+
+
+class CommonJsonEncoder(json.JSONEncoder):
+    def default(self, o: Any):
+        if isinstance(o, datetime):
+            return Time.serialize(o)
+        if isinstance(o, JsonString):
+            return o.serialize()
+        if isinstance(o, PreconfiguredBaseModel):
+            return o.shallow_dict()
+
+        return super().default(o)
+
+
+class LegacyJsonEncoder(CommonJsonEncoder):
+    def default(self, o: Any):
+        if isinstance(o, AssetBase):
+            return o.as_legacy()
+        if isinstance(o, LegacyRepresentation):
+            return (o[0], o[1].dict(by_alias=True, exclude_none=True))
+        return super().default(o)
+
+
+class Hf26JsonEncoder(CommonJsonEncoder):
+    def default(self, o: Any):
+        if isinstance(o, AssetBase):
+            return o.as_nai()
+        return super().default(o)
+
+
+def __workaround_communication_problem_with_node(send_request: Callable) -> Callable:
+    """Workaround for "Unable to acquire database/forkdb lock" problem in node"""
+
+    def __implementation(*args, **kwargs):
+        while True:
+            try:
+                return send_request(*args, **kwargs)
+            except (UnableToAcquireDatabaseLockError, UnableToAcquireForkdbLockError) as exception:
+                logger.debug(
+                    'Ignored "Unable to acquire '
+                    f"{'database' if isinstance(UnableToAcquireDatabaseLockError, exception) else 'forkdb'} lock"
+                    f"error during sending request: {exception.request}"
+                )
+                continue
+
+    return __implementation
+
+
+@__workaround_communication_problem_with_node
+def request(url: str, message: dict, use_nai_assets: bool = False, max_attempts=3, seconds_between_attempts=0.2):
+    assert max_attempts > 0
+
+    json_encoder = LegacyJsonEncoder if not use_nai_assets else Hf26JsonEncoder
+    body = json.dumps(message, cls=json_encoder)
+    headers = {"Content-Type": "application/json"}
+    decoded_content = "<NO RESPONSE>"
+
+    for attempts_left in reversed(range(max_attempts)):
+        response = requests.post(url, data=body, headers=headers)
+        status_code = response.status_code
+        try:
+            decoded_content = response.content.decode("utf-8")
+        except UnicodeDecodeError as exception:
+            logger.error(
+                "An error occurred while decoding the response content.\n\n"
+                "Response content:\n"
+                f"{response.content}"
+            )  # fmt: skip
+            raise exception
+
+        response = json.loads(decoded_content)
+
+        if status_code == 200:
+            if "result" in response:
+                return response
+
+            if "error" in response:
+                logger.debug(f"Error in response from {url}: message={body}, response={response}")
+            else:
+                raise CommunicationError(url, request=body, response=response)
+        else:
+            logger.debug(
+                f"Received bad status code {status_code} != 200 from {url}, message={body}, response={response}"
+            )
+
+        if attempts_left > 0:
+            time.sleep(seconds_between_attempts)
+
+    raise CommunicationError(url, request=body, response=decoded_content)
