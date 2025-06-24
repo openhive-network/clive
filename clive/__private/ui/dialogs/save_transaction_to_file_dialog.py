6d4facc5926d9e39fe5547ca70f13c27342dbda0
Mateusz Żebrak <mzebrak@syncad.com>
2025-06-24 14:27:54 +0200

    Extract BinaryCheckBox and SignedCheckbox classes

--- clive/__private/ui/dialogs/save_transaction_to_file_dialog.py
+++ clive/__private/ui/dialogs/save_transaction_to_file_dialog.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 from textual.containers import Horizontal
 from textual.widgets import Checkbox
 
+from clive.__private.ui.clive_widget import CliveWidget
 from clive.__private.ui.dialogs.select_file_base_dialog import SelectFileBaseDialog
 from clive.__private.ui.styling import colorize_path
 
@@ -26,6 +27,32 @@ class Switches(Horizontal):
     """
 
 
+class BinaryCheckbox(Checkbox):
+    def __init__(self) -> None:
+        super().__init__("Binary?")
+
+
+class SignedCheckbox(Checkbox, CliveWidget):
+    def __init__(self) -> None:
+        super().__init__("Signed?", value=self._initial_value, disabled=self._should_be_disabled)
+
+    @property
+    def _initial_value(self) -> bool:
+        return self.profile.transaction.is_signed
+
+    @property
+    def _should_be_disabled(self) -> bool:
+        """
+        Determine if the checkbox should be disabled.
+
+        The checkbox should be disabled when there are no profile keys because it's impossible
+        to sign the transaction but should also allow for removing a signature if the transaction is already signed.
+        """
+        is_transaction_already_signed = self.profile.transaction.is_signed
+        has_profile_keys = bool(self.profile.keys)
+        return not has_profile_keys and not is_transaction_already_signed
+
+
 class SaveTransactionToFileDialog(SelectFileBaseDialog[bool]):
     def __init__(self, sign_key: PublicKey | None) -> None:
         super().__init__("Save transaction to file", "Save file")
@@ -33,22 +60,16 @@ class SaveTransactionToFileDialog(SelectFileBaseDialog[bool]):
 
     @property
     def _is_binary_checked(self) -> bool:
-        return self.query_exactly_one("#binary-checkbox", Checkbox).value
+        return self.query_exactly_one(BinaryCheckbox).value
 
     @property
     def _is_signed_checked(self) -> bool:
-        return self.query_exactly_one("#signed-checkbox", Checkbox).value
+        return self.query_exactly_one(SignedCheckbox).value
 
     def additional_content_after_input(self) -> ComposeResult:
         with Switches():
-            yield Checkbox("Binary?", id="binary-checkbox")
-            yield self._create_signed_checkbox()
-
-    def _create_signed_checkbox(self) -> Checkbox:
-        is_transaction_already_signed = self.profile.transaction.is_signed
-        has_profile_keys = bool(self.profile.keys)
-        disabled = not has_profile_keys and not is_transaction_already_signed
-        return Checkbox("Signed?", value=is_transaction_already_signed, disabled=disabled, id="signed-checkbox")
+            yield BinaryCheckbox()
+            yield SignedCheckbox()
 
     async def _perform_confirmation(self) -> bool:
         return await self._save_transaction_to_file()
