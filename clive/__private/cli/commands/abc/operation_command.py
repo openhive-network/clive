3ca20de3ba48a40b2fa26930e779a2c89f3fb7b8
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-02 12:52:55 +0200

    Docstrings format in operation_command.py

--- clive/__private/cli/commands/abc/operation_command.py
+++ clive/__private/cli/commands/abc/operation_command.py
@@ -13,6 +13,16 @@ if TYPE_CHECKING:
 
 @dataclass(kw_only=True)
 class OperationCommand(PerformActionsOnTransactionCommand, ABC):
+    """
+    Class for commands that create operations.
+
+    Args:
+        sign: The signature to use for the operation.
+        force_unsign: If True, forces the operation to be unsigned, default - False.
+        save_file: The file to save the operation to.
+        broadcast: If True, broadcasts the operation after creation.
+    """
+
     sign: str | None
     force_unsign: bool = field(init=False, default=False)
     save_file: str | None
@@ -20,12 +30,29 @@ class OperationCommand(PerformActionsOnTransactionCommand, ABC):
 
     @abstractmethod
     async def _create_operation(self) -> OperationUnion:
-        """Get the operation to be processed."""
+        """
+        Get the operation to be processed.
+
+        Returns:
+            OperationUnion: The operation object.
+        """
 
     async def _get_transaction_content(self) -> TransactionConvertibleType:
+        """
+        Get the transaction content for the transaction.
+
+        Returns:
+            TransactionConvertibleType: The transaction convertible type.
+        """
         return await self._create_operation()
 
     async def validate(self) -> None:
+        """
+        Validate the command.
+
+        Returns:
+            None
+        """
         self._validate_if_broadcast_is_used_without_force_unsign()
         self._validate_if_broadcasting_signed_transaction()
         await super().validate()
