58a1f85d5db431ca521d7ed4440bf518b9d37c7c
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-03 11:34:52 +0200

    Docstrings format in show_memo_key.py

--- clive/__private/cli/commands/show/show_memo_key.py
+++ clive/__private/cli/commands/show/show_memo_key.py
@@ -9,9 +9,22 @@ from clive.__private.cli.commands.abc.world_based_command import WorldBasedComma
 
 @dataclass(kw_only=True)
 class ShowMemoKey(WorldBasedCommand):
+    """
+    Show the memo key of a specified account.
+
+    Args:
+        account_name: The name of the account whose memo key is to be displayed.
+    """
+
     account_name: str
 
     async def _run(self) -> None:
+        """
+        Run the command to show the memo key of the specified account.
+
+        Returns:
+            None: This method does not return any value, it prints the memo key to the console.
+        """
         accounts = (await self.world.commands.find_accounts(accounts=[self.account_name])).result_or_raise
         account = accounts[0]
 
