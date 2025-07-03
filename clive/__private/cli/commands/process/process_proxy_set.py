766000bdf0df829b72a3317d39b03578f13dee90
Wojciech Prokop <prokopw581@gmail.com>
2025-07-03 12:20:45 +0000

    Docstrings format in process_proxy_set.py

--- clive/__private/cli/commands/process/process_proxy_set.py
+++ clive/__private/cli/commands/process/process_proxy_set.py
@@ -8,10 +8,24 @@ from clive.__private.models.schemas import AccountWitnessProxyOperation
 
 @dataclass(kw_only=True)
 class ProcessProxySet(OperationCommand):
+    """
+    Class to set a witness proxy for an account.
+
+    Args:
+        account_name: The name of the account to set the proxy for.
+        proxy: The witness proxy to set for the account.
+    """
+
     account_name: str
     proxy: str
 
     async def _create_operation(self) -> AccountWitnessProxyOperation:
+        """
+        Create an operation to set the witness proxy for the account.
+
+        Returns:
+            AccountWitnessProxyOperation: The operation to set the witness proxy.
+        """
         return AccountWitnessProxyOperation(
             account=self.account_name,
             proxy=self.proxy,
