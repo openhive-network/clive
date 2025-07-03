1d7569e067b8ccbb77e6d92416277712ce2a5592
Wojciech Prokop <prokopw581@gmail.com>
2025-07-03 12:48:57 +0000

    Docstrings format in process_withdrawal_cancel.py

--- clive/__private/cli/commands/process/process_withdrawal_cancel.py
+++ clive/__private/cli/commands/process/process_withdrawal_cancel.py
@@ -8,10 +8,24 @@ from clive.__private.models.schemas import CancelTransferFromSavingsOperation
 
 @dataclass(kw_only=True)
 class ProcessWithdrawalCancel(OperationCommand):
+    """
+    Class to handle the cancellation of a withdrawal from savings.
+
+    Args:
+        from_account: The account from which the withdrawal is being canceled.
+        request_id: The ID of the withdrawal request to be canceled.
+    """
+
     from_account: str
     request_id: int
 
     async def _create_operation(self) -> CancelTransferFromSavingsOperation:
+        """
+        Create an instance with the provided details.
+
+        Returns:
+            CancelTransferFromSavingsOperation: An operation instance for canceling the withdrawal.
+        """
         return CancelTransferFromSavingsOperation(
             from_=self.from_account,
             request_id=self.request_id,
