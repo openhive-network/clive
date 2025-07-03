17524d752c0f246ffa1fff2b63a54705015817df
Wojciech Prokop <prokopw581@gmail.com>
2025-07-03 10:55:41 +0000

    Docstrings format in chain_id.py

--- clive/__private/cli/commands/configure/chain_id.py
+++ clive/__private/cli/commands/configure/chain_id.py
@@ -7,13 +7,34 @@ from clive.__private.cli.commands.abc.world_based_command import WorldBasedComma
 
 @dataclass(kw_only=True)
 class SetChainId(WorldBasedCommand):
+    """
+    Class that sets the chain ID.
+
+    Args:
+        chain_id: The chain ID to set.
+    """
+
     chain_id: str
 
     async def _run(self) -> None:
+        """
+        Set the chain ID for the current profile.
+
+        Returns:
+            None
+        """
         self.profile.set_chain_id(self.chain_id)
 
 
 @dataclass(kw_only=True)
 class UnsetChainId(WorldBasedCommand):
+    """Class that unsets the chain ID."""
+
     async def _run(self) -> None:
+        """
+        Unset the chain ID for the current profile.
+
+        Returns:
+            None
+        """
         self.profile.unset_chain_id()
