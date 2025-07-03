f9a4c8a1512a73f12bc8eb6d9446d1e4447c5eab
Wojciech Prokop <prokopw581@gmail.com>
2025-07-03 12:19:28 +0000

    Docstrings format in process_proxy_clear.py

--- clive/__private/cli/commands/process/process_proxy_clear.py
+++ clive/__private/cli/commands/process/process_proxy_clear.py
@@ -8,9 +8,22 @@ from clive.__private.models.schemas import AccountWitnessProxyOperation
 
 @dataclass(kw_only=True)
 class ProcessProxyClear(OperationCommand):
+    """
+    Class to clear the proxy for a given account.
+
+    Args:
+        account_name: The name of the account for which to clear the proxy.
+    """
+
     account_name: str
 
     async def _create_operation(self) -> AccountWitnessProxyOperation:
+        """
+        Create an operation to clear the proxy for the specified account.
+
+        Returns:
+            AccountWitnessProxyOperation: An operation with the account name and an empty proxy.
+        """
         return AccountWitnessProxyOperation(
             account=self.account_name,
             proxy="",
