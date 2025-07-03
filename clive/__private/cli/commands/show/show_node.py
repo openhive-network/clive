6da5c1885470ea7b201a46281eaf3b60ceb488fb
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-03 11:41:10 +0200

    Docstrings format in show_node.py

--- clive/__private/cli/commands/show/show_node.py
+++ clive/__private/cli/commands/show/show_node.py
@@ -9,5 +9,13 @@ from clive.__private.cli.commands.abc.world_based_command import WorldBasedComma
 
 @dataclass(kw_only=True)
 class ShowNode(WorldBasedCommand):
+    """Show the node address of the current profile."""
+
     async def _run(self) -> None:
+        """
+        Show the node address of the current profile.
+
+        Returns:
+            None: This method does not return anything, it prints the node address in console.
+        """
         typer.echo(self.profile.node_address)
