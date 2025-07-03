a0133fc8faf415728dc70041a518720e0937f156
Wojciech Prokop <prokopw581@gmail.com>
2025-07-03 11:25:51 +0000

    Docstrings format in node.py

--- clive/__private/cli/commands/configure/node.py
+++ clive/__private/cli/commands/configure/node.py
@@ -9,8 +9,23 @@ from clive.__private.cli.commands.abc.world_based_command import WorldBasedComma
 
 @dataclass(kw_only=True)
 class SetNode(WorldBasedCommand):
+    """
+    Class to set the node address in the profile.
+
+    Args:
+        node_address: The address of the node to set in the profile.
+    """
+
     node_address: str
 
     async def _run(self) -> None:
+        """
+        Run the command to set the node address in the profile.
+
+        This method converts the node address to an HttpUrl and sets it in the profile.
+
+        Returns:
+            None
+        """
         url = HttpUrl(self.node_address)
         self.profile._set_node_address(url)
