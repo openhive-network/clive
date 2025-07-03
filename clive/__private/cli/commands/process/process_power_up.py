57d71e08dd2b4804e79e3498012f22a918ffff43
Wojciech Prokop <prokopw581@gmail.com>
2025-07-03 12:18:07 +0000

    Docstrings format in process_power_up.py

--- clive/__private/cli/commands/process/process_power_up.py
+++ clive/__private/cli/commands/process/process_power_up.py
@@ -12,11 +12,26 @@ if TYPE_CHECKING:
 
 @dataclass(kw_only=True)
 class ProcessPowerUp(OperationCommand):
+    """
+    Class to handle the power-up operation in the Hive blockchain.
+
+    Args:
+        from_account: The account initiating the power-up.
+        to_account: The account receiving the power-up.
+        amount: The amount of Hive to be powered up.
+    """
+
     from_account: str
     to_account: str
     amount: Asset.Hive
 
     async def _create_operation(self) -> TransferToVestingOperation:
+        """
+        Create an operation for the power-up operation.
+
+        Returns:
+            TransferToVestingOperation: The operation to be executed.
+        """
         return TransferToVestingOperation(
             from_=self.from_account,
             to=self.to_account,
