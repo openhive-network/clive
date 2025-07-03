5919c813c00fd9b5219f86b0179176723a14c877
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-03 11:38:26 +0200

    Docstrings format in show_new_account_token.py

--- clive/__private/cli/commands/show/show_new_account_token.py
+++ clive/__private/cli/commands/show/show_new_account_token.py
@@ -9,9 +9,22 @@ from clive.__private.cli.commands.abc.world_based_command import WorldBasedComma
 
 @dataclass(kw_only=True)
 class ShowNewAccountToken(WorldBasedCommand):
+    """
+    Show the number of new-account-tokens claimed by a specific account.
+
+    Args:
+        account_name: The name of the account to check for new-account-tokens.
+    """
+
     account_name: str
 
     async def _run(self) -> None:
+        """
+        Run the command to show the number of new-account-tokens claimed by the specified account.
+
+        Returns:
+            None: This method does not return any value, it prints the number of new-account-tokens to the console.
+        """
         accounts = (await self.world.commands.find_accounts(accounts=[self.account_name])).result_or_raise
         pending_claimed_accounts = accounts[0].pending_claimed_accounts
 
