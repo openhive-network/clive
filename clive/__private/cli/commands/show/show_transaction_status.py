5b207e5ff42d39ac44de2794224ea029991f3217
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-03 12:03:31 +0200

    Docstrings format in show_transaction_status.py

--- clive/__private/cli/commands/show/show_transaction_status.py
+++ clive/__private/cli/commands/show/show_transaction_status.py
@@ -9,9 +9,22 @@ from clive.__private.cli.commands.abc.world_based_command import WorldBasedComma
 
 @dataclass(kw_only=True)
 class ShowTransactionStatus(WorldBasedCommand):
+    """
+    Show the status of a transaction.
+
+    Args:
+        transaction_id: The ID of the transaction to show the status for.
+    """
+
     transaction_id: str
 
     async def _run(self) -> None:
+        """
+        Show the status of a transaction.
+
+        Returns:
+            None: The method prints the transaction status into the console.
+        """
         status = await self.world.commands.find_transaction(transaction_id=self.transaction_id)
 
         typer.echo(status.result_or_raise)
