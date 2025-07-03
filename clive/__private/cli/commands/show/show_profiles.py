a437435ed63b7ba905ac62001fa90cd98a218e25
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-03 11:55:35 +0200

    Docstrings format in show_profiles.py

--- clive/__private/cli/commands/show/show_profiles.py
+++ clive/__private/cli/commands/show/show_profiles.py
@@ -10,5 +10,13 @@ from clive.__private.core.profile import Profile
 
 @dataclass(kw_only=True)
 class ShowProfiles(ExternalCLICommand):
+    """Show the stored profiles."""
+
     async def _run(self) -> None:
+        """
+        Show the stored profiles, and display them in a console.
+
+        Returns:
+            None
+        """
         typer.echo(f"Stored profiles are: {Profile.list_profiles()}")
