2aa0489535105ba16b28e4771f9abf60d5920884
Wojciech Prokop <prokopw581@gmail.com>
2025-07-03 12:42:11 +0000

    Docstrings format in process_vote_witness.py

--- clive/__private/cli/commands/process/process_vote_witness.py
+++ clive/__private/cli/commands/process/process_vote_witness.py
@@ -8,11 +8,26 @@ from clive.__private.models.schemas import AccountWitnessVoteOperation
 
 @dataclass(kw_only=True)
 class ProcessVoteWitness(OperationCommand):
+    """
+    Class to vote for or against a witness.
+
+    Args:
+        account_name: The name of the account casting the vote.
+        witness_name: The name of the witness to vote for or against.
+        approve: True to approve the witness, False to disapprove.
+    """
+
     account_name: str
     witness_name: str
     approve: bool
 
     async def _create_operation(self) -> AccountWitnessVoteOperation:
+        """
+        Create an operation to vote for or against a witness.
+
+        Returns:
+            AccountWitnessVoteOperation: The operation containing the vote details.
+        """
         return AccountWitnessVoteOperation(
             account=self.account_name,
             witness=self.witness_name,
