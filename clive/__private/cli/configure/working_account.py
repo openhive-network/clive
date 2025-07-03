6bb91b7d19c62b6c6b55da469b9b79e8edc7baad
Wojciech Prokop <prokopw581@gmail.com>
2025-07-03 13:14:39 +0000

    Docstrings format in clive/__private/cli/configure/working_account.py

--- clive/__private/cli/configure/working_account.py
+++ clive/__private/cli/configure/working_account.py
@@ -21,7 +21,16 @@ async def switch_working_account(
     account_name: str = _account_name_switch_argument,
     account_name_option: str | None = argument_related_options.account_name,
 ) -> None:
-    """Switch the working account."""
+    """
+    Switch the working account.
+
+    Args:
+        account_name: The name of the account to switch to. This is required as an argument or option.
+        account_name_option: An alternative way to specify the account name, if provided.
+
+    Returns:
+        None: This function does not return anything. It performs the action of switching the working account.
+    """
     from clive.__private.cli.commands.configure.working_account import SwitchWorkingAccount
 
     await SwitchWorkingAccount(account_name=EnsureSingleAccountNameValue().of(account_name, account_name_option)).run()
