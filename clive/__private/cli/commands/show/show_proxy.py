476b410d9e82575664ab949ccd6472d1efdc9971
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-03 12:02:25 +0200

    Docstrings format in show_proxy.py

--- clive/__private/cli/commands/show/show_proxy.py
+++ clive/__private/cli/commands/show/show_proxy.py
@@ -10,9 +10,25 @@ from clive.__private.cli.styling import colorize_content_not_available
 
 @dataclass(kw_only=True)
 class ShowProxy(WorldBasedCommand):
+    """
+    Show the proxy of an account.
+
+    Args:
+        account_name: The name of the account to show the proxy for.
+    """
+
     account_name: str
 
     async def _run(self) -> None:
+        """
+        Show the proxy of an account.
+
+        This method retrieves the account's proxy and displays it in the console.
+        If the account has no proxy set, it will print a message indicating that.
+
+        Returns:
+            None: The method prints the proxy information into the console.
+        """
         console = Console()
         accounts = (await self.world.commands.find_accounts(accounts=[self.account_name])).result_or_raise
         proxy = accounts[0].proxy
