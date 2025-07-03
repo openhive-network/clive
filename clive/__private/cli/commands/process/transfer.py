fdef00423df2fc3cde91868438f6bd5c70e75d2b
Wojciech Prokop <prokopw581@gmail.com>
2025-07-03 12:52:30 +0000

    Docstrings format in transfer.py

--- clive/__private/cli/commands/process/transfer.py
+++ clive/__private/cli/commands/process/transfer.py
@@ -12,12 +12,28 @@ if TYPE_CHECKING:
 
 @dataclass(kw_only=True)
 class Transfer(OperationCommand):
+    """
+    Class to transfer assets from one account to another.
+
+    Args:
+        from_account: The account to transfer assets from.
+        to: The account to transfer assets to.
+        amount: The amount of assets to transfer.
+        memo: An optional memo for the transfer.
+    """
+
     from_account: str
     to: str
     amount: Asset.LiquidT
     memo: str
 
     async def _create_operation(self) -> TransferOperation:
+        """
+        Create an operation instance based on the provided parameters.
+
+        Returns:
+            TransferOperation: An instance of TransferOperation with the specified parameters.
+        """
         return TransferOperation(
             from_=self.from_account,
             to=self.to,
