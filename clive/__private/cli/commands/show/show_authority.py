f8f40ed01ba3a427f69575671ab7cf807a82e0b2
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-03 11:00:56 +0200

    Docstrings format in show_authority.py

--- clive/__private/cli/commands/show/show_authority.py
+++ clive/__private/cli/commands/show/show_authority.py
@@ -14,10 +14,26 @@ if TYPE_CHECKING:
 
 @dataclass(kw_only=True)
 class ShowAuthority(WorldBasedCommand):
+    """
+    Show authority of the account.
+
+    Args:
+        account_name: The name of the account whose authority is to be shown.
+        authority: The type of authority to show (e.g., "active", "owner", "posting").
+    """
+
     account_name: str
     authority: AuthorityType
 
     async def _run(self) -> None:
+        """
+        Show the authority of the specified account.
+
+        Prints a table with the account or public key and its corresponding weight.
+
+        Returns:
+            None
+        """
         accounts = (await self.world.commands.find_accounts(accounts=[self.account_name])).result_or_raise
         account = accounts[0]
 
