7efa2dae40948d9c4c668a967ff7f2abf7806629
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-04 10:58:09 +0200

    Docstrings format in notify.py

--- clive/__private/cli/notify.py
+++ clive/__private/cli/notify.py
@@ -11,6 +11,16 @@ if TYPE_CHECKING:
 
 
 def notify(message: str, *, level: NotifyLevel = "info") -> None:
+    """
+    Print a notification message to the console with appropriate styling based on the level (warning/error).
+
+    Args:
+        message: The message to be printed.
+        level: The level of the notification. Can be "info", "warning", or "error".
+
+    Returns:
+        None: This function does not return any value. It prints the message directly to the console.
+    """
     if level == "warning":
         message = colorize_warning(message)
     elif level == "error":
