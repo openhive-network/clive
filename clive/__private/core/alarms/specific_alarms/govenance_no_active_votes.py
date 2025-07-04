3f22f3af36ee6a046e5a132cad5f23598bab949d
Wojciech Prokop <prokopw581@gmail.com>
2025-07-04 12:03:27 +0000

    Docstrings format in govenance_no_active_votes.py

--- clive/__private/core/alarms/specific_alarms/govenance_no_active_votes.py
+++ clive/__private/core/alarms/specific_alarms/govenance_no_active_votes.py
@@ -16,14 +16,32 @@ if TYPE_CHECKING:
 
 @dataclass
 class GovernanceNoActiveVotesAlarmData(AlarmDataNeverExpiresWithoutAction):
+    """
+    Class representing the data for the GovernanceNoActiveVotes alarm.
+
+    Args:
+        expiration_date: The date when the governance vote expires.
+    """
+
     expiration_date: datetime
 
 
 class GovernanceNoActiveVotes(Alarm[DateTimeAlarmIdentifier, GovernanceNoActiveVotesAlarmData]):
+    """Class representing an alarm for no active governance votes."""
+
     ALARM_DESCRIPTION = GOVERNANCE_COMMON_ALARM_DESCRIPTION
     FIX_ALARM_INFO = "You should cast votes for witnesses and proposals or set a proxy."
 
     def update_alarm_status(self, data: AccountAlarmsData) -> None:
+        """
+        Update the status of the alarm based on the provided account alarms data.
+
+        Args:
+            data: The account alarms data containing governance vote expiration information.
+
+        Returns:
+            None
+        """
         expiration = data.governance_vote_expiration_ts
         if is_null_date(expiration):
             new_identifier = DateTimeAlarmIdentifier(value=expiration)
@@ -33,4 +51,10 @@ class GovernanceNoActiveVotes(Alarm[DateTimeAlarmIdentifier, GovernanceNoActiveV
         self.disable_alarm()
 
     def get_alarm_basic_info(self) -> str:
+        """
+        Return a basic description of the alarm.
+
+        Returns:
+            str: A string indicating that there are no active governance votes.
+        """
         return "No active governance votes"
