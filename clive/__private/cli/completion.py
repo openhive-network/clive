3f9dc795319389dc62047eb2cb1c4a87495618a2
Wojciech Prokop <prokopw581@gmail.com>
2025-07-04 10:44:38 +0000

    Docstrings format in completion.py

--- clive/__private/cli/completion.py
+++ clive/__private/cli/completion.py
@@ -4,4 +4,10 @@ import os
 
 
 def is_tab_completion_active() -> bool:
+    """
+    Check if tab completion is active by looking for a specific environment variable.
+
+    Returns:
+        bool: True if tab completion is active, False otherwise.
+    """
     return "_CLIVE_COMPLETE" in os.environ
