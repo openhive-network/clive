7f9c0b56fef5563e59b9214394dab03457f67394
Wojciech Prokop <prokopw581@gmail.com>
2025-07-04 12:29:18 +0000

    Docstrings format in alarm.py

--- clive/__private/core/alarms/alarm.py
+++ clive/__private/core/alarms/alarm.py
@@ -46,6 +46,17 @@ class Alarm[AlarmIdentifierT: AlarmIdentifier, AlarmDataT](ABC):
         alarm_data: AlarmDataT | None = None,
         is_harmless: bool = False,
     ) -> None:
+        """
+        Initialize an alarm with an identifier and data.
+
+        Args:
+            identifier: An instance of `AlarmIdentifier` that uniquely identifies the alarm.
+            alarm_data: An instance of the data class that holds information about the alarm.
+            is_harmless: A boolean indicating whether the alarm is harmless (default is False).
+
+        Returns:
+            None
+        """
         super().__init__()
         self.identifier = identifier
         self.alarm_data = alarm_data
@@ -53,25 +64,68 @@ class Alarm[AlarmIdentifierT: AlarmIdentifier, AlarmDataT](ABC):
         self.is_harmless = is_harmless
 
     def __init_subclass__(cls, **kwargs: Any) -> None:
+        """
+        Register the subclass in the `_alarm_name_class_map` when a class is subclassed.
+
+        Args:
+            kwargs: Additional keyword arguments passed to the superclass initializer
+
+        Returns:
+            None
+        """
         super().__init_subclass__(**kwargs)
         cls._alarm_name_class_map[cls.get_name()] = cls
 
     @classmethod
     def get_name(cls) -> str:
+        """
+        Get the name of the alarm class in snake_case format.
+
+        Returns:
+            str: The name of the alarm class.
+        """
         return underscore(cls.__name__)
 
     @classmethod
     def all_alarm_classes(cls) -> list[type[AnyAlarm]]:
+        """
+        Get a list of all registered alarm classes.
+
+        Returns:
+            list: A list of all alarm classes.
+        """
         return list(cls._alarm_name_class_map.values())
 
     @classmethod
     def get_alarm_class_by_name(cls, name: str) -> type[AnyAlarm]:
+        """
+        Get an alarm class by its name.
+
+        Args:
+            name: The name of the alarm class in snake_case format.
+
+        Raises:
+            AssertionError: If the alarm class with the given name does not exist.
+
+        Returns:
+            type: The alarm class corresponding to the given name.
+        """
         try:
             return cls._alarm_name_class_map[name]
         except KeyError as error:
             raise AssertionError(f"Alarm class with name '{name}' does not exist.") from error
 
     def enable_alarm(self, identifier: AlarmIdentifierT, alarm_data: AlarmDataT) -> None:
+        """
+        Enable the alarm with the given identifier and data.
+
+        Args:
+            identifier: An instance of `AlarmIdentifier` that uniquely identifies the alarm.
+            alarm_data: An instance of the data class that holds information about the alarm.
+
+        Returns:
+            None
+        """
         if identifier == self.identifier:
             if not self.has_data:
                 # means we're loading the alarm from storage and we need to update the data
@@ -84,6 +138,12 @@ class Alarm[AlarmIdentifierT: AlarmIdentifier, AlarmDataT](ABC):
         self.alarm_data = alarm_data
 
     def disable_alarm(self) -> None:
+        """
+        Disable the alarm by resetting its state.
+
+        Returns:
+            None
+        """
         self.is_active = False
         self.is_harmless = False
         self.identifier = None
@@ -91,26 +151,69 @@ class Alarm[AlarmIdentifierT: AlarmIdentifier, AlarmDataT](ABC):
 
     @property
     def alarm_data_ensure(self) -> AlarmDataT:
+        """
+        Ensure that alarm data is available and return it.
+
+        Raises:
+            AssertionError: If alarm data is not available or active.
+
+        Returns:
+            AlarmDataT: The data associated with the alarm.
+        """
         assert self.alarm_data is not None, "You're trying to access alarm data that is not available/active."
         return self.alarm_data
 
     @property
     def identifier_ensure(self) -> AlarmIdentifierT:
+        """
+        Ensure that the alarm identifier is available and return it.
+
+        Raises:
+            AssertionError: If the alarm identifier is not available.
+
+        Returns:
+            AlarmIdentifierT: The identifier of the alarm.
+        """
         assert self.identifier is not None, "You're trying to access alarm identifier that is not available."
         return self.identifier
 
     @property
     def has_data(self) -> bool:
+        """
+        Check if the alarm has associated data.
+
+        Returns:
+            bool: True if the alarm has data, False otherwise.
+        """
         return self.alarm_data is not None
 
     @property
     def has_identifier(self) -> bool:
+        """
+        Check if the alarm has an identifier.
+
+        Returns:
+            bool: True if the alarm has an identifier, False otherwise.
+        """
         return self.identifier is not None
 
     @abstractmethod
     def update_alarm_status(self, data: AccountAlarmsData) -> None:
-        """Change alarm status based on data retrieved during `update_alarms_data`."""
+        """
+        Change alarm status based on data retrieved during `update_alarms_data`.
+
+        Args:
+            data: An instance of `AccountAlarmsData` containing the latest data for the alarm.
+
+        Returns:
+            None
+        """
 
     @abstractmethod
     def get_alarm_basic_info(self) -> str:
-        """Return the simplest possible information about the alarm."""
+        """
+        Return the simplest possible information about the alarm.
+
+        Returns:
+            str: A string containing basic information about the alarm.
+        """
