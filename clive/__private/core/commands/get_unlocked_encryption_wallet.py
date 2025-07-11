d6e5213508c3f17de124ab23e7c86bad0dc4844d
Wojciech Prokop <prokopw581@gmail.com>
2025-07-11 12:45:47 +0000

    Fix pydoclint errors in core/commands/get_unlocked_encryption_wallet.py.GetUnlockedEncryptionWallet

--- clive/__private/core/commands/get_unlocked_encryption_wallet.py
+++ clive/__private/core/commands/get_unlocked_encryption_wallet.py
@@ -23,7 +23,12 @@ class NoEncryptionWalletUnlockedError(CommandError):
 
 @dataclass(kw_only=True)
 class GetUnlockedEncryptionWallet(CommandWithResult[AsyncUnlockedWallet]):
-    """Get the unlocked encryption wallet - the one containing encryption key managed by clive."""
+    """
+    Get the unlocked encryption wallet - the one containing encryption key managed by clive.
+
+    Attributes:
+        session: The session to use for fetching the encryption wallet.
+    """
 
     session: AsyncSession
 
