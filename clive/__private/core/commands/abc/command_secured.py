cd1cfe2cf234b47c1492722b4cac7a9f954bcedd
Wojciech Prokop <prokopw581@gmail.com>
2025-07-11 12:35:28 +0000

    Fix pydoclint errors in core/commands/abc/command_secured.py.CommandPasswordSecured

--- clive/__private/core/commands/abc/command_secured.py
+++ clive/__private/core/commands/abc/command_secured.py
@@ -12,6 +12,9 @@ class CommandPasswordSecured(Command, ABC):
     A command that requires a password to proceed.
 
     In case the password is invalid, there could be `beekeepy.exceptions.InvalidPasswordError` raised.
+
+    Attributes:
+        password: The password required to execute the command.
     """
 
     password: str
