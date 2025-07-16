202f7523219e8773da6fd99bcb8efa62504afff2
Radek Masłowski <rmaslowski@syncad.com>
2025-07-16 07:21:56 +0000

    Remove ABC, you shouldn't inherit from ABC and msgspec.Struct

--- clive/__private/core/alarms/alarm_identifier.py
+++ clive/__private/core/alarms/alarm_identifier.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-from abc import ABC
-
 from clive.__private.models.schemas import HiveDateTime, PreconfiguredBaseModel
 
 
-class AlarmIdentifier(PreconfiguredBaseModel, ABC):
+class AlarmIdentifier(PreconfiguredBaseModel):
     """Base class for alarm identifiers."""
 
 
