69389c5f42ce00a9b2f3fb320fc7c773eba3cf8b
Radek Masłowski <rmaslowski@syncad.com>
2025-07-16 12:50:44 +0000

    Use strict int conversion in iwax function

--- clive/__private/core/iwax.py
+++ clive/__private/core/iwax.py
@@ -1,13 +1,18 @@
 from __future__ import annotations
 
 import datetime
-from typing import TYPE_CHECKING, Protocol, cast
+from collections.abc import Callable
+from functools import wraps
+from typing import TYPE_CHECKING, Any, Protocol, TypeVar, cast
 
 import wax
 from clive.__private.core.constants.precision import HIVE_PERCENT_PRECISION_DOT_PLACES
 from clive.__private.core.decimal_conventer import DecimalConverter
 from clive.__private.core.percent_conversions import hive_percent_to_percent
-from clive.__private.models.schemas import convert_to_representation
+from clive.__private.models.schemas import (
+    HiveInt,
+    convert_to_representation,
+)
 from clive.exceptions import CliveError
 
 if TYPE_CHECKING:
@@ -16,6 +21,22 @@ if TYPE_CHECKING:
     from clive.__private.core.keys import PrivateKey, PublicKey
     from clive.__private.models import Asset, Transaction
     from clive.__private.models.schemas import OperationUnion, PriceFeed
+
+F = TypeVar("F", bound=Callable[..., Any])
+
+
+def cast_hiveint_args(func: F) -> F:
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:  # noqa: ANN401
+        def hiveint_to_int(value: Any) -> Any:  # noqa: ANN401
+            return int(value) if isinstance(value, HiveInt) else value
+
+        new_args = tuple(hiveint_to_int(arg) for arg in args)
+        new_kwargs = {k: hiveint_to_int(v) for k, v in kwargs.items()}
+
+        return func(*new_args, **new_kwargs)
+
+    return cast("F", wrapper)
 
 
 class HpAPRProtocol(Protocol):
@@ -72,7 +93,7 @@ def to_python_json_asset(asset: Asset.AnyT) -> wax.python_json_asset:
         case "VESTS":
             return wax.vests(amount=int(asset.amount))
         case _:
-            raise UnknownAssetTypeError(asset.nai)
+            raise UnknownAssetTypeError(asset.nai())
 
 
 def __validate_wax_response(response: wax.python_result) -> None:
@@ -83,10 +104,8 @@ def __validate_wax_response(response: wax.python_result) -> None:
 def __as_binary_json(item: OperationUnion | Transaction) -> bytes:
     from clive.__private.models import Transaction
 
-    if not isinstance(item, Transaction):
-        item = convert_to_representation(item)
-
-    return item.json().encode()
+    to_serialize = item if isinstance(item, Transaction) else convert_to_representation(item)
+    return to_serialize.json().encode()
 
 
 def validate_transaction(transaction: Transaction) -> None:
@@ -139,6 +158,7 @@ def generate_private_key() -> PrivateKey:
     return PrivateKey(value=result.result.decode())
 
 
+@cast_hiveint_args
 def calculate_manabar_full_regeneration_time(
     now: int, max_mana: int, current_mana: int, last_update_time: int
 ) -> datetime.datetime:
@@ -149,6 +169,7 @@ def calculate_manabar_full_regeneration_time(
     return datetime.datetime.fromtimestamp(int(result.result.decode()), tz=datetime.UTC)
 
 
+@cast_hiveint_args
 def calculate_current_manabar_value(now: int, max_mana: int, current_mana: int, last_update_time: int) -> int:
     result = wax.calculate_current_manabar_value(
         now=now, max_mana=max_mana, current_mana=current_mana, last_update_time=last_update_time
@@ -157,6 +178,7 @@ def calculate_current_manabar_value(now: int, max_mana: int, current_mana: int, 
     return int(result.result.decode())
 
 
+@cast_hiveint_args
 def general_asset(asset_num: int, amount: int) -> Asset.AnyT:
     return from_python_json_asset(wax.general_asset(asset_num=asset_num, amount=amount))
 
@@ -165,22 +187,25 @@ def get_tapos_data(block_id: str) -> wax.python_ref_block_data:
     return wax.get_tapos_data(block_id.encode())
 
 
+@cast_hiveint_args
 def hive(amount: int) -> Asset.Hive:
     return cast("Asset.Hive", from_python_json_asset(wax.hive(amount)))
 
 
+@cast_hiveint_args
 def hbd(amount: int) -> Asset.Hbd:
     return cast("Asset.Hbd", from_python_json_asset(wax.hbd(amount)))
 
 
+@cast_hiveint_args
 def vests(amount: int) -> Asset.Vests:
     return cast("Asset.Vests", from_python_json_asset(wax.vests(amount)))
 
 
 def calculate_hp_apr(data: HpAPRProtocol) -> Decimal:
     result = wax.calculate_hp_apr(
-        head_block_num=data.head_block_number,
-        vesting_reward_percent=data.vesting_reward_percent,
+        head_block_num=int(data.head_block_number),
+        vesting_reward_percent=int(data.vesting_reward_percent),
         virtual_supply=to_python_json_asset(data.virtual_supply),
         total_vesting_fund_hive=to_python_json_asset(data.total_vesting_fund_hive),
     )
@@ -197,6 +222,7 @@ def calculate_hbd_to_hive(_hbd: Asset.Hbd, current_price_feed: PriceFeed) -> Ass
     return cast("Asset.Hive", from_python_json_asset(result))
 
 
+@cast_hiveint_args
 def calculate_vests_to_hp(_vests: int | Asset.Vests, data: TotalVestingProtocol) -> Asset.Hive:
     vests_json_asset = wax.vests(_vests) if isinstance(_vests, int) else to_python_json_asset(_vests)
     result = wax.calculate_vests_to_hp(
@@ -216,12 +242,14 @@ def calculate_hp_to_vests(_hive: Asset.Hive, data: TotalVestingProtocol) -> Asse
     return cast("Asset.Vests", from_python_json_asset(result))
 
 
+@cast_hiveint_args
 def calculate_current_inflation_rate(head_block_num: int) -> Decimal:
     result = wax.calculate_inflation_rate_for_block(head_block_num)
     __validate_wax_response(result)
     return hive_percent_to_percent(result.result.decode())
 
 
+@cast_hiveint_args
 def calculate_witness_votes_hp(votes: int, data: TotalVestingProtocol) -> Asset.Hive:
     result = wax.calculate_witness_votes_hp(
         votes=votes,
