ac54ba4b8f9ef9d508dd6cf43ff67f20aaa6d177
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-20 19:23:01 +0200

    Fix pydoclint errors in ui/widgets/inputs/text_input.py.TextInput

--- clive/__private/ui/widgets/inputs/text_input.py
+++ clive/__private/ui/widgets/inputs/text_input.py
@@ -13,7 +13,26 @@ if TYPE_CHECKING:
 
 
 class TextInput(CliveValidatedInput[str]):
-    """An input for a text value."""
+    """
+    An input for a text value.
+
+    Args:
+        title: The title of the input.
+        value: The initial value of the input.
+        placeholder: The placeholder text for the input.
+        always_show_title: Whether to always show the title.
+        include_title_in_placeholder_when_blurred: Whether to include the title when the input is blurred.
+        show_invalid_reasons: Whether to show reasons for invalid input.
+        required: Whether the input is required.
+        password: Whether the input should be treated as a password (hidden input).
+        suggester: A suggester for autocompletion.
+        validators: Validators to apply to the input value.
+        validate_on: When to validate the input.
+        valid_empty: Whether the input can be valid when empty.
+        id: The ID of the input widget.
+        classes: Additional CSS classes for the input.
+        disabled: Whether the input is disabled.
+    """
 
     def __init__(
         self,
