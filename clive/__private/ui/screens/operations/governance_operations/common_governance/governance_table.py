1d3ccd603f3a1e7f5364ec0194304986d3dee81a
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-20 16:18:35 +0200

    Fix pydoclint errors in ui/screens/operations/governance_operations/common_governance/governance_table.py.GovernanceTableRow.create_row_content

--- clive/__private/ui/screens/operations/governance_operations/common_governance/governance_table.py
+++ clive/__private/ui/screens/operations/governance_operations/common_governance/governance_table.py
@@ -156,7 +156,12 @@ class GovernanceTableRow(Grid, CliveWidget, Generic[GovernanceDataT], AbstractCl
 
     @abstractmethod
     def create_row_content(self) -> ComposeResult:
-        """Contains all the information that should be displayed about the item."""
+        """
+        Contains all the information that should be displayed about the item.
+
+        Returns:
+            A composition of widgets that represent the row content.
+        """
 
     @abstractmethod
     def get_action_row_id(self) -> str:
