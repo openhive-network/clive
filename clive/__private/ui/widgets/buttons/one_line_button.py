ddab4e92e97bea74f112c2c09f8a543ca568daab
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-20 18:41:15 +0200

    Fix pydoclint errors in ui/widgets/buttons/one_line_button.py.OneLineButton

--- clive/__private/ui/widgets/buttons/one_line_button.py
+++ clive/__private/ui/widgets/buttons/one_line_button.py
@@ -4,7 +4,12 @@ from clive.__private.ui.widgets.buttons.clive_button import CliveButton
 
 
 class OneLineButton(CliveButton):
-    """Button that is without border around it, so it can be used in one line."""
+    """
+    Button that is without border around it, so it can be used in one line.
+
+    Attributes:
+        DEFAULT_CSS: Default CSS styles for the OneLineButton.
+    """
 
     class Pressed(CliveButton.Pressed):
         """Used to identify exactly that OneLineButton was pressed."""
