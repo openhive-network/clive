fd135b4e4f0ade56d2b5a6e3db2d26ea02e38f93
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-20 18:57:09 +0200

    Fix pydoclint errors in ui/widgets/clive_basic/clive_radio_button.py.CliveRadioButton

--- clive/__private/ui/widgets/clive_basic/clive_radio_button.py
+++ clive/__private/ui/widgets/clive_basic/clive_radio_button.py
@@ -4,6 +4,11 @@ from textual.widgets import RadioButton
 
 
 class CliveRadioButton(RadioButton):
-    """Due to bug in Ubuntu we have to replace icon of the RadioButton by simple 'O'."""
+    """
+    Due to bug in Ubuntu we have to replace icon of the RadioButton by simple 'O'.
+
+    Attributes:
+        BUTTON_INNER: The inner content of the radio button, which is a simple 'O'.
+    """
 
     BUTTON_INNER = "O"
