2f2c1d80260a1d2455185a5505b66c85f7588ce4
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-20 19:27:07 +0200

    Fix pydoclint errors in ui/widgets/place_taker.py.PlaceTaker

--- clive/__private/ui/widgets/place_taker.py
+++ clive/__private/ui/widgets/place_taker.py
@@ -4,7 +4,12 @@ from textual.widgets import Static
 
 
 class PlaceTaker(Static):
-    """A widget that takes up space but does not render anything."""
+    """
+    A widget that takes up space but does not render anything.
+
+    Attributes:
+        DEFAULT_CSS: The default CSS styles for the widget.
+    """
 
     DEFAULT_CSS = """
     PlaceTaker {
