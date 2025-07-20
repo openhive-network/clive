ca8a4af619fdd1ba9dd36ffba02d4baa9d91d55a
Dawid Nocinski <nocinskidawid@gmail.com>
2025-07-20 12:17:40 +0200

    Fix pydoclint errors in ui/screens/account_details/authority.py.AuthorityTable

--- clive/__private/ui/screens/account_details/authority/authority.py
+++ clive/__private/ui/screens/account_details/authority/authority.py
@@ -287,7 +287,16 @@ class MemoItem(CliveCheckerboardTableRow):
 
 
 class AuthorityTable(CliveCheckerboardTable):
-    """A table containing all entries of a single type of authority."""
+    """
+    A table containing all entries of a single type of authority.
+
+    Attributes:
+        NO_CONTENT_TEXT: Text displayed when there are no entries in the authority.
+
+    Args:
+        single_authority: An authority object or a memo key.
+        filter_pattern: Patterns to filter the entries in the authority.
+    """
 
     NO_CONTENT_TEXT = "No entries in authority"
 
