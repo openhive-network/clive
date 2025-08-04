7d3f27c68c71c9bdeb79b52567af1cb481009cad
Radek Masłowski <rmaslowski@syncad.com>
2025-08-04 08:09:09 +0000

    fixup! Use meta property instead of meta() function

--- clive/__private/settings/_safe_settings.py
+++ clive/__private/settings/_safe_settings.py
@@ -334,7 +334,7 @@ class SafeSettings:
             value_ = cast("str", value)
 
             if not is_matching_model(value_, ChainId):
-                details = f"Chain ID should be {ChainId.meta.max_length} characters long."  # type: ignore[attr-defined]
+                details = f"Chain ID should be {ChainId.meta().max_length} characters long."  # type: ignore[attr-defined]
                 raise SettingsValueError(setting_name=setting_name, value=value_, details=details)
             return value_
 
