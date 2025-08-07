961e81b44c16fbf27906846b5e41c99189197473
Marcin Sobczyk <msobczyk@syncad.com>
2025-08-07 10:26:13 +0000

    Replace type aliases inside ProfileSTorageModel with class definitions
    Because mypy doesnt recognize types of aliases defined as
    ClassVar[TypeAlias]

--- clive/__private/storage/migrations/v0.py
+++ clive/__private/storage/migrations/v0.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from collections.abc import Sequence  # noqa: TC003
 from pathlib import Path  # noqa: TC003
-from typing import Any, ClassVar, Self, TypeAlias
+from typing import Any, Self
 
 from clive.__private.core.alarms.specific_alarms import (
     ChangingRecoveryAccountInProgress,
@@ -21,91 +21,7 @@ from clive.__private.models.schemas import (
     Uint32t,
     field,
 )
-from clive.__private.storage.migrations.base import ProfileStorageBase
-
-
-class DateTimeAlarmIdentifierStorageModel(PreconfiguredBaseModel):
-    value: HiveDateTime
-
-
-class RecoveryAccountWarningListedAlarmIdentifierStorageModel(PreconfiguredBaseModel):
-    recovery_account: str
-
-
-AllAlarmIdentifiersStorageModel = (
-    DateTimeAlarmIdentifierStorageModel | RecoveryAccountWarningListedAlarmIdentifierStorageModel
-)
-
-
-class AlarmStorageModelBase(PreconfiguredBaseModel, tag_field="name", kw_only=True):
-    @classmethod
-    def get_name(cls) -> str:
-        assert isinstance(cls.__struct_config__.tag, str), "Alarm storage models must have a string tag."
-        return cls.__struct_config__.tag
-
-    is_harmless: bool = False
-    """Identifies the occurrence of specific alarm among other possible alarms of same type. E.g. end date."""
-
-
-class RecoveryAccountWarningListedStorageModel(
-    AlarmStorageModelBase, tag=RecoveryAccountWarningListed.get_name(), kw_only=True
-):
-    identifier: RecoveryAccountWarningListedAlarmIdentifierStorageModel
-
-
-class GovernanceVotingExpirationStorageModel(
-    AlarmStorageModelBase, tag=GovernanceVotingExpiration.get_name(), kw_only=True
-):
-    identifier: DateTimeAlarmIdentifierStorageModel
-
-
-class GovernanceNoActiveVotesStorageModel(AlarmStorageModelBase, tag=GovernanceNoActiveVotes.get_name(), kw_only=True):
-    identifier: DateTimeAlarmIdentifierStorageModel
-
-
-class DecliningVotingRightsInProgressStorageModel(
-    AlarmStorageModelBase, tag=DecliningVotingRightsInProgress.get_name(), kw_only=True
-):
-    identifier: DateTimeAlarmIdentifierStorageModel
-
-
-class ChangingRecoveryAccountInProgressStorageModel(
-    AlarmStorageModelBase, tag=ChangingRecoveryAccountInProgress.get_name(), kw_only=True
-):
-    identifier: DateTimeAlarmIdentifierStorageModel
-
-
-AllAlarmStorageModel = (
-    RecoveryAccountWarningListedStorageModel
-    | GovernanceVotingExpirationStorageModel
-    | GovernanceNoActiveVotesStorageModel
-    | DecliningVotingRightsInProgressStorageModel
-    | ChangingRecoveryAccountInProgressStorageModel
-)
-
-
-class TrackedAccountStorageModel(PreconfiguredBaseModel):
-    name: str
-    alarms: Sequence[AllAlarmStorageModel] = []
-
-
-class KeyAliasStorageModel(PreconfiguredBaseModel):
-    alias: str
-    public_key: str
-
-
-class TransactionCoreStorageModel(PreconfiguredBaseModel):
-    operations: list[OperationRepresentationUnion] = []  # noqa: RUF012
-    ref_block_num: Uint16t = 0
-    ref_block_prefix: Uint32t = 0
-    expiration: HiveDateTime = field(default_factory=lambda: HiveDateTime(utc_epoch()))
-    extensions: list[Any] = []  # noqa: RUF012
-    signatures: list[Signature] = []  # noqa: RUF012
-
-
-class TransactionStorageModel(PreconfiguredBaseModel):
-    transaction_core: TransactionCoreStorageModel
-    transaction_file_path: Path | None = None
+from clive.__private.storage.migrations.base import AlarmStorageModelBase, ProfileStorageBase
 
 
 class ProfileStorageModel(ProfileStorageBase, kw_only=True):
@@ -119,21 +35,68 @@ class ProfileStorageModel(ProfileStorageBase, kw_only=True):
     node_address: str
     should_enable_known_accounts: bool = True
 
-    _AllAlarmIdentifiersStorageModel: ClassVar[TypeAlias] = AllAlarmIdentifiersStorageModel
-    _AllAlarmStorageModel: ClassVar[TypeAlias] = AllAlarmStorageModel
-    _TrackedAccountStorageModel: ClassVar[TypeAlias] = TrackedAccountStorageModel
-    _KeyAliasStorageModel: ClassVar[TypeAlias] = KeyAliasStorageModel
-    _TransactionCoreStorageModel: ClassVar[TypeAlias] = TransactionCoreStorageModel
-    _TransactionStorageModel: ClassVar[TypeAlias] = TransactionStorageModel
-    _DateTimeAlarmIdentifierStorageModel: ClassVar[TypeAlias] = DateTimeAlarmIdentifierStorageModel
-    _RecoveryAccountWarningListedAlarmIdentifierStorageModel: ClassVar[TypeAlias] = (
-        RecoveryAccountWarningListedAlarmIdentifierStorageModel
+    class DateTimeAlarmIdentifierStorageModel(PreconfiguredBaseModel):
+        value: HiveDateTime
+
+    class RecoveryAccountWarningListedAlarmIdentifierStorageModel(PreconfiguredBaseModel):
+        recovery_account: str
+
+    AllAlarmIdentifiersStorageModel = (
+        DateTimeAlarmIdentifierStorageModel | RecoveryAccountWarningListedAlarmIdentifierStorageModel
     )
-    _RecoveryAccountWarningListedStorageModel: ClassVar[TypeAlias] = RecoveryAccountWarningListedStorageModel
-    _GovernanceVotingExpirationStorageModel: ClassVar[TypeAlias] = GovernanceVotingExpirationStorageModel
-    _GovernanceNoActiveVotesStorageModel: ClassVar[TypeAlias] = GovernanceNoActiveVotesStorageModel
-    _DecliningVotingRightsInProgressStorageModel: ClassVar[TypeAlias] = DecliningVotingRightsInProgressStorageModel
-    _ChangingRecoveryAccountInProgressStorageModel: ClassVar[TypeAlias] = ChangingRecoveryAccountInProgressStorageModel
+
+    class RecoveryAccountWarningListedStorageModel(
+        AlarmStorageModelBase, tag=RecoveryAccountWarningListed.get_name(), kw_only=True
+    ):
+        identifier: ProfileStorageModel.RecoveryAccountWarningListedAlarmIdentifierStorageModel
+
+    class GovernanceVotingExpirationStorageModel(
+        AlarmStorageModelBase, tag=GovernanceVotingExpiration.get_name(), kw_only=True
+    ):
+        identifier: ProfileStorageModel.DateTimeAlarmIdentifierStorageModel
+
+    class GovernanceNoActiveVotesStorageModel(
+        AlarmStorageModelBase, tag=GovernanceNoActiveVotes.get_name(), kw_only=True
+    ):
+        identifier: ProfileStorageModel.DateTimeAlarmIdentifierStorageModel
+
+    class DecliningVotingRightsInProgressStorageModel(
+        AlarmStorageModelBase, tag=DecliningVotingRightsInProgress.get_name(), kw_only=True
+    ):
+        identifier: ProfileStorageModel.DateTimeAlarmIdentifierStorageModel
+
+    class ChangingRecoveryAccountInProgressStorageModel(
+        AlarmStorageModelBase, tag=ChangingRecoveryAccountInProgress.get_name(), kw_only=True
+    ):
+        identifier: ProfileStorageModel.DateTimeAlarmIdentifierStorageModel
+
+    AllAlarmStorageModel = (
+        RecoveryAccountWarningListedStorageModel
+        | GovernanceVotingExpirationStorageModel
+        | GovernanceNoActiveVotesStorageModel
+        | DecliningVotingRightsInProgressStorageModel
+        | ChangingRecoveryAccountInProgressStorageModel
+    )
+
+    class TrackedAccountStorageModel(PreconfiguredBaseModel):
+        name: str
+        alarms: Sequence[ProfileStorageModel.AllAlarmStorageModel] = []
+
+    class KeyAliasStorageModel(PreconfiguredBaseModel):
+        alias: str
+        public_key: str
+
+    class TransactionCoreStorageModel(PreconfiguredBaseModel):
+        operations: list[OperationRepresentationUnion] = []  # noqa: RUF012
+        ref_block_num: Uint16t = 0
+        ref_block_prefix: Uint32t = 0
+        expiration: HiveDateTime = field(default_factory=lambda: HiveDateTime(utc_epoch()))
+        extensions: list[Any] = []  # noqa: RUF012
+        signatures: list[Signature] = []  # noqa: RUF012
+
+    class TransactionStorageModel(PreconfiguredBaseModel):
+        transaction_core: ProfileStorageModel.TransactionCoreStorageModel
+        transaction_file_path: Path | None = None
 
     @classmethod
     def upgrade(cls, old: ProfileStorageBase) -> Self:
