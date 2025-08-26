587d16234d5dad5b86cce2f8b3f759ee4e88fd49
Mateusz Kudela <mkudela@syncad.com>
2025-08-26 12:10:03 +0000

    Allow RemoveKeyAliasDialog to remove more than one key

--- clive/__private/ui/dialogs/remove_key_alias_dialog.py
+++ clive/__private/ui/dialogs/remove_key_alias_dialog.py
@@ -1,31 +1,63 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from clive.__private.ui.dialogs.confirm_action_dialog import ConfirmActionDialog
+
+if TYPE_CHECKING:
+    from clive.__private.core.keys.keys import PublicKeyAliased
 
 
 class RemoveKeyAliasDialog(ConfirmActionDialog):
     """
-    Dialog to confirm if the user wants to remove key alias from profile.
+    Dialog to confirm if the user wants to remove key alias or many aliases from profile.
 
     Args:
-        key_alias: Alias of key that is about to be removed.
-
-    Raises:
-        KeyNotFoundError: When key with given alias was not found in the profile.
+        *key_aliases: Key aliases that are about to be removed.
     """
 
-    def __init__(self, key_alias: str) -> None:
-        self._public_key_aliased = self.profile.keys.get_from_alias(key_alias)
-        super().__init__(
-            border_title="Removing the key alias",
-            confirm_question=(
-                f"You are about to remove a `{self._public_key_aliased.alias}` key alias from the profile.\n"
-                f"Are you sure you want to proceed?"
-            ),
-        )
+    def __init__(self, *key_aliases: str | PublicKeyAliased) -> None:
+        self._key_aliases = key_aliases
+
+        super().__init__(border_title="Removing the key alias", confirm_question=self._get_confirm_question())
+
+    @property
+    def is_removing_single_alias(self) -> bool:
+        return len(self.keys_to_remove) == 1
+
+    @property
+    def keys_to_remove(self) -> list[PublicKeyAliased]:
+        """
+        Get all the aliased key instances that are about to be removed.
+
+        Raises:
+            KeyNotFoundError: When given key alias is not found in the profile.
+
+        Returns:
+            Aliased keys to remove.
+        """
+        return [self.profile.keys.get_from_alias(item) for item in self._key_aliases]
 
     async def _perform_confirmation(self) -> bool:
-        self.profile.keys.remove(self._public_key_aliased)
-        self.notify(f"Key alias `{self._public_key_aliased.alias}` was removed.")
+        notify_text = self._get_notify_text()
+        self.profile.keys.remove(*self.keys_to_remove)
+        self.notify(notify_text)
         self.app.trigger_profile_watchers()
         return True
+
+    def _get_confirm_question(self) -> str:
+        keys_to_remove = self.keys_to_remove
+        if self.is_removing_single_alias:
+            key_alias = keys_to_remove[0].alias
+            part_of_question = f"a `{key_alias}` key alias"
+        else:
+            aliases = [key.alias for key in keys_to_remove]
+            part_of_question = f"{', '.join(f'`{alias}`' for alias in aliases)} key aliases"
+
+        return f"You are about to remove {part_of_question} from the profile.\nAre you sure you want to proceed?"
+
+    def _get_notify_text(self) -> str:
+        if self.is_removing_single_alias:
+            key_alias = self.keys_to_remove[0].alias
+            return f"Key alias `{key_alias}` was removed."
+        return "Many key aliases were removed."
