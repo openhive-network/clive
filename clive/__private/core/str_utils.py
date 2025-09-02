48f2ca84e61a6468ffaefe7b46e44f4bc28ef60b
Mateusz Kudela <mkudela@syncad.com>
2025-09-02 12:25:07 +0000

    Add Matchable class to str_utils

--- clive/__private/core/str_utils.py
+++ clive/__private/core/str_utils.py
@@ -1,6 +1,25 @@
 from __future__ import annotations
 
 import re
+from abc import ABC, abstractmethod
+
+
+class Matchable(ABC):
+    @abstractmethod
+    def is_matching_pattern(self, *patterns: str) -> bool:
+        """
+        Checks if the provided patterns match a certain condition.
+
+        This method is meant to be implemented by subclasses and defines a
+        custom logic to evaluate whether the supplied pattern(s) satisfy
+        a specific criterion.
+
+        Args:
+            *patterns: The patterns to check against.
+
+        Returns:
+            True if the patterns match the condition, otherwise False.
+        """
 
 
 def is_text_matching_pattern(text: str, *patterns: str) -> bool:
