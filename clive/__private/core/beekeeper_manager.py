e4d0029a09aaad905b5faad6aa28f83330e9c2e4
Bartek Wrona <wrona@syncad.com>
2025-09-19 01:04:42 +0200

    WIP: beekeeper_manager encapsulates beekepy imports pointing to AsyncBeekeeper, BeekeeperSettings and AsyncSession. Beekeepy must be updated to eliminate pulling whole package when only small part (e.g. communication specific) is needed

--- clive/__private/core/beekeeper_manager.py
+++ clive/__private/core/beekeeper_manager.py
@@ -2,15 +2,18 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Final
 
-from beekeepy import AsyncBeekeeper, AsyncSession
-from beekeepy import Settings as BeekeepySettings
+from beekeepy._interface.abc import (
+    AsyncBeekeeper,
+    AsyncUnlockedWallet,
+    AsyncSession
+)
+
+from beekeepy._interface import InterfaceSettings as BeekeepySettings
 
 from clive.__private.settings import safe_settings
 from clive.exceptions import CliveError
 
 if TYPE_CHECKING:
-    from beekeepy import AsyncSession, AsyncUnlockedWallet
-
     from clive.__private.core.wallet_container import WalletContainer
 
 
