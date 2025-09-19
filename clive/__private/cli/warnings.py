e0fb18ce50ef2b44a57fccfa4ca127186bffa067
Wiesław Kędzierski <wkedzierski@syncad.com>
2025-09-19 10:39:56 +0200

    Extend functionality of typer_echo_warnings. Now it accepts category, and new_message arguments.

--- clive/__private/cli/warnings.py
+++ clive/__private/cli/warnings.py
@@ -11,9 +11,21 @@ if TYPE_CHECKING:
 
 
 @contextmanager
-def typer_echo_warnings() -> Iterator[None]:
+def typer_echo_warnings(category: type[Warning] | None = None, new_message: str | Warning = "") -> Iterator[None]:
+    if category is None and new_message:
+        raise AssertionError("'new_message' shouldn't be used without category")
+
     with warnings.catch_warnings(record=True) as catched_warnings:
         yield
 
         for warning in catched_warnings:
-            print_warning(f"{warning.message}")
+            if category is None:
+                print_warning(str(warning.message))
+                continue
+
+            if issubclass(warning.category, category):
+                print_warning(str(new_message) if new_message else str(warning.message))
+            else:
+                warnings.warn_explicit(
+                    warning.message, warning.category, warning.filename, warning.lineno, warning.source
+                )
