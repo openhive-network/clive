125ffff35f6f2d58c2ba497fcaf167f1c958e574
Radek Masłowski <rmaslowski@syncad.com>
2025-09-22 06:32:59 +0000

    wip

--- clive/skrypty/test_first.py
+++ clive/skrypty/test_first.py
@@ -1,18 +1,23 @@
-from clive.__private.core.world import World
+from __future__ import annotations
+
+import loguru
+
 from clive.__private.core.accounts.accounts import TrackedAccount
+from clive.__private.core.world import World
+
 
 async def test_first() -> None:
     world = World()
     await world.setup()
     await world.create_new_profile("alice")
 
-    #pokazywanie profili
+    # pokazywanie profili
     show_profile = world.profile
 
-    #pokazywanie balansów
+    # pokazywanie balansów
     account = TrackedAccount(name="alice")
 
     await world.commands.update_node_data(accounts=[account])
     balances = account.data
-
-    pass
+    loguru.logger.info(balances)
+    loguru.logger.info(show_profile)
