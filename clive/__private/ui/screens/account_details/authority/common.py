f38789af939d3f6c92b7669e3cbeb185cd4c57b0
Mateusz Kudela <mkudela@syncad.com>
2025-09-24 13:20:35 +0000

    Divide FilterAuthority into normal and extended version, add TopContainer

--- clive/__private/ui/screens/account_details/authority/common.py
+++ clive/__private/ui/screens/account_details/authority/common.py
@@ -2,13 +2,20 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from textual.containers import Horizontal
+from textual.containers import Container, Horizontal
 from textual.widgets import Static
 
 from clive.__private.core.constants.tui.class_names import CLIVE_EVEN_COLUMN_CLASS_NAME, CLIVE_ODD_COLUMN_CLASS_NAME
+from clive.__private.ui.screens.account_details.authority.filter_authority import (
+    FilterAuthority,
+    FilterAuthorityExtended,
+)
 
 if TYPE_CHECKING:
     from textual.app import ComposeResult
+
+    from clive.__private.core.accounts.accounts import TrackedAccount
+    from clive.__private.ui.widgets.buttons.clive_button import CliveButton
 
 
 class AuthorityHeader(Horizontal):
@@ -35,3 +42,53 @@ class AuthorityHeader(Horizontal):
         else:
             yield Static("Key", classes=f"{CLIVE_ODD_COLUMN_CLASS_NAME} memo-key")
             yield Static(self._last_column_header_title, classes=f"{CLIVE_EVEN_COLUMN_CLASS_NAME} action")
+
+
+class TopContainer(Horizontal):
+    """
+    Container with FilterAuthority widget and additional action button.
+
+    To use version with extended filtering (by accounts), provide an account to the init method.
+
+    Attributes:
+        DEFAULT_CSS: The default CSS styling for the container.
+
+    Args:
+        action_button: The button that will be placed next to the filter authority widget.
+        account: The account that will be initially selected in the filter authority widget.
+    """
+
+    DEFAULT_CSS = """
+    TopContainer {
+        margin: 1 0;
+        width: auto;
+        height: auto;
+
+        #button-container {
+            margin: 2 0 0 2;
+            padding-right: 1;
+            width: 1fr;
+            height: auto;
+
+            CliveButton {
+                min-width: 1fr;
+            }
+        }
+    }
+    """
+
+    def __init__(
+        self,
+        *,
+        action_button: CliveButton,
+        account: TrackedAccount | None = None,
+    ) -> None:
+        super().__init__()
+        self._account = account
+        self._action_button = action_button
+
+    def compose(self) -> ComposeResult:
+        filter_widget = FilterAuthorityExtended(self._account) if self._account else FilterAuthority()
+        yield filter_widget
+        with Container(id="button-container"):
+            yield self._action_button
