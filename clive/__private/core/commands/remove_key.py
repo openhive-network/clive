f84788e8bf886bab270dfa7c967b0276ca6fdee8
Mateusz Żebrak <mzebrak@syncad.com>
2025-09-25 12:54:10 +0200

    Remove dead code from RemoveKey core command

--- clive/__private/core/commands/remove_key.py
+++ clive/__private/core/commands/remove_key.py
@@ -1,28 +1,18 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
 from clive.__private.core.commands.abc.command_in_unlocked import CommandInUnlocked
-from clive.__private.core.keys import PublicKey, PublicKeyAliased
-from clive.exceptions import CliveError
+
+if TYPE_CHECKING:
+    from clive.__private.core.keys import PublicKey
 
 
 @dataclass(kw_only=True)
 class RemoveKey(CommandInUnlocked):
-    key_to_remove: PublicKey | PublicKeyAliased
-
-    action_name: str = field(init=False)
-
-    def __post_init__(self) -> None:
-        self.action_name = f"Removing a `{self.__get_key_description()}` key."
+    key_to_remove: PublicKey
 
     async def _execute(self) -> None:
         public_key = self.key_to_remove.value
         await self.unlocked_wallet.remove_key(key=public_key)
-
-    def __get_key_description(self) -> str:
-        if isinstance(self.key_to_remove, PublicKey):
-            return self.key_to_remove.value
-        if isinstance(self.key_to_remove, PublicKeyAliased):
-            return self.key_to_remove.alias
-        raise CliveError("Not implemented")
