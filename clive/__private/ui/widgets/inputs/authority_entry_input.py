092acae1e432e6231be9153f3d12975d9e940f30
Mateusz Kudela <mkudela@syncad.com>
2025-10-03 12:56:53 +0000

    Add AuthorityEntryInput, AuthorityEntryMemoInput and AuthorityWeightInput

---
+++ clive/__private/ui/widgets/inputs/authority_entry_input.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from clive.__private.core.authority.entries import AuthorityEntryBase
+from clive.__private.ui.widgets.inputs.text_input import TextInput
+from clive.__private.validators.authority_entry_validator import AuthorityEntryValidator
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+    from textual.suggester import Suggester
+    from textual.validation import Validator
+    from textual.widgets._input import InputValidationOn
+
+
+class AuthorityEntryInput(TextInput):
+    """
+    Input for regular authority entry value - key or account.
+
+    Args:
+        title: The title of the input.
+        value: The initial value of the input.
+        placeholder: Placeholder text for the input.
+        always_show_title: Whether to always show the title (by default it is shown only when focused).
+        include_title_in_placeholder_when_blurred: Whether to include the title in the placeholder when blurred.
+        show_invalid_reasons: Whether to show reasons for invalid input.
+        required: Whether the input is required.
+        suggester: A suggester for auto-completion.
+        validators: Validators for the input.
+        validate_on: When to validate the input.
+        valid_empty: Whether an empty input is considered as valid.
+        id: The ID of the input in the DOM.
+        classes: The CSS classes for the input.
+        disabled: Whether the input is disabled.
+    """
+
+    def __init__(
+        self,
+        title: str = "Account or public key",
+        value: str | None = None,
+        placeholder: str = "",
+        *,
+        always_show_title: bool = True,
+        include_title_in_placeholder_when_blurred: bool = True,
+        show_invalid_reasons: bool = True,
+        required: bool = True,
+        suggester: Suggester | None = None,
+        validators: Validator | Iterable[Validator] | None = None,
+        validate_on: Iterable[InputValidationOn] | None = None,
+        valid_empty: bool = False,
+        id: str | None = None,  # noqa: A002
+        classes: str | None = None,
+        disabled: bool = False,
+    ) -> None:
+        super().__init__(
+            title=title,
+            value=value,
+            placeholder=placeholder,
+            always_show_title=always_show_title,
+            include_title_in_placeholder_when_blurred=include_title_in_placeholder_when_blurred,
+            show_invalid_reasons=show_invalid_reasons,
+            required=required,
+            suggester=suggester,
+            validators=validators if validators else AuthorityEntryValidator(),
+            validate_on=validate_on,
+            valid_empty=valid_empty,
+            id=id,
+            classes=classes,
+            disabled=disabled,
+        )
+
+    @property
+    def holds_account_name(self) -> bool:
+        entry_type = AuthorityEntryBase.determine_entry_type(self.input.value)
+        return entry_type == "account"
---
+++ clive/__private/ui/widgets/inputs/authority_weight_input.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from clive.__private.core.constants.authority import DEFAULT_AUTHORITY_WEIGHT
+from clive.__private.core.constants.tui.placeholders import INTEGER_PLACEHOLDER
+from clive.__private.ui.widgets.inputs.integer_input import IntegerInput
+from clive.__private.validators.authority_weight_validator import AuthorityWeightValidator
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+    from textual.validation import Validator
+    from textual.widgets._input import InputValidationOn
+
+
+class AuthorityWeightInput(IntegerInput):
+    """
+    Input for authority weight.
+
+    Args:
+        value: The initial value of the input.
+        placeholder: Placeholder text for the input.
+        always_show_title: Whether to always show the title (by default it is shown only when focused).
+        include_title_in_placeholder_when_blurred: Whether to include the title in the placeholder when blurred.
+        show_invalid_reasons: Whether to show reasons for invalid input.
+        required: Whether the input is required.
+        validators: Validators for the input.
+        validate_on: When to validate the input.
+        valid_empty: Whether an empty input is considered as valid.
+        id: The ID of the input in the DOM.
+        classes: The CSS classes for the input.
+        disabled: Whether the input is disabled.
+    """
+
+    def __init__(
+        self,
+        value: str | int | None = DEFAULT_AUTHORITY_WEIGHT,
+        placeholder: str = INTEGER_PLACEHOLDER,
+        *,
+        always_show_title: bool = True,
+        include_title_in_placeholder_when_blurred: bool = True,
+        show_invalid_reasons: bool = True,
+        required: bool = True,
+        validators: Validator | Iterable[Validator] | None = None,
+        validate_on: Iterable[InputValidationOn] | None = None,
+        valid_empty: bool = False,
+        id: str | None = None,  # noqa: A002
+        classes: str | None = None,
+        disabled: bool = False,
+    ) -> None:
+        super().__init__(
+            title="Weight",
+            value=str(value) if value is not None else None,
+            placeholder=placeholder,
+            always_show_title=always_show_title,
+            include_title_in_placeholder_when_blurred=include_title_in_placeholder_when_blurred,
+            show_invalid_reasons=show_invalid_reasons,
+            required=required,
+            validators=validators or [AuthorityWeightValidator()],
+            validate_on=validate_on,
+            valid_empty=valid_empty,
+            id=id,
+            classes=classes,
+            disabled=disabled,
+        )
