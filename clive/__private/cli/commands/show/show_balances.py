dadff96bd2410534215cd470bf15993289c67957
Radek Masłowski <rmaslowski@syncad.com>
2025-10-08 07:16:46 +0000

    Use SI interface in CLI

--- clive/__private/cli/commands/show/show_balances.py
+++ clive/__private/cli/commands/show/show_balances.py
@@ -6,8 +6,8 @@ from rich.table import Table
 
 from clive.__private.cli.commands.abc.world_based_command import WorldBasedCommand
 from clive.__private.cli.print_cli import print_cli
-from clive.__private.core.accounts.accounts import TrackedAccount
 from clive.__private.models.asset import Asset
+from clive.__private.si.core.show import ShowBalances as ShowBalancesSi
 
 
 @dataclass(kw_only=True)
@@ -15,20 +15,16 @@ class ShowBalances(WorldBasedCommand):
     account_name: str
 
     async def _run(self) -> None:
-        account = TrackedAccount(name=self.account_name)
-
-        await self.world.commands.update_node_data(accounts=[account])
-
+        balances = await ShowBalancesSi(world=self.world, account_name=self.account_name).run()
         table = Table(title=f"Balances of `{self.account_name}` account")
 
         table.add_column("Type", justify="left", style="cyan", no_wrap=True)
         table.add_column("Amount", justify="right", style="green", no_wrap=True)
-        data = account.data
-        table.add_row("HBD Liquid", f"{Asset.pretty_amount(data.hbd_balance)}")
-        table.add_row("HBD Savings", f"{Asset.pretty_amount(data.hbd_savings)}")
-        table.add_row("HBD Unclaimed", f"{Asset.pretty_amount(data.hbd_unclaimed)}")
-        table.add_row("HIVE Liquid", f"{Asset.pretty_amount(data.hive_balance)}")
-        table.add_row("HIVE Savings", f"{Asset.pretty_amount(data.hive_savings)}")
-        table.add_row("HIVE Unclaimed", f"{Asset.pretty_amount(data.hive_unclaimed)}")
+        table.add_row("HBD Liquid", f"{Asset.pretty_amount(balances.hbd_liquid)}")
+        table.add_row("HBD Savings", f"{Asset.pretty_amount(balances.hbd_savings)}")
+        table.add_row("HBD Unclaimed", f"{Asset.pretty_amount(balances.hbd_unclaimed)}")
+        table.add_row("HIVE Liquid", f"{Asset.pretty_amount(balances.hive_liquid)}")
+        table.add_row("HIVE Savings", f"{Asset.pretty_amount(balances.hive_savings)}")
+        table.add_row("HIVE Unclaimed", f"{Asset.pretty_amount(balances.hive_unclaimed)}")
 
         print_cli(table)
