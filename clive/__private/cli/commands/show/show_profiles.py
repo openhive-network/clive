63b387208ccb63b6b3a2fd8a17f8c962ce9e4d8c
Radek Masłowski <rmaslowski@syncad.com>
2025-10-08 07:16:46 +0000

    Use SI interface in CLI

--- clive/__private/cli/commands/show/show_profiles.py
+++ clive/__private/cli/commands/show/show_profiles.py
@@ -4,10 +4,10 @@ from dataclasses import dataclass
 
 from clive.__private.cli.commands.abc.external_cli_command import ExternalCLICommand
 from clive.__private.cli.print_cli import print_cli
-from clive.__private.core.profile import Profile
+from clive.__private.si.core.show import ShowProfiles as ShowProfilesSi
 
 
 @dataclass(kw_only=True)
 class ShowProfiles(ExternalCLICommand):
     async def _run(self) -> None:
-        print_cli(f"Stored profiles are: {Profile.list_profiles()}")
+        print_cli(f"Stored profiles are: {await ShowProfilesSi().run()}")
