c044b4e9c71512b74547a37ebc6895c551226fc4
Mateusz Żebrak <mzebrak@syncad.com>
2025-10-08 12:01:06 +0200

    Use extract_params in UpdateAuthority chained command

--- tests/clive-local-tools/clive_local_tools/cli/chaining/update_authority.py
+++ tests/clive-local-tools/clive_local_tools/cli/chaining/update_authority.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from clive_local_tools.cli.chaining.chained_command import ChainedCommand
+from clive_local_tools.cli.command_options import extract_params
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -25,15 +26,7 @@ class UpdateAuthority(ChainedCommand):
         save_file: Path | None = None,
         autosign: bool | None = None,
     ) -> UpdateAuthority:
-        self._add_command_to_chain(
-            "add-key",
-            key=key,
-            weight=weight,
-            sign_with=sign_with,
-            broadcast=broadcast,
-            save_file=save_file,
-            autosign=autosign,
-        )
+        self._add_command_to_chain("add-key", **extract_params(locals()))
         return self
 
     def add_account(  # noqa: PLR0913
@@ -46,15 +39,7 @@ class UpdateAuthority(ChainedCommand):
         save_file: Path | None = None,
         autosign: bool | None = None,
     ) -> UpdateAuthority:
-        self._add_command_to_chain(
-            "add-account",
-            account=account,
-            weight=weight,
-            sign_with=sign_with,
-            broadcast=broadcast,
-            save_file=save_file,
-            autosign=autosign,
-        )
+        self._add_command_to_chain("add-account", **extract_params(locals()))
         return self
 
     def remove_key(
@@ -66,14 +51,7 @@ class UpdateAuthority(ChainedCommand):
         save_file: Path | None = None,
         autosign: bool | None = None,
     ) -> UpdateAuthority:
-        self._add_command_to_chain(
-            "remove-key",
-            key=key,
-            sign_with=sign_with,
-            broadcast=broadcast,
-            save_file=save_file,
-            autosign=autosign,
-        )
+        self._add_command_to_chain("remove-key", **extract_params(locals()))
         return self
 
     def remove_account(
@@ -85,14 +63,7 @@ class UpdateAuthority(ChainedCommand):
         save_file: Path | None = None,
         autosign: bool | None = None,
     ) -> UpdateAuthority:
-        self._add_command_to_chain(
-            "remove-account",
-            account=account,
-            sign_with=sign_with,
-            broadcast=broadcast,
-            save_file=save_file,
-            autosign=autosign,
-        )
+        self._add_command_to_chain("remove-account", **extract_params(locals()))
         return self
 
     def modify_key(  # noqa: PLR0913
@@ -105,15 +76,7 @@ class UpdateAuthority(ChainedCommand):
         save_file: Path | None = None,
         autosign: bool | None = None,
     ) -> UpdateAuthority:
-        self._add_command_to_chain(
-            "modify-key",
-            key=key,
-            weight=weight,
-            sign_with=sign_with,
-            broadcast=broadcast,
-            save_file=save_file,
-            autosign=autosign,
-        )
+        self._add_command_to_chain("modify-key", **extract_params(locals()))
         return self
 
     def modify_account(  # noqa: PLR0913
@@ -126,15 +89,7 @@ class UpdateAuthority(ChainedCommand):
         save_file: Path | None = None,
         autosign: bool | None = None,
     ) -> UpdateAuthority:
-        self._add_command_to_chain(
-            "modify-account",
-            account=account,
-            weight=weight,
-            sign_with=sign_with,
-            broadcast=broadcast,
-            save_file=save_file,
-            autosign=autosign,
-        )
+        self._add_command_to_chain("modify-account", **extract_params(locals()))
         return self
 
 
