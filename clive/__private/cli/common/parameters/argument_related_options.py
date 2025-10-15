69b759cd1ef6f9f200df9bceb274cd242a415ca7
Marcin Sobczyk <msobczyk@syncad.com>
2025-10-15 14:05:55 +0000

    Construct typer Option from Argument
    Needed for commands that can accept parameter both as option and argument

--- clive/__private/cli/common/parameters/argument_related_options.py
+++ clive/__private/cli/common/parameters/argument_related_options.py
@@ -11,9 +11,8 @@ from typing import TYPE_CHECKING, Any
 
 import typer
 
-from clive.__private.cli.common.parameters import options
-from clive.__private.cli.common.parameters.modified_param import modified_param
-from clive.__private.cli.common.parsers import public_key
+from clive.__private.cli.common.parameters import arguments, options
+from clive.__private.cli.common.parameters.modified_param import argument_to_option, modified_param
 from clive.__private.core.constants.cli import LOOK_INTO_ARGUMENT_OPTION_HELP
 
 if TYPE_CHECKING:
@@ -55,38 +54,10 @@ chain_id = _make_argument_related_option("--chain-id")
 
 node_address = _make_argument_related_option("--node-address")
 
-owner_key = _make_argument_related_option(
-    typer.Option(
-        None,
-        "--owner",
-        parser=public_key,
-        help="Owner public key that will be set for account.",
-    )
-)
+owner_key = _make_argument_related_option(argument_to_option(arguments.owner_key, "--owner"))
 
-active_key = _make_argument_related_option(
-    typer.Option(
-        None,
-        "--active",
-        parser=public_key,
-        help="Active public key that will be set for account.",
-    )
-)
+active_key = _make_argument_related_option(argument_to_option(arguments.active_key, "--active"))
 
-posting_key = _make_argument_related_option(
-    typer.Option(
-        None,
-        "--posting",
-        parser=public_key,
-        help="Posting public key that will be set for account.",
-    )
-)
+posting_key = _make_argument_related_option(argument_to_option(arguments.posting_key, "--posting"))
 
-memo_key = _make_argument_related_option(
-    typer.Option(
-        None,
-        "--memo",
-        parser=public_key,
-        help="Memo public key that will be set for account.",
-    )
-)
+memo_key = _make_argument_related_option(argument_to_option(arguments.memo_key, "--memo"))
