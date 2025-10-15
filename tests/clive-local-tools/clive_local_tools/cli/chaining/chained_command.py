5b02e3cfe77d0f2190475c20ac22fece7c4b0c17
Marcin Sobczyk <msobczyk@syncad.com>
2025-10-15 11:42:37 +0000

    In cli tests accept option values as mapping instead of kwargs

--- tests/clive-local-tools/clive_local_tools/cli/chaining/chained_command.py
+++ tests/clive-local-tools/clive_local_tools/cli/chaining/chained_command.py
@@ -4,27 +4,53 @@ from typing import TYPE_CHECKING
 
 import test_tools as tt
 
-from clive_local_tools.cli.command_options import kwargs_to_cli_options
+from clive_local_tools.cli.command_options import build_cli_options, stringify_parameter_value
 from clive_local_tools.cli.exceptions import CLITestCommandError
 
 if TYPE_CHECKING:
+    from collections.abc import Iterable, Mapping
+
     from click.testing import Result
     from typer.testing import CliRunner
 
     from clive.__private.cli.clive_typer import CliveTyper
-    from clive_local_tools.cli.command_options import CliOptionT
+    from clive_local_tools.cli.command_options import CliOptionT, StringConvertibleOptionTypes
 
 
 class ChainedCommand:
-    def __init__(self, typer: CliveTyper, runner: CliRunner, command: list[str], **cli_options: CliOptionT) -> None:
+    def __init__(
+        self,
+        typer: CliveTyper,
+        runner: CliRunner,
+        command: list[str],
+        *,
+        cli_positional_args: Iterable[StringConvertibleOptionTypes] | None = None,
+        cli_named_options: Mapping[str, CliOptionT] | None = None,
+    ) -> None:
         self.__typer = typer
         self.__runner = runner
-        self.__full_command = [*command, *kwargs_to_cli_options(**cli_options)]
+        positional = (
+            [stringify_parameter_value(arg) for arg in cli_positional_args] if cli_positional_args is not None else []
+        )
+        named = build_cli_options(cli_named_options) if cli_named_options is not None else []
+        self.__full_command = [*command, *positional, *named]
         self.__was_invoked = False
 
-    def _add_command_to_chain(self, next_command: str, **cli_options: CliOptionT) -> None:
+    def _add_command_to_chain(
+        self,
+        next_command: str,
+        *,
+        cli_positional_args: Iterable[StringConvertibleOptionTypes] | None = None,
+        cli_named_options: Mapping[str, CliOptionT] | None = None,
+    ) -> None:
         self.__full_command.append(next_command)
-        self.__full_command.extend(kwargs_to_cli_options(**cli_options))
+
+        positional = (
+            [stringify_parameter_value(arg) for arg in cli_positional_args] if cli_positional_args is not None else []
+        )
+        named = build_cli_options(cli_named_options) if cli_named_options is not None else []
+        self.__full_command.extend(positional)
+        self.__full_command.extend(named)
 
     def fire(self) -> Result:
         assert self.__was_invoked is False, f"Command '{self.__full_command}' was already invoked."
