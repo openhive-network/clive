9db33947cb96ce068c08238d2dc743ef7cc08425
Mateusz Kudela <mkudela@syncad.com>
2025-10-22 13:16:31 +0000

    Add set of dialogs to modify authority

--- clive/__private/ui/dialogs/__init__.py
+++ clive/__private/ui/dialogs/__init__.py
@@ -1,29 +1,41 @@
 from __future__ import annotations
 
+from clive.__private.ui.dialogs.add_authority_entry_dialog import AddAuthorityEntryDialog
 from clive.__private.ui.dialogs.add_tracked_account_dialog import AddTrackedAccountDialog
 from clive.__private.ui.dialogs.alarm_info_dialog import AlarmInfoDialog
 from clive.__private.ui.dialogs.confirm_invalidate_signatures_dialog import ConfirmInvalidateSignaturesDialog
+from clive.__private.ui.dialogs.edit_authority_total_threshold_dialog import EditAuthorityTotalThresholdDialog
 from clive.__private.ui.dialogs.edit_key_alias_dialog import EditKeyAliasDialog
+from clive.__private.ui.dialogs.edit_memo_entry_dialog import EditMemoEntryDialog
+from clive.__private.ui.dialogs.edit_regular_entry_dialog import EditRegularEntryDialog
 from clive.__private.ui.dialogs.load_key_from_file_dialog import LoadKeyFromFileDialog
 from clive.__private.ui.dialogs.load_transaction_from_file_dialog import LoadTransactionFromFileDialog
 from clive.__private.ui.dialogs.mark_alarm_as_harmless_dialog import MarkAlarmAsHarmlessDialog
 from clive.__private.ui.dialogs.new_key_alias_dialog import NewKeyAliasDialog
 from clive.__private.ui.dialogs.remove_key_alias_dialog import RemoveKeyAliasDialog
+from clive.__private.ui.dialogs.restore_or_remove_entry_dialog import RestoreOrRemoveEntryDialog
 from clive.__private.ui.dialogs.save_transaction_to_file_dialog import SaveTransactionToFileDialog
+from clive.__private.ui.dialogs.start_authority_modification_again_dialog import StartAuthorityModificationAgainDialog
 from clive.__private.ui.dialogs.switch_working_account_dialog import SwitchWorkingAccountDialog
 from clive.__private.ui.dialogs.witness_details_dialog import WitnessDetailsDialog
 
 __all__ = [
+    "AddAuthorityEntryDialog",
     "AddTrackedAccountDialog",
     "AlarmInfoDialog",
     "ConfirmInvalidateSignaturesDialog",
+    "EditAuthorityTotalThresholdDialog",
     "EditKeyAliasDialog",
+    "EditMemoEntryDialog",
+    "EditRegularEntryDialog",
     "LoadKeyFromFileDialog",
     "LoadTransactionFromFileDialog",
     "MarkAlarmAsHarmlessDialog",
     "NewKeyAliasDialog",
     "RemoveKeyAliasDialog",
+    "RestoreOrRemoveEntryDialog",
     "SaveTransactionToFileDialog",
+    "StartAuthorityModificationAgainDialog",
     "SwitchWorkingAccountDialog",
     "WitnessDetailsDialog",
 ]
