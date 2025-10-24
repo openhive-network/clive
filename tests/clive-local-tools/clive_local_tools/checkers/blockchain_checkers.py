00e106f1d797cbfa29df7f4a88727503e7e2cabc
Marcin Sobczyk <msobczyk@syncad.com>
2025-10-24 09:41:07 +0000

    Simplify tests for update-witness

--- tests/clive-local-tools/clive_local_tools/checkers/blockchain_checkers.py
+++ tests/clive-local-tools/clive_local_tools/checkers/blockchain_checkers.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
 
+from decimal import Decimal
 from typing import TYPE_CHECKING
 
 import beekeepy.exceptions as bke
 import pytest
+import test_tools as tt
 from click.testing import Result
 
+from clive.__private.core.percent_conversions import percent_to_hive_percent
 from clive_local_tools.helpers import get_transaction_id_from_output
 
 if TYPE_CHECKING:
-    import test_tools as tt
-
-    from clive.__private.models.schemas import GetTransaction, OperationBase, OperationUnion
+    from clive.__private.models.schemas import GetTransaction, OperationBase, OperationUnion, Witness
 
 
 def _ensure_transaction_id(trx_id_or_result: Result | str) -> str:
@@ -88,3 +89,51 @@ def assert_operation_type_in_blockchain(
         f"{transaction}."
     )
     assert not types_to_check, message
+
+
+def assert_witness_property(
+    property_name: str, property_value: str | int | Decimal | tt.Asset, witness: Witness
+) -> None:
+    """Assert that given proparty has expected value in the witness object obtained by api call to blockchain."""
+    message = f"Witness property '{property_name}' does not have expected value."
+
+    match property_name:
+        case "account_creation_fee":
+            assert witness.props.account_creation_fee is not None  #  https://gitlab.syncad.com/hive/schemas/-/issues/46
+            assert witness.props.account_creation_fee == property_value, (
+                f"{message} Expected: {property_value}, actual: {witness.props.account_creation_fee}"
+            )
+        case "maximum_block_size":
+            assert witness.props.maximum_block_size == property_value, (
+                f"{message} Expected: {property_value}, actual: {witness.props.maximum_block_size}"
+            )
+        case "hbd_interest_rate":
+            assert isinstance(property_value, Decimal), "For hbd_interest_rate property_value must be Decimal."
+            expected_api_value = percent_to_hive_percent(property_value)
+            assert witness.props.hbd_interest_rate == expected_api_value, (
+                f"{message} Expected: {expected_api_value}, actual: {witness.props.hbd_interest_rate}"
+            )
+        case "new_signing_key":
+            assert witness.signing_key == property_value, (
+                f"{message} Expected: {property_value}, actual: {witness.signing_key}"
+            )
+        case "hbd_exchange_rate":
+            assert witness.hbd_exchange_rate.base == property_value, (
+                f"{message} Expected base: {property_value}, actual base: {witness.hbd_exchange_rate.quote}"
+            )
+            expected_quote = tt.Asset.Hive(1)
+            assert witness.hbd_exchange_rate.quote == expected_quote, (
+                f"{message} Expected quote: {expected_quote}, actual quote: {witness.hbd_exchange_rate.base}"
+            )
+        case "url":
+            assert witness.url == property_value, f"{message} Expected: {property_value}, actual: {witness.url}"
+        case "account_subsidy_budget":
+            assert witness.props.account_subsidy_budget == property_value, (
+                f"{message} Expected: {property_value}, actual: {witness.props.account_subsidy_budget}"
+            )
+        case "account_subsidy_decay":
+            assert witness.props.account_subsidy_decay == property_value, (
+                f"{message} Expected: {property_value}, actual: {witness.props.account_subsidy_decay}"
+            )
+        case _:
+            pytest.fail(f"Unknown property: '{property_name}'.")
