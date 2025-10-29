1f591be002a9b4e93a15869d6e0193bfcbe02ba4
Radek Masłowski <rmaslowski@syncad.com>
2025-10-29 14:23:43 +0000

    wip

--- script_interface_examples/interface_presentation.py
+++ script_interface_examples/interface_presentation.py
@@ -1,79 +1,63 @@
 from __private.si.base import clive_use_unlocked_profile
 
+"""
+1. Usunięcie metody no_sign()
+2. Zmiana get_transaction() na as_transaction_object()
+3. Dodaj przykład na pakowanie dodatkowych operacji do transakcji
+4. Ograniczenie występowania po sobie metod finalizujących (broadcast, save_to, as_transaction_object)
+"""
 
 async def interface_presentation() -> str:
+
+    ### Opcje podpisywania
     async with clive_use_unlocked_profile() as clive:
-        transfer = await clive.process.transfer(
+        await clive.process.transfer(
             from_account="alice",
             to_account="gtg",
             amount="1.000 HIVE",
             memo="Test transfer",
         ).autosign().broadcast()
 
-        transfer = await clive.process.transfer(
+        await clive.process.transfer(
             from_account="alice",
             to_account="gtg",
             amount="1.000 HIVE",
             memo="Test transfer",
-        ).no_sign().broadcast()
+        ).broadcast()
 
-        transfer = await clive.process.transfer(
+        await clive.process.transfer(
             from_account="alice",
             to_account="gtg",
             amount="1.000 HIVE",
             memo="Test transfer",
         ).sign_with("alice").broadcast()
 
-        transfer = await clive.process.transfer(
+        ### Wszystkie opcje finalizujące
+        await clive.process.transfer(
             from_account="alice",
             to_account="gtg",
             amount="1.000 HIVE",
             memo="Test transfer",
         ).autosign().save_to(path="transfer1.json", force_save_format="json")
 
-        transfer = await clive.process.transfer(
+        await clive.process.transfer(
             from_account="alice",
             to_account="gtg",
             amount="1.000 HIVE",
             memo="Test transfer",
-        ).autosign().get_transaction()
-
-        transfer = await clive.process.transfer(
-            from_account="alice",
-            to_account="gtg",
-            amount="1.000 HIVE",
-            memo="Test transfer",
-        ).autosign().get_transaction()
-        transfer2 = await transfer.broadcast()
-        transfer3 = await transfer2.save_to("transfer2.json", force_save_format="json")
-
-        transfer4 = await clive.process.transfer(
-            from_account="alice",
-            to_account="gtg",
-            amount="1.000 HIVE",
-            memo="Test transfer",
-        ).autosign().save_to(path="transfer1.json", force_save_format="json")
-        transfer5 = await transfer4.get_transaction()
-        transfer6 = await transfer5.broadcast()
-
-        transfer7 = await (
-            await (
-                await clive.process.transfer(
-                    from_account="alice",
-                    to_account="gtg",
-                    amount="1.000 HIVE",
-                    memo="Test transfer",
-                )
-                .autosign()
-                .save_to(path="transfer1.json", force_save_format="json")
-            ).get_transaction()
-        ).broadcast()
-
-        transaction = await clive.process.transaction(
-            from_file="/workspace/clive_workspace/clive/script_interface_examples/transfer.json", already_signed_mode="override"
         ).autosign().broadcast()
 
-        update_authority = (
+        transfer = await clive.process.transfer(
+            from_account="alice",
+            to_account="gtg",
+            amount="1.000 HIVE",
+            memo="Test transfer",
+        ).autosign().as_transaction_object()
+        transfer2 = await transfer.broadcast()
+        transfer3 = await transfer.save_to("transfer2.json", force_save_format="json")
+
+        ### Przykład update_authority
+        update_active_authority = (
             await clive.process.update_active_authority(
                 account_name="alice",
                 threshold=2,
@@ -82,6 +66,42 @@ async def interface_presentation() -> str:
             .add_key(
                 key="STM5iuVuYcsZmCzXHJT9VbVvHATPa28cLMrf5zKEkzqKc73e22Jtr", weight=1
             )
-            .no_sign()
-            .get_transaction()
+            .as_transaction_object()
+        )
+
+        ### Pakowanie dodatkowych operacji do transakcji
+        transfer_transaction = (
+            await clive.process.transfer(
+                from_account="alice",
+                to_account="gtg",
+                amount="1.000 HIVE",
+                memo="Test transfer",
+            )
+            .sign_with("alice")
+            .as_transaction_object()
+        )
+
+        transfer_and_update_active_authority = (
+            await clive.process.load_transaction(transaction=transfer_transaction)
+            .process.update_active_authority(
+                account_name="alice",
+                threshold=2,
+            )
+            .autosign()
+            .broadcast()
+        )
+
+        transfer_and_update_active_authority2 = (
+            await clive.process.transfer(
+                from_account="alice",
+                to_account="gtg",
+                amount="1.000 HIVE",
+                memo="Test transfer",
+            )
+            .process.update_active_authority(
+                account_name="alice",
+                threshold=2,
+            )
+            .sign_with("alice")
+            .broadcast()
         )
