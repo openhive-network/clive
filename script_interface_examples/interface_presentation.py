ba8ff89fe9bd0756310ada3815036746fa490851
Radek Masłowski <rmaslowski@syncad.com>
2025-10-30 10:16:49 +0000

    wip

--- script_interface_examples/interface_presentation.py
+++ script_interface_examples/interface_presentation.py
@@ -3,14 +3,15 @@ from __private.si.base import clive_use_unlocked_profile
 """
 1. Usunięcie metody no_sign()
 2. Zmiana get_transaction() na as_transaction_object()
-3. Dodaj przykład na pakowanie dodatkowych operacji do transakcji
-4. Ograniczenie występowania po sobie metod finalizujących (broadcast, save_to, as_transaction_object)
+3. Dodano przykład na pakowanie dodatkowych operacji do transakcji
+4. Dodano przykład na możliwość zrobienia multisignu, wzorowany na mechaniźmie z cli
+5. Ograniczenie występowania po sobie metod finalizujących (broadcast, save_fie, as_transaction_object)
 """
 
 async def interface_presentation() -> str:
 
-    ### Opcje podpisywania
     async with clive_use_unlocked_profile() as clive:
+        ### Opcje podpisywania
         await clive.process.transfer(
             from_account="alice",
             to_account="gtg",
@@ -25,12 +26,21 @@ async def interface_presentation() -> str:
             memo="Test transfer",
         ).broadcast()
 
-        await clive.process.transfer(
+        transfer = await clive.process.transfer(
             from_account="alice",
             to_account="gtg",
             amount="1.000 HIVE",
             memo="Test transfer",
         ).sign_with("alice").broadcast()
+
+        # Podpisywanie wielokrotne, wzorowane na mechaniźmie, który mamy w "cli"
+        await clive.process.transfer(
+            from_account="alice",
+            to_account="gtg",
+            amount="1.000 HIVE",
+            memo="Test transfer",
+        ).sign_with("alice").as_transaction_object()
+        await clive.process.load_transaction(transaction=transfer_transaction, already_signed_mode="multisign").sign_with("bob").broadcast()
 
         ### Wszystkie opcje finalizujące
         await clive.process.transfer(
@@ -38,7 +48,7 @@ async def interface_presentation() -> str:
             to_account="gtg",
             amount="1.000 HIVE",
             memo="Test transfer",
-        ).autosign().save_to(path="transfer1.json", force_save_format="json")
+        ).autosign().save_file(path="transfer1.json", force_save_format="json")
 
         await clive.process.transfer(
             from_account="alice",
@@ -54,7 +64,7 @@ async def interface_presentation() -> str:
             memo="Test transfer",
         ).autosign().as_transaction_object()
         transfer2 = await transfer.broadcast()
-        transfer3 = await transfer.save_to("transfer2.json", force_save_format="json")
+        transfer3 = await transfer.save_file("transfer2.json", force_save_format="json")
 
         ### Przykład update_authority
         update_active_authority = (
@@ -80,18 +90,10 @@ async def interface_presentation() -> str:
             .sign_with("alice")
             .as_transaction_object()
         )
+        await clive.process.load_transaction(transaction=transfer_transaction).process.update_active_authority(account_name="alice", threshold=2,).autosign().broadcast()
+
 
         transfer_and_update_active_authority = (
-            await clive.process.load_transaction(transaction=transfer_transaction)
-            .process.update_active_authority(
-                account_name="alice",
-                threshold=2,
-            )
-            .autosign()
-            .broadcast()
-        )
-
-        transfer_and_update_active_authority2 = (
             await clive.process.transfer(
                 from_account="alice",
                 to_account="gtg",
@@ -103,5 +105,5 @@ async def interface_presentation() -> str:
                 threshold=2,
             )
             .sign_with("alice")
-            .broadcast()
+            .save_file("transfer_and_update_active_authority2.json", force_save_format="json")
         )
