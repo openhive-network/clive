1198b507685a63503379017186fe9583cf9e0cd3
Mateusz Żebrak <mzebrak@syncad.com>
2025-11-14 09:33:12 +0100

    Properly handle wax transaction validation error

--- clive/__private/core/commands/build_transaction.py
+++ clive/__private/core/commands/build_transaction.py
@@ -4,13 +4,24 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 from clive.__private.core import iwax
+from clive.__private.core.commands.abc.command import CommandError
 from clive.__private.core.commands.abc.command_with_result import CommandWithResult
 from clive.__private.core.commands.update_transaction_metadata import UpdateTransactionMetadata
 from clive.__private.core.ensure_transaction import TransactionConvertibleType, ensure_transaction
+from clive.__private.core.iwax import WaxOperationFailedError
 from clive.__private.models.transaction import Transaction
 
 if TYPE_CHECKING:
     from clive.__private.core.node import Node
+
+
+class TransactionWaxValidationError(CommandError):
+    def __init__(self, command: BuildTransaction, transaction: Transaction, wax_error_details: str) -> None:
+        self.transaction = transaction
+        self.wax_error_details = wax_error_details
+
+        reason = f"Transaction validation failed.\nWax error details:\n{wax_error_details}"
+        super().__init__(command, reason)
 
 
 @dataclass(kw_only=True)
@@ -22,7 +33,10 @@ class BuildTransaction(CommandWithResult[Transaction]):
 
     async def _execute(self) -> None:
         transaction = ensure_transaction(self.content)
-        iwax.validate_transaction(transaction)
+        try:
+            iwax.validate_transaction(transaction)
+        except WaxOperationFailedError as error:
+            raise TransactionWaxValidationError(self, transaction, str(error)) from error
 
         if not transaction.is_tapos_set or self.force_update_metadata:
             assert self.node is not None, "node is required so that transaction metadata can be updated"
