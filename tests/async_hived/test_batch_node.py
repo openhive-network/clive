eceaab74833537634ce8501bd0edd76569cd8d8b
Mateusz Żebrak <mzebrak@syncad.com>
2025-11-24 09:29:33 +0100

    Remove try except from test_async_batch_node
    Looks like it was added because this test was running on a production
    api.hive.blog node, this isn't the case anymore.

--- tests/async_hived/test_batch_node.py
+++ tests/async_hived/test_batch_node.py
@@ -4,22 +4,18 @@ from typing import TYPE_CHECKING
 
 import pytest
 from beekeepy.exceptions import CommunicationError
-from msgspec import ValidationError  # TODO: new msgspec refactor should encapsulate schemas !!!
 
 if TYPE_CHECKING:
     from clive.__private.core.commands.data_retrieval.async_hived.async_handle import AsyncHived
 
 
 async def test_async_batch_node(async_node: AsyncHived) -> None:
-    try:
-        async with await async_node.batch() as node:
-            dynamic_properties = await node.api.database.get_dynamic_global_properties()
-            config = await node.api.database.get_config()
+    async with await async_node.batch() as node:
+        dynamic_properties = await node.api.database.get_dynamic_global_properties()
+        config = await node.api.database.get_config()
 
-        assert len(dynamic_properties.dict()) != 0, "Dynamic global properties should not be empty"
-        assert len(config.dict()) != 0, "Config should not be empty"
-    except ValidationError as ex:
-        repr(ex).index("Object missing required field `HIVE_CUSTOM_OP_BLOCK_LIMIT")
+    assert len(dynamic_properties.dict()) != 0, "Dynamic global properties should not be empty"
+    assert len(config.dict()) != 0, "Config should not be empty"
 
 
 async def test_async_batch_node_error_response_delayed(async_node: AsyncHived) -> None:
