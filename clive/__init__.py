575935790b5c5037d3b0c2f9fbafd1a9ce7b61e6
Radek Masłowski <rmaslowski@syncad.com>
2025-12-04 12:47:04 +0000

    fixup! Prepare python script interface for clive

--- clive/__init__.py
+++ clive/__init__.py
@@ -1,12 +1,5 @@
 from __future__ import annotations
 
-from clive.__private.si.base import CliveSi, UnlockedCliveSi, clive_use_unlocked_profile
-
 __version__ = "0.0.0"
 
-__all__ = [
-    "CliveSi",
-    "UnlockedCliveSi",
-    "__version__",
-    "clive_use_unlocked_profile",
-]
+__all__ = ["__version__"]
